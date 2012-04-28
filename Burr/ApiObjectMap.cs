b21dd3d9e4feb8032dc20c71323d1969212db060
Tim Clem <timothy.clem@gmail.com>
2012-04-27 22:35:37 -0700

    Test out all fields on user object

--- Burr/ApiObjectMap.cs
+++ Burr/ApiObjectMap.cs
@@ -25,7 +25,24 @@ namespace Burr
         {
             return new User
             {
-                AvatarUrl = (string)jObj["avatar_url"]
+                Followers = (int)jObj["followers"],
+                Type = (string)jObj["type"],
+                Hireable = (bool)jObj["hireable"],
+                AvatarUrl = (string)jObj["avatar_url"],
+                Bio = (string)jObj["bio"],
+                HtmlUrl = (string)jObj["html_url"],
+                CreatedAt = DateTimeOffset.Parse((string)jObj["created_at"]),
+                PublicRepos = (int)jObj["public_repos"],
+                Blog = (string)jObj["blog"],
+                Url = (string)jObj["url"],
+                PublicGists = (int)jObj["public_gists"],
+                Following = (int)jObj["following"],
+                Company = (string)jObj["company"],
+                Name = (string)jObj["name"],
+                Location = (string)jObj["location"],
+                Id = (int)jObj["id"],
+                Email = (string)jObj["email"],
+                Login = (string)jObj["login"]
             };
         }
     }
