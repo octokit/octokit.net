96ba4fdb98831d22e21a8d9b98ac53ee4e788ae7
Tim Clem <timothy.clem@gmail.com>
2012-04-28 12:55:01 -0700

    Basic tests for request and response objects

---
+++ Burr.Tests/Http/RequestTests.cs
@@ -0,0 +1,20 @@
+﻿using Burr.Http;
+using FluentAssertions;
+using Xunit;
+
+namespace Burr.Tests.Http
+{
+    public class RequestTests
+    {
+        public class TheConstructor
+        {
+            [Fact]
+            public void InitializesAllRequiredProperties()
+            {
+                var r = new Request();
+
+                r.Headers.Should().NotBeNull();
+            }
+        }
+    }
+}
---
+++ Burr.Tests/Http/ResponseTests.cs
@@ -0,0 +1,20 @@
+﻿using Burr.Http;
+using FluentAssertions;
+using Xunit;
+
+namespace Burr.Tests.Http
+{
+    public class ResponseTests
+    {
+        public class TheConstructor
+        {
+            [Fact]
+            public void InitializesAllRequiredProperties()
+            {
+                var r = new Response<string>();
+
+                r.Headers.Should().NotBeNull();
+            }
+        }
+    }
+}
