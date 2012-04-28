641543625d07d5c8f83f8852e535341d51db6da6
Tim Clem <timothy.clem@gmail.com>
2012-04-28 16:46:43 -0700

    Add in full user response fixture and test

--- Burr.Tests/ApiObjectMapTests.cs
+++ Burr.Tests/ApiObjectMapTests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Burr.SimpleJSON;
 using Xunit;
 using FluentAssertions;
+using System.Diagnostics;
 
 namespace Burr.Tests
 {
@@ -52,6 +53,39 @@ namespace Burr.Tests
                 user.Id.Should().Be(136521);
                 user.Email.Should().Be("timothy.clem@gmail.com");
                 user.Login.Should().Be("tclem");
+            }
+
+            [Fact]
+            public void ProperlyMapsToAuthenticatedUser()
+            {
+                var map = new ApiObjectMap();
+
+                var user = map.For<User>(JSONDecoder.Decode(Fixtures.UserFullJson.GetResourceAsString()));
+
+                user.Should().NotBeNull();
+                user.Followers.Should().Be(69);
+                user.Type.Should().Be("User");
+                user.Hireable.Should().BeFalse();
+                user.AvatarUrl.Should().Be("https://secure.gravatar.com/avatar/2f4861b27dc35663ed271d39f5358261?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png");
+                user.Bio.Should().BeNull();
+                user.HtmlUrl.Should().Be("https://github.com/tclem");
+                user.CreatedAt.Should().Be(DateTimeOffset.Parse("2009-10-07T20:26:53Z"));
+                user.PublicRepos.Should().Be(20);
+                user.Blog.Should().Be("http://timclem.wordpress.com");
+                user.Url.Should().Be("https://api.github.com/users/tclem");
+                user.PublicGists.Should().Be(6);
+                user.Following.Should().Be(8);
+                user.Company.Should().Be("GitHub");
+                user.Name.Should().Be("Tim Clem");
+                user.Location.Should().Be("San Francisco, CA");
+                user.Id.Should().Be(136521);
+                user.Email.Should().Be("timothy.clem@gmail.com");
+                user.Login.Should().Be("tclem");
+                user.Plan.Should().NotBeNull();
+                user.Plan.Collaborators.Should().Be(25);
+                user.Plan.Space.Should().Be(6291456);
+                user.Plan.Name.Should().Be("large");
+                user.Plan.PrivateRepos.Should().Be(50);
             }
 
             [Fact]
--- Burr.Tests/Fixtures/Fixtures.cs
+++ Burr.Tests/Fixtures/Fixtures.cs
@@ -4,6 +4,9 @@ namespace Burr.Tests
     public static class Fixtures
     {
         public static EmbeddedResource UserJson = 
-            new EmbeddedResource(typeof(Fixtures).Assembly, "Burr.Tests.Fixtures.user.json", "user.json");
+            new EmbeddedResource(typeof(Fixtures).Assembly, "Burr.Tests.Fixtures.user.json");
+
+        public static EmbeddedResource UserFullJson =
+            new EmbeddedResource(typeof(Fixtures).Assembly, "Burr.Tests.Fixtures.user_full.json");
     }
 }
