36b5dd2d273a8d88de529bc8e9e03333fb1276d0
Tim Clem <timothy.clem@gmail.com>
2012-04-28 17:31:11 -0700

    WIP - Add in start of header parser

---
+++ Burr.Tests/Middleware/ApiInfoParserTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using Burr.Http;
+using Burr.Tests.TestHelpers;
+using Xunit;
+using FluentAssertions;
+
+namespace Burr.Tests
+{
+    public class ApiInfoParserTests
+    {
+        public class TheConstructor
+        {
+            [Fact]
+            public void ThrowsForBadArguments()
+            {
+                Assert.Throws<ArgumentNullException>(() => new ApiInfoParser(null));
+            }
+        }
+
+        public class TheAfterMethod
+        {
+            public class FakeGitHubModel : IGitHubModel
+            {
+                public FakeGitHubModel()
+                {
+                    ApiInfo = new ApiInfo();
+                }
+                public ApiInfo ApiInfo
+                {
+                    get;
+                    private set;
+                }
+            }
+
+            [Fact]
+            public async Task ParsesApiInfoFromHeaders()
+            {
+                var env = new Env<FakeGitHubModel>() { Response = new Response<FakeGitHubModel>() };
+                env.Response.Headers.Add("X-Accepted-OAuth-Scopes", "user");
+                env.Response.Headers.Add("X-OAuth-Scopes", "user, public_repo, repo, gist");
+                env.Response.BodyAsObject = new FakeGitHubModel();
+                var h = new ApiInfoParser(env.ApplicationMock().Object);
+
+                await h.Call(env);
+
+                env.Response.BodyAsObject.Should().NotBeNull();
+                var i = env.Response.BodyAsObject.ApiInfo;
+                i.Should().NotBeNull();
+                i.AcceptedOauthScopes.Should().BeEquivalentTo(new[] { "user" });
+                i.OauthScopes.Should().BeEquivalentTo(new string[] { "user", "public_repo", "repo", "gist" });
+            }
+        }
+    }
+}
---
+++ Burr/Middleware/ApiInfoParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burr.Http
+{
+    public class ApiInfoParser : ResponseHandler
+    {
+        public ApiInfoParser(IApplication app)
+            : base(app)
+        {
+        }
+
+        protected override void After<T>(Env<T> env)
+        {
+            var model = env.Response.BodyAsObject as IGitHubModel;
+            if (model == null) return;
+
+            if (env.Response.Headers.ContainsKey("X-Accepted-OAuth-Scopes"))
+            {
+                model.ApiInfo.AcceptedOauthScopes = env.Response.Headers["X-Accepted-OAuth-Scopes"]
+                    .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim())
+                    .ToArray();
+            }
+
+            if (env.Response.Headers.ContainsKey("X-OAuth-Scopes"))
+            {
+                model.ApiInfo.OauthScopes = env.Response.Headers["X-OAuth-Scopes"]
+                    .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim())
+                    .ToArray();
+            }
+        }
+    }
+}
