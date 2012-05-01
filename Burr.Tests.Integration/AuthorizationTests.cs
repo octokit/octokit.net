4ac6313f0e36f2afddf9f9f866232839a05d810c
Tim Clem <timothy.clem@gmail.com>
2012-04-30 17:33:14 -0700

    Stub out api in integration tests

---
+++ Burr.Tests.Integration/AuthorizationTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Burr.Http;
+using FluentAssertions;
+using Xunit;
+using Moq;
+
+namespace Burr.Tests.Integration
+{
+    public class AuthorizationTests
+    {
+        [Fact]
+        public async Task CanGetAuthorizations()
+        {
+            //var github = new GitHubClient { "" };
+            //var auths = await github.Authorizations.GetAllAsync();
+        }
+    }
+}
