041d58a4b7f1354c96722cae6f68eeb5825a9e07
Haacked <haacked@gmail.com>
2013-01-27 21:06:16 -0800

    Add SshKeysEndpoint

---
+++ Octopi/Endpoints/SshKeysEndpoint.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Octopi.Http;
+
+namespace Octopi.Endpoints
+{
+    public class SshKeysEndpoint : ApiEndpoint<SshKey>, ISshKeysEndpoint
+    {
+        public SshKeysEndpoint(IConnection connection) : base(connection)
+        {
+        }
+
+        public async Task<SshKey> Get(long id)
+        {
+            var endpoint = new Uri(string.Format("/user/keys/{0}", id), UriKind.Relative);
+
+            return await Get(endpoint);
+        }
+
+        public async Task<IReadOnlyCollection<SshKey>> GetAll(string user)
+        {
+            var endpoint = new Uri(string.Format("/users/{0}/keys", user), UriKind.Relative);
+
+            return await GetAll(endpoint);
+        }
+
+        public async Task<IReadOnlyCollection<SshKey>> GetAllForCurrent()
+        {
+            var endpoint = new Uri("/user/keys", UriKind.Relative);
+
+            return await GetAll(endpoint);
+        }
+
+        public async Task<SshKey> Create(SshKeyUpdate key)
+        {
+            Ensure.ArgumentNotNull(key, "key");
+
+            var endpoint = new Uri("/user/keys", UriKind.Relative);
+            return await Create(endpoint, key);
+        }
+
+        public async Task<SshKey> Update(long id, SshKeyUpdate key)
+        {
+            Ensure.ArgumentNotNull(key, "key");
+
+            var endpoint = new Uri(string.Format("/user/keys/{0}", id), UriKind.Relative);
+            return await Update(endpoint, key);
+        }
+
+        public async Task Delete(long id)
+        {
+            var endpoint = new Uri(string.Format("/user/keys/{0}", id), UriKind.Relative);
+
+            await Delete(endpoint);
+        }
+    }
+}
--- Octopi/GitHubModels.cs
+++ Octopi/GitHubModels.cs
@@ -285,7 +285,42 @@ namespace Octopi
         /// The api URL for this organization.
         /// </summary>
         public string Url { get; set; }
-    
+    }
+
+    public class SshKey
+    {
+        /// <summary>
+        /// The system-wide unique Id for this user.
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// The SSH Key
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// The title of the SSH key
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The api URL for this organization.
+        /// </summary>
+        public string Url { get; set; }
+    }
+
+    public class SshKeyUpdate
+    {
+        /// <summary>
+        /// The SSH Key
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// The title of the SSH key
+        /// </summary>
+        public string Title { get; set; }
     }
 
     /// <summary>
