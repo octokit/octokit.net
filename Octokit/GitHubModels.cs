3c52d13bffab9df992192b186786c2c8fb3b2409
Haacked <haacked@gmail.com>
2013-09-23 16:34:05 -0700

    Port ssh key helpers from GHfW

--- Octokit/GitHubModels.cs
+++ Octokit/GitHubModels.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Octokit.Http;
 
@@ -353,6 +354,24 @@ namespace Octokit
         /// The api URL for this organization.
         /// </summary>
         public string Url { get; set; }
+    }
+
+    /// <summary>
+    /// Represents the data and name parsed from the Ssh key.
+    /// </summary>
+    public class SshKeyInfo
+    {
+        public SshKeyInfo(string data, string name)
+        {
+            Ensure.ArgumentNotNull(data, "data");
+            Ensure.ArgumentNotNull(name, "name");
+
+            Data = data;
+            Name = name;
+        }
+
+        public string Data { get; private set; }
+        public string Name { get; private set; }
     }
 
     public class SshKeyUpdate
