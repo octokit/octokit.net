76e509978f97aaafa29a6e02a7086da607b62a6d
Haacked <haacked@gmail.com>
2013-10-03 16:02:53 -0700

    Add Observable GetEmails method

--- Octokit.Reactive/Clients/ObservableUsersClient.cs
+++ Octokit.Reactive/Clients/ObservableUsersClient.cs
@@ -5,32 +5,37 @@ namespace Octokit.Reactive.Clients
 {
     public class ObservableUsersClient : IObservableUsersClient
     {
-        readonly IUsersClient client;
+        readonly IUsersClient _client;
 
         public ObservableUsersClient(IUsersClient client)
         {
             Ensure.ArgumentNotNull(client, "client");
 
-            this.client = client;
+            this._client = client;
         }
 
         public IObservable<User> Get(string login)
         {
             Ensure.ArgumentNotNull(login, "login");
 
-            return client.Get(login).ToObservable();
+            return _client.Get(login).ToObservable();
         }
 
         public IObservable<User> Current()
         {
-            return client.Current().ToObservable();
+            return _client.Current().ToObservable();
         }
 
         public IObservable<User> Update(UserUpdate user)
         {
             Ensure.ArgumentNotNull(user, "user");
 
-            return client.Update(user).ToObservable();
+            return _client.Update(user).ToObservable();
+        }
+
+        public IObservable<IReadOnlyList<EmailAddress>> GetEmails()
+        {
+            return _client.GetEmails().ToObservable();
         }
     }
 }
