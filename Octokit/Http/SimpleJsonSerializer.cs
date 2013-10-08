0444f8630aa4d29f2ec090a9dd702400b7b0e7dc
half-ogre <nemo@half-ogre.com>
2013-10-07 17:06:20 -0700

    omit null values from serialized objects

--- Octokit/Http/SimpleJsonSerializer.cs
+++ Octokit/Http/SimpleJsonSerializer.cs
@@ -1,4 +1,10 @@
-﻿namespace Octokit.Http
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Octokit.Reflection;
+
+namespace Octokit.Http
 {
     public class SimpleJsonSerializer : IJsonSerializer
     {
@@ -19,6 +25,32 @@
             protected override string MapClrMemberNameToJsonFieldName(string clrPropertyName)
             {
                 return clrPropertyName.ToRubyCase();
+            }
+
+            // This is overridden so that null values are omitted from serialized objects.
+            [SuppressMessage("Microsoft.Design", "CA1007:UseGenericsWhereAppropriate", Justification = "Need to support .NET 2")]
+            protected override bool TrySerializeUnknownTypes(object input, out object output)
+            {
+                if (input == null) throw new ArgumentNullException("input");
+                output = null;
+                Type type = input.GetType();
+                if (type.FullName == null)
+                    return false;
+                IDictionary<string, object> obj = new JsonObject();
+                IDictionary<string, ReflectionUtils.GetDelegate> getters = GetCache[type];
+                foreach (KeyValuePair<string, ReflectionUtils.GetDelegate> getter in getters)
+                {
+                    if (getter.Value != null)
+                    {
+                        var value = getter.Value(input);
+                        if (value == null)
+                            continue;
+
+                        obj.Add(MapClrMemberNameToJsonFieldName(getter.Key), value);
+                    }
+                }
+                output = obj;
+                return true;
             }
         }
     }
