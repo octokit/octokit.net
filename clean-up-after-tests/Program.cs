c6e176b82e3e065962400b50c21ab49376aab7ce
half-ogre <nemo@half-ogre.com>
2013-10-09 14:34:31 -0700

    add exe to clean up after tests

---
+++ clean-up-after-tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clean_up_after_tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
