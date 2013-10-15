6dc68a29af23e11aa7334b3e1c88deee9274c7e6
half-ogre <nemo@half-ogre.com>
2013-10-15 11:44:03 -0700

    add API doc for ApiConnection

--- Octokit/Http/IApiConnection.cs
+++ Octokit/Http/IApiConnection.cs
@@ -13,15 +13,15 @@ namespace Octokit
     {
         [SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", MessageId = "Get",
             Justification = "It's fiiiine. It's fine. Trust us.")]
-        Task<T> Get<T>(Uri endpoint, IDictionary<string, string> parameters);
-        Task<string> GetHtml(Uri endpoint, IDictionary<string, string> parameters);
-        Task<IReadOnlyList<T>> GetAll<T>(Uri endpoint, IDictionary<string, string> parameters);
-        Task<T> Post<T>(Uri endpoint, object data);
+        Task<T> Get<T>(Uri uri, IDictionary<string, string> parameters);
+        Task<string> GetHtml(Uri uri, IDictionary<string, string> parameters);
+        Task<IReadOnlyList<T>> GetAll<T>(Uri uri, IDictionary<string, string> parameters);
+        Task<T> Post<T>(Uri uri, object data);
         Task<T> Post<T>(Uri uri, Stream rawData, string contentType, string accepts); 
-        Task<T> Put<T>(Uri endpoint, object data);
-        Task<T> Put<T>(Uri endpoint, object data, string twoFactorAuthenticationCode);
-        Task<T> Patch<T>(Uri endpoint, object data);
+        Task<T> Put<T>(Uri uri, object data);
+        Task<T> Put<T>(Uri uri, object data, string twoFactorAuthenticationCode);
+        Task<T> Patch<T>(Uri uri, object data);
         [SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter", Justification="Legitimate, but I'm not fixing it just yet.")]
-        Task Delete<T>(Uri endpoint);
+        Task Delete<T>(Uri uri);
     }
 }
