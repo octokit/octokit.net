1736ad2a6bc0cfcf72d5c66a8df3addb3de32b7b
half-ogre <nemo@half-ogre.com>
2013-10-15 15:57:27 -0700

    add doc for Org API client

--- Octokit/Clients/OrganizationsClient.cs
+++ Octokit/Clients/OrganizationsClient.cs
@@ -6,12 +6,28 @@ using System.Threading.Tasks;
 
 namespace Octokit
 {
+    /// <summary>
+    /// A client for GitHub's Orgs API.
+    /// </summary>
+    /// <remarks>
+    /// See the <a href="http://developer.github.com/v3/orgs/">Orgs API documentation</a> for more information.
+    /// </remarks>
     public class OrganizationsClient : ApiClient, IOrganizationsClient
     {
-        public OrganizationsClient(IApiConnection client) : base(client)
+        /// <summary>
+        /// Initializes a new GitHub Orgs API client.
+        /// </summary>
+        /// <param name="connection">An API connection.</param>
+        public OrganizationsClient(IApiConnection connection) : base(connection)
         {
         }
 
+        /// <summary>
+        /// Returns the specified <see cref="Organization"/>.
+        /// </summary>
+        /// <param name="org">login of the organization to get.</param>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>The specified <see cref="Organization"/>.</returns>
         public async Task<Organization> Get(string org)
         {
             Ensure.ArgumentNotNullOrEmptyString(org, "org");
@@ -20,6 +36,11 @@ namespace Octokit
             return await Client.Get<Organization>(endpoint);
         }
 
+        /// <summary>
+        /// Returns all <see cref="Organization" />s for the current user.
+        /// </summary>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>A list of the current user's <see cref="Organization"/>s.</returns>
         public async Task<IReadOnlyList<Organization>> GetAllForCurrent()
         {
             var endpoint = new Uri("/user/orgs", UriKind.Relative);
@@ -27,6 +48,11 @@ namespace Octokit
             return await Client.GetAll<Organization>(endpoint);
         }
 
+        /// <summary>
+        /// Returns all <see cref="Organization" />s for the specified user.
+        /// </summary>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>A list of the specified user's <see cref="Organization"/>s.</returns>
         public async Task<IReadOnlyList<Organization>> GetAll(string user)
         {
             Ensure.ArgumentNotNullOrEmptyString(user, "user");
