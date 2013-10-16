1a028ac580ce8130967fb2f5761dd025faed3722
Haacked <haacked@gmail.com>
2013-10-16 16:25:23 -0700

    Add ApiUrls for RepositoriesClient

---
+++ Octokit/Helpers/ApiUrls.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Octokit
+{
+    /// <summary>
+    /// Class for retrieving GitHub ApI URLs
+    /// </summary>
+    public static class ApiUrls
+    {
+        static readonly Uri _currentUserRepositoriesUrl = new Uri("user/repos", UriKind.Relative);
+        static readonly Uri _currentUserOrganizationsUrl = new Uri("/user/orgs", UriKind.Relative);
+        static readonly Uri _currentUserSshKeys = new Uri("/user/keys", UriKind.Relative);
+        static readonly Uri _currentUserEmailsEndpoint = new Uri("/user/emails", UriKind.Relative);
+
+        /// <summary>
+        /// Returns the <see cref="Uri"/> that returns all of the repositories for the currently logged in user in
+        /// response to a GET request. A POST to this URL creates a new repository.
+        /// </summary>
+        /// <returns></returns>
+        public static Uri Repositories()
+        {
+            return _currentUserRepositoriesUrl;
+        }
+
+        /// <summary>
+        /// Returns the <see cref="Uri"/> that returns all of the repositories for the specified login.
+        /// </summary>
+        /// <param name="login">The login for the user</param>
+        /// <returns></returns>
+        public static Uri Repositories(string login)
+        {
+            return "/users/{0}/repos".FormatUri(login);
+        }
+
+        /// <summary>
+        /// Returns the <see cref="Uri"/> that returns all of the repositories for the specified organization in
+        /// response to a GET request. A POST to this URL creates a new repository for the organization.
+        /// </summary>
+        /// <param name="organization">The name of the organization</param>
+        /// <returns></returns>
+        public static Uri OrganizationRepositories(string organization)
+        {
+            return "/orgs/{0}/repos".FormatUri(organization);
+        }
+
+        /// <summary>
+        /// Returns the <see cref="Uri"/> that returns all of the organizations for the currently logged in user.
+        /// </summary>
+        /// <returns></returns>
+        public static Uri Organizations()
+        {
+            return _currentUserOrganizationsUrl;
+        }
+
+        /// <summary>
+        /// Returns the <see cref="Uri"/> that returns all of the organizations for the specified login.
+        /// </summary>
+        /// <param name="login">The login for the user</param>
+        /// <returns></returns>
+        public static Uri Organizations(string login)
+        {
+            return "/users/{0}/orgs".FormatUri(login);
+        }
+
+        /// <summary>
+        /// Returns the <see cref="Uri"/> that returns all of the SSH keys for the currently logged in user.
+        /// </summary>
+        /// <returns></returns>
+        public static Uri SshKeys()
+        {
+            return _currentUserSshKeys;
+        }
+
+        /// <summary>
+        /// Returns the <see cref="Uri"/> that returns all of the SSH keys for the specified user.
+        /// </summary>
+        /// <param name="login">The login for the user</param>
+        /// <returns></returns>
+        public static Uri SshKeys(string login)
+        {
+            return "/users/{0}/keys".FormatUri(login);
+        }
+
+        /// <summary>
+        /// Returns the <see cref="Uri"/> that returns all of the email addresses for the currently logged in user.
+        /// </summary>
+        /// <returns></returns>
+        public static Uri Emails()
+        {
+            return _currentUserEmailsEndpoint;
+        }
+    }
+}
