ee2418e5a936b45abf6004aadfb176106879e280
half-ogre <nemo@half-ogre.com>
2013-10-16 12:14:25 -0700

    fix API doc for the Repos API client interface

--- Octokit/IRepositoriesClient.cs
+++ Octokit/IRepositoriesClient.cs
@@ -1,91 +1,128 @@
+#if NET_45
 using System.Collections.Generic;
+#endif
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
 using System.Threading.Tasks;
 
 namespace Octokit
 {
+    /// <summary>
+    /// A client for GitHub's Repositories API.
+    /// </summary>
+    /// <remarks>
+    /// See the <a href="http://developer.github.com/v3/repos/">Repositories API documentation</a> for more details.
+    /// </remarks>
     public interface IRepositoriesClient
     {
         /// <summary>
         /// Creates a new repository for the current user.
         /// </summary>
-        /// <param name="newRepository">A <see cref="NewRepository"/> instance describing the new repository to create</param>
-        /// <returns>A <see cref="Repository"/> instance for the created repository</returns>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/repos/#create">API documentation</a> for more information.
+        /// </remarks>
+        /// <param name="newRepository">A <see cref="NewRepository"/> instance describing the new repository to create.</param>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>A <see cref="Repository"/> instance for the created repository.</returns>
         Task<Repository> Create(NewRepository newRepository);
 
         /// <summary>
         /// Creates a new repository in the specified organization.
         /// </summary>
-        /// <param name="organizationLogin">The login of the organization in which to create the repostiory</param>
-        /// <param name="newRepository">A <see cref="NewRepository"/> instance describing the new repository to create</param>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/repos/#create">API documentation</a> for more information.
+        /// </remarks>
+        /// <param name="organizationLogin">Login of the organization in which to create the repostiory.</param>
+        /// <param name="newRepository">A <see cref="NewRepository"/> instance describing the new repository to create.</param>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
         /// <returns>A <see cref="Repository"/> instance for the created repository</returns>
         Task<Repository> Create(string organizationLogin, NewRepository newRepository);
 
         /// <summary>
-        /// Deletes a repository for the specified owner and name.
+        /// Deletes the specified repository.
         /// </summary>
-        /// <param name="owner">The owner of the repository</param>
-        /// <param name="name">The name of the repository</param>
-        /// <remarks>Deleting a repository requires admin access. If OAuth is used, the `delete_repo` scope is required.</remarks>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/repos/#delete-a-repository">API documentation</a> for more information.
+        /// Deleting a repository requires admin access. If OAuth is used, the `delete_repo` scope is required.
+        /// </remarks>
+        /// <param name="owner">The owner of the repository.</param>
+        /// <param name="name">The name of the repository.</param>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
         Task Delete(string owner, string name);
 
         /// <summary>
-        /// Retrieves the <see cref="Repository"/> for the specified owner and name.
+        /// Gets the specified repository.
         /// </summary>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/repos/#get">API documentation</a> for more information.
+        /// </remarks>
         /// <param name="owner">The owner of the repository.</param>
         /// <param name="name">The name of the repository.</param>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
         /// <returns>A <see cref="Repository"/></returns>
         [SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", MessageId = "Get")]
         Task<Repository> Get(string owner, string name);
         
         /// <summary>
-        /// Retrieves every <see cref="Repository"/> that belongs to the current user.
+        /// Gets all repositories owned by the current user.
         /// </summary>
         /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/repos/#list-your-repositories">API documentation</a> for more information.
         /// The default page size on GitHub.com is 30.
         /// </remarks>
         /// <exception cref="AuthorizationException">Thrown if the client is not authenticated.</exception>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
         /// <returns>A <see cref="IReadOnlyPagedCollection{Repository}"/> of <see cref="Repository"/>.</returns>
         [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate",
             Justification = "Makes a network request")]
         Task<IReadOnlyList<Repository>> GetAllForCurrent();
         
         /// <summary>
-        /// Retrieves every <see cref="Repository"/> that belongs to the specified user.
+        /// Gets all repositories owned by the specified user.
         /// </summary>
         /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/repos/#list-user-repositories">API documentation</a> for more information.
         /// The default page size on GitHub.com is 30.
         /// </remarks>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
         /// <returns>A <see cref="IReadOnlyPagedCollection{Repository}"/> of <see cref="Repository"/>.</returns>
         [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate",
             Justification = "Makes a network request")]
         Task<IReadOnlyList<Repository>> GetAllForUser(string login);
 
         /// <summary>
-        /// Retrieves every <see cref="Repository"/> that belongs to the specified organization.
+        /// Gets all repositories owned by the specified organization.
         /// </summary>
         /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/repos/#list-organization-repositories">API documentation</a> for more information.
         /// The default page size on GitHub.com is 30.
         /// </remarks>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
         /// <returns>A <see cref="IReadOnlyPagedCollection{Repository}"/> of <see cref="Repository"/>.</returns>
         [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate",
             Justification = "Makes a network request")]
         Task<IReadOnlyList<Repository>> GetAllForOrg(string organization);
 
         /// <summary>
-        /// Returns the <see cref="Readme"/> associated with the specified repository.
+        /// Gets the preferred README for the specified repository.
         /// </summary>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/repos/contents/#get-the-readme">API documentation</a> for more information.
+        /// </remarks>
         /// <param name="owner">The owner of the repository.</param>
         /// <param name="name">The name of the repository.</param>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
         /// <returns></returns>
         Task<Readme> GetReadme(string owner, string name);
 
         /// <summary>
-        /// Returns just the HTML portion of the README without the surrounding HTML document. 
+        /// Gets the perferred README's HTML for the specified repository.
         /// </summary>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/repos/contents/#get-the-readme">API documentation</a> for more information.
+        /// </remarks>
         /// <param name="owner">The owner of the repository.</param>
         /// <param name="name">The name of the repository.</param>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
         /// <returns></returns>
         Task<string> GetReadmeHtml(string owner, string name);
     }
