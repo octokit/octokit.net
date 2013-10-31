abb8b86286d914fbc22fde757afa4327cc3cd0b7
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-31 12:53:21 +0100

    Fixing the packaging paths in the build script.

--- build.fsx
+++ build.fsx
@@ -12,13 +12,14 @@ let reactiveProjectSummary = reactiveProjectDescription // TODO: write a summary
 
 let buildDir = "./Octokit/bin"
 let reactiveBuildDir = "./Octokit.Reactive/bin"
-let packageDir = "./packaging/octokit/"
-let reactivePackageDir = "./packaging/octokit/reactive"
+let packagingRoot = "./packaging/"
+let packagingDir = packagingRoot @@ "octokit"
+let reactivePackagingDir = packagingRoot @@ "octokit.reactive"
 
 let version = "0.1.1" // TODO: Retrieve this from release notes or CI
 
 Target "Clean" (fun _ ->
-    CleanDirs [buildDir; reactiveBuildDir; packageDir; reactivePackageDir]
+    CleanDirs [buildDir; reactiveBuildDir; packagingRoot; packagingDir; reactivePackagingDir]
 )
 
 Target "BuildApp" (fun _ ->
@@ -27,42 +28,42 @@ Target "BuildApp" (fun _ ->
 )
 
 Target "CreateOctokitPackage" (fun _ ->
-    let net45Dir = packageDir @@ "lib/net45/"
-    let netcore45Dir = packageDir @@ "lib/netcore45/"
+    let net45Dir = packagingDir @@ "lib/net45/"
+    let netcore45Dir = packagingDir @@ "lib/netcore45/"
     CleanDirs [net45Dir; netcore45Dir]
 
     CopyFile net45Dir (buildDir @@ "Release/Net40/Octokit.dll") // TODO: this a bug in the sln?!
     CopyFile netcore45Dir (buildDir @@ "Release/NetCore45/Octokit.dll")
-    CopyFiles packageDir ["LICENSE.txt"; "README.md"]
+    CopyFiles packagingDir ["LICENSE.txt"; "README.md"]
 
     NuGet (fun p -> 
         {p with
             Authors = authors
             Project = projectName
             Description = projectDescription                               
-            OutputPath = "./packaging/"
+            OutputPath = packagingRoot
             Summary = projectSummary
-            WorkingDir = packageDir
+            WorkingDir = packagingDir
             Version = version
             AccessKey = getBuildParamOrDefault "nugetkey" ""
             Publish = hasBuildParam "nugetkey" }) "octokit.nuspec"
 )
 
 Target "CreateOctokitReactivePackage" (fun _ ->
-    let net45Dir = reactivePackageDir @@ "lib/net45/"
+    let net45Dir = reactivePackagingDir @@ "lib/net45/"
     CleanDirs [net45Dir]
 
     CopyFile net45Dir (reactiveBuildDir @@ "Release/Net40/Octokit.Reactive.dll") // TODO: this a bug in the sln?!    
-    CopyFiles packageDir ["LICENSE.txt"; "README.md"]
+    CopyFiles reactivePackagingDir ["LICENSE.txt"; "README.md"]
 
     NuGet (fun p -> 
         {p with
             Authors = authors
             Project = reactiveProjectName
             Description = reactiveProjectDescription                               
-            OutputPath = "./packaging/"
+            OutputPath = packagingRoot
             Summary = reactiveProjectSummary
-            WorkingDir = reactivePackageDir
+            WorkingDir = reactivePackagingDir
             Version = version
             Dependencies =
                 ["Octokit", RequireExactly (NormalizeVersion version)
