46a6c47e02e60e98506839850af237aefeeaab28
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-31 12:31:36 +0100

    Create the Octokit NuGet package with the NuGet task as discussed in #116

--- build.fsx
+++ build.fsx
@@ -1,6 +1,11 @@
 #r @"tools\FAKE.Core\tools\FakeLib.dll"
 open Fake 
 
+let projectName = "Octokit"
+let authors = ["GitHub"]
+let projectDescription = "An async-based GitHub API client library for .NET"
+let projectSummary = projectDescription // TODO: write a summary
+
 let buildDir = "./Octokit/bin"
 let reactiveBuildDir = "./Octokit.Reactive/bin"
 let packageDir = "./packaging/octokit/"
@@ -23,6 +28,17 @@ Target "CreateOctokitPackage" (fun _ ->
     CopyFile net45Dir (buildDir @@ "Release/Net40/Octokit.dll") // TODO: this a bug in the sln?!
     CopyFile netcore45Dir (buildDir @@ "Release/NetCore45/Octokit.dll")
     CopyFiles packageDir ["LICENSE.txt"; "README.md"]
+
+    NuGet (fun p -> 
+        {p with
+            Authors = authors
+            Project = projectName
+            Description = projectDescription                               
+            OutputPath = "./packaging/"
+            Summary = projectSummary
+            WorkingDir = packageDir
+            AccessKey = getBuildParamOrDefault "nugetkey" ""
+            Publish = hasBuildParam "nugetkey" }) "octokit.nuspec"
 )
 
 Target "CreateOctokitReactivePackage" (fun _ ->
