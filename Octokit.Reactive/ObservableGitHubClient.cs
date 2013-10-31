ff71007bf6a45c6631b2393134a00052e11f9f9a
Perry Taylor <ptaylor@rcrracing.com>
2013-10-30 21:26:56 -0400

    Add Simple constructors

--- Octokit.Reactive/ObservableGitHubClient.cs
+++ Octokit.Reactive/ObservableGitHubClient.cs
@@ -1,4 +1,6 @@
-﻿using Octokit.Reactive.Clients;
+﻿using System;
+using System.Net.Http.Headers;
+using Octokit.Reactive.Clients;
 
 namespace Octokit.Reactive
 {
@@ -6,6 +8,26 @@ namespace Octokit.Reactive
     {
         readonly IGitHubClient _gitHubClient;
 
+        public ObservableGitHubClient(ProductHeaderValue productInformation)
+            : this(new GitHubClient(productInformation))
+        {
+        }
+
+        public ObservableGitHubClient(ProductHeaderValue productInformation, ICredentialStore credentialStore)
+            :this (new GitHubClient(productInformation, credentialStore))
+        {
+        }
+
+        public ObservableGitHubClient(ProductHeaderValue productInformation, Uri baseAddress)
+            :this(new GitHubClient(productInformation, baseAddress))
+        {   
+        }
+
+        public ObservableGitHubClient(ProductHeaderValue productInformation, ICredentialStore credentialStore, Uri baseAddress)
+            :this(new GitHubClient(productInformation, credentialStore, baseAddress))
+        {
+            
+        }
         public ObservableGitHubClient(IGitHubClient gitHubClient)
         {
             Ensure.ArgumentNotNull(gitHubClient, "githubClient");
