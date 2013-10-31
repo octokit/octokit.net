6530de74f577dfa1bfb6f9ceda552d63f3253b6a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-31 12:46:04 +0100

    Create the Octokit.Reactive NuGet package and set dependencies to Octokit as discussed in #116

--- build.fsx
+++ build.fsx
@@ -6,10 +6,16 @@ let authors = ["GitHub"]
 let projectDescription = "An async-based GitHub API client library for .NET"
 let projectSummary = projectDescription // TODO: write a summary
 
+let reactiveProjectName = "Octokit.Reactive"
+let reactiveProjectDescription = "An IObservable based GitHub API client library for .NET using Reactive Extensions"
+let reactiveProjectSummary = reactiveProjectDescription // TODO: write a summary
+
 let buildDir = "./Octokit/bin"
 let reactiveBuildDir = "./Octokit.Reactive/bin"
 let packageDir = "./packaging/octokit/"
 let reactivePackageDir = "./packaging/octokit/reactive"
+
+let version = "0.1.1" // TODO: Retrieve this from release notes or CI
 
 Target "Clean" (fun _ ->
     CleanDirs [buildDir; reactiveBuildDir; packageDir; reactivePackageDir]
@@ -37,6 +43,7 @@ Target "CreateOctokitPackage" (fun _ ->
             OutputPath = "./packaging/"
             Summary = projectSummary
             WorkingDir = packageDir
+            Version = version
             AccessKey = getBuildParamOrDefault "nugetkey" ""
             Publish = hasBuildParam "nugetkey" }) "octokit.nuspec"
 )
@@ -47,6 +54,21 @@ Target "CreateOctokitReactivePackage" (fun _ ->
 
     CopyFile net45Dir (reactiveBuildDir @@ "Release/Net40/Octokit.Reactive.dll") // TODO: this a bug in the sln?!    
     CopyFiles packageDir ["LICENSE.txt"; "README.md"]
+
+    NuGet (fun p -> 
+        {p with
+            Authors = authors
+            Project = reactiveProjectName
+            Description = reactiveProjectDescription                               
+            OutputPath = "./packaging/"
+            Summary = reactiveProjectSummary
+            WorkingDir = reactivePackageDir
+            Version = version
+            Dependencies =
+                ["Octokit", RequireExactly (NormalizeVersion version)
+                 "Rx-Main", RequireExactly "2.1.30214"] // TODO: Retrieve this from the referenced package
+            AccessKey = getBuildParamOrDefault "nugetkey" ""
+            Publish = hasBuildParam "nugetkey" }) "Octokit.Reactive.nuspec"
 )
 
 Target "Default" DoNothing
