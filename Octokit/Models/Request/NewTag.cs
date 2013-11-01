167bec8064928ba86058d94ec5d3cbae6e095f1a
John Nye <ninjanye@gmail.com>
2013-11-01 23:14:24 +0000

    Seperate NewTag class from Tag file

---
+++ Octokit/Models/Request/NewTag.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace Octokit
+{
+    public class NewTag
+    {
+        [DataMember(Name = "tag")]
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public string Object { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1721:PropertyNamesShouldNotMatchGetMethods", 
+            Justification = "Property name as defined by web api")]
+        public NewTagType Type { get; set; }
+        public Tagger Tagger { get; set; }        
+    }
+
+    /// <summary>
+    /// Represents the type of object being tagged
+    /// </summary>
+    public enum NewTagType
+    {
+        Commit,
+        Blob,
+        Tree
+    }
+}
