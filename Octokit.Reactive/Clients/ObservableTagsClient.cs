36e2e325e3cc0400cdd8daecc972b8de0562dfa2
John Nye <ninjanye@gmail.com>
2013-11-01 20:33:42 +0000

    Create ObservableTagsClient

---
+++ Octokit.Reactive/Clients/ObservableTagsClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reactive.Threading.Tasks;
+
+namespace Octokit.Reactive
+{
+    public class ObservableTagsClient : IObservableTagsClient
+    {
+        readonly ITagsClient _client;
+
+        public ObservableTagsClient(IGitHubClient client)
+        {
+            Ensure.ArgumentNotNull(client, "client");
+
+            this._client = client.Tag;
+        }
+
+        public IObservable<Tag> Get(string owner, string name, string reference)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
+            Ensure.ArgumentNotNullOrEmptyString(name, "name");
+            Ensure.ArgumentNotNullOrEmptyString(reference, "reference");
+
+            return this._client.Get(owner, name, reference).ToObservable();
+        }
+
+        public IObservable<Tag> Create(string owner, string name, NewTag tag)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
+            Ensure.ArgumentNotNullOrEmptyString(name, "name");
+            Ensure.ArgumentNotNull(tag, "tag");
+
+            return this._client.Create(owner, name, tag).ToObservable();
+        }
+    }
+}
