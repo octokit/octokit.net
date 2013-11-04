a9514e30b8bcd35d641017fe69cc2b85ea517900
Haacked <haacked@gmail.com>
2013-11-04 13:16:50 -0800

    Add code analysis to Release build
    This should make it so code analysis build runs on the CI server.
    Previously, it was only set for Debug builds.

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -4,6 +4,8 @@ using System.Reflection;
 [assembly: AssemblyProductAttribute("Octokit")]
 [assembly: AssemblyVersionAttribute("0.1.2")]
 [assembly: AssemblyFileVersionAttribute("0.1.2")]
-
-class AssemblyVersionInformation { public const string Version = "0.1.2"; }
-
+namespace System {
+    internal static class AssemblyVersionInformation {
+        internal const string Version = "0.1.2";
+    }
+}
