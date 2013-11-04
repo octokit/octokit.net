13676d57386028f08dc03837f2a53b7e2eac1ddc
James R Sconfitto <jugglingnutcase@gmail.com>
2013-11-03 23:22:08 -0500

    Add tests for the starred client

---
+++ Octokit.Tests/Clients/StarredClientTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NSubstitute;
+using Xunit;
+
+namespace Octokit.Tests.Clients
+{
+    public class StarredClientTests
+    {
+        public class TheGetAllForCurrentMethod
+        {
+            [Fact]
+            public void RequestsCorrectUrl()
+            {
+                var endpoint = new Uri("user/starred", UriKind.Relative);
+                var connection = Substitute.For<IApiConnection>();
+                var client = new StarredClient(connection);
+
+                client.GetAllForCurrent();
+
+                connection.Received().GetAll<Repository>(endpoint);
+            }
+        }
+
+        public class TheGetAllForUserMethod
+        {
+            [Fact]
+            public void RequestsCorrectUrl()
+            {
+                var endpoint = new Uri("users/banana/starred", UriKind.Relative);
+                var connection = Substitute.For<IApiConnection>();
+                var client = new StarredClient(connection);
+
+                client.GetAllForUser("banana");
+
+                connection.Received().GetAll<Repository>(endpoint);
+            }
+        }
+    }
+}
