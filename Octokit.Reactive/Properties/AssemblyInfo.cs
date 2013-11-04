61c952516f8c0114d6bb07637dc6b8ae52382cf0
Haacked <haacked@gmail.com>
2013-11-04 13:31:25 -0800

    Set ComVisible(false) in SolutionInfo
    Rather than set it in each AssemblyInfo.cs file.

--- Octokit.Reactive/Properties/AssemblyInfo.cs
+++ Octokit.Reactive/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Octokit.Reactive")]
 [assembly: AssemblyDescription("An IObservable based GitHub API client library for .NET using Reactive Extensions")]
-[assembly: ComVisible(false)]
--- Octokit/Properties/AssemblyInfo.cs
+++ Octokit/Properties/AssemblyInfo.cs
@@ -1,9 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Octokit")]
 [assembly: AssemblyDescription("An async-based GitHub API client library for .NET")]
-[assembly: ComVisible(false)]
 [assembly: InternalsVisibleTo("Octokit.Tests")]
 [assembly: InternalsVisibleTo("Octokit.Tests-NetCore45")]
