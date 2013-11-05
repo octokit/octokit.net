334cd136cc9843d744d2b53ec8deb1e4567b1dad
pltaylor <pltaylor3@gmail.com>
2013-11-05 12:52:26 -0500

    Apply naming suppression to allow build to build.

--- Octokit/Clients/IActivitiesClient.cs
+++ Octokit/Clients/IActivitiesClient.cs
@@ -2,6 +2,7 @@
 {
     public interface IActivitiesClient
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", MessageId = "Event")]
         IEventsClient Event { get; }
     }
 }
