643dcc7246e9b7641920231e6736c707b5773d76
pltaylor <pltaylor3@gmail.com>
2013-11-04 20:44:20 -0500

    Fill out the activity model

--- Octokit/Clients/ActivitiesClient.cs
+++ Octokit/Clients/ActivitiesClient.cs
@@ -1,8 +1,26 @@
 ﻿
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace Octokit
 {
-    public class ActivitiesClient : IActivitiesClient
+    public class ActivitiesClient: ApiClient, IActivitiesClient
     {
+        public ActivitiesClient(IApiConnection apiConnection)
+            : base(apiConnection)
+        {
+        }
 
+        /// <summary>
+        /// Gets all the public events
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/activity/events/#list-public-events
+        /// </remarks>
+        /// <returns>All the public <see cref="Activity"/>s for the particular user.</returns>
+        public Task<IReadOnlyList<Activity>> GetAll()
+        {
+            return ApiConnection.GetAll<Activity>(ApiUrls.Events());
+        }
     }
 }
---
+++ Octokit/Models/Response/Activity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Octokit
+{
+    public class Activity
+    {
+        /// <summary>
+        /// The type of the activity.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Whether the activity event is public or not.
+        /// </summary>
+        public bool Public { get; set; }
+        
+        /// <summary>
+        /// The repository associated with the activity event.
+        /// </summary>
+        public Repository Repo { get; set; }
+
+        /// <summary>
+        /// The user associated with the activity event.
+        /// </summary>
+        public User Actor { get; set; }
+
+        /// <summary>
+        /// The organization associated with the activity event.
+        /// </summary>
+        public Organization Org { get; set; }
+
+        /// <summary>
+        /// The date the activity event was created.
+        /// </summary>
+        public DateTimeOffset CreatedAt { get; set; }
+
+        /// <summary>
+        /// The activity event Id.
+        /// </summary>
+        public int Id { get; set; }
+    }
+}
