a67ce05cef0456262eb45e0372c9855464d9d3d2
Josh Sullivan <jaysche.sullivan@gmail.com>
2013-11-05 11:44:54 -0500

    Documented missing properties; Whitespace and typo tomfoolery patched

--- Octokit/Models/Response/PullRequest.cs
+++ Octokit/Models/Response/PullRequest.cs
@@ -14,8 +14,19 @@ namespace Octokit
         /// </summary>
         public int Number { get; set; }
 
+        /// <summary>
+        /// The URL for the pull request page.
+        /// </summary>
         public Uri HtmlUrl { get; set; }
+
+        /// <summary>
+        /// The URL for the pull request's diff (.diff) file.
+        /// </summary>
         public Uri DiffUrl { get; set; }
+
+        /// <summary>
+        /// The URL for the pull request's patch (.patch) file.
+        /// </summary>
         public Uri PatchUrl { get; set; }
     }
 }
