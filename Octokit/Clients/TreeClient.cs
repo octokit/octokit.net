73f9c5296478571816273580b2372b50074600b0
pltaylor <pltaylor3@gmail.com>
2013-11-07 13:29:51 -0500

    Write Create method

--- Octokit/Clients/TreeClient.cs
+++ Octokit/Clients/TreeClient.cs
@@ -13,12 +13,12 @@ namespace Octokit
         /// Gets a Tree Response for a given SHA.
         /// </summary>
         /// <remarks>
-        /// http://developer.github.com/v3/issues/comments/#get-a-single-comment
+        /// http://developer.github.com/v3/git/trees/#get-a-tree
         /// </remarks>
         /// <param name="owner">The owner of the repository</param>
         /// <param name="name">The name of the repository</param>
         /// <param name="reference">The SHA that references the tree</param>
-        /// <returns>The <see cref="IssueComment"/>s for the specified Issue Comment.</returns>
+        /// <returns>The <see cref="TreeResponse"/> for the specified Tree.</returns>
         public Task<TreeResponse> Get(string owner, string name, string reference)
         {
             Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
@@ -26,6 +26,25 @@ namespace Octokit
             Ensure.ArgumentNotNullOrEmptyString(reference, "reference");
 
             return ApiConnection.Get<TreeResponse>(ApiUrls.Tree(owner, name, reference));
+        }
+
+        /// <summary>
+        /// Creates a new Tree in the specified repo
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/git/trees/#create-a-tree
+        /// </remarks>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        /// <param name="newTree">The value of the new tree</param>
+        /// <returns>The <see cref="TreeResponse"/> that was just created.</returns>
+        public Task<TreeResponse> Create(string owner, string name, NewTree newTree)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
+            Ensure.ArgumentNotNullOrEmptyString(name, "name");
+            Ensure.ArgumentNotNull(newTree, "newTree");
+
+            return ApiConnection.Post<TreeResponse>(ApiUrls.IssueComments(owner, name), newTree);
         }
     }
 }
---
+++ Octokit/Models/Request/NewTree.cs
@@ -0,0 +1,15 @@
+﻿namespace Octokit
+{
+    public class NewTree
+    {
+        /// <summary>
+        /// The SHA1 of the tree you want to update with new data.
+        /// </summary>
+        public string Base_tree { get; set; }
+
+        /// <summary>
+        /// The list of Tree Items for this new Tree item.
+        /// </summary>
+        public NewTreeItem[] Tree { get; set; }
+    }
+}
