9288dc4fea35723aa5bde27ac7e68b28566e64f8
andreasohlund <andreasohlund2@gmail.com>
2013-11-09 15:06:07 +0100

    Adding support for releases with a null publish date. Closes #204

--- Octokit.Tests.Integration/ReleasesClientTests.cs
+++ Octokit.Tests.Integration/ReleasesClientTests.cs
@@ -22,6 +22,20 @@ namespace Octokit.Tests.Integration
                 Assert.True(releases.Count > 5);
                 Assert.True(releases.Any(release => release.TagName == "v0.18.0"));
             }
+
+            [IntegrationTest]
+            public async Task ReturnsReleasesWithNullPublishDate()
+            {
+                var github = new GitHubClient(new ProductHeaderValue("OctokitTests"))
+                {
+                    Credentials = Helper.Credentials
+                };
+
+                var releases = await github.Release.GetAll("Particular", "ServiceInsight");
+
+                Assert.True(releases.Count == 1);
+                Assert.False(releases.First().PublishedAt.HasValue);
+            }
         }
     }
 }
