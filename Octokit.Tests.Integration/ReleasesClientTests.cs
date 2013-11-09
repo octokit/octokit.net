c35f773f025f5fb4bbc46e365f100372f7f3c33f
Brendan Forster <brendan@github.com>
2013-11-09 17:03:06 -0600

    defined Release integration test and placeholders

--- Octokit.Tests.Integration/ReleasesClientTests.cs
+++ Octokit.Tests.Integration/ReleasesClientTests.cs
@@ -11,6 +11,9 @@ namespace Octokit.Tests.Integration
         public class TheGetReleasesMethod : IDisposable
         {
             readonly IReleasesClient _releaseClient;
+            readonly Repository _repository;
+            readonly string _repositoryOwner;
+            readonly string _repositoryName;
 
             public TheGetReleasesMethod()
             {
@@ -19,6 +22,19 @@ namespace Octokit.Tests.Integration
                     Credentials = Helper.Credentials
                 };
                 _releaseClient = github.Release;
+
+                var repoName = Helper.MakeNameWithTimestamp("public-repo");
+                _repository = github.Repository.Create(new NewRepository { Name = repoName }).Result;
+                _repositoryOwner = _repository.Owner.Login;
+                _repositoryName = _repository.Name;
+
+                // TODO: create test blob
+                // TODO: create test tree
+                // TODO: create test commit
+                // TODO: update master reference to latest commit
+
+                var releaseWithNoUpdate = new ReleaseUpdate("0.1");
+                var release = github.Release.CreateRelease(_repositoryOwner, _repositoryName, releaseWithNoUpdate).Result;
             }
 
             [IntegrationTest]
@@ -33,7 +49,7 @@ namespace Octokit.Tests.Integration
             [IntegrationTest]
             public async Task ReturnsReleasesWithNullPublishDate()
             {
-                var releases = await _releaseClient.GetAll("Particular", "ServiceInsight");
+                var releases = await _releaseClient.GetAll(_repositoryOwner, _repositoryName);
 
                 Assert.True(releases.Count == 1);
                 Assert.False(releases.First().PublishedAt.HasValue);
@@ -41,7 +57,7 @@ namespace Octokit.Tests.Integration
 
             public void Dispose()
             {
-                
+                Helper.DeleteRepo(_repository);
             }
         }
     }
