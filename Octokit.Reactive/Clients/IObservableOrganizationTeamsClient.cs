9f2cd30d3f140eb4c5a1b14bbd9cfddf4406e264
Haroon <haroon.ahmed25@gmail.com>
2013-11-10 10:05:17 +0000

    prepping reactive classes

---
+++ Octokit.Reactive/Clients/IObservableOrganizationTeamsClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reactive;
+
+namespace Octokit.Reactive
+{
+    /// <summary>
+    /// A client for GitHub's Org Teams API.
+    /// </summary>
+    /// <remarks>
+    /// See the <a href="http://developer.github.com/v3/orgs/teams/">Orgs API documentation</a> for more information.
+    /// </remarks>
+    public interface IObservableOrganizationTeamsClient
+    {
+        /// <summary>
+        /// Returns all <see cref="Team" />s for the current org.
+        /// </summary>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>A list of the orgs's teams <see cref="TeamItem"/>s.</returns>
+        IObservable<TeamItem> GetAllTeams(string org);
+
+        /// <summary>
+        /// Returns newly created <see cref="Team" /> for the current org.
+        /// </summary>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>Newly created <see cref="Team"/></returns>
+        IObservable<Team> CreateTeam(string org, NewTeam team);
+
+        /// <summary>
+        /// Returns updated <see cref="Team" /> for the current org.
+        /// </summary>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>Updated <see cref="Team"/></returns>
+        IObservable<Team> UpdateTeam(int id, UpdateTeam team);
+
+        /// <summary>
+        /// Delete a team - must have owner permissions to this
+        /// </summary>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns></returns>
+        IObservable<Unit> DeleteTeam(int id);
+
+    }
+}
---
+++ Octokit.Reactive/Clients/ObservableOrganizationTeamsClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Threading.Tasks;
+using Octokit.Reactive.Internal;
+
+namespace Octokit.Reactive
+{
+    public class ObservableOrganizationTeamsClient : IObservableOrganizationTeamsClient
+    {
+        //readonly IConnection _connection;
+
+        /// <summary>
+        /// Initializes a new Organization Teams API client.
+        /// </summary>
+        /// <param name="client">An <see cref="IGitHubClient" /> used to make the requests</param>
+        public ObservableOrganizationTeamsClient(IGitHubClient client)
+        {
+            Ensure.ArgumentNotNull(client, "client");
+            //_connection = client.Connection;
+        }
+
+        public IObservable<TeamItem> GetAllTeams(string org)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<Team> CreateTeam(string org, NewTeam team)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<Team> UpdateTeam(int id, UpdateTeam team)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<Unit> DeleteTeam(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
