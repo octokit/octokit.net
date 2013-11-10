3b102ffe37627a3884e89e76a08422ee9c51b786
Haroon <haroon.ahmed25@gmail.com>
2013-11-10 10:32:53 +0000

    implement all the other methods

--- Octokit.Reactive/Clients/ObservableOrganizationTeamsClient.cs
+++ Octokit.Reactive/Clients/ObservableOrganizationTeamsClient.cs
@@ -8,6 +8,7 @@ namespace Octokit.Reactive
     public class ObservableOrganizationTeamsClient : IObservableOrganizationTeamsClient
     {
         readonly IConnection _connection;
+        readonly ITeamsClient _client;
 
         /// <summary>
         /// Initializes a new Organization Teams API client.
@@ -17,28 +18,28 @@ namespace Octokit.Reactive
         {
             Ensure.ArgumentNotNull(client, "client");
             _connection = client.Connection;
+            _client = client.Organization.Team;
         }
 
         public IObservable<TeamItem> GetAllTeams(string org)
         {
             Ensure.ArgumentNotNullOrEmptyString(org, "org");
-
             return _connection.GetAndFlattenAllPages<TeamItem>(ApiUrls.OrganizationTeams(org));
         }
 
         public IObservable<Team> CreateTeam(string org, NewTeam team)
         {
-            throw new NotImplementedException();
+            return _client.CreateTeam(org, team).ToObservable();
         }
 
         public IObservable<Team> UpdateTeam(int id, UpdateTeam team)
         {
-            throw new NotImplementedException();
+            return _client.UpdateTeam(id, team).ToObservable();
         }
 
         public IObservable<Unit> DeleteTeam(int id)
         {
-            throw new NotImplementedException();
+            return _client.DeleteTeam(id).ToObservable();
         }
     }
 }
