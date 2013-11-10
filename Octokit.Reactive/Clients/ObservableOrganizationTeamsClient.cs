61b95f6a4d63a67f25642c5637852ac4ef3938c8
Haroon <haroon.ahmed25@gmail.com>
2013-11-10 10:46:52 +0000

    document my code

--- Octokit.Reactive/Clients/ObservableOrganizationTeamsClient.cs
+++ Octokit.Reactive/Clients/ObservableOrganizationTeamsClient.cs
@@ -21,22 +21,42 @@ namespace Octokit.Reactive
             _client = client.Organization.Team;
         }
 
+        /// <summary>
+        /// Returns all <see cref="Team" />s for the current org.
+        /// </summary>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>A list of the orgs's teams <see cref="TeamItem"/>s.</returns>
         public IObservable<Team> GetAllTeams(string org)
         {
             Ensure.ArgumentNotNullOrEmptyString(org, "org");
             return _connection.GetAndFlattenAllPages<Team>(ApiUrls.OrganizationTeams(org));
         }
 
+        /// <summary>
+        /// Returns newly created <see cref="Team" /> for the current org.
+        /// </summary>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>Newly created <see cref="Team"/></returns>
         public IObservable<Team> CreateTeam(string org, NewTeam team)
         {
             return _client.CreateTeam(org, team).ToObservable();
         }
 
+        /// <summary>
+        /// Returns updated <see cref="Team" /> for the current org.
+        /// </summary>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>Updated <see cref="Team"/></returns>
         public IObservable<Team> UpdateTeam(int id, UpdateTeam team)
         {
             return _client.UpdateTeam(id, team).ToObservable();
         }
 
+        /// <summary>
+        /// Delete a team - must have owner permissions to this
+        /// </summary>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns></returns>
         public IObservable<Unit> DeleteTeam(int id)
         {
             return _client.DeleteTeam(id).ToObservable();
