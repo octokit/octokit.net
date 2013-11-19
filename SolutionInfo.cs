602502a928e67bac38c82695165a5fd226cbd9cf
Haacked <haacked@gmail.com>
2013-11-19 09:52:59 -0800

    Release notes for 0.1.5

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,11 +3,11 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProductAttribute("Octokit")]
-[assembly: AssemblyVersionAttribute("0.1.4")]
-[assembly: AssemblyFileVersionAttribute("0.1.4")]
+[assembly: AssemblyVersionAttribute("0.1.5")]
+[assembly: AssemblyFileVersionAttribute("0.1.5")]
 [assembly: ComVisibleAttribute(false)]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.1.4";
+        internal const string Version = "0.1.5";
     }
 }
