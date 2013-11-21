e01da11af5c821225995711ede0a74c37a7bd2ea
Haroon <haroon.ahmed25@gmail.com>
2013-11-21 09:03:59 +0000

    updated language params

--- Octokit/Models/Request/SearchTerm.cs
+++ Octokit/Models/Request/SearchTerm.cs
@@ -1,3 +1,4 @@
+using Octokit.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,7 +27,7 @@ namespace Octokit
             Stars = stars;
             Forks = forks;
             Language = language;
-            
+
             if (inQualifiers != null && inQualifiers.Count > 0)
                 In = inQualifiers.Distinct().ToList();
         }
@@ -257,131 +258,250 @@ namespace Octokit
 
     public enum Language
     {
-        ABAP,
+        Abap,
+        [Parameter(Value = "ActionScript")]
         ActionScript,
         Ada,
         Apex,
+        [Parameter(Value = "AppleScript")]
         AppleScript,
         Arc,
         Arduino,
-        ASP,
+        [Parameter(Value = "ApacheConf")]
+        ApacheConf,
+        Asp,
         Assembly,
         Augeas,
+        [Parameter(Value = "AutoHotKey")]
         AutoHotKey,
         Awk,
+        Batchfile,
+        Befunge,
+        [Parameter(Value = "BlitzMax")]
+        BlitzMax,
         Boo,
         Bro,
         C,
-        CSharp,
-        CPlusPlus,
+        [Parameter(Value = "C2hs Haskell")]
+        C2hsHaskell,
         Ceylon,
-        CLIPS,
+        Chuck,
+        Clips,
         Clojure,
-        COBOL,
+        Cobol,
+        Cmake,
+        [Parameter(Value = "C-ObjDump")]
+        CObjDump,
+        [Parameter(Value = "CoffeeScript")]
         CoffeeScript,
+        [Parameter(Value = "ColdFusion")]
         ColdFusion,
         CommonLisp,
         Coq,
+        [Parameter(Value = "C++")]
+        CPlusPlus,
+        [Parameter(Value = "C#")]
+        CSharp,
         Css,
+        [Parameter(Value = "Cpp-ObjDump")]
+        CppObjDump,
+        Cucumber,
+        Cython,
         D,
+        [Parameter(Value = "D-ObjDump")]
+        DObjDump,
+        [Parameter(Value = "DarcsPatch")]
+        DarcsPatch,
         Dart,
+        [Parameter(Value = "DCPU-16 ASM")]
         Dcpu16Asm,
         Dot,
         Dylan,
-        EC,
+        Ec,
+        [Parameter(Value = "Ecere Projects")]
+        EcereProjects,
         Ecl,
+        Edn,
         Eiffel,
         Elixer,
         Elm,
         EmacsLisp,
         Erlang,
+        [Parameter(Value = "F#")]
         FSharp,
         Factor,
         Fancy,
         Fantom,
+        Fish,
         Forth,
-        Fortron,
+        Fortran,
+        Gas,
+        Genshi,
+        [Parameter(Value = "Gentoo Build")]
+        GentooBuild,
+        [Parameter(Value = "Gentoo Eclass")]
+        GentooEclass,
+        [Parameter(Value = "Gettext Catalog")]
+        GettextCatalog,
+        Glsl,
         Go,
         Gosu,
+        Groff,
         Groovy,
+        [Parameter(Value = "Groovy Server Pages")]
+        GroovyServerPages,
+        Haml,
+        [Parameter(Value = "HandleBars")]
+        HandleBars,
         Haskell,
         Haxe,
+        Http,
+        Ini,
         Io,
         Ioke,
+        [Parameter(Value = "IRC log")]
+        IrcLog,
         J,
         Java,
+        [Parameter(Value = "Java Server Pages")]
+        JavaServerPages,
         JavaScript,
+        Json,
         Julia,
         Kotlin,
         Lasso,
+        Less,
+        Lfe,
+        [Parameter(Value = "LillyPond")]
+        LillyPond,
+        [Parameter(Value = "Literate CoffeeScript")]
+        LiterateCoffeeScript,
+        [Parameter(Value = "Literate Haskell")]
+        LiterateHaskell,
+        [Parameter(Value = "LiveScript")]
         LiveScript,
+        Llvm,
         Logos,
         Logtalk,
         Lua,
         M,
+        Makefile,
+        Mako,
         Markdown,
         Matlab,
         Max,
+        [Parameter(Value = "MiniD")]
+        MiniD,
         Mirah,
         Monkey,
+        Moocode,
         Moonscript,
-        Nemerie,
+        Mupad,
+        Myghty,
+        Nemerle,
+        Nginx,
         Nimrod,
+        Nsis,
         Nu,
+        [Parameter(Value = "NumPY")]
+        NumPY,
+        [Parameter(Value = "ObjDump")]
+        ObjDump,
         ObjectiveC,
         ObjectiveJ,
+        [Parameter(Value = "OCaml")]
         OCaml,
-        Omgrofi,
+        Omgrofl,
         Ooc,
         Opa,
+        [Parameter(Value = "OpenCL")]
+        OpenCL,
+        [Parameter(Value = "OpenEdge ABL")]
         OpenEdgeAbl,
         Parrot,
+        [Parameter(Value = "Parrot Assembly")]
+        ParrotAssembly,
+        [Parameter(Value = "Parrot Internal Representation")]
+        ParrotInternalRepresentation,
         Pascal,
         Perl,
         Php,
         Pike,
+        [Parameter(Value = "PogoScript")]
         PogoScript,
+        [Parameter(Value = "PowerShell")]
         PowerShell,
         Processing,
         Prolog,
         Puppet,
+        [Parameter(Value = "Pure Data")]
         PureData,
         Python,
+        [Parameter(Value = "Python traceback")]
+        PythonTraceback,
         R,
         Racket,
+        [Parameter(Value = "Ragel in Ruby Host")]
         RagelInRubyHost,
+        [Parameter(Value = "Raw token data")]
+        RawTokenData,
         Rebol,
+        Redcode,
+        [Parameter(Value = "reStructuredText")]
+        ReStructuredText,
+        Rhtml,
         Rouge,
         Ruby,
         Rust,
         Scala,
         Scheme,
+        Sage,
+        Sass,
         Scilab,
+        Scss,
         Self,
         Shell,
         Slash,
         Smalltalk,
+        Smarty,
         Squirrel,
+        [Parameter(Value = "Standard ML")]
         StandardML,
+        [Parameter(Value = "SuperCollider")]
         SuperCollider,
         Tcl,
+        Tcsh,
+        Tea,
+        [Parameter(Value = "TeX")]
+        TeX,
+        Textile,
+        Toml,
         Turing,
+        Twig,
         Txl,
+        [Parameter(Value = "TypeScript")]
         TypeScript,
+        [Parameter(Value = "Unified Parallel C")]
+        UnifiedParalelC,
         Unkown,
         Vala,
         Verilog,
         Vhdl,
+        [Parameter(Value = "VimL")]
         VimL,
         VisualBasic,
         Volt,
         Wisp,
         Xc,
         Xml,
+        [Parameter(Value = "XProc")]
         XProc,
+        [Parameter(Value = "XQuery")]
         XQuery,
+        Xs,
         Xslt,
-        XLend
+        Xtend,
+        Yaml
     }
 
     public enum QualifierOperator
