0e3686578b5e07dd1f811a5fe401dcffc952b6b5
Kristian Hellang <kristian@hellang.com>
2013-11-22 01:27:57 +0100

    Added tests for the Get method

---
+++ Octokit.Tests/Clients/ReferencesClientTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using NSubstitute;
+
+using Octokit.Tests.Helpers;
+
+using Xunit;
+
+namespace Octokit.Tests.Clients
+{
+    public class ReferencesClientTests
+    {
+        public class TheGetMethod
+        {
+            [Fact]
+            public async void EnsuresNonNullArguments()
+            {
+                var client = new ReferencesClient(Substitute.For<IApiConnection>());
+
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.Get(null, "name", "heads/develop"));
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.Get("owner", null, "heads/develop"));
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.Get("owner", "name", null));
+                await AssertEx.Throws<ArgumentException>(async () => await client.Get("", "name", "heads/develop"));
+                await AssertEx.Throws<ArgumentException>(async () => await client.Get("owner", "", "heads/develop"));
+                await AssertEx.Throws<ArgumentException>(async () => await client.Get("owner", "name", ""));
+            }
+
+            [Fact]
+            public void RequestsCorrectUrl()
+            {
+                var connection = Substitute.For<IApiConnection>();
+                var client = new ReferencesClient(connection);
+
+                client.Get("owner", "repo", "heads/develop");
+
+                connection.Received().Get<Reference>(Arg.Is<Uri>(u => u.ToString() == "repos/owner/repo/git/refs/heads/develop"), null);
+            }
+        }
+    }
+}
