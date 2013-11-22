0b389da051573cc88ee56ac1a561840963737980
Kristian Hellang <kristian@hellang.com>
2013-11-22 01:05:00 +0100

    Added implementation stub. Test is passing

---
+++ Octokit/Clients/ReferencesClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Octokit
+{
+    public class ReferencesClient : ApiClient, IReferencesClient
+    {
+        public ReferencesClient(IApiConnection apiConnection) : base(apiConnection) { }
+
+        public Task<GitReference> Get(string owner, string repo, string reference)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
