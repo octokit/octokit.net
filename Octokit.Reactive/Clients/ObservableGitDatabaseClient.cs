db7624a4ba9da84964ced895353b28d7999f7b90
Kristian Hellang <kristian@hellang.com>
2013-11-25 00:57:45 +0100

    Added reactive client

--- Octokit.Reactive/Clients/ObservableGitDatabaseClient.cs
+++ Octokit.Reactive/Clients/ObservableGitDatabaseClient.cs
@@ -6,9 +6,11 @@
         {
             Tag = new ObservableTagsClient(client);
             Commit = new ObservableCommitsClient(client);
+            Reference = new ObservableReferencesClient(client);
         }
 
         public IObservableTagsClient Tag { get; set; }
         public IObservableCommitsClient Commit { get; set; }
+        public IObservableReferencesClient Reference { get; set; }
     }
 }
