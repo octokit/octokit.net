5766204c84754b24f64e0c73f5ba33115dbffd72
Kristian Hellang <kristian@hellang.com>
2013-11-24 23:44:30 +0100

    Added missing tests

--- Octokit/Models/Request/NewReference.cs
+++ Octokit/Models/Request/NewReference.cs
@@ -2,7 +2,16 @@
 {
     public class NewReference
     {
-        public string Ref { get; set; }
-        public string Sha { get; set; }
+        public NewReference(string @ref, string sha)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(@ref, "ref");
+            Ensure.ArgumentNotNullOrEmptyString(sha, "sha");
+
+            Ref = @ref;
+            Sha = sha;
+        }
+
+        public string Ref { get; private set; }
+        public string Sha { get; private set; }
     }
 }
