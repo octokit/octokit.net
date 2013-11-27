aef785e37dc8c06f2328b450bfd5564c06faf46b
Brendan Forster <brendan@github.com>
2013-11-27 11:22:03 -0800

    added Trees to the base GitDatabaseClient, added some initial tests

--- Octokit/Clients/GitDatabaseClient.cs
+++ Octokit/Clients/GitDatabaseClient.cs
@@ -6,12 +6,14 @@
             : base(apiConnection)
         {
             Blob = new BlobsClient(apiConnection);
+            Tree = new TreesClient(apiConnection);
             Tag = new TagsClient(apiConnection);
             Commit = new CommitsClient(apiConnection);
             Reference = new ReferencesClient(apiConnection);
         }
 
         public IBlobsClient Blob { get; set; }
+        public ITreesClient Tree { get; set; }
         public ITagsClient Tag { get; set; }
         public ICommitsClient Commit { get; set; }
         public IReferencesClient Reference { get; set; }
