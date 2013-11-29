92ad3171d65dcd20af356d6f838dc5eb4b94ed5f
Brendan Forster <brendan@github.com>
2013-11-28 16:04:16 -0800

    stripped out some unnecessary whitespace

--- Octokit.Tests.Integration/Clients/GistsClientTests.cs
+++ Octokit.Tests.Integration/Clients/GistsClientTests.cs
@@ -1,31 +1,27 @@
-﻿using System;
-using System.Linq;
-using System.Net.Http.Headers;
+﻿using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Octokit;
+using Octokit.Tests.Integration;
 using Xunit;
 
-namespace Octokit.Tests.Integration
+public class GistsClientTests
 {
-    public class GistsClientTests 
+    readonly IGistsClient _fixture;
+
+    public GistsClientTests()
     {
-        readonly IGitHubClient _gitHubClient;
-        readonly IGistsClient _gistsClient;
-
-        public GistsClientTests()
+        var client = new GitHubClient(new ProductHeaderValue("OctokitTests"))
         {
-            this._gitHubClient = new GitHubClient(new ProductHeaderValue("OctokitTests"))
-            {
-                Credentials = Helper.Credentials
-            };
+            Credentials = Helper.Credentials
+        };
 
-            this._gistsClient = this._gitHubClient.Gist;
-        }
+        _fixture = client.Gist;
+    }
 
-        [IntegrationTest]
-        public async Task CanGetGist()
-        {
-            var retrieved = await this._gistsClient.Get("6305249");
-            Assert.NotNull(retrieved);
-        }
+    [IntegrationTest]
+    public async Task CanGetGist()
+    {
+        var retrieved = await _fixture.Get("6305249");
+        Assert.NotNull(retrieved);
     }
 }
--- Octokit.Tests.Integration/Clients/ReleasesClientTests.cs
+++ Octokit.Tests.Integration/Clients/ReleasesClientTests.cs
@@ -2,60 +2,59 @@
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Octokit;
+using Octokit.Tests.Integration;
 using Xunit;
 
-namespace Octokit.Tests.Integration
+public class ReleasesClientTests
 {
-    public class ReleasesClientTests
+    public class TheGetReleasesMethod : IDisposable
     {
-        public class TheGetReleasesMethod : IDisposable
+        readonly IReleasesClient _releaseClient;
+        readonly Repository _repository;
+        readonly string _repositoryOwner;
+        readonly string _repositoryName;
+        readonly GitHubClient _github;
+
+        public TheGetReleasesMethod()
         {
-            readonly IReleasesClient _releaseClient;
-            readonly Repository _repository;
-            readonly string _repositoryOwner;
-            readonly string _repositoryName;
-            readonly GitHubClient _github;
-
-            public TheGetReleasesMethod()
+            _github = new GitHubClient(new ProductHeaderValue("OctokitTests"))
             {
-                _github = new GitHubClient(new ProductHeaderValue("OctokitTests"))
-                {
-                    Credentials = Helper.Credentials
-                };
-                _releaseClient = _github.Release;
+                Credentials = Helper.Credentials
+            };
+            _releaseClient = _github.Release;
 
-                var repoName = Helper.MakeNameWithTimestamp("public-repo");
-                _repository = _github.Repository.Create(new NewRepository { Name = repoName, AutoInit = true }).Result;
-                _repositoryOwner = _repository.Owner.Login;
-                _repositoryName = _repository.Name;
-            }
+            var repoName = Helper.MakeNameWithTimestamp("public-repo");
+            _repository = _github.Repository.Create(new NewRepository { Name = repoName, AutoInit = true }).Result;
+            _repositoryOwner = _repository.Owner.Login;
+            _repositoryName = _repository.Name;
+        }
 
-            [IntegrationTest]
-            public async Task ReturnsReleases()
-            {
-                var releases = await _releaseClient.GetAll("git-tfs", "git-tfs");
+        [IntegrationTest]
+        public async Task ReturnsReleases()
+        {
+            var releases = await _releaseClient.GetAll("git-tfs", "git-tfs");
 
-                Assert.True(releases.Count > 5);
-                Assert.True(releases.Any(release => release.TagName == "v0.18.0"));
-            }
+            Assert.True(releases.Count > 5);
+            Assert.True(releases.Any(release => release.TagName == "v0.18.0"));
+        }
 
-            [IntegrationTest]
-            public async Task ReturnsReleasesWithNullPublishDate()
-            {
-                // create a release without a publish date
-                var releaseWithNoUpdate = new ReleaseUpdate("0.1") { Draft = true };
-                var release = _releaseClient.CreateRelease(_repositoryOwner, _repositoryName, releaseWithNoUpdate).Result;
+        [IntegrationTest]
+        public async Task ReturnsReleasesWithNullPublishDate()
+        {
+            // create a release without a publish date
+            var releaseWithNoUpdate = new ReleaseUpdate("0.1") { Draft = true };
+            var release = _releaseClient.CreateRelease(_repositoryOwner, _repositoryName, releaseWithNoUpdate).Result;
 
-                var releases = await _releaseClient.GetAll(_repositoryOwner, _repositoryName);
+            var releases = await _releaseClient.GetAll(_repositoryOwner, _repositoryName);
 
-                Assert.True(releases.Count == 1);
-                Assert.False(releases.First().PublishedAt.HasValue);
-            }
+            Assert.True(releases.Count == 1);
+            Assert.False(releases.First().PublishedAt.HasValue);
+        }
 
-            public void Dispose()
-            {
-                Helper.DeleteRepo(_repository);
-            }
+        public void Dispose()
+        {
+            Helper.DeleteRepo(_repository);
         }
     }
 }
