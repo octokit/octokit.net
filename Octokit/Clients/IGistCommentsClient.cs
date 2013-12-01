78678c2b1b451e1778951ee8d382119af1894aab
Kristian Hellang <kristian@hellang.com>
2013-12-01 22:00:11 +0100

    Added stubs for IGistCommentsClient

---
+++ Octokit/Clients/IGistCommentsClient.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace Octokit
+{
+    public interface IGistCommentsClient
+    {
+        [SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", MessageId = "Get", 
+            Justification = "Method makes a network request")]
+        Task<GistComment> Get(int gistId, int commentId);
+        Task<IReadOnlyList<GistComment>> GetForGist(int gistId);
+        Task<GistComment> Create(int gistId, string comment);
+        Task<GistComment> Update(int gistId, int commentId, string comment);
+        Task Delete(int gistId, int commentId);
+    }
+}
---
+++ Octokit/Models/Response/GistComment.cs
@@ -0,0 +1,7 @@
+﻿namespace Octokit
+{
+    public class GistComment
+    {
+         
+    }
+}
