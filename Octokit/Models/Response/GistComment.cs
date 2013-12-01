b1d0a090a0b45068fa9f9c07ef504ea8d90a9bcc
Kristian Hellang <kristian@hellang.com>
2013-12-01 22:36:47 +0100

    Fleshed out the GistComment model

--- Octokit/Models/Response/GistComment.cs
+++ Octokit/Models/Response/GistComment.cs
@@ -1,7 +1,37 @@
-﻿namespace Octokit
+﻿using System;
+
+namespace Octokit
 {
     public class GistComment
     {
-         
+        /// <summary>
+        /// The gist comment id.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The URL for this gist comment.
+        /// </summary>
+        public Uri Url { get; set; }
+
+        /// <summary>
+        /// The body of this gist comment.
+        /// </summary>
+        public string Body { get; set; }
+
+        /// <summary>
+        /// The user that created this gist comment.
+        /// </summary>
+        public User User { get; set; }
+
+        /// <summary>
+        /// The date this comment was created.
+        /// </summary>
+        public DateTimeOffset CreatedAt { get; set; }
+
+        /// <summary>
+        /// The date this comment was last updated.
+        /// </summary>
+        public DateTimeOffset? UpdatedAt { get; set; }
     }
 }
