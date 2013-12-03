f23a9beed1b60d2e5c249f1d46084316461ed39b
Robert Mills <robert@sleepymills.com>
2013-12-02 22:44:28 -0500

    Complete Gist API

--- Octokit.Tests.Integration/GistsClientTests.cs
+++ Octokit.Tests.Integration/GistsClientTests.cs
@@ -12,6 +12,7 @@ namespace Octokit.Tests.Integration
     {
         readonly IGitHubClient _gitHubClient;
         readonly IGistsClient _gistsClient;
+        readonly string testGistId = "6305249";
 
         public GistsClientTests()
         {
@@ -26,12 +27,12 @@ namespace Octokit.Tests.Integration
         [IntegrationTest]
         public async Task CanGetGist()
         {
-            var retrieved = await this._gistsClient.Get("6305249");
+            var retrieved = await this._gistsClient.Get(testGistId);
             Assert.NotNull(retrieved);
         }
 
         [IntegrationTest]
-        public async Task CanCreateAndDeleteAGist()
+        public async Task CanCreateEditAndDeleteAGist()
         {
             var newGist = new NewGist();
             newGist.Description = "my new gist";
@@ -45,7 +46,94 @@ namespace Octokit.Tests.Integration
             Assert.Equal(newGist.Description, createdGist.Description);
             Assert.Equal(newGist.Public, createdGist.Public);
 
+            var gistUpdate = new GistUpdate();
+            gistUpdate.Description = "my newly updated gist";
+            var gistFileUpdate = new GistFileUpdate
+            {
+                NewFileName = "myNewGistTestFile.cs",
+                Content = "new GistsClient(connection).Edit();"
+            };
+
+            gistUpdate.Files.Add("myGistTestFile.cs", gistFileUpdate);
+
+            var updatedGist = await this._gistsClient.Edit(createdGist.Id, gistUpdate);
+
+            Assert.NotNull(updatedGist);
+            Assert.Equal<string>(updatedGist.Description, gistUpdate.Description);
+
             Assert.DoesNotThrow(async () => { await this._gistsClient.Delete(createdGist.Id); });
+        }
+
+        [IntegrationTest]
+        public async Task CanStarAndUnstarAGist()
+        {
+            Assert.DoesNotThrow(async () => { await this._gistsClient.Star(testGistId); });
+
+            bool isStarredTrue = await this._gistsClient.IsStarred(testGistId);
+            Assert.True(isStarredTrue);
+
+            Assert.DoesNotThrow(async () => { await this._gistsClient.Unstar(testGistId); });
+
+            bool isStarredFalse = await this._gistsClient.IsStarred(testGistId);
+            Assert.False(isStarredFalse);
+        }
+
+        [IntegrationTest]
+        public async Task CanForkAGist()
+        {
+            var forkedGist = await this._gistsClient.Fork(testGistId);
+            
+            Assert.NotNull(forkedGist);
+
+            await this._gistsClient.Delete(forkedGist.Id);
+        }
+
+        [IntegrationTest]
+        public async Task CanListGists()
+        {
+            //Time is tricky between local and remote, be leinent
+            var startTime = DateTimeOffset.Now.Subtract(TimeSpan.FromHours(1));
+            var newGist = new NewGist();
+            newGist.Description = "my new gist";
+            newGist.Public = true;
+
+            newGist.Files.Add("myGistTestFile.cs", "new GistsClient(connection).Create();");
+
+            var createdGist = await this._gistsClient.Create(newGist);
+
+            //Test get all Gists
+            var gists = await this._gistsClient.GetAll();
+            Assert.NotNull(gists);
+
+            //Test get all Gists since startTime
+            gists = await this._gistsClient.GetAll(startTime);
+
+            Assert.NotNull(gists);
+            Assert.True(gists.Count > 0);
+
+            //Make sure we can successfully request gists for another user
+            Assert.DoesNotThrow(async () => { await this._gistsClient.GetAllForUser("FakeHaacked"); });
+            Assert.DoesNotThrow(async () => { await this._gistsClient.GetAllForUser("FakeHaacked", startTime); });
+
+            //Test public gists
+            var publicGists = await this._gistsClient.GetAllPublic();
+            Assert.True(publicGists.Count > 1);
+
+            var publicGistsSinceStartTime = await this._gistsClient.GetAllPublic(startTime);
+            Assert.True(publicGistsSinceStartTime.Count > 0);
+
+            //Test starred gists
+            await this._gistsClient.Star(createdGist.Id);
+            var starredGists = await this._gistsClient.GetAllStarred();
+
+            Assert.NotNull(starredGists);
+            Assert.True(starredGists.Any(x => x.Id == createdGist.Id));
+
+            var starredGistsSinceStartTime = await this._gistsClient.GetAllStarred(startTime);
+            Assert.NotNull(starredGistsSinceStartTime);
+            Assert.True(starredGistsSinceStartTime.Any(x => x.Id == createdGist.Id));
+
+            await this._gistsClient.Delete(createdGist.Id);
         }
     }
 }
--- Octokit/Clients/GistsClient.cs
+++ Octokit/Clients/GistsClient.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Octokit
@@ -52,6 +54,18 @@ namespace Octokit
         }
 
         /// <summary>
+        /// Creates a fork of a gist
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/gists/#fork-a-gist
+        /// </remarks>
+        /// <param name="id">The id of the gist to fork</param>
+        public Task<Gist> Fork(string id)
+        {
+            return ApiConnection.Post<Gist>(ApiUrls.ForkGist(id), new object());
+        }
+
+        /// <summary>
         /// Deletes a gist
         /// </summary>
         /// <remarks>
@@ -63,6 +77,190 @@ namespace Octokit
             Ensure.ArgumentNotNull(id, "id");
 
             return ApiConnection.Delete(ApiUrls.Gist(id));
+        }
+
+        /// <summary>
+        /// List the authenticated user’s gists or if called anonymously, 
+        /// this will return all public gists
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/gists/#list-gists
+        /// </remarks>
+        /// <param name="since">Only gists updated at or after this time are returned</param>
+        public Task<IReadOnlyList<Gist>> GetAll()
+        {
+            return ApiConnection.GetAll<Gist>(ApiUrls.Gist());
+        }
+
+        /// <summary>
+        /// List the authenticated user’s gists or if called anonymously, 
+        /// this will return all public gists
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/gists/#list-gists
+        /// </remarks>
+        /// <param name="since">Only gists updated at or after this time are returned</param>
+        public Task<IReadOnlyList<Gist>> GetAll(DateTimeOffset since)
+        {
+            var request = new GistRequest(since);
+            return ApiConnection.GetAll<Gist>(ApiUrls.Gist(), request.ToParametersDictionary());
+        }
+
+        /// <summary>
+        /// Lists all public gists
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/gists/#list-gists
+        /// </remarks>
+        public Task<IReadOnlyList<Gist>> GetAllPublic()
+        {
+            return ApiConnection.GetAll<Gist>(ApiUrls.PublicGists());
+        }
+
+        /// <summary>
+        /// Lists all public gists
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/gists/#list-gists
+        /// </remarks>
+        /// <param name="since">Only gists updated at or after this time are returned</param>
+        public Task<IReadOnlyList<Gist>> GetAllPublic(DateTimeOffset since)
+        {
+            var request = new GistRequest(since);
+            return ApiConnection.GetAll<Gist>(ApiUrls.PublicGists(), request.ToParametersDictionary());
+        }
+
+        /// <summary>
+        /// List the authenticated user’s starred gists
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/gists/#list-gists
+        /// </remarks>
+        public Task<IReadOnlyList<Gist>> GetAllStarred()
+        {
+            return ApiConnection.GetAll<Gist>(ApiUrls.StarredGists());
+        }
+
+        /// <summary>
+        /// List the authenticated user’s starred gists
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/gists/#list-gists
+        /// </remarks>
+        /// <param name="since">Only gists updated at or after this time are returned</param>
+        public Task<IReadOnlyList<Gist>> GetAllStarred(DateTimeOffset since)
+        {
+            var request = new GistRequest(since);
+            return ApiConnection.GetAll<Gist>(ApiUrls.StarredGists(), request.ToParametersDictionary());
+        }
+
+        /// <summary>
+        /// List a user's gists
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/gists/#list-gists
+        /// </remarks>
+        /// <param name="user">The user</param>
+        public Task<IReadOnlyList<Gist>> GetAllForUser(string user)
+        {
+            Ensure.ArgumentNotNull(user, "user");
+
+            return ApiConnection.GetAll<Gist>(ApiUrls.UsersGists(user));
+        }
+
+        /// <summary>
+        /// List a user's gists
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/gists/#list-gists
+        /// </remarks>
+        /// <param name="user">The user</param>
+        /// <param name="since">Only gists updated at or after this time are returned</param>
+        public Task<IReadOnlyList<Gist>> GetAllForUser(string user, DateTimeOffset since)
+        {
+            Ensure.ArgumentNotNull(user, "user");
+
+            var request = new GistRequest(since);
+            return ApiConnection.GetAll<Gist>(ApiUrls.UsersGists(user), request.ToParametersDictionary());
+        }
+
+        /// <summary>
+        /// Edits a gist
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/gists/#edit-a-gist
+        /// </remarks>
+        /// <param name="id">The id of the gist</param>
+        /// <param name="gistUpdate">The update to the gist</param>
+        public Task<Gist> Edit(string id, GistUpdate gistUpdate)
+        {
+            Ensure.ArgumentNotNull(id, "id");
+            Ensure.ArgumentNotNull(gistUpdate, "gistUpdate");
+
+            var filesAsJsonObject = new JsonObject();
+            foreach (var kvp in gistUpdate.Files)
+            {
+                filesAsJsonObject.Add(kvp.Key, new { Content = kvp.Value.Content, Filename = kvp.Value.NewFileName });
+            }
+
+            var gist = new
+            {
+                Description = gistUpdate.Description,
+                Files = filesAsJsonObject
+            };
+
+            return ApiConnection.Patch<Gist>(ApiUrls.Gist(id), gist);
+        }
+
+        /// <summary>
+        /// Stars a gist
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/gists/#star-a-gist
+        /// </remarks>
+        /// <param name="id">The id of the gist</param>
+        public Task Star(string id)
+        {
+            return ApiConnection.Put(ApiUrls.StarGist(id));
+        }
+
+        /// <summary>
+        /// Unstars a gist
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/gists/#unstar-a-gist
+        /// </remarks>
+        /// <param name="id">The id of the gist</param>
+        public Task Unstar(string id)
+        {
+            return ApiConnection.Delete(ApiUrls.StarGist(id));
+        }
+
+        /// <summary>
+        /// Checks if the gist is starred
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/gists/#check-if-a-gist-is-starred
+        /// </remarks>
+        /// <param name="id">The id of the gist</param>
+        public async Task<bool> IsStarred(string id)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(id, "id");
+
+            try
+            {
+                var response = await Connection.GetAsync<object>(ApiUrls.StarGist(id), null, null)
+                                               .ConfigureAwait(false);
+                if (response.StatusCode != HttpStatusCode.NotFound && response.StatusCode != HttpStatusCode.NoContent)
+                {
+                    throw new ApiException("Invalid Status Code returned. Expected a 204 or a 404", response.StatusCode);
+                }
+                return response.StatusCode == HttpStatusCode.NoContent;
+            }
+            catch (NotFoundException)
+            {
+                return false;
+            }
         }
     }
 }
