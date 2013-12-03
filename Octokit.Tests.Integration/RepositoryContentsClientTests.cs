4bed7e2d79e5e7adac46156a1e27960068b4702f
Brendan Forster <brendan@github.com>
2013-12-03 10:48:07 +0000

    added integration tests for working against the Contents API

---
+++ Octokit.Tests.Integration/RepositoryContentsClientTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Octokit.Tests.Integration
+{
+    public class RepositoryContentsClientTests
+    {
+        [IntegrationTest]
+        public async Task GetContentForASpecificFolder()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("OctokitTests"))
+            {
+                Credentials = Helper.Credentials
+            };
+            var fixture = client.Repository.Content;
+
+            var result = await fixture.GetContents("octokit", "octokit.net", "Octokit");
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+        }
+
+        [IntegrationTest]
+        public async Task GetContentForASpecificFolderUsesExtensionMethods()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("OctokitTests"))
+            {
+                Credentials = Helper.Credentials
+            };
+            var fixture = client.Repository.Content;
+
+            var result = await fixture.GetContents("octokit", "octokit.net", "Octokit");
+
+            Assert.NotEmpty(result.Files());
+            Assert.NotEmpty(result.Directories());
+        }
+
+        [IntegrationTest]
+        public async Task GetContentForRootReturnsList()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("OctokitTests"))
+            {
+                Credentials = Helper.Credentials
+            };
+            var fixture = client.Repository.Content;
+
+            var result = await fixture.GetContents("octokit", "octokit.net");
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+        }
+
+        [IntegrationTest]
+        public async Task GetContentForReadmeReturnsAnItem()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("OctokitTests"))
+            {
+                Credentials = Helper.Credentials
+            };
+            var fixture = client.Repository.Content;
+
+            var result = await fixture.GetFile("octokit", "octokit.net", "README.md");
+
+            Assert.NotNull(result);
+            Assert.True(result.Size > 0);
+        }
+    }
+}
