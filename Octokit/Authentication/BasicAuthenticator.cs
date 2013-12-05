07da40dda889bf39ed645ec50819815fc6f19f93
VikramShetty <Vikramshetty.cancer@gmail.com>
2013-12-05 22:19:43 +0530

    updated documentation as per suggestion by @Shiftkey

--- Octokit/Authentication/BasicAuthenticator.cs
+++ Octokit/Authentication/BasicAuthenticator.cs
@@ -8,10 +8,10 @@ namespace Octokit.Internal
     class BasicAuthenticator : IAuthenticationHandler
     {
         ///<summary>
-        /// 
+        ///Authenticate a request using the basic access authentication scheme
         ///</summary>
-        ///<param name="request">Http Header request</param>
-        ///<param name="credentials">Login & Password</param>
+        ///<param name="request">The request to authenticate</param>
+        ///<param name="credentials">The credentials to attach to the request</param>
         ///<remarks>
         ///See the <a href="http://developer.github.com/v3/#basic-authentication">Basic Authentication documentation</a> for more information.
         ///</remarks>
