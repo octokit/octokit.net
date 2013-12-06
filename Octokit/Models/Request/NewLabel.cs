18a3d3982bdbc2dc51ed8156c9c84961d6388b70
Andre Rodrigues <andrerod@microsoft.com>
2013-12-06 15:29:35 +0000

    Apply more review comments

--- Octokit/Models/Request/NewLabel.cs
+++ Octokit/Models/Request/NewLabel.cs
@@ -1,10 +1,15 @@
-﻿namespace Octokit
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Octokit
 {
     /// <summary>
     /// Describes a new label to create via the <see cref="IIssuesLabelsClient.Create(string,string,NewLabel)"/> method.
     /// </summary>
     public class NewLabel
     {
+        private string _color;
+
         public NewLabel(string name, string color)
         {
             Name = name;
@@ -19,6 +24,20 @@
         /// <summary>
         /// Color of the label (required).
         /// </summary>
-        public string Color { get; set; }
+        public string Color
+        {
+            get { return _color; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value)
+                    && value.Length == 6
+                    && Regex.IsMatch(value, @"\A\b[0-9a-fA-F]+\b\Z"))
+                {
+                    throw new ArgumentOutOfRangeException("value", "Color should be an hexadecimal string of length 6");
+                }
+
+                _color = value;
+            }
+        }
     }
 }
