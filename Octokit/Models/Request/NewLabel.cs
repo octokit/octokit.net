4764de6212414d3e16b0915a3c2f0bf002eda90b
Andre Rodrigues <andrerod@microsoft.com>
2013-12-08 15:27:44 +0100

    Remove null check

--- Octokit/Models/Request/NewLabel.cs
+++ Octokit/Models/Request/NewLabel.cs
@@ -29,8 +29,7 @@ namespace Octokit
             get { return _color; }
             set
             {
-                if (string.IsNullOrEmpty(value)
-                    || !Regex.IsMatch(value, @"\A\b[0-9a-fA-F]{6}\b\Z"))
+                if (!Regex.IsMatch(value, @"\A\b[0-9a-fA-F]{6}\b\Z"))
                 {
                     throw new ArgumentOutOfRangeException("value", "Color should be an hexadecimal string of length 6");
                 }
