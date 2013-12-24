f30e77478c52fff61e9cb63ef21e7d3bfedf09ad
Haroon <haroon.ahmed25@gmail.com>
2013-12-24 21:22:39 +0000

    prepped ObservableSearchClient

---
+++ Octokit.Reactive/Clients/ObservableSearchClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Threading.Tasks;
+using Octokit.Reactive.Internal;
+
+namespace Octokit.Reactive
+{
+    public class ObservableSearchClient : IObservableSearchClient
+    {
+        readonly IRepositoriesClient _client;
+        readonly IConnection _connection;
+
+        public ObservableSearchClient(IGitHubClient client)
+        {
+            Ensure.ArgumentNotNull(client, "client");
+
+            _client = client.Repository;
+            _connection = client.Connection;
+        }
+
+        public IObservable<Repository> SearchRepo(SearchRepositoriesRequest search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<User> SearchUsers(SearchUsersRequest search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<Issue> SearchIssues(SearchIssuesRequest search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<SearchCode> SearchCode(SearchCodeRequest search)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
