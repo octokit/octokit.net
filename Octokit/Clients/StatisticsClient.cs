91b059274c825d6e03ea0888274a906bc06f0d88
Amy Palamountain <amy@palamounta.in>
2014-01-09 22:14:06 +1300

    Stubbed implementation of IStatisticsClient

---
+++ Octokit/Clients/StatisticsClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Octokit
+{
+    public class StatisticsClient : ApiClient, IStatisticsClient
+    {
+        /// <summary>
+        /// Instantiates a new GitHub Statistics API client.
+        /// </summary>
+        /// <param name="apiConnection">An API connection</param>
+        public StatisticsClient(IApiConnection apiConnection) : base(apiConnection)
+        {
+        }
+
+        /// <summary>
+        /// Returns a <see cref="User"/> for the current authenticated user.
+        /// </summary>
+        /// <exception cref="AuthorizationException">Thrown if the client is not authenticated.</exception>
+        /// <returns>A <see cref="User"/></returns>
+        public Task<Contributor> Contributors()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
