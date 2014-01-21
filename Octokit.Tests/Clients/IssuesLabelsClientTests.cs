c3e8d4a484280fecf876b0f189b13241829be887
Peter MacNaughton <peter@macnaughton.info>
2014-01-21 12:00:48 -0700

    Implemented missing methods for IssueLabels...
    Client, as well as unit tests.

--- Octokit.Tests/Clients/IssuesLabelsClientTests.cs
+++ Octokit.Tests/Clients/IssuesLabelsClientTests.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using NSubstitute;
 using Octokit.Tests.Helpers;
 using Xunit;
+using System.Collections.Generic;
 
 namespace Octokit.Tests.Clients
 {
@@ -77,6 +78,128 @@ namespace Octokit.Tests.Clients
 
                 await AssertEx.Throws<ArgumentNullException>(async () => await client.Get(null, "name", "label"));
                 await AssertEx.Throws<ArgumentNullException>(async () => await client.Get("owner", null, "label"));
+            }
+        }
+
+        public class TheAddToIssueMethod
+        {
+            readonly string[] labels = { "foo", "bar" };
+
+            [Fact]
+            public void PostsToCorrectUrl()
+            {
+                var connection = Substitute.For<IApiConnection>();
+                var client = new IssuesLabelsClient(connection);
+
+                client.AddToIssue("fake", "repo", 42, labels);
+
+                connection.Received().Post<IReadOnlyList<Label>>(Arg.Is<Uri>(u => u.ToString() == "repos/fake/repo/issues/42/labels"), Arg.Any<string[]>());
+            }
+
+            [Fact]
+            public async Task EnsuresNonNullArguments()
+            {
+                var client = new IssuesLabelsClient(Substitute.For<IApiConnection>());
+
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.AddToIssue(null, "name", 42, labels));
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.AddToIssue("owner", null, 42, labels));
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.AddToIssue("owner", "name", 42, null));
+            }
+        }
+
+        public class TheRemoveFromIssueMethod
+        {
+            [Fact]
+            public void DeleteCorrectUrl()
+            {
+                var connection = Substitute.For<IApiConnection>();
+                var client = new IssuesLabelsClient(connection);
+
+                client.RemoveFromIssue("fake", "repo", 42, "label");
+
+                connection.Received().Delete(Arg.Is<Uri>(u => u.ToString() == "repos/fake/repo/issues/42/labels/label"));
+            }
+
+            [Fact]
+            public async Task EnsuresNonNullArguments()
+            {
+                var client = new IssuesLabelsClient(Substitute.For<IApiConnection>());
+
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.RemoveFromIssue(null, "name", 42, "label"));
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.RemoveFromIssue("owner", null, 42, "label"));
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.RemoveFromIssue("owner", "name", 42, null));
+            }
+        }
+
+        public class TheReplaceForIssueMethod
+        {
+            readonly string[] labels = { "foo", "bar" };
+
+            [Fact]
+            public void PutsToCorrectUrl()
+            {
+                var connection = Substitute.For<IApiConnection>();
+                var client = new IssuesLabelsClient(connection);
+
+                client.ReplaceAllForIssue("fake", "repo", 42, labels);
+
+                connection.Received().Put<IReadOnlyList<Label>>(Arg.Is<Uri>(u => u.ToString() == "repos/fake/repo/issues/42/labels"), Arg.Any<string[]>());
+            }
+
+            [Fact]
+            public async Task EnsuresNonNullArguments()
+            {
+                var client = new IssuesLabelsClient(Substitute.For<IApiConnection>());
+
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.ReplaceAllForIssue(null, "name", 42, labels));
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.ReplaceAllForIssue("owner", null, 42, labels));
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.ReplaceAllForIssue("owner", "name", 42, null));
+            }
+        }
+
+        public class TheRemoveAllFromIssueMethod
+        {
+            [Fact]
+            public void DeletesCorrectUrl()
+            {
+                var connection = Substitute.For<IApiConnection>();
+                var client = new IssuesLabelsClient(connection);
+
+                client.RemoveAllFromIssue("fake", "repo", 42);
+
+                connection.Received().Delete(Arg.Is<Uri>(u => u.ToString() == "repos/fake/repo/issues/42/labels"));
+            }
+
+            [Fact]
+            public async Task EnsuresNonNullArguments()
+            {
+                var client = new IssuesLabelsClient(Substitute.For<IApiConnection>());
+
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.RemoveAllFromIssue(null, "name", 42));
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.RemoveAllFromIssue("owner", null, 42));
+            }
+        }
+
+        public class TheGetForMilestoneMethod
+        {
+            [Fact]
+            public void GetsCorrectUrl()
+            {
+                var connection = Substitute.For<IApiConnection>();
+                var client = new IssuesLabelsClient(connection);
+
+                client.GetForMilestone("fake", "repo", 42);
+
+                connection.Received().GetAll<Label>(Arg.Is<Uri>(u => u.ToString() == "repos/fake/repo/milestones/42/labels"));
+            }
+
+            [Fact]
+            public async Task EnsuresNonNullArguments()
+            {
+                var client = new IssuesLabelsClient(Substitute.For<IApiConnection>());
+
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.GetForMilestone(null, "name", 42));
+                await AssertEx.Throws<ArgumentNullException>(async () => await client.GetForMilestone("owner", null, 42));
             }
         }
     }
