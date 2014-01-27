1737834a591a4f5f43ce22950ad0dc73318decaf
Henrik Andersson <henrik.a.andersson@outlook.com>
2014-01-27 13:38:01 +1000

    Stub out observable User Followers API

---
+++ Octokit.Reactive/Clients/IObservableUserFollowersClient.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octokit.Reactive.Clients
+{
+    public interface IObservableUserFollowersClient
+    {
+        /// <summary>
+        /// List the authenticated user’s followers
+        /// </summary>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/users/followers/#list-followers-of-a-user">API documentation</a> for more information.
+        /// </remarks>
+        /// <returns></returns>
+        IObservable<User> GetAllForCurrent();
+
+        /// <summary>
+        /// List a user’s followers
+        /// </summary>
+        /// <param name="login">The login name for the user</param>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/users/followers/#list-followers-of-a-user">API documentation</a> for more information.
+        /// </remarks>
+        /// <returns></returns>
+        IObservable<User> GetAll(string login);
+
+        /// <summary>
+        /// List who the authenticated user is following
+        /// </summary>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/users/followers/#list-users-followed-by-another-user">API documentation</a> for more information.
+        /// </remarks>
+        /// <returns></returns>
+        IObservable<User> GetFollowingForCurrent();
+
+        /// <summary>
+        /// List who a user is following
+        /// </summary>
+        /// <param name="login">The login name of the user</param>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/users/followers/#list-users-followed-by-another-user">API documentation</a> for more information.
+        /// </remarks>
+        /// <returns></returns>
+        IObservable<User> GetFollowing(string login);
+
+        /// <summary>
+        /// Check if the authenticated user follows another user
+        /// </summary>
+        /// <param name="following">The login name of the other user</param>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/users/followers/#check-if-you-are-following-a-user">API documentation</a> for more information.
+        /// </remarks>
+        /// <returns></returns>
+        IObservable<bool> IsFollowingForCurrent(string following);
+
+        /// <summary>
+        /// Check if one user follows another user
+        /// </summary>
+        /// <param name="login">The login name of the user</param>
+        /// <param name="following">The login name of the other user</param>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/users/followers/#check-if-one-user-follows-another">API documentation</a> for more information.
+        /// </remarks>
+        /// <returns></returns>
+        IObservable<bool> IsFollowing(string login, string following);
+
+        /// <summary>
+        /// Follow a user
+        /// </summary>
+        /// <param name="login">The login name of the user to follow</param>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/users/followers/#follow-a-user">API documentation</a> for more information.
+        /// </remarks>
+        /// <returns></returns>
+        IObservable<bool> Follow(string login);
+
+        /// <summary>
+        /// Unfollow a user
+        /// </summary>
+        /// <param name="login">The login name of the user to unfollow</param>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/users/followers/#unfollow-a-user">API documentation</a> for more information.
+        /// </remarks>
+        /// <returns></returns>
+        IObservable<Unit> Unfollow(string login);
+    }
+}
