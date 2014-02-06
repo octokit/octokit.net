6bdceee7b4af541b7008011ca0c3d5c89b5e151c
Brendan Forster <brendan@github.com>
2014-02-07 10:50:09 +1100

    added missing properties to IObservableIssuesClient

---
+++ Octokit.Reactive/Clients/ObservableIssuesLabelsClient.cs
@@ -0,0 +1,15 @@
+﻿namespace Octokit.Reactive
+{
+    public class ObservableIssuesLabelsClient : IObservableIssuesLabelsClient
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        IIssuesLabelsClient _client;
+
+        public ObservableIssuesLabelsClient(IGitHubClient client)
+        {
+            Ensure.ArgumentNotNull(client, "client");
+
+            _client = client.Issue.Labels;
+        }
+    }
+}
