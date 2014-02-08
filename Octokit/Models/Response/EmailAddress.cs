1fffebc4460d099298acbfe5f7e830fd885ab526
Peter MacNaughton <peter@macnaughton.info>
2014-02-07 23:34:16 -0700

    Added DebuggerDisplay to EmailAddress model

--- Octokit/Models/Response/EmailAddress.cs
+++ Octokit/Models/Response/EmailAddress.cs
@@ -1,11 +1,34 @@
+using System;
+using System.Diagnostics;
+using System.Globalization;
+
 namespace Octokit
 {
+    [DebuggerDisplay("DebuggerDisplay,nq")]
     public class EmailAddress
     {
+        /// <summary>
+        /// The email address
+        /// </summary>
         public string Email { get; set; }
 
+        /// <summary>
+        /// true if the email is verified; otherwise false
+        /// </summary>
         public bool Verified { get; set; }
 
+        /// <summary>
+        /// true if this is the users primary email; otherwise false
+        /// </summary>
         public bool Primary { get; set; }
+
+        private string DebuggerDisplay
+        {
+            get
+            {
+                return String.Format(CultureInfo.InvariantCulture,
+                    "EmailAddress: Email: {0}; Primary: {1}, Verified: {2}", Email, Primary, Verified);
+            }
+        }
     }
 }
