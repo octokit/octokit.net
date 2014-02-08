3f6bb2419320b3d7af3658ca5f170441bec02767
Peter MacNaughton <peter@macnaughton.info>
2014-02-08 12:50:45 -0700

    Integration tests for ObservableEmailsClient
    This only tests the GetAll method at this point. Would like to have
    Delete implemented before testing Add so that we can do cleanup.

---
+++ Octokit.Tests.Integration/Reactive/ObservableUserEmailsClientTests.cs
@@ -0,0 +1,24 @@
+﻿using Octokit.Reactive;
+using System.Net.Http.Headers;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Octokit.Tests.Integration
+{
+    public class ObservableUserEmailsClientTests
+    {
+        [IntegrationTest]
+        public async Task CanGetEmail()
+        {
+            var github = new GitHubClient(new ProductHeaderValue("OctokitTests"))
+            {
+                Credentials = Helper.Credentials
+            };
+            var client = new ObservableUserEmailsClient(github);
+
+            var email = await client.GetAll();
+            Assert.NotNull(email);
+        }
+    }
+}
