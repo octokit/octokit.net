600467b3aa0e4435c7bcf5d3bf94d7182236c535
Sam Williamson <samuel.g.williamson@gmail.com>
2014-02-20 13:44:56 -0500

    Added DebuggerDisplay message to Feed

--- Octokit/Models/Response/Feed.cs
+++ Octokit/Models/Response/Feed.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Threading.Tasks;
 
 namespace Octokit
@@ -17,5 +19,13 @@ namespace Octokit
         // TODO: Note, the deserializer didn't work when this was named Links
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "links"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1707:IdentifiersShouldNotContainUnderscores")]
         public FeedLinks _links { get; set; }
+
+        internal string DebuggerDisplay
+        {
+            get
+            {
+                return String.Format(CultureInfo.InvariantCulture, "Public Url: {0} ", CurrentUserPublicUrl);
+            }
+        }
     }
 }
