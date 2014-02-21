42ca81af43ac697b64e011d07c811550488f5913
Sam Williamson <samuel.g.williamson@gmail.com>
2014-02-20 19:57:35 -0500

    Added XML comments to model classes
    Added XML comments to model classes: copied feed definitions from github
    developer docs

--- Octokit/Models/Response/Feed.cs
+++ Octokit/Models/Response/Feed.cs
@@ -6,17 +6,48 @@ using System.Threading.Tasks;
 
 namespace Octokit
 {
+    /// <summary>
+    /// Lists all the feeds available to the authenticating user:
+    /// </summary>
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class Feed
     {
+        /// <summary>
+        /// The GitHub global public timeline
+        /// </summary>
         public string TimelineUrl { get; set; }
+
+        /// <summary>
+        /// The public timeline for any user, using URI template
+        /// </summary>
         public string UserUrl { get; set; }
+
+        /// <summary>
+        /// The public timeline for the authenticated user
+        /// </summary>
         public string CurrentUserPublicUrl { get; set; }
+
+        /// <summary>
+        /// The private timeline for the authenticated user
+        /// </summary>
         public string CurrentUserUrl { get; set; }
+
+        /// <summary>
+        /// The private timeline for activity created by the authenticated user
+        /// </summary>
         public string CurrentUserActorUrl { get; set; }
+
+        /// <summary>
+        /// The private timeline for the authenticated user for a given organization, using URI template
+        /// </summary>
         public string CurrentUserOrganizationUrl { get; set; }
 
+
+        
         // TODO: Note, the deserializer didn't work when this was named Links
+        /// <summary>
+        /// List of feed urls including feed url and feed type, e.g. application/atom+xml
+        /// </summary>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "links"), System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1707:IdentifiersShouldNotContainUnderscores")]
         public FeedLinks _links { get; set; }
 
