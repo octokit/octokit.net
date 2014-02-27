04d6e9b386f033aef8d1e5bfc245d33b14321c65
Peter MacNaughton <peter@macnaughton.info>
2014-02-26 22:09:23 -0700

    Generate documentation comments for the
    `Reactive` project

--- Octokit.Reactive/Clients/ObservableUserEmailsClient.cs
+++ Octokit.Reactive/Clients/ObservableUserEmailsClient.cs
@@ -5,6 +5,12 @@ using System.Reactive.Threading.Tasks;
 
 namespace Octokit.Reactive
 {
+    /// <summary>
+    /// A client for GitHub's User Emails API.
+    /// </summary>
+    /// <remarks>
+    /// See the <a href="http://developer.github.com/v3/users/emails/">User Emails API documentation</a> for more information.
+    /// </remarks>
     public class ObservableUserEmailsClient : IObservableUserEmailsClient
     {
         readonly IUserEmailsClient _client;
@@ -18,11 +24,26 @@ namespace Octokit.Reactive
             _connection = client.Connection;
         }
 
+        /// <summary>
+        /// Gets all email addresses for the authenticated user.
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/users/emails/#list-email-addresses-for-a-user
+        /// </remarks>
+        /// <returns>The <see cref="EmailAddress"/>es for the authenticated user.</returns>
         public IObservable<EmailAddress> GetAll()
         {
             return _connection.GetAndFlattenAllPages<EmailAddress>(ApiUrls.Emails());
         }
 
+        /// <summary>
+        /// Adds email addresses for the authenticated user.
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/users/emails/#add-email-addresses
+        /// </remarks>
+        /// <param name="emailAddresses">The email addresses to add.</param>
+        /// <returns>Returns the added <see cref="EmailAddress"/>es.</returns>
         public IObservable<string> Add(params string[] emailAddresses)
         {
             return _client.Add(emailAddresses).ToObservable().SelectMany(a => a);
