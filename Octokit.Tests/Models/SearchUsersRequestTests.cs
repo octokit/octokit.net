9eb14f3c51c3d25a86856949e4f1753284adc213
Haacked <haacked@gmail.com>
2014-02-26 18:38:57 -0800

    Make readonly collections truly readonly
    These methods actually returned mutable collections that happen
    to implement the readonly interfaces. User could cast them to
    the actual type and add things. I'd rather avoid even that
    possibility by making these truly return readonly stuff.

---
+++ Octokit.Tests/Models/SearchUsersRequestTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Octokit;
+using Xunit;
+
+internal class SearchUsersRequestTests
+{
+    public class TheMergedQualifiersMethod
+    {
+        [Fact]
+        public void ReturnsAReadOnlyDictionary()
+        {
+            var request = new SearchUsersRequest("test");
+
+            var result = request.MergedQualifiers();
+
+            // If I can cast this to a writeable collection, then that defeats the purpose of a read only.
+            Assert.Null(result as ICollection<string>);
+        }
+    }
+}
