18e3855690452d0529cc1647d8ec88c5d3059a2c
Will Froese <will.froese@greatbigsolutions.com>
2014-02-27 22:04:26 -0500

    Add IObservableRepositoryCommentsClient as well as an implementation

--- Octokit.Reactive/Clients/ObservableRepositoryCommentsClient.cs
+++ Octokit.Reactive/Clients/ObservableRepositoryCommentsClient.cs
@@ -1,24 +1,21 @@
-﻿
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System;
+using System.Reactive;
+using System.Reactive.Threading.Tasks;
+using Octokit.Reactive.Internal;
 
-namespace Octokit
+namespace Octokit.Reactive
 {
-    /// <summary>
-    /// A client for GitHub's Repository Comments API.
-    /// </summary>
-    /// <remarks>
-    /// See the <a href="http://developer.github.com/v3/repos/comments/">Repository Comments API documentation</a> for more information.
-    /// </remarks>
-    public class RepositoryCommentsClient : ApiClient, IRepositoryCommentsClient
+    public class ObservableRepositoryCommentsClient : IObservableRepositoryCommentsClient
     {
-        /// <summary>
-        /// Instantiates a new GitHub Repository Comments API client.
-        /// </summary>
-        /// <param name="apiConnection">An API connection</param>
-        public RepositoryCommentsClient(IApiConnection apiConnection)
-            : base(apiConnection)
+        readonly IRepositoryCommentsClient _client;
+        readonly IConnection _connection;
+
+        public ObservableRepositoryCommentsClient(IGitHubClient client)
         {
+            Ensure.ArgumentNotNull(client, "client");
+
+            _client = client.Repository.RepositoryComments;
+            _connection = client.Connection;
         }
 
         /// <summary>
@@ -29,12 +26,12 @@ namespace Octokit
         /// <param name="name">The name of the repository</param>
         /// <param name="number">The comment id</param>
         /// <returns></returns>
-        public Task<CommitComment> Get(string owner, string name, int number)
+        public IObservable<CommitComment> Get(string owner, string name, int number)
         {
             Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
             Ensure.ArgumentNotNullOrEmptyString(name, "name");
 
-            return ApiConnection.Get<CommitComment>(ApiUrls.CommitComment(owner, name, number));
+            return _client.Get(owner, name, number).ToObservable();
         }
 
         /// <summary>
@@ -44,12 +41,12 @@ namespace Octokit
         /// <param name="owner">The owner of the repository</param>
         /// <param name="name">The name of the repository</param>
         /// <returns></returns>
-        public Task<IReadOnlyList<CommitComment>> GetForRepository(string owner, string name)
+        public IObservable<CommitComment> GetForRepository(string owner, string name)
         {
             Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
             Ensure.ArgumentNotNullOrEmptyString(name, "name");
 
-            return ApiConnection.GetAll<CommitComment>(ApiUrls.CommitComments(owner, name));
+            return _connection.GetAndFlattenAllPages<CommitComment>(ApiUrls.CommitComments(owner, name));
         }
 
         /// <summary>
@@ -60,13 +57,13 @@ namespace Octokit
         /// <param name="name">The name of the repository</param>
         /// <param name="number">The sha of the commit</param>
         /// <returns></returns>
-        public Task<IReadOnlyList<CommitComment>> GetForCommit(string owner, string name, string sha)
+        public IObservable<CommitComment> GetForCommit(string owner, string name, string sha)
         {
             Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
             Ensure.ArgumentNotNullOrEmptyString(name, "name");
             Ensure.ArgumentNotNullOrEmptyString(sha, "sha");
 
-            return ApiConnection.GetAll<CommitComment>(ApiUrls.CommitComments(owner, name, sha));
+            return _connection.GetAndFlattenAllPages<CommitComment>(ApiUrls.CommitComments(owner, name, sha));
         }
 
         /// <summary>
@@ -78,14 +75,14 @@ namespace Octokit
         /// <param name="sha">The sha reference of commit</param>
         /// <param name="newCommitComment">The new comment to add to the commit</param>
         /// <returns></returns>
-        public Task<CommitComment> Create(string owner, string name, string sha, NewCommitComment newCommitComment)
+        public IObservable<CommitComment> Create(string owner, string name, string sha, NewCommitComment newCommitComment)
         {
             Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
             Ensure.ArgumentNotNullOrEmptyString(name, "name");
             Ensure.ArgumentNotNullOrEmptyString(sha, "sha");
             Ensure.ArgumentNotNull(newCommitComment, "newCommitComment");
 
-            return ApiConnection.Post<CommitComment>(ApiUrls.CommitComments(owner, name, sha), newCommitComment);
+            return _client.Create(owner, name, sha, newCommitComment).ToObservable();
         }
 
         /// <summary>
@@ -97,13 +94,13 @@ namespace Octokit
         /// <param name="number">The comment number</param>
         /// <param name="commentUpdate">The modified comment</param>
         /// <returns></returns>
-        public Task<CommitComment> Update(string owner, string name, int number, string commentUpdate)
+        public IObservable<CommitComment> Update(string owner, string name, int number, string commentUpdate)
         {
             Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
             Ensure.ArgumentNotNullOrEmptyString(name, "name");
             Ensure.ArgumentNotNull(commentUpdate, "commentUpdate");
 
-            return ApiConnection.Patch<CommitComment>(ApiUrls.CommitComment(owner, name, number), new BodyWrapper(commentUpdate));
+            return _client.Update(owner, name, number, commentUpdate).ToObservable();
         }
 
         /// <summary>
@@ -114,12 +111,12 @@ namespace Octokit
         /// <param name="name">The name of the repository</param>
         /// <param name="number">The comment id</param>
         /// <returns></returns>
-        public Task Delete(string owner, string name, int number)
+        public IObservable<Unit> Delete(string owner, string name, int number)
         {
             Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
             Ensure.ArgumentNotNullOrEmptyString(name, "name");
 
-            return ApiConnection.Delete(ApiUrls.CommitComment(owner, name, number));
+            return _client.Delete(owner, name, number).ToObservable();
         }
     }
 }
