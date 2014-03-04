11514473f9b7b20639af31a14d2486df816cd53c
Brendan Forster <brendan@github.com>
2014-03-04 22:10:37 +1100

    added a simple integration test and got it passing

---
+++ Octokit.Tests.Integration/Clients/SearchClientTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Octokit;
+using Octokit.Tests.Integration;
+using Xunit;
+
+public class SearchClientTests
+{
+    readonly IGitHubClient _gitHubClient;
+
+    public SearchClientTests()
+    {
+        _gitHubClient = new GitHubClient(new ProductHeaderValue("OctokitTests"))
+        {
+            Credentials = Helper.Credentials
+        };
+    }
+
+    [Fact]
+    public async Task SearchForCSharpRepositories()
+    {
+        var request = new SearchRepositoriesRequest("csharp");
+        var repos = await _gitHubClient.Search.SearchRepo(request);
+
+        Assert.NotEmpty(repos.Items);
+    }
+}
---
+++ Octokit/Models/Response/SearchRepositoryResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Octokit
+{
+    public class SearchRepositoryResult
+    {
+        public int TotalCount { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public IList<Repository> Items { get; set; }
+    }
+}
