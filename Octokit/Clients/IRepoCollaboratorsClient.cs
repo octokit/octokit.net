7dc70a2192d85494b3ec6435e9726d8882710599
Brendan Forster <brendan@github.com>
2014-03-08 16:33:46 +1100

    :lipstick: using statements, incorrect docs, deprecated Octokit.Response namespace

--- Octokit/Clients/IRepoCollaboratorsClient.cs
+++ Octokit/Clients/IRepoCollaboratorsClient.cs
@@ -1,7 +1,6 @@
 #if NET_45
 using System.Collections.Generic;
 #endif
-using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
 namespace Octokit
--- Octokit/Clients/IssuesLabelsClient.cs
+++ Octokit/Clients/IssuesLabelsClient.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Linq;
 
 namespace Octokit
 {
