460232b522ab6a0b90a780306dfc8e68189df900
Brendan Forster <brendan@github.com>
2014-03-14 15:48:55 +1100

    generate those xml-docs

--- Octokit/Clients/RepositoryCommitsClient.cs
+++ Octokit/Clients/RepositoryCommitsClient.cs
@@ -1,4 +1,4 @@
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace Octokit
 {
@@ -11,6 +11,14 @@ namespace Octokit
             _apiConnection = apiConnection;
         }
 
+        /// <summary>
+        /// Compare two references in a repository
+        /// </summary>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        /// <param name="base">The reference to use as the base commit</param>
+        /// <param name="head">The reference to use as the head commit</param>
+        /// <returns></returns>
         public Task<CompareResult> Compare(string owner, string name, string @base, string head)
         {
             Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
