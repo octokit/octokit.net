9c17bd3db8ec77dd5d2383bc312182131a758be4
hknielsen <eblefar@gmail.com>
2014-03-18 17:25:29 +0100

    Add models for comparing

---
+++ Octokit/Models/Response/CommitExtendedInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Octokit
+{
+    public class CommitExtendedInfo : GitReference
+    {
+        public Author Author { get; set; }
+        public string CommentsUrl { get; set; }
+        public Commit Commit { get; set; }
+        public Author Committer { get; set; }
+        public string HtmlUrl { get; set; }
+        public IReadOnlyList<GitReference> Parents { get; set; }
+    }
+}
