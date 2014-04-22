f76c74bd8097dbc0314942c41e1448798711ccaa
Daniel Cazzulino <daniel@cazzulino.com>
2014-04-22 17:24:52 -0300

    Fix build errors from renamed ITeamsClient members.

--- Octokit.Tests.Integration/Clients/TeamsClientTests.cs
+++ Octokit.Tests.Integration/Clients/TeamsClientTests.cs
@@ -22,7 +22,7 @@ public class TeamsClientTests
             var newTeam = new NewTeam("Test");
 
             var e = await AssertEx.Throws<AuthorizationException>(async
-                () => await github.Organization.Team.CreateTeam(Helper.Organization, newTeam));
+                () => await github.Organization.Team.Create(Helper.Organization, newTeam));
 
             Assert.Equal(HttpStatusCode.Unauthorized, e.StatusCode);
         }
@@ -37,7 +37,7 @@ public class TeamsClientTests
             var newTeam = new NewTeam("Test");
 
             var e = await AssertEx.Throws<AuthorizationException>(async
-                () => await github.Organization.Team.CreateTeam(Helper.Organization, newTeam));
+                () => await github.Organization.Team.Create(Helper.Organization, newTeam));
             Assert.Equal(HttpStatusCode.Unauthorized, e.StatusCode);
         }
 
@@ -51,7 +51,7 @@ public class TeamsClientTests
 
             var newTeam = new NewTeam(Guid.NewGuid().ToString());
 
-            var team = await github.Organization.Team.CreateTeam(Helper.Organization, newTeam);
+            var team = await github.Organization.Team.Create(Helper.Organization, newTeam);
 
             Assert.Equal(newTeam.Name, team.Name);
         }
@@ -65,7 +65,7 @@ public class TeamsClientTests
         {
             var github = new GitHubClient(new ProductHeaderValue("OctokitTests")) { Credentials = Helper.Credentials };
 
-            team = github.Organization.Team.GetAllTeams(Helper.Organization).Result.First();
+            team = github.Organization.Team.GetAll(Helper.Organization).Result.First();
         }
 
         //TODO: seems like a bug in Github: it's actually returning the membership information!
