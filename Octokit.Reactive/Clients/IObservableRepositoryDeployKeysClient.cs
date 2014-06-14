83a85b5bbb86b774e20d23e185f463ca6c070a70
Henrik Andersson <henrik.a.andersson@outlook.com>
2014-06-15 09:26:18 +1000

    Observable repo deploy keys client

---
+++ Octokit.Reactive/Clients/IObservableRepositoryDeployKeysClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octokit.Reactive
+{
+    public interface IObservableRepositoryDeployKeysClient
+    {
+        /// <summary>
+        /// Get a single deploy key by number for a repository.
+        /// </summary>
+        /// <remarks>
+        /// See the <a href="https://developer.github.com/v3/repos/keys/#get"> API documentation</a> for more information.
+        /// </remarks>
+        /// <param name="owner">The owner of the repository.</param>
+        /// <param name="name">The name of the repository.</param>
+        /// <param name="number">The id of the deploy key.</param>
+        [SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", MessageId = "Get")]
+        IObservable<DeployKey> Get(string owner, string name, int number);
+
+        /// <summary>
+        /// Get all deploy keys for a repository.
+        /// </summary>
+        /// <remarks>
+        /// See the <a href="https://developer.github.com/v3/repos/keys/#list"> API documentation</a> for more information.
+        /// </remarks>
+        /// <param name="owner">The owner of the repository.</param>
+        /// <param name="name">The name of the repository.</param>
+        IObservable<DeployKey> GetAll(string owner, string name);
+
+        /// <summary>
+        /// Creates a new deploy key for a repository.
+        /// </summary>
+        /// <remarks>
+        /// See the <a href="https://developer.github.com/v3/repos/keys/#create"> API documentation</a> for more information.
+        /// </remarks>
+        /// <param name="owner">The owner of the repository.</param>
+        /// <param name="name">The name of the repository.</param>
+        /// <param name="newDeployKey">The deploy key to create for the repository.</param>
+        /// <returns></returns>
+        IObservable<DeployKey> Create(string owner, string name, NewDeployKey newDeployKey);
+
+        /// <summary>
+        /// Deletes a deploy key from a repository.
+        /// </summary>
+        /// <remarks>
+        /// See the <a href="https://developer.github.com/v3/repos/keys/#delete"> API documentation</a> for more information.
+        /// </remarks>
+        /// <param name="owner">The owner of the repository.</param>
+        /// <param name="name">The name of the repository.</param>
+        /// <param name="number">The id of the deploy key to delete.</param>
+        /// <returns></returns>
+        IObservable<Unit> Delete(string owner, string name, int number);
+    }
+}
