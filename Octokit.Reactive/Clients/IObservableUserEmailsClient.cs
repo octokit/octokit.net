b03c634b68c97f53fea1e73b9116c67be147d545
Brendan Forster <brendan@github.com>
2014-07-08 10:30:48 +0930

    updated observable clients

--- Octokit.Reactive/Clients/IObservableUserEmailsClient.cs
+++ Octokit.Reactive/Clients/IObservableUserEmailsClient.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
-
+using System.Reactive;
 
 namespace Octokit.Reactive
 {
@@ -31,5 +31,15 @@ namespace Octokit.Reactive
         /// <param name="emailAddresses">The email addresses to add.</param>
         /// <returns>Returns the added <see cref="EmailAddress"/>es.</returns>
         IObservable<EmailAddress> Add(params string[] emailAddresses);
+
+        /// <summary>
+        /// Deletes email addresses for the authenticated user.
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/users/emails/#delete-email-addresses
+        /// </remarks>
+        /// <param name="emailAddresses">The email addresses to delete.</param>
+        /// <returns>Returns the added <see cref="EmailAddress"/>es.</returns>
+        IObservable<Unit> Delete(params string[] emailAddresses);
     }
 }
