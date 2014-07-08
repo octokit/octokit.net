364f46d444a6219bcf190a9476d6bdec28dc5ddf
Brendan Forster <brendan@github.com>
2014-07-08 11:25:53 +0930

    added DebuggerDisplay attributes

--- Octokit/Models/Request/PublicKey.cs
+++ Octokit/Models/Request/PublicKey.cs
@@ -1,8 +1,21 @@
-﻿namespace Octokit
+﻿using System;
+using System.Globalization;
+
+namespace Octokit
 {
     public class PublicKey
     {
         public int Id { get; set; }
         public string Key { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+
+        internal string DebuggerDisplay
+        {
+            get
+            {
+                return String.Format(CultureInfo.InvariantCulture, "ID: {0} Key: {1}", Id, Key);
+            }
+        }
     }
 }
