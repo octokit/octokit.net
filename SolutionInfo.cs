545cc30476772512289dd56e64e29f62382750c4
Brendan Forster <brendan@github.com>
2014-07-12 08:00:25 +0930

    version bump

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,11 +3,11 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProductAttribute("Octokit")]
-[assembly: AssemblyVersionAttribute("0.3.5")]
-[assembly: AssemblyFileVersionAttribute("0.3.5")]
+[assembly: AssemblyVersionAttribute("0.4.0")]
+[assembly: AssemblyFileVersionAttribute("0.4.0")]
 [assembly: ComVisibleAttribute(false)]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.3.5";
+        internal const string Version = "0.4.0";
     }
 }
