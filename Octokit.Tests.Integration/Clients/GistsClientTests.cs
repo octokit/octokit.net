1b4d02b1427f592df5c9ea6457f7d275bbc0a93f
Haacked <haacked@gmail.com>
2014-11-07 16:05:53 -0800

    Ensure no async void methods
    Added self tests to find async void methods. It found some cases where
    we passed an async lambda to a method that did not accept Action<Task>
    or Func<Task>. I fixed those tests.

--- Octokit.Tests.Integration/Clients/GistsClientTests.cs
+++ Octokit.Tests.Integration/Clients/GistsClientTests.cs
@@ -55,7 +55,7 @@ public class GistsClientTests
         Assert.NotNull(updatedGist);
         Assert.Equal(updatedGist.Description, gistUpdate.Description);
 
-        Assert.DoesNotThrow(async () => { await _fixture.Delete(createdGist.Id); });
+        await _fixture.Delete(createdGist.Id);
     }
 
     [IntegrationTest]
---
+++ Octokit.Tests/Helpers/ReflectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+public static class ReflectionExtensions
+{
+    public static string GetAsyncVoidMethodsList(this Assembly assembly)
+    {
+        return String.Join("\r\n",
+            GetLoadableTypes(assembly)
+                .SelectMany(type => type.GetMethods())
+                .Where(HasAttribute<AsyncStateMachineAttribute>)
+                .Where(method => method.ReturnType == typeof(void))
+                .Select(method =>
+                    String.Format("Method '{0}' of '{1}' has an async void return type and that's bad",
+                        method.Name,
+                        method.DeclaringType.Name))
+                .ToList());
+    }
+
+    public static IEnumerable<Type> GetLoadableTypes(this Assembly assembly)
+    {
+        try
+        {
+            return assembly.GetTypes();
+        }
+        catch (ReflectionTypeLoadException e)
+        {
+            return e.Types.Where(t => t != null);
+        }
+    }
+
+    public static bool HasAttribute<TAttribute>(this MethodInfo method) where TAttribute : Attribute
+    {
+        return method.GetCustomAttributes(typeof(TAttribute), false).Any();
+    }
+}
