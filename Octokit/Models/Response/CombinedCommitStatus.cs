05548c747b68e0a31f8048f3188c564a451a01f2
Kristian Hellang <kristian@hellang.com>
2014-12-10 00:35:58 +0100

    Added properties to CombinedCommitStatus

--- Octokit/Models/Response/CombinedCommitStatus.cs
+++ Octokit/Models/Response/CombinedCommitStatus.cs
@@ -1,7 +1,35 @@
-﻿namespace Octokit
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Octokit
 {
     public class CombinedCommitStatus
     {
-         
+        /// <summary>
+        /// The combined state of the commits.
+        /// </summary>
+        public CommitState State { get; set; }
+
+        /// <summary>
+        /// The SHA of the reference.
+        /// </summary>
+        public string Sha { get; set; }
+
+        /// <summary>
+        /// The total number of statuses.
+        /// </summary>
+        public int TotalCount { get; set; }
+
+        /// <summary>
+        /// The statuses.
+        /// </summary>
+        // TODO: This ought to be an IReadOnlyList<ApiErrorDetail> but we need to add support to SimpleJson for that.
+        [SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public IList<CommitStatus> Statuses { get; set; }
+
+        /// <summary>
+        /// The repository of the reference.
+        /// </summary>
+        public Repository Repository { get; set; }
     }
 }
