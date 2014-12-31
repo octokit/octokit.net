2b98962c8020cc0f224abc12fee5a3af6706e450
Haacked <haacked@gmail.com>
2014-12-30 18:34:50 -0800

    Add missing namespace for release builds

--- Octokit/Helpers/StringExtensions.cs
+++ Octokit/Helpers/StringExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 
