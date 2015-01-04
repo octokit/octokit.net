c56e5711c93c5fb8eaea5d061236f93359338511
Brendan Forster <brendan@github.com>
2015-01-04 14:53:09 +0930

    stubbed an initial test

---
+++ Octokit.Tests.Integration/Clients/AuthorizationClientTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Octokit.Tests.Integration.Clients
+{
+    public class AuthorizationClientTests
+    {
+        public class TheCreateMethod
+        {
+            [ApplicationTest]
+            public async Task CanCreateAnAuthorization()
+            {
+                var client = Helper.GetAuthenticatedClient();
+                var newAuthorization = new NewAuthorization();
+                // TODO: probably some more setup work here
+                var authorization = await client.Authorization.Create(newAuthorization);
+
+                Assert.NotNull(authorization);
+            }
+        }
+
+    }
+}
---
+++ Octokit.Tests.Integration/Helpers/ApplicationTestAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Octokit.Tests.Integration
+{
+    public class ApplicationTestDiscoverer : IXunitTestCaseDiscoverer
+    {
+        public IEnumerable<IXunitTestCase> Discover(ITestMethod testMethod, IAttributeInfo factAttribute)
+        {
+            if (String.IsNullOrWhiteSpace(Helper.ClientId)
+                && String.IsNullOrWhiteSpace(Helper.ClientSecret))
+            {
+                return Enumerable.Empty<IXunitTestCase>();
+            }
+            else
+            {
+                return new[] { new XunitTestCase(testMethod) };
+            }
+        }
+    }
+
+    [XunitTestCaseDiscoverer("Octokit.Tests.Integration.ApplicationTestDiscoverer", "Octokit.Tests.Integration")]
+    public class ApplicationTestAttribute : FactAttribute
+    {
+    }
+}
