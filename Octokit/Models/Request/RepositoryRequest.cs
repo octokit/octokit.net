ee4f07c475a7c7624bca0dd5edf8cedc2bacaca5
Brendan Forster <brendan@github.com>
2015-03-06 09:04:35 +0930

    add new endpoint for filtering user's repositories

---
+++ Octokit/Models/Request/RepositoryRequest.cs
@@ -0,0 +1,33 @@
+﻿using Octokit.Internal;
+
+namespace Octokit
+{
+    public class RepositoryRequest : RequestParameters
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1721:PropertyNamesShouldNotMatchGetMethods")]
+        public RepositoryType Type { get; set; }
+
+        public RepositorySort Sort { get; set; }
+
+        public SortDirection Direction { get; set; }
+    }
+
+    public enum RepositoryType
+    {
+        All,
+        Owner,
+        Public,
+        Private,
+        Member
+    }
+
+    public enum RepositorySort
+    {
+        Created,
+        Updated,
+        Pushed,
+
+        [Parameter(Value = "full_name")]
+        FullName
+    }
+}
