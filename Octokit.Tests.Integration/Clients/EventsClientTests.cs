ce5848838d8934418b82091c868ee23c117a3fa7
David Martin <dwmartin41@gmail.com>
2015-03-08 00:33:22 +0000

    added integration test for event payloads

--- Octokit.Tests.Integration/Clients/EventsClientTests.cs
+++ Octokit.Tests.Integration/Clients/EventsClientTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Octokit.Tests.Integration.Clients
@@ -13,7 +16,83 @@ namespace Octokit.Tests.Integration.Clients
                 var github = Helper.GetAuthenticatedClient();
                 var events = await github.Activity.Events.GetUserPerformed("shiftkey");
                 Assert.NotEmpty(events);
-            } 
+            }
+        }
+
+        public class EventPayloads
+        {
+            readonly IEnumerable<Activity> _events; 
+            public EventPayloads()
+            {
+                var github = Helper.GetAuthenticatedClient();
+                _events = github.Activity.Events.GetUserPerformed("shiftkey").Result; 
+            }
+
+            [Fact]
+            public void AllEventsHavePayloads()
+            {
+                Assert.True(_events.All(e => e.Payload != null));
+            }
+
+            [Fact]
+            public void IssueCommentPayloadEventDeserializesCorrectly()
+            {
+                var commentEvent = _events.FirstOrDefault(e => e.Id == "2628548686");
+                Assert.NotNull(commentEvent);
+                Assert.Equal("IssueCommentEvent", commentEvent.Type);
+                var commentPayload = commentEvent.Payload as IssueCommentPayload;
+                Assert.NotNull(commentPayload);
+                Assert.Equal("created", commentPayload.Action);
+                Assert.NotNull(commentPayload.Comment);
+                Assert.Equal("@joshvera just going to give this a once-over to ensure it matches up with our other conventions before merging", commentPayload.Comment.Body);
+                Assert.NotNull(commentPayload.Issue);
+                Assert.Equal(742, commentPayload.Issue.Number);
+            }
+
+            [Fact]
+            public void PushEventPayloadDeserializesCorrectly()
+            {
+                var pushEvent = _events.FirstOrDefault(e => e.Id == "2628858765");
+                Assert.NotNull(pushEvent);
+                Assert.Equal("PushEvent", pushEvent.Type);
+                var pushPayload = pushEvent.Payload as PushEventPayload;
+                Assert.NotNull(pushPayload);
+                Assert.NotNull(pushPayload.Commits);
+                Assert.Equal(1, pushPayload.Commits.Count);
+                Assert.Equal("3cdcba0ccbea0e6d13ae94249fbb294d71648321", pushPayload.Commits.FirstOrDefault().Sha);
+                Assert.Equal("3cdcba0ccbea0e6d13ae94249fbb294d71648321", pushPayload.Head);
+                Assert.Equal("refs/heads/release-candidate", pushPayload.Ref);
+                Assert.Equal(1, pushPayload.Size);
+            }
+
+            [Fact]
+            public void PREventPayloadDeserializesCorrectly()
+            {
+                var prEvent = _events.FirstOrDefault(e => e.Id == "2628718313");
+                Assert.NotNull(prEvent);
+                Assert.Equal("PullRequestEvent", prEvent.Type);
+                var prPayload = prEvent.Payload as PullRequestEventPayload;
+                Assert.NotNull(prPayload);
+                Assert.Equal("opened", prPayload.Action);
+                Assert.Equal(743, prPayload.Number);
+                Assert.NotNull(prPayload.PullRequest);
+                Assert.Equal(743, prPayload.PullRequest.Number);
+            }
+
+            [Fact]
+            public void PRReviewCommentEventPayloadDeserializesCorrectly()
+            {
+                var prrcEvent = _events.First(e => e.Id == "2623246246");
+                Assert.NotNull(prrcEvent);
+                Assert.Equal("PullRequestReviewCommentEvent", prrcEvent.Type);
+                var prrcPayload = prrcEvent.Payload as PullRequestCommentPayload;
+                Assert.NotNull(prrcPayload);
+                Assert.Equal("created", prrcPayload.Action);
+                Assert.NotNull(prrcPayload.Comment);
+                Assert.Equal("Suuuuuuuuure :P", prrcPayload.Comment.Body);
+                Assert.NotNull(prrcPayload.PullRequest);
+                Assert.Equal(737, prrcPayload.PullRequest.Number);
+            }
         }
     }
 }
