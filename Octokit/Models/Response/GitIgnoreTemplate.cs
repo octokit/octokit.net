e0c1fe35fcd30f82d27c9dccb9bc85c7eb87bc45
Haacked <haacked@gmail.com>
2015-03-17 13:02:35 -0700

    Add DebuggerDisplay to GitIgnoreTemplate

--- Octokit/Models/Response/GitIgnoreTemplate.cs
+++ Octokit/Models/Response/GitIgnoreTemplate.cs
@@ -1,5 +1,10 @@
-﻿namespace Octokit
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Octokit
 {
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class GitIgnoreTemplate
     {
         public GitIgnoreTemplate(string name, string source)
@@ -17,5 +22,14 @@
 
         public string Name { get; protected set; }
         public string Source { get; protected set; }
+
+        internal string DebuggerDisplay
+        {
+            get
+            {
+                return String.Format(CultureInfo.InvariantCulture, "GitIgnore: {0}", Name);
+            }
+        }
+
     }
 }
