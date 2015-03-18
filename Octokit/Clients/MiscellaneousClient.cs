35902dea51748115e400c06c0e8cb7d023dbd1c4
Haacked <haacked@gmail.com>
2015-03-17 18:33:20 -0700

    Reformat doc and let R# sort it out

--- Octokit/Clients/MiscellaneousClient.cs
+++ Octokit/Clients/MiscellaneousClient.cs
@@ -1,25 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 #if NET_45
 using System.Collections.ObjectModel;
 #endif
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Octokit
 {
     /// <summary>
-    /// A client for GitHub's miscellaneous APIs.
+    ///     A client for GitHub's miscellaneous APIs.
     /// </summary>
     /// <remarks>
-    /// See the <a href="http://developer.github.com/v3/misc/">Miscellaneous API documentation</a> for more details.
+    ///     See the <a href="http://developer.github.com/v3/misc/">Miscellaneous API documentation</a> for more details.
     /// </remarks>
     public class MiscellaneousClient : IMiscellaneousClient
     {
         readonly IConnection _connection;
 
         /// <summary>
-        /// Initializes a new GitHub miscellaneous API client.
+        ///     Initializes a new GitHub miscellaneous API client.
         /// </summary>
         /// <param name="connection">An API connection</param>
         public MiscellaneousClient(IConnection connection)
@@ -30,21 +30,21 @@ namespace Octokit
         }
 
         /// <summary>
-        /// Gets all the emojis available to use on GitHub.
+        ///     Gets all the emojis available to use on GitHub.
         /// </summary>
         /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
-        /// <returns>An <see cref="IReadOnlyDictionary{TKey,TValue}"/> of emoji and their URI.</returns>
+        /// <returns>An <see cref="IReadOnlyDictionary{TKey,TValue}" /> of emoji and their URI.</returns>
         public async Task<IReadOnlyList<Emoji>> GetEmojis()
         {
             var endpoint = new Uri("emojis", UriKind.Relative);
             var response = await _connection.Get<Dictionary<string, string>>(endpoint, null, null)
-                                            .ConfigureAwait(false);
+                .ConfigureAwait(false);
             return new ReadOnlyCollection<Emoji>(
                 response.Body.Select(kvp => new Emoji(kvp.Key, new Uri(kvp.Value))).ToArray());
         }
 
         /// <summary>
-        /// Gets the rendered Markdown for the specified plain-text Markdown document.
+        ///     Gets the rendered Markdown for the specified plain-text Markdown document.
         /// </summary>
         /// <param name="markdown">A plain-text Markdown document</param>
         /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
@@ -53,12 +53,12 @@ namespace Octokit
         {
             var endpoint = new Uri("markdown/raw", UriKind.Relative);
             var response = await _connection.Post<string>(endpoint, markdown, "text/html", "text/plain")
-                                            .ConfigureAwait(false);
+                .ConfigureAwait(false);
             return response.Body;
         }
 
         /// <summary>
-        /// List all templates available to pass as an option when creating a repository.
+        ///     List all templates available to pass as an option when creating a repository.
         /// </summary>
         /// <returns>A list of template names</returns>
         public async Task<IReadOnlyList<string>> GetGitIgnoreTemplates()
@@ -66,12 +66,12 @@ namespace Octokit
             var endpoint = new Uri("gitignore/templates", UriKind.Relative);
 
             var response = await _connection.Get<string[]>(endpoint, null, null)
-                                  .ConfigureAwait(false);
+                .ConfigureAwait(false);
             return new ReadOnlyCollection<string>(response.Body);
         }
 
         /// <summary>
-        /// Retrieves the source for a single GitIgnore template
+        ///     Retrieves the source for a single GitIgnore template
         /// </summary>
         /// <param name="templateName"></param>
         /// <returns>A template and its source</returns>
@@ -87,8 +87,8 @@ namespace Octokit
         }
 
         /// <summary>
-        /// Returns a list of the licenses shown in the license picker on GitHub.com. This is not a comprehensive
-        /// list of all possible OSS licenses.
+        ///     Returns a list of the licenses shown in the license picker on GitHub.com. This is not a comprehensive
+        ///     list of all possible OSS licenses.
         /// </summary>
         /// <remarks>This is a PREVIEW API! Use it at your own risk.</remarks>
         /// <returns>A list of licenses available on the site</returns>
@@ -98,12 +98,12 @@ namespace Octokit
             var endpoint = new Uri("licenses", UriKind.Relative);
 
             var response = await _connection.Get<LicenseMetadata[]>(endpoint, null, previewAcceptsHeader)
-                                            .ConfigureAwait(false);
+                .ConfigureAwait(false);
             return new ReadOnlyCollection<LicenseMetadata>(response.Body);
         }
 
         /// <summary>
-        /// Retrieves a license based on the licence key such as "mit"
+        ///     Retrieves a license based on the licence key such as "mit"
         /// </summary>
         /// <param name="key"></param>
         /// <returns>A <see cref="License" /> that includes the license key, text, and attributes of the license.</returns>
@@ -113,7 +113,7 @@ namespace Octokit
             var endpoint = new Uri("licenses/" + Uri.EscapeUriString(key), UriKind.Relative);
 
             var response = await _connection.Get<License>(endpoint, null, previewAcceptsHeader)
-                                            .ConfigureAwait(false);
+                .ConfigureAwait(false);
             return response.Body;
         }
     }
