d40ebeb267e3fa29a1a6d7ec6fe07331389c5fb3
Brendan Forster <brendan@github.com>
2015-03-19 16:38:46 +0930

    added overload for providing API options

--- Octokit/Clients/IRepositoriesClient.cs
+++ Octokit/Clients/IRepositoriesClient.cs
@@ -128,7 +128,17 @@ namespace Octokit
         /// </summary>
         /// <remarks>
         /// See the <a href="http://developer.github.com/v3/repos/#list-your-repositories">API documentation</a> for more information.
-        /// The default page size on GitHub.com is 30.
+        /// </remarks>
+        /// <exception cref="AuthorizationException">Thrown if the client is not authenticated.</exception>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>A <see cref="IReadOnlyPagedCollection{Repository}"/> of <see cref="Repository"/>.</returns>
+        Task<IReadOnlyList<Repository>> GetAllForCurrent(ApiOptions options);
+
+        /// <summary>
+        /// Gets all repositories owned by the current user.
+        /// </summary>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/repos/#list-your-repositories">API documentation</a> for more information.
         /// </remarks>
         /// <param name="request">Search parameters to filter results on</param>
         /// <exception cref="AuthorizationException">Thrown if the client is not authenticated.</exception>
--- Octokit/Clients/RepositoriesClient.cs
+++ Octokit/Clients/RepositoriesClient.cs
@@ -207,7 +207,20 @@ namespace Octokit
         /// </summary>
         /// <remarks>
         /// See the <a href="http://developer.github.com/v3/repos/#list-your-repositories">API documentation</a> for more information.
-        /// The default page size on GitHub.com is 30.
+        /// </remarks>
+        /// <exception cref="AuthorizationException">Thrown if the client is not authenticated.</exception>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>A <see cref="IReadOnlyPagedCollection{Repository}"/> of <see cref="Repository"/>.</returns>
+        public Task<IReadOnlyList<Repository>> GetAllForCurrent(ApiOptions options)
+        {
+            return ApiConnection.GetAll<Repository>(ApiUrls.Repositories(), options);
+        }
+
+        /// <summary>
+        /// Gets all repositories owned by the current user.
+        /// </summary>
+        /// <remarks>
+        /// See the <a href="http://developer.github.com/v3/repos/#list-your-repositories">API documentation</a> for more information.
         /// </remarks>
         /// <param name="request">Search parameters to filter results on</param>
         /// <exception cref="AuthorizationException">Thrown if the client is not authenticated.</exception>
