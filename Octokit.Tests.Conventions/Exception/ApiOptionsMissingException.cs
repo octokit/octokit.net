f9f72106be7f5a4b6842b20b031b824425e5704e
Brendan Forster <brendan@github.com>
2015-03-19 17:27:39 +0930

    implemented a test to verify that overloads are implemented for GetAll usages

---
+++ Octokit.Tests.Conventions/Exception/ApiOptionsMissingException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Octokit.Tests.Conventions
+{
+    public class ApiOptionsMissingException : Exception
+    {
+        public ApiOptionsMissingException(Type type, IEnumerable<MethodInfo> methods)
+            : base(CreateMessage(type, methods)) { }
+
+        static string CreateMessage(Type type, IEnumerable<MethodInfo> methods)
+        {
+            var methodsFormatted = String.Join("\r\n", methods.Select(FormatMethod));
+            return "Methods found on type {0} require an overload which accepts an parameter of type ApiOptions:\r\n{1}"
+                      .FormatWithNewLine(
+                          type.Name,
+                          methodsFormatted);
+        }
+
+        static string FormatMethod(MethodInfo m)
+        {
+            var parametersFormatteds = m.GetParameters()
+                .Select(p => string.Format("{0} {1}", p.ParameterType.Name, p.Name));
+
+            var parameterList = string.Join(", ", parametersFormatteds);
+
+            return string.Format(" - {0}({1})", m.Name, parameterList);
+        }
+    }
+}
