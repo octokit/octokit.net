29be7ad695a6b4a4bbdcbabd2d514af87c304be4
Brendan Forster <brendan@github.com>
2015-03-19 16:07:33 +0930

    add ApiOptions to support tailoring the response details

---
+++ Octokit/Models/Request/ApiOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Octokit.Models.Request
+{
+    public class ApiOptions
+    {
+        public static readonly ApiOptions None = new ApiOptions();
+
+        public int? StartPage { get; set; }
+        public int? PageCount { get; set; }
+        public int? PageSize { get; set; }
+        public string Accepts { get; set; }
+    }
+
+}
