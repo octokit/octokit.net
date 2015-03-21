d20457417587eaa108a2103c6e4c335597ad6c3b
Brendan Forster <brendan@github.com>
2015-03-21 15:54:08 +0930

    add DebuggerDisplay symbol

--- Octokit/Models/Request/ApiOptions.cs
+++ Octokit/Models/Request/ApiOptions.cs
@@ -1,5 +1,12 @@
-﻿namespace Octokit
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+
+namespace Octokit
 {
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class ApiOptions
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2104:DoNotDeclareReadOnlyMutableReferenceTypes")]
@@ -9,6 +16,36 @@
         public int? PageCount { get; set; }
         public int? PageSize { get; set; }
         public string Accepts { get; set; }
+
+        internal string DebuggerDisplay
+        {
+            get
+            {
+                List<string> values = new List<string>();
+
+                if (StartPage.HasValue)
+                {
+                    values.Add("StartPage: " + StartPage.Value);
+                }
+
+                if (PageCount.HasValue)
+                {
+                    values.Add("PageCount: " + PageCount.Value);
+                }
+
+                if (PageSize.HasValue)
+                {
+                    values.Add("PageSize: " + PageSize.Value);
+                }
+
+                if (!String.IsNullOrWhiteSpace(Accepts))
+                {
+                    values.Add("Accepts: " + Accepts);
+                }
+
+                return String.Join(", ", values);
+            }
+        }
     }
 
 }
