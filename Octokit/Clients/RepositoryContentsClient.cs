514ed613f3b68e90e86034870be477fffa52ea6b
Henrik Andersson <henrik.a.andersson@outlook.com>
2015-03-21 23:32:58 +1000

    Add another overload to GetArchiveLink

--- Octokit/Clients/RepositoryContentsClient.cs
+++ Octokit/Clients/RepositoryContentsClient.cs
@@ -86,7 +86,23 @@ namespace Octokit
         /// <returns></returns>
         public Task<string> GetArchiveLink(string owner, string name)
         {
-            return GetArchiveLink(owner, name, ArchiveFormat.Tarball, "master");
+            return GetArchiveLink(owner, name, ArchiveFormat.Tarball, string.Empty);
+        }
+
+        /// <summary>
+        /// This method will return a 302 to a URL to download a tarball or zipball archive for a repository.
+        /// Please make sure your HTTP framework is configured to follow redirects or you will need to use the 
+        /// Location header to make a second GET request.
+        /// Note: For private repositories, these links are temporary and expire quickly.
+        /// </summary>
+        /// <remarks>https://developer.github.com/v3/repos/contents/#get-archive-link</remarks>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        /// <param name="archiveFormat">The format of the archive. Can be either tarball or zipball</param>
+        /// <returns></returns>
+        public Task<string> GetArchiveLink(string owner, string name, ArchiveFormat archiveFormat)
+        {
+            return GetArchiveLink(owner, name, archiveFormat, string.Empty);
         }
 
         /// <summary>
