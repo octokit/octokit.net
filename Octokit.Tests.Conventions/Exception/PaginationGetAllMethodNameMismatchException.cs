b0ab485ed13344c4443f9a1f047077bb47eba485
Henrik Andersson <henrik.a.andersson@outlook.com>
2015-03-22 19:09:08 +1000

    Add a convention test

---
+++ Octokit.Tests.Conventions/Exception/PaginationGetAllMethodNameMismatchException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octokit.Tests.Conventions
+{
+    public class PaginationGetAllMethodNameMismatchException : Exception
+    {
+        public PaginationGetAllMethodNameMismatchException(Type type, IEnumerable<MethodInfo> methods)
+            : base(CreateMessage(type, methods)) { }
+
+        static string CreateMessage(Type type, IEnumerable<MethodInfo> methods)
+        {
+            var methodsFormatted = String.Join("\r\n", methods.Select(m => String.Format(" - {0}", m)));
+            return "Methods found on type {0} should follow the 'GetAll*' naming convention:\r\n{1}"
+                      .FormatWithNewLine(
+                          type.Name,
+                          methodsFormatted);
+        }
+    }
+}
