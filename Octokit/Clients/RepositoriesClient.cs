01665bce78276b838b2344a354e511eca2aabacd
Henrik Andersson <henrik.a.andersson@outlook.com>
2015-03-23 23:22:47 +1000

    Add 'since` parameter for public repositories

--- Octokit/Clients/RepositoriesClient.cs
+++ Octokit/Clients/RepositoriesClient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 #if NET_45
 using System.Collections.Generic;
 #endif
@@ -185,6 +186,24 @@ namespace Octokit
         public Task<IReadOnlyList<Repository>> GetAllPublic()
         {
             return ApiConnection.GetAll<Repository>(ApiUrls.AllPublicRepositories());
+        }
+
+        /// <summary>
+        /// Gets all public repositories since the integer ID of the last Repository that you�ve seen.
+        /// </summary>
+        /// <remarks>
+        /// See the <a href="https://developer.github.com/v3/repos/#list-all-public-repositories">API documentation</a> for more information.
+        /// The default page size on GitHub.com is 30.
+        /// </remarks>
+        /// <param name="request">Search parameters of the last repository seen</param>
+        /// <exception cref="AuthorizationException">Thrown if the client is not authenticated.</exception>
+        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+        /// <returns>A <see cref="IReadOnlyPagedCollection{Repository}"/> of <see cref="Repository"/>.</returns>
+        public Task<IReadOnlyList<Repository>> GetAllPublic(PublicRepositoryRequest request)
+        {
+            Ensure.ArgumentNotNull(request, "request");
+
+            return ApiConnection.GetAll<Repository>(ApiUrls.AllPublicRepositories(), request.ToParametersDictionary());
         }
 
         /// <summary>
---
+++ Octokit/Models/Request/PublicRepositoryRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octokit
+{
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class PublicRepositoryRequest : RequestParameters
+    {
+        public PublicRepositoryRequest()
+        {
+        }
+
+        public long Since { get; set; }
+
+        internal string DebuggerDisplay
+        {
+            get
+            {
+                return String.Format(CultureInfo.InvariantCulture, "Since: {0} ", Since);
+            }
+        }
+    }
+}
