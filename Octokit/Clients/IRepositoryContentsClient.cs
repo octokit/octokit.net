9a634e471ccc8c1cae910a54023466fab3c34354
Brendan Forster <brendan@github.com>
2015-06-01 08:10:20 +0930

    docs docs docs

--- Octokit/Clients/IRepositoryContentsClient.cs
+++ Octokit/Clients/IRepositoryContentsClient.cs
@@ -67,7 +67,7 @@ namespace Octokit
         /// <remarks>https://developer.github.com/v3/repos/contents/#get-archive-link</remarks>
         /// <param name="owner">The owner of the repository</param>
         /// <param name="name">The name of the repository</param>
-        /// <returns></returns>
+        /// <returns>The binary contents of the archive</returns>
         Task<byte[]> GetArchive(string owner, string name);
 
         /// <summary>
@@ -91,7 +91,7 @@ namespace Octokit
         /// <param name="owner">The owner of the repository</param>
         /// <param name="name">The name of the repository</param>
         /// <param name="archiveFormat">The format of the archive. Can be either tarball or zipball</param>
-        /// <returns></returns>
+        /// <returns>The binary contents of the archive</returns>
         Task<byte[]> GetArchive(string owner, string name, ArchiveFormat archiveFormat);
 
         /// <summary>
@@ -117,7 +117,7 @@ namespace Octokit
         /// <param name="name">The name of the repository</param>
         /// <param name="archiveFormat">The format of the archive. Can be either tarball or zipball</param>
         /// <param name="reference">A valid Git reference.</param>
-        /// <returns></returns>
+        /// <returns>The binary contents of the archive</returns>
         Task<byte[]> GetArchive(string owner, string name, ArchiveFormat archiveFormat, string reference);
 
         /// <summary>
