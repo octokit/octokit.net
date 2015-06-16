06e8b3c4259657658f3634444a3ef6ab1c76cc90
Brendan Forster <brendan@github.com>
2015-06-16 22:29:02 +0930

    sketching out the exception necessary when raising specific merge exceptions

---
+++ Octokit/Exceptions/PullRequestNotMergeableException.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Octokit
+{
+    /// <summary>
+    /// Represents an error that occurs when the pull request is in an
+    /// unmergeable state
+    /// </summary>
+#if !NETFX_CORE
+    [Serializable]
+#endif
+    public class PullRequestNotMergeableException : Exception
+    {
+        public PullRequestNotMergeableException()
+            : base("The pull request is not in a mergeable state")
+        {
+        }
+
+        public PullRequestNotMergeableException(string message)
+            : base(message)
+        {
+        }
+
+        public PullRequestNotMergeableException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+#if !NETFX_CORE
+        /// <summary>
+        /// Constructs an instance of PullRequestNotMergeableException.
+        /// </summary>
+        /// <param name="info">
+        /// The <see cref="SerializationInfo"/> that holds the
+        /// serialized object data about the exception being thrown.
+        /// </param>
+        /// <param name="context">
+        /// The <see cref="StreamingContext"/> that contains
+        /// contextual information about the source or destination.
+        /// </param>
+        protected PullRequestNotMergeableException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+#endif
+    }
+}
