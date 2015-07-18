cb41c58581790fa7fed8b7c8a442b2d914f4d27c
Brendan Forster <brendan@github.com>
2015-07-19 08:39:22 +0930

    :lipstick: cleanup

--- Octokit/Models/Request/SearchIssuesRequest.cs
+++ Octokit/Models/Request/SearchIssuesRequest.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Globalization;
 using System.Diagnostics.CodeAnalysis;
-using System.Text.RegularExpressions;
 
 namespace Octokit
 {
