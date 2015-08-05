a43f403ca0d105ff0270130fd39de084a8031ea5
Phil Haack <haacked@gmail.com>
2015-08-04 21:28:58 -0700

    Add back in the reflection namespace

--- Octokit/Helpers/StringExtensions.cs
+++ Octokit/Helpers/StringExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 
