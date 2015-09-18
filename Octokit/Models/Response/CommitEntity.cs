ce5acfc522cc948175d75cc454d6d2d1276df836
William Barbosa <william.sb@hotmail.com>
2015-09-18 19:03:13 -0300

    Creates CommitEntity for GitHubCommit
    Creates the entity that corresponds to the actual payload returned by
    the server to represent the Author and Committer of a commit

---
+++ Octokit/Models/Response/CommitEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octokit
+{
+    /// <summary>
+    /// Information about a file in a repository. It does not include the contents of the file.
+    /// </summary>
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class CommitEntity
+    {
+        /// <summary>
+        /// Name of the user
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Email of the user
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Time the commit happened
+        /// </summary>
+        public DateTime Date { get; set; }
+    }
+}
