8eba9a6a9cf044d0d3e590f436ab17f976a4c1b0
William Barbosa <william.sb@hotmail.com>
2015-09-22 23:02:17 -0300

    RepositoryContext class and Extension methods

---
+++ Octokit.Tests.Integration/Helpers/RepositoryContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octokit.Tests.Integration.Helpers
+{
+    internal sealed class RepositoryContext : IDisposable
+    {
+        internal RepositoryContext(Repository repo)
+        {
+            Repository = repo;
+        }
+
+        internal Repository Repository { get; private set; }
+
+        public void Dispose()
+        {
+            Helper.DeleteRepo(Repository);
+        }
+    }
+}
