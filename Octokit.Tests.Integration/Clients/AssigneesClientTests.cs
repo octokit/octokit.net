b794c3581b8dae8aa779ca75af4a519af7773553
William Barbosa <william.sb@hotmail.com>
2015-09-26 00:11:10 -0300

    Refactors AssigneesClient and CommitsClient tests

--- Octokit.Tests.Integration/Clients/AssigneesClientTests.cs
+++ Octokit.Tests.Integration/Clients/AssigneesClientTests.cs
@@ -2,12 +2,13 @@
 using System.Threading.Tasks;
 using Octokit;
 using Octokit.Tests.Integration;
+using Octokit.Tests.Integration.Helpers;
 using Xunit;
 
 public class AssigneesClientTests
 {
     readonly IGitHubClient _gitHubClient;
-    readonly Repository _repository;
+    readonly RepositoryContext _context;
     readonly string _owner;
 
     public AssigneesClientTests()
@@ -15,20 +16,20 @@ public class AssigneesClientTests
         _gitHubClient = Helper.GetAuthenticatedClient();
         var repoName = Helper.MakeNameWithTimestamp("public-repo");
 
-        _repository = _gitHubClient.Repository.Create(new NewRepository(repoName)).Result;
-        _owner = _repository.Owner.Login;
+        _context = _gitHubClient.CreateRepositoryContext(new NewRepository(repoName)).Result;
+        _owner = _context.Repository.Owner.Login;
     }
 
     [IntegrationTest]
     public async Task CanCheckAssignees()
     {
         var isAssigned = await
-            _gitHubClient.Issue.Assignee.CheckAssignee(_owner, _repository.Name, "FakeHaacked");
+            _gitHubClient.Issue.Assignee.CheckAssignee(_owner, _context.Repository.Name, "FakeHaacked");
         Assert.False(isAssigned);
-        
+
         // Repository owner is always an assignee
         isAssigned = await
-            _gitHubClient.Issue.Assignee.CheckAssignee(_owner, _repository.Name, _owner);
+            _gitHubClient.Issue.Assignee.CheckAssignee(_owner, _context.Repository.Name, _owner);
         Assert.True(isAssigned);
     }
 
@@ -36,12 +37,7 @@ public class AssigneesClientTests
     public async Task CanListAssignees()
     {
         // Repository owner is always an assignee
-        var assignees = await _gitHubClient.Issue.Assignee.GetAllForRepository(_owner, _repository.Name);
+        var assignees = await _gitHubClient.Issue.Assignee.GetAllForRepository(_owner, _context.Repository.Name);
         Assert.True(assignees.Any(u => u.Login == Helper.UserName));
-    }
-
-    public void Dispose()
-    {
-        Helper.DeleteRepo(_repository);
     }
 }
--- Octokit.Tests.Integration/Clients/CommitsClientTests.cs
+++ Octokit.Tests.Integration/Clients/CommitsClientTests.cs
@@ -1,58 +1,49 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Octokit;
+using Octokit.Tests.Integration.Helpers;
 using Octokit.Tests.Integration;
 using Xunit;
 
-public class CommitsClientTests : IDisposable
+public class CommitsClientTests
 {
-    readonly IGitHubClient _client;
-    readonly Repository _repository;
-    readonly ICommitsClient _fixture;
-    readonly string _owner;
-
-    public CommitsClientTests()
-    {
-        _client = Helper.GetAuthenticatedClient();
-
-        var repoName = Helper.MakeNameWithTimestamp("public-repo");
-        _fixture = _client.GitDatabase.Commit;
-        _repository = _client.Repository.Create(new NewRepository(repoName) { AutoInit = true }).Result;
-        _owner = _repository.Owner.Login;
-    }
+    public CommitsClientTests() { }
 
     [IntegrationTest]
     public async Task CanCreateAndRetrieveCommit()
     {
-        var blob = new NewBlob
+        var client = Helper.GetAuthenticatedClient();
+        var fixture = client.GitDatabase.Commit;
+        
+        using (var context = await client.CreateRepositoryContext("public-repo"))
         {
-            Content = "Hello World!",
-            Encoding = EncodingType.Utf8
-        };
-        var blobResult = await _client.GitDatabase.Blob.Create(_owner, _repository.Name, blob);
+            var owner = context.Repository.Owner.Login;
 
-        var newTree = new NewTree();
-        newTree.Tree.Add(new NewTreeItem
-        {
-            Type = TreeType.Blob,
-            Mode = FileMode.File,
-            Path = "README.md",
-            Sha = blobResult.Sha
-        });
+            var blob = new NewBlob
+            {
+                Content = "Hello World!",
+                Encoding = EncodingType.Utf8
+            };
+            var blobResult = await client.GitDatabase.Blob.Create(owner, context.Repository.Name, blob);
 
-        var treeResult = await _client.GitDatabase.Tree.Create(_owner, _repository.Name, newTree);
+            var newTree = new NewTree();
+            newTree.Tree.Add(new NewTreeItem
+            {
+                Type = TreeType.Blob,
+                Mode = FileMode.File,
+                Path = "README.md",
+                Sha = blobResult.Sha
+            });
 
-        var newCommit = new NewCommit("test-commit", treeResult.Sha);
+            var treeResult = await client.GitDatabase.Tree.Create(owner, context.Repository.Name, newTree);
 
-        var commit = await _fixture.Create(_owner, _repository.Name, newCommit);
-        Assert.NotNull(commit);
+            var newCommit = new NewCommit("test-commit", treeResult.Sha);
 
-        var retrieved = await _fixture.Get(_owner, _repository.Name, commit.Sha);
-        Assert.NotNull(retrieved);
-    }
+            var commit = await fixture.Create(owner, context.Repository.Name, newCommit);
+            Assert.NotNull(commit);
 
-    public void Dispose()
-    {
-        Helper.DeleteRepo(_repository);
+            var retrieved = await fixture.Get(owner, context.Repository.Name, commit.Sha);
+            Assert.NotNull(retrieved);
+        }
     }
 }
