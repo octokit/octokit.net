ee27faf6662addbbb8a0ad2b9058c9682d166a03
Haacked <haacked@gmail.com>
2015-09-27 14:58:00 -0700

    Add the ability to create a readonly deploy key

--- Octokit/Models/Request/NewDeployKey.cs
+++ Octokit/Models/Request/NewDeployKey.cs
@@ -14,6 +14,15 @@ namespace Octokit
 
         public string Key { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the key will only be able to read repository contents. Otherwise, 
+        /// the key will be able to read and write.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [read only]; otherwise, <c>false</c>.
+        /// </value>
+        public bool ReadOnly { get; set; }
+
         internal string DebuggerDisplay
         {
             get { return String.Format(CultureInfo.InvariantCulture, "Key: {0}, Title: {1}", Key, Title); }
