2f4eedcf62fb57c9123f1153bff1553ff1a32001
William Barbosa <william.sb@hotmail.com>
2015-09-30 20:21:21 -0300

    Removes integer overload
    Plus extra ensures

--- Octokit/Clients/RepositoryContentsClient.cs
+++ Octokit/Clients/RepositoryContentsClient.cs
@@ -189,21 +189,6 @@ namespace Octokit
         /// <returns>The binary contents of the archive</returns>
         public Task<byte[]> GetArchive(string owner, string name, ArchiveFormat archiveFormat, string reference)
         {
-            return GetArchive(owner, name, archiveFormat, string.Empty, 60);
-        }
-
-        /// <summary>
-        /// Get an archive of a given repository's contents, in a specific format
-        /// </summary>
-        /// <remarks>https://developer.github.com/v3/repos/contents/#get-archive-link</remarks>
-        /// <param name="owner">The owner of the repository</param>
-        /// <param name="name">The name of the repository</param>
-        /// <param name="archiveFormat">The format of the archive. Can be either tarball or zipball</param>
-        /// <param name="reference">A valid Git reference.</param>
-        /// <param name="timeout"> Timeout in minutes </param>
-        /// <returns>The binary contents of the archive</returns>
-        public Task<byte[]> GetArchive(string owner, string name, ArchiveFormat archiveFormat, string reference, int timeout)
-        {
             return GetArchive(owner, name, archiveFormat, string.Empty, TimeSpan.FromMinutes(60));
         }
 
@@ -221,6 +206,7 @@ namespace Octokit
         {
             Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
             Ensure.ArgumentNotNullOrEmptyString(name, "name");
+            Ensure.GreaterThanZero(timeout, "timeout");
 
             var endpoint = ApiUrls.RepositoryArchiveLink(owner, name, archiveFormat, reference);
 
