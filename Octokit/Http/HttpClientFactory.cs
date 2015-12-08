e9c3e0b812c04c2456215323ba839717f39cefc9
Brendan Forster <brendan@github.com>
2015-12-08 13:35:54 +1030

    stubbed a simple implementation to mimic our current setup

--- Octokit/Http/HttpClientFactory.cs
+++ Octokit/Http/HttpClientFactory.cs
@@ -1,6 +1,18 @@
-﻿namespace Octokit.Http
+﻿using System.Net.Http;
+using Octokit.Internal;
+
+namespace Octokit
 {
     public static class HttpClientFactory
     {
+        public static HttpClient Create()
+        {
+            var handler = HttpMessageHandlerFactory.CreateDefault();
+
+            var http = new HttpClient(new RedirectHandler { InnerHandler = handler });
+
+            return http;
+        }
+
     }
 }
