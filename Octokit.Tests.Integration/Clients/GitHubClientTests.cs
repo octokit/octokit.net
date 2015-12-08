9bcb18e64dcb1f07ab05c744de36dba81b97e9e9
Brendan Forster <brendan@github.com>
2015-12-08 22:48:54 +1030

    :lipstick: using statements

--- Octokit.Tests.Integration/Clients/GitHubClientTests.cs
+++ Octokit.Tests.Integration/Clients/GitHubClientTests.cs
@@ -2,9 +2,6 @@
 using Octokit.Tests.Integration;
 using Octokit.Tests.Integration.Helpers;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
