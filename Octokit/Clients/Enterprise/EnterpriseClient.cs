2f4b855e84b3fdca9b9e563ac2a982fc35a725c0
Ryan Gribble <ryan.gribble@tattsgroup.com>
2015-12-24 13:54:11 +1000

    Add Enterprise base API stub

---
+++ Octokit/Clients/Enterprise/EnterpriseClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Octokit
+{
+    /// <summary>
+    /// A client for GitHub's Users API.
+    /// </summary>
+    /// <remarks>
+    /// See the <a href="http://developer.github.com/v3/users/">Users API documentation</a> for more information.
+    /// </remarks>
+    public class EnterpriseClient : ApiClient, IEnterpriseClient
+    {
+        static readonly Uri _enterpriseEndpoint = new Uri("enterprise", UriKind.Relative);
+
+        /// <summary>
+        /// Instantiates a new GitHub Enterprise API client.
+        /// </summary>
+        /// <param name="apiConnection">An API connection</param>
+        public EnterpriseClient(IApiConnection apiConnection) : base(apiConnection)
+        {
+        }
+    }
+}
---
+++ Octokit/Clients/Enterprise/IEnterpriseClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace Octokit
+{
+    /// <summary>
+    /// A client for GitHub's Enterprise API.
+    /// </summary>
+    /// <remarks>
+    /// See the <a href="http://developer.github.com/v3/enterprise/">Enterprise API documentation</a> for more information.
+    /// </remarks>
+    public interface IEnterpriseClient
+    {
+    }
+}
