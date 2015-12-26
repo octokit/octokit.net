08581e6c6e3a18f28d9b04a349b3a47cd207d7d0
Ryan Gribble <ryan.gribble@tattsgroup.com>
2015-12-27 01:31:28 +1000

    complete integration tests

--- Octokit.Tests.Integration/Clients/Enterprise/EnterpriseAdminStatsClientTest.cs
+++ Octokit.Tests.Integration/Clients/Enterprise/EnterpriseAdminStatsClientTest.cs
@@ -32,4 +32,194 @@ public class EnterpriseAdminStatsClientTest
         Assert.NotNull(adminStats.Gists);
         Assert.NotNull(adminStats.Comments);
     }
+
+    [GitHubEnterpriseTest]
+    public async Task CanGetReposStatistics()
+    {
+        var adminStats = await
+            _github.Enterprise.AdminStats.GetStatistics(AdminStatsType.Repos);
+
+        Assert.NotNull(adminStats);
+        Assert.NotNull(adminStats.Repos);
+        Assert.Null(adminStats.Hooks);
+        Assert.Null(adminStats.Pages);
+        Assert.Null(adminStats.Orgs);
+        Assert.Null(adminStats.Users);
+        Assert.Null(adminStats.Pulls);
+        Assert.Null(adminStats.Issues);
+        Assert.Null(adminStats.Milestones);
+        Assert.Null(adminStats.Gists);
+        Assert.Null(adminStats.Comments);
+    }
+
+    [GitHubEnterpriseTest]
+    public async Task CanGetHooksStatistics()
+    {
+        var adminStats = await
+            _github.Enterprise.AdminStats.GetStatistics(AdminStatsType.Hooks);
+
+        Assert.NotNull(adminStats);
+        Assert.Null(adminStats.Repos);
+        Assert.NotNull(adminStats.Hooks);
+        Assert.Null(adminStats.Pages);
+        Assert.Null(adminStats.Orgs);
+        Assert.Null(adminStats.Users);
+        Assert.Null(adminStats.Pulls);
+        Assert.Null(adminStats.Issues);
+        Assert.Null(adminStats.Milestones);
+        Assert.Null(adminStats.Gists);
+        Assert.Null(adminStats.Comments);
+    }
+
+    [GitHubEnterpriseTest]
+    public async Task CanGetPagesStatistics()
+    {
+        var adminStats = await
+            _github.Enterprise.AdminStats.GetStatistics(AdminStatsType.Pages);
+
+        Assert.NotNull(adminStats);
+        Assert.Null(adminStats.Repos);
+        Assert.Null(adminStats.Hooks);
+        Assert.NotNull(adminStats.Pages);
+        Assert.Null(adminStats.Orgs);
+        Assert.Null(adminStats.Users);
+        Assert.Null(adminStats.Pulls);
+        Assert.Null(adminStats.Issues);
+        Assert.Null(adminStats.Milestones);
+        Assert.Null(adminStats.Gists);
+        Assert.Null(adminStats.Comments);
+    }
+
+    [GitHubEnterpriseTest]
+    public async Task CanGetOrgsStatistics()
+    {
+        var adminStats = await
+            _github.Enterprise.AdminStats.GetStatistics(AdminStatsType.Orgs);
+
+        Assert.NotNull(adminStats);
+        Assert.Null(adminStats.Repos);
+        Assert.Null(adminStats.Hooks);
+        Assert.Null(adminStats.Pages);
+        Assert.NotNull(adminStats.Orgs);
+        Assert.Null(adminStats.Users);
+        Assert.Null(adminStats.Pulls);
+        Assert.Null(adminStats.Issues);
+        Assert.Null(adminStats.Milestones);
+        Assert.Null(adminStats.Gists);
+        Assert.Null(adminStats.Comments);
+    }
+
+    [GitHubEnterpriseTest]
+    public async Task CanGetUsersStatistics()
+    {
+        var adminStats = await
+            _github.Enterprise.AdminStats.GetStatistics(AdminStatsType.Users);
+
+        Assert.NotNull(adminStats);
+        Assert.Null(adminStats.Repos);
+        Assert.Null(adminStats.Hooks);
+        Assert.Null(adminStats.Pages);
+        Assert.Null(adminStats.Orgs);
+        Assert.NotNull(adminStats.Users);
+        Assert.Null(adminStats.Pulls);
+        Assert.Null(adminStats.Issues);
+        Assert.Null(adminStats.Milestones);
+        Assert.Null(adminStats.Gists);
+        Assert.Null(adminStats.Comments);
+    }
+
+    [GitHubEnterpriseTest]
+    public async Task CanGetPullsStatistics()
+    {
+        var adminStats = await
+            _github.Enterprise.AdminStats.GetStatistics(AdminStatsType.Pulls);
+
+        Assert.NotNull(adminStats);
+        Assert.Null(adminStats.Repos);
+        Assert.Null(adminStats.Hooks);
+        Assert.Null(adminStats.Pages);
+        Assert.Null(adminStats.Orgs);
+        Assert.Null(adminStats.Users);
+        Assert.NotNull(adminStats.Pulls);
+        Assert.Null(adminStats.Issues);
+        Assert.Null(adminStats.Milestones);
+        Assert.Null(adminStats.Gists);
+        Assert.Null(adminStats.Comments);
+    }
+
+    [GitHubEnterpriseTest]
+    public async Task CanGetIssuesStatistics()
+    {
+        var adminStats = await
+            _github.Enterprise.AdminStats.GetStatistics(AdminStatsType.Issues);
+
+        Assert.NotNull(adminStats);
+        Assert.Null(adminStats.Repos);
+        Assert.Null(adminStats.Hooks);
+        Assert.Null(adminStats.Pages);
+        Assert.Null(adminStats.Orgs);
+        Assert.Null(adminStats.Users);
+        Assert.Null(adminStats.Pulls);
+        Assert.NotNull(adminStats.Issues);
+        Assert.Null(adminStats.Milestones);
+        Assert.Null(adminStats.Gists);
+        Assert.Null(adminStats.Comments);
+    }
+
+    [GitHubEnterpriseTest]
+    public async Task CanGetMilstonesStatistics()
+    {
+        var adminStats = await
+            _github.Enterprise.AdminStats.GetStatistics(AdminStatsType.Milestones);
+
+        Assert.NotNull(adminStats);
+        Assert.Null(adminStats.Repos);
+        Assert.Null(adminStats.Hooks);
+        Assert.Null(adminStats.Pages);
+        Assert.Null(adminStats.Orgs);
+        Assert.Null(adminStats.Users);
+        Assert.Null(adminStats.Pulls);
+        Assert.Null(adminStats.Issues);
+        Assert.NotNull(adminStats.Milestones);
+        Assert.Null(adminStats.Gists);
+        Assert.Null(adminStats.Comments);
+    }
+
+    [GitHubEnterpriseTest]
+    public async Task CanGetGistsStatistics()
+    {
+        var adminStats = await
+            _github.Enterprise.AdminStats.GetStatistics(AdminStatsType.Gists);
+
+        Assert.NotNull(adminStats);
+        Assert.Null(adminStats.Repos);
+        Assert.Null(adminStats.Hooks);
+        Assert.Null(adminStats.Pages);
+        Assert.Null(adminStats.Orgs);
+        Assert.Null(adminStats.Users);
+        Assert.Null(adminStats.Pulls);
+        Assert.Null(adminStats.Issues);
+        Assert.Null(adminStats.Milestones);
+        Assert.NotNull(adminStats.Gists);
+        Assert.Null(adminStats.Comments);
+    }
+
+    [GitHubEnterpriseTest]
+    public async Task CanGetCommentsStatistics()
+    {
+        var adminStats = await
+            _github.Enterprise.AdminStats.GetStatistics(AdminStatsType.Comments);
+
+        Assert.NotNull(adminStats);
+        Assert.Null(adminStats.Repos);
+        Assert.Null(adminStats.Hooks);
+        Assert.Null(adminStats.Pages);
+        Assert.Null(adminStats.Orgs);
+        Assert.Null(adminStats.Users);
+        Assert.Null(adminStats.Pulls);
+        Assert.Null(adminStats.Issues);
+        Assert.Null(adminStats.Milestones);
+        Assert.Null(adminStats.Gists);
+        Assert.NotNull(adminStats.Comments);
+    }
 }
