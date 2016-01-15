1a5e3d15dc7ca4ded16d498bd5682e0e053ffd01
Mordechai Zuber <mez613@gmail.com>
2016-01-15 12:38:23 +0200

    Add regular interface and models

---
+++ Octokit/Clients/IRepositoryPagesClient.cs
@@ -0,0 +1,40 @@
+﻿using Octokit.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octokit.Clients
+{
+    /// <summary>
+    /// A client for GitHub's Repository Pages API.
+    /// </summary>
+    /// <remarks>
+    /// See the <a href="https://developer.github.com/v3/repos/pages//">Repository Pages API documentation</a> for more information.
+    /// </remarks>
+    interface IRepositoryPagesClient
+    {
+        /// <summary>
+        /// Gets the page metadata for a given repository
+        /// </summary>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        /// <returns></returns>
+        Task<IReadOnlyList<Page>> Get(string owner, string repositoryName);
+        /// <summary>
+        /// Gets all build metadata for a given repository
+        /// </summary>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        /// <returns></returns>
+        Task<IReadOnlyList<PagesBuild>> GetBuilds(string owner, string repositoryName);
+        /// <summary>
+        /// Gets the build metadata for the last build for a given repository
+        /// </summary>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        /// <returns></returns>
+        Task<PagesBuild> GetLatestBuild(string owner, string repositoryName);
+    }
+}
---
+++ Octokit/Models/Response/PagesBuild.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octokit.Models.Response
+{
+    /// <summary>
+    /// Metadata of a Github Pages build.
+    /// </summary>
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class PagesBuild
+    {
+        public PagesBuild() { }
+
+        public PagesBuild(string url, PagesBuildStatus status, User pusher, Commit commit, TimeSpan duration, DateTime createdAt, DateTime updatedAt)
+        {
+            Url = url;
+            Status = status;
+            Pusher = pusher;
+            Commit = commit;
+            Duration = duration;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
+
+        /// <summary>
+        /// The pages's API URL.
+        /// </summary>
+        public string Url { get; protected set; }
+        /// <summary>
+        /// The status of the build.
+        /// </summary>
+        public PagesBuildStatus Status { get; protected set; }
+        /// <summary>
+        /// The user whose commit intiated the build.
+        /// </summary>
+        public User Pusher { get; protected set; }
+        /// <summary>
+        /// Commit SHA.
+        /// </summary>
+        public Commit Commit { get; protected set; }
+        /// <summary>
+        /// Duration of the build
+        /// </summary>
+        public TimeSpan Duration { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
+    }
+}
