ed8b798ec92ef5a48723310788591ad50f675d39
Phil Haack <haacked@gmail.com>
2016-02-07 14:14:07 -0800

    Code to test URL for an enterprise instance.
    Based on code we use in GitHub Desktop for Windows.

---
+++ Octokit.Tests/Clients/Enterprise/EnterpriseProbeTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NSubstitute;
+using Octokit;
+using Octokit.Internal;
+using Octokit.Tests;
+using Xunit;
+
+public class EnterpriseProbeTests
+{
+    public class TheProbeMethod
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("\r\n")]
+        [InlineData("\n")]
+        public async Task ReturnsExistsFor200ResponseWithSha1(string lineEnding)
+        {
+            object responseBody = "ac2e56021f6d357663e5c112bdeb591b403dd4d2" + lineEnding;
+            var response = Substitute.For<IResponse>();
+            response.StatusCode.Returns(HttpStatusCode.OK);
+            response.Body.Returns(responseBody);
+            response.Headers["Server"].Returns("GitHub.com");
+            var productHeader = new ProductHeaderValue("GHfW", "99");
+            var httpClient = Substitute.For<IHttpClient>();
+            httpClient.Send(Args.Request, CancellationToken.None).Returns(Task.FromResult(response));
+            var enterpriseProbe = new EnterpriseProbe(productHeader, httpClient);
+
+            var result = await enterpriseProbe.Probe(new Uri("https://example.com/"));
+
+            Assert.Equal(EnterpriseProbeResult.Ok, result);
+            httpClient.Received(1).Send(Arg.Is<Request>(r =>
+                r.BaseAddress == new Uri("https://example.com") &&
+                r.Endpoint == new Uri("/site/sha", UriKind.Relative) &&
+                r.Method == HttpMethod.Get &&
+                r.Timeout == TimeSpan.FromSeconds(3) &&
+                r.Headers["User-Agent"] == "GHfW/99"
+                ), CancellationToken.None);
+        }
+
+        [Theory]
+        [InlineData("ac2e56021f6d357663e5c112bdeb591b403dd4d2A")] // 41 chars instead of 40
+        [InlineData("ac2e56021f6d357663e5c112bdeb591b403dd4dz")] // 40 chars, but one out of range
+        public async Task ReturnsDefinitelyNotEnterpriseExistsForNotSha1(object responseBody)
+        {
+            var response = Substitute.For<IResponse>();
+            response.StatusCode.Returns(HttpStatusCode.OK);
+            response.Body.Returns(responseBody);
+            response.Headers["Server"].Returns("GitHub.com");
+            var productHeader = new ProductHeaderValue("GHfW", "99");
+            var httpClient = Substitute.For<IHttpClient>();
+            httpClient.Send(Args.Request, CancellationToken.None).Returns(Task.FromResult(response));
+            var enterpriseProbe = new EnterpriseProbe(productHeader, httpClient);
+
+            var result = await enterpriseProbe.Probe(new Uri("https://example.com/"));
+
+            Assert.Equal(EnterpriseProbeResult.NotFound, result);
+        }
+
+        [Fact]
+        public async Task ReturnsDefinitelyNotEnterpriseExistsForNot200Response()
+        {
+            object responseBody = "ac2e56021f6d357663e5c112bdeb591b403dd4d2";
+            var response = Substitute.For<IResponse>();
+            response.StatusCode.Returns(HttpStatusCode.Redirect);
+            response.Body.Returns(responseBody);
+            response.Headers["Server"].Returns("GitHub.com");
+            var productHeader = new ProductHeaderValue("GHfW", "99");
+            var httpClient = Substitute.For<IHttpClient>();
+            httpClient.Send(Args.Request, CancellationToken.None).Returns(Task.FromResult(response));
+            var enterpriseProbe = new EnterpriseProbe(productHeader, httpClient);
+
+            var result = await enterpriseProbe.Probe(new Uri("https://example.com/"));
+
+            Assert.Equal(EnterpriseProbeResult.NotFound, result);
+        }
+
+        [Fact]
+        public async Task ReturnsDefinitelyNotEnterpriseExistsForNotGitHubDotComServer()
+        {
+            object responseBody = "ac2e56021f6d357663e5c112bdeb591b403dd4d2";
+            var response = Substitute.For<IResponse>();
+            response.StatusCode.Returns(HttpStatusCode.OK);
+            response.Body.Returns(responseBody);
+            response.Headers["Server"].Returns("NotGitHub.com");
+            var productHeader = new ProductHeaderValue("GHfW", "99");
+            var httpClient = Substitute.For<IHttpClient>();
+            httpClient.Send(Args.Request, CancellationToken.None).Returns(Task.FromResult(response));
+            var enterpriseProbe = new EnterpriseProbe(productHeader, httpClient);
+
+            var result = await enterpriseProbe.Probe(new Uri("https://example.com/"));
+
+            Assert.Equal(EnterpriseProbeResult.NotFound, result);
+        }
+
+        [Fact]
+        public async Task ReturnsDefinitelyNotEnterpriseExistsForException()
+        {
+            var httpClient = Substitute.For<IHttpClient>();
+            httpClient.Send(Args.Request, CancellationToken.None).ThrowsAsync(new InvalidOperationException());
+            var productHeader = new ProductHeaderValue("GHfW", "99");
+            var enterpriseProbe = new EnterpriseProbe(productHeader, httpClient);
+
+            var result = await enterpriseProbe.Probe(new Uri("https://example.com/"));
+
+            Assert.Equal(EnterpriseProbeResult.Failed, result);
+        }
+    }
+}
---
+++ Octokit/Clients/Enterprise/EnterpriseProbe.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Octokit.Internal;
+
+namespace Octokit
+{
+    /// <summary>
+    /// Service used to check if a GitHub Enterprise instance lives at a given URL.
+    /// </summary>
+    public class EnterpriseProbe : IEnterpriseProbe
+    {
+        static readonly Uri endPoint = new Uri("/site/sha", UriKind.Relative);
+        readonly ProductHeaderValue productHeader;
+        readonly IHttpClient httpClient;
+
+        public EnterpriseProbe(ProductHeaderValue productHeader, IHttpClient httpClient)
+        {
+            Ensure.ArgumentNotNull(productHeader, "productHeader");
+            Ensure.ArgumentNotNull(httpClient, "httpClient");
+
+            this.productHeader = productHeader;
+            this.httpClient = httpClient;
+        }
+
+        /// <summary>
+        /// Makes a request to the specified URL and returns whether or not the probe could definitively determine that a GitHub
+        /// Enterprise Instance exists at the specified URL.
+        /// </summary>
+        /// <remarks>
+        /// The probe checks the absolute path /site/sha at the specified <paramref name="enterpriseBaseUrl" />.
+        /// </remarks>
+        /// <param name="enterpriseBaseUrl">The URL to test</param>
+        /// <returns>An <see cref="EnterpriseProbeResult" /> with either <see cref="EnterpriseProbeResult.Ok"/>,
+        /// <see cref="EnterpriseProbeResult.NotFound"/>, or <see cref="EnterpriseProbeResult.Failed"/> in the case the request failed</returns>
+        public async Task<EnterpriseProbeResult> Probe(Uri enterpriseBaseUrl)
+        {
+            var request = new Request
+            {
+                Method = HttpMethod.Get,
+                BaseAddress = enterpriseBaseUrl,
+                Endpoint = endPoint,
+                Timeout = TimeSpan.FromSeconds(3)
+            };
+            request.Headers.Add("User-Agent", productHeader.ToString());
+
+            try
+            {
+                var response = await httpClient.Send(request);
+                return response == null
+                    ? EnterpriseProbeResult.Failed
+                    : (IsEnterpriseResponse(response)
+                        ? EnterpriseProbeResult.Ok
+                        : EnterpriseProbeResult.NotFound);
+            }
+            catch (Exception)
+            {
+                return EnterpriseProbeResult.Failed;
+            }
+        }
+
+        static Regex _shaRegex = new Regex("^[a-f0-9]{40}\r?$", RegexOptions.IgnoreCase);
+
+        static bool IsEnterpriseResponse(IResponse response)
+        {
+            return response.StatusCode == HttpStatusCode.OK
+                && response.Headers["Server"] == "GitHub.com"
+                && IsSha1(response.Body as string);
+        }
+
+        static bool IsSha1(string body)
+        {
+            return !String.IsNullOrEmpty(body) && _shaRegex.IsMatch(body);
+        }
+    }
+
+    public enum EnterpriseProbeResult
+    {
+        /// <summary>
+        /// Yep! It's an Enterprise server
+        /// </summary>
+        Ok,
+
+        /// <summary>
+        /// Got a response from a server, but it wasn't an Enterprise server
+        /// </summary>
+        NotFound,
+
+        /// <summary>
+        /// Request timed out or DNS failed. So it's probably the case it's not an enterprise server but 
+        /// we can't know for sure.
+        /// </summary>
+        Failed
+    }
+}
