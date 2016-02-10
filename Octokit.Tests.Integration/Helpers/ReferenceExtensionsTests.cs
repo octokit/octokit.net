6d8e7dcd2208c497ccda24ed64708f8944388a4a
Mordechai Zuber <mez613@gmail.com>
2016-02-11 01:37:03 +0200

    Add extension to create  a branch

---
+++ Octokit.Tests.Integration/Helpers/ReferenceExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using Octokit.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Octokit.Tests.Integration.Helpers
+{
+    public class ReferenceExtensionsTests
+    {
+        [IntegrationTest]
+        public async Task CreateABranch()
+        {
+            var client = Helper.GetAuthenticatedClient();
+            var fixture = client.Git.Reference;
+
+            using (var context = await client.CreateRepositoryContext("public-repo"))
+            {
+                var branchFromMaster = await fixture.CreateBranch(context.RepositoryOwner, context.RepositoryName, "patch-1");
+
+                var branchFromPath = await fixture.CreateBranch(context.RepositoryOwner, context.RepositoryName, "patch-2", branchFromMaster);
+
+                var allBrancheNames = (await client.Repository.GetAllBranches(context.RepositoryOwner, context.RepositoryName)).Select(b => b.Name);
+
+                Assert.Contains("patch-1", allBrancheNames);
+                Assert.Contains("patch-2", allBrancheNames);
+            }
+        }
+    }
+}
