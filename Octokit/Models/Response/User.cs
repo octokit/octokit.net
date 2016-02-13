1b3fef81f26b9f4972f82c359d63438c4baf54bc
Ryan Gribble <ryan.gribble@gmail.com>
2016-02-14 00:59:30 +1000

    Initial implementation of missing UserOrganization client methods

--- Octokit/Models/Response/User.cs
+++ Octokit/Models/Response/User.cs
@@ -14,17 +14,28 @@ namespace Octokit
     {
         public User() { }
 
-        public User(string avatarUrl, string bio, string blog, int collaborators, string company, DateTimeOffset createdAt, int diskUsage, string email, int followers, int following, bool? hireable, string htmlUrl, int totalPrivateRepos, int id, string location, string login, string name, int ownedPrivateRepos, Plan plan, int privateGists, int publicGists, int publicRepos, string url, bool siteAdmin, string ldapDistinguishedName)
+        public User(string avatarUrl, string bio, string blog, int collaborators, string company, DateTimeOffset createdAt, int diskUsage, string email, int followers, int following, bool? hireable, string htmlUrl, int totalPrivateRepos, int id, string location, string login, string name, int ownedPrivateRepos, Plan plan, int privateGists, int publicGists, int publicRepos, string url, bool siteAdmin, string ldapDistinguishedName, DateTimeOffset? suspendedAt)
             : base(avatarUrl, bio, blog, collaborators, company, createdAt, diskUsage, email, followers, following, hireable, htmlUrl, totalPrivateRepos, id, location, login, name, ownedPrivateRepos, plan, privateGists, publicGists, publicRepos, AccountType.User, url)
         {
             SiteAdmin = siteAdmin;
             LdapDistinguishedName = ldapDistinguishedName;
+            SuspendedAt = suspendedAt;
         }
 
         /// <summary>
         /// Whether or not the user is an administrator of the site
         /// </summary>
         public bool SiteAdmin { get; protected set; }
+
+        /// <summary>
+        /// When the user was suspended, if at all (GitHub Enterprise)
+        /// </summary>
+        public DateTimeOffset? SuspendedAt { get; protected set; }
+
+        /// <summary>
+        /// Whether or not the user is currently suspended
+        /// </summary>
+        public bool Suspended { get { return SuspendedAt.HasValue; } }
 
         /// <summary>
         /// LDAP Binding (GitHub Enterprise only)
