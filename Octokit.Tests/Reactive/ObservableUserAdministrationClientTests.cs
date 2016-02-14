bd10379846ace99266788cd6c7c71a7630864fed
Ryan Gribble <ryan.gribble@gmail.com>
2016-02-14 23:17:06 +1000

    Add Reactive implementation and unit tests

--- Octokit.Tests/Reactive/ObservableUserAdministrationClientTests.cs
+++ Octokit.Tests/Reactive/ObservableUserAdministrationClientTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using NSubstitute;
 using Octokit.Reactive;
 using Xunit;
@@ -7,10 +8,75 @@ namespace Octokit.Tests.Reactive
 {
     public class ObservableUserAdministrationClientTests
     {
+        public class TheCreateMethod
+        {
+            [Fact]
+            public void CallsIntoClient()
+            {
+                var gitHubClient = Substitute.For<IGitHubClient>();
+                var client = new ObservableUserAdministrationClient(gitHubClient);
+
+                client.Create(new NewUser("auser", "email@company.com"));
+
+                gitHubClient.User.Administration.Received().Create(
+                    Arg.Is<NewUser>(a => 
+                        a.Login == "auser" &&
+                        a.Email == "email@company.com"));
+            }
+        }
+
+        public class TheRenameMethod
+        {
+            [Fact]
+            public void CallsIntoClient()
+            {
+                var gitHubClient = Substitute.For<IGitHubClient>();
+                var client = new ObservableUserAdministrationClient(gitHubClient);
+
+                client.Rename("auser", new UserRename("renamed-user"));
+
+                gitHubClient.User.Administration.Received().Rename(
+                    "auser",
+                    Arg.Is<UserRename>(a =>
+                        a.Login == "renamed-user"));
+            }
+        }
+
+        public class TheCreateImpersonationTokenMethod
+        {
+            [Fact]
+            public void CallsIntoClient()
+            {
+                var gitHubClient = Substitute.For<IGitHubClient>();
+                var client = new ObservableUserAdministrationClient(gitHubClient);
+
+                client.CreateImpersonationToken("auser", new NewImpersonationToken(new string[] { "public_repo" }));
+
+                gitHubClient.User.Administration.Received().CreateImpersonationToken(
+                    "auser",
+                    Arg.Is<NewImpersonationToken>(a =>
+                        a.Scopes.ToList()[0] == "public_repo"));
+            }
+        }
+
+        public class TheDeleteImpersonationTokenMethod
+        {
+            [Fact]
+            public void CallsIntoClient()
+            {
+                var gitHubClient = Substitute.For<IGitHubClient>();
+                var client = new ObservableUserAdministrationClient(gitHubClient);
+
+                client.DeleteImpersonationToken("auser");
+
+                gitHubClient.User.Administration.Received().DeleteImpersonationToken("auser");
+            }
+        }
+
         public class ThePromoteMethod
         {
             [Fact]
-            public void GetsFromClientPromtePromote()
+            public void CallsIntoClient()
             {
                 var gitHubClient = Substitute.For<IGitHubClient>();
                 var client = new ObservableUserAdministrationClient(gitHubClient);
@@ -24,7 +90,7 @@ namespace Octokit.Tests.Reactive
         public class TheDemoteMethod
         {
             [Fact]
-            public void GetsFromClientDemoteDemote()
+            public void CallsIntoClient()
             {
                 var gitHubClient = Substitute.For<IGitHubClient>();
                 var client = new ObservableUserAdministrationClient(gitHubClient);
@@ -38,7 +104,7 @@ namespace Octokit.Tests.Reactive
         public class TheSuspendMethod
         {
             [Fact]
-            public void GetsFromClientSuspendSuspend()
+            public void CallsIntoClient()
             {
                 var gitHubClient = Substitute.For<IGitHubClient>();
                 var client = new ObservableUserAdministrationClient(gitHubClient);
@@ -52,7 +118,7 @@ namespace Octokit.Tests.Reactive
         public class TheUnsuspendMethod
         {
             [Fact]
-            public void GetsFromClientUnsuspendUnsuspend()
+            public void CallsIntoClient()
             {
                 var gitHubClient = Substitute.For<IGitHubClient>();
                 var client = new ObservableUserAdministrationClient(gitHubClient);
@@ -60,6 +126,54 @@ namespace Octokit.Tests.Reactive
                 client.Unsuspend("auser");
 
                 gitHubClient.User.Administration.Received().Unsuspend("auser");
+            }
+        }
+
+        public class TheListAllPublicKeysMethod
+        {
+            [Fact]
+            public void RequestsTheCorrectUrl()
+            {
+                var gitHubClient = Substitute.For<IGitHubClient>();
+                var client = new ObservableUserAdministrationClient(gitHubClient);
+
+                var expectedUri = "admin/keys";
+
+                client.ListAllPublicKeys();
+
+                gitHubClient.Connection.Received().Get<System.Collections.Generic.List<PublicKey>>(
+                    Arg.Is<Uri>(a =>
+                        a.ToString() == expectedUri),
+                    null,
+                    null);
+            }
+        }
+
+        public class TheDeleteMethod
+        {
+            [Fact]
+            public void CallsIntoClient()
+            {
+                var gitHubClient = Substitute.For<IGitHubClient>();
+                var client = new ObservableUserAdministrationClient(gitHubClient);
+
+                client.Delete("auser");
+
+                gitHubClient.User.Administration.Received().Delete("auser");
+            }
+        }
+
+        public class TheDeletePublicKeyMethod
+        {
+            [Fact]
+            public void CallsIntoClient()
+            {
+                var gitHubClient = Substitute.For<IGitHubClient>();
+                var client = new ObservableUserAdministrationClient(gitHubClient);
+
+                client.DeletePublicKey(1);
+
+                gitHubClient.User.Administration.Received().DeletePublicKey(1);
             }
         }
 
