0832c2af3f2ec99feb200e314ecda8307cc823f1
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-03-11 11:43:21 +0530

    Added Migrations API models for requests and responses.

---
+++ Octokit/Models/Request/Enterprise/StartMigrationRequest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Octokit
+{
+    public class StartMigrationRequest
+    {
+        public StartMigrationRequest(
+            IReadOnlyList<string> repositories
+            ) :
+            this(repositories, false, false)
+        { }
+
+        public StartMigrationRequest(
+            IReadOnlyList<string> repositories, 
+            bool lockRepositories) :
+            this(repositories, lockRepositories, false)
+        { }
+
+        public StartMigrationRequest(
+            IReadOnlyList<string> repositories,
+            bool lockRepositories,
+            bool excludeAttachments)
+        {
+            this.Repositories = repositories;
+            this.LockRepositories = lockRepositories;
+            this.ExcludeAttachments = excludeAttachments;
+        }
+
+        public IReadOnlyList<string> Repositories { get; private set; }
+
+        public bool LockRepositories { get; private set; }
+
+        public bool ExcludeAttachments { get; private set; }
+    }
+}
---
+++ Octokit/Models/Response/Enterprise/Migration.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace Octokit
+{
+    public class Migration
+    {
+        public Migration(
+            int id,
+            string guid,
+            string state,
+            bool lockRepositories,
+            bool excludeAttachments,
+            string url,
+            string createdAt,
+            string updatedAt,
+            IReadOnlyList<Repository> repositories)
+        {
+            Id = id;
+            Guid = guid;
+            State = state;
+            LockRepositories = lockRepositories;
+            ExcludeAttachments = excludeAttachments;
+            Url = url;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+            Repositories = repositories;
+        }
+
+        public int Id { get; private set; }
+
+        public string Guid { get; private set; }
+
+        public string State { get; private set; }
+
+        public bool LockRepositories { get; private set; }
+
+        public bool ExcludeAttachments { get; private set; }
+
+        public string Url { get; private set; }
+
+        public string CreatedAt { get; private set; }
+
+        public string UpdatedAt { get; private set; }
+
+        public IReadOnlyList<Repository> Repositories { get; private set; }
+
+
+    }
+}
