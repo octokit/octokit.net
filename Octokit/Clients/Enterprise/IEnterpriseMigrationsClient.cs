665b70ca8c6512840e3c5ba41346da33175202d8
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-03-11 12:02:23 +0530

    Added Migrations Client interface.

---
+++ Octokit/Clients/Enterprise/IEnterpriseMigrationsClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Octokit
+{
+    public interface IEnterpriseMigrationsClient
+    {
+        Task<Migration> GetStatus(
+            string org,
+            int id);
+
+        Task<List<Migration>> GetMigrations(
+            string org);
+
+        Task<Migration> Start(
+            string org,
+            StartMigrationRequest migration);
+
+        Task<string> GetArchive(
+            string org,
+            int id);
+
+        Task<string> DeleteArchive(
+            string org,
+            int id);
+    }
+}
