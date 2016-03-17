f8c776485636efb77fb48d060fa39ce18efdb679
aedampir@gmail.com <aedampir@gmail.com>
2016-03-17 15:37:23 +0700

    New unit tests have been added to check ObservableAuthorizationsClient implementation.

---
+++ Octokit.Tests/Reactive/ObservableAuthorizationsClientTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using NSubstitute;
+using Octokit.Reactive;
+using Xunit;
+
+namespace Octokit.Tests.Reactive
+{
+    public class ObservableAuthorizationsClientTests
+    {
+        public class TheGetAllMethod
+        {
+            [Fact]
+            public void GetsCorrectUrl()
+            {
+                var client = Substitute.For<IGitHubClient>();
+                var authEndpoint = new ObservableAuthorizationsClient(client);
+
+                authEndpoint.GetAll();
+
+                client.Connection.Received(1).Get<List<Authorization>>(Arg.Is<Uri>(u => u.ToString() == "authorizations"),
+                    Arg.Is<Dictionary<string, string>>(dictionary => dictionary.Count == 0), null);
+            }
+
+            [Fact]
+            public void GetsCorrectUrlWithApiOption()
+            {
+                var client = Substitute.For<IGitHubClient>();
+                var authEndpoint = new ObservableAuthorizationsClient(client);
+
+                authEndpoint.GetAll(ApiOptions.None);
+
+                client.Connection.Received(1).Get<List<Authorization>>(Arg.Is<Uri>(u => u.ToString() == "authorizations"),
+                    Arg.Is<Dictionary<string, string>>(dictionary => dictionary.Count == 0), null);
+            }
+        }
+
+        public class TheCtor
+        {
+            [Fact]
+            public void EnsuresNonNullArguments()
+            {
+                Assert.Throws<ArgumentNullException>(
+                    () => new ObservableAuthorizationsClient(null));
+            }
+        }
+    }
+}
