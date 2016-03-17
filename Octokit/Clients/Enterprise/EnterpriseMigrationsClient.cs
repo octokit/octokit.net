b867f4583b9f2570a99956c31e467e8bec0da8cb
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-03-17 16:13:56 +0530

    :memo:ed Client and its interface.

--- Octokit/Clients/Enterprise/EnterpriseMigrationsClient.cs
+++ Octokit/Clients/Enterprise/EnterpriseMigrationsClient.cs
@@ -1,15 +1,33 @@
 ﻿using System.Collections.Generic;
-using System.Net;
 using System.Threading.Tasks;
 
 namespace Octokit
 {
+    /// <summary>
+    /// A client for GitHub's Migrations API.
+    /// </summary>
+    /// <remarks>
+    /// See <a href="https://developer.github.com/v3/migration/migrations/">docs</a>
+    /// for more information.
+    /// </remarks>
     public class EnterpriseMigrationsClient : ApiClient, IEnterpriseMigrationsClient
     {
+        /// <summary>
+        /// Instantiates a GitHub Migrations API client.
+        /// </summary>
+        /// <param name="apiConnection">An API connection.</param>
         public EnterpriseMigrationsClient(IApiConnection apiConnection) : base(apiConnection)
-        {
-        }
+        { }
 
+        /// <summary>
+        /// Get the status of a migration
+        /// </summary>
+        /// <remarks>
+        /// https://developer.github.com/v3/migration/migrations/#get-the-status-of-a-migration
+        /// </remarks>
+        /// <param name="org">The organization which is migrating.</param>
+        /// <param name="id">Migration ID of the organization.</param>
+        /// <returns>A <see cref="Migration"/> object representing the state of migration.</returns>
         public async Task<Migration> GetStatus(string org, int id)
         {
             Ensure.ArgumentNotNullOrEmptyString(org, "org");
@@ -19,6 +37,14 @@ namespace Octokit
             return await ApiConnection.Get<Migration>(endpoint);
         }
 
+        /// <summary>
+        /// Gets the list of the most recent migrations of the the organization.
+        /// </summary>
+        /// <remarks>
+        /// https://developer.github.com/v3/migration/migrations/#get-a-list-of-migrations
+        /// </remarks>
+        /// <param name="org">The organization of which to list migrations.</param>
+        /// <returns>List of most recent <see cref="Migration"/>s.</returns>
         public async Task<List<Migration>> GetMigrations(string org)
         {
             Ensure.ArgumentNotNullOrEmptyString(org, "org");
@@ -28,6 +54,16 @@ namespace Octokit
             return await ApiConnection.Get<List<Migration>>(endpoint);
         }
 
+        /// <summary>
+        /// Starts a new migration specified for the given organization.
+        /// </summary>
+        /// <remarks>
+        /// https://developer.github.com/v3/migration/migrations/#start-a-migration
+        /// </remarks>
+        /// <param name="org">The organization for which to start a migration.</param>
+        /// <param name="migration">Sprcifies parameters for the migration in a 
+        /// <see cref="StartMigrationRequest"/> object.</param>
+        /// <returns>The started migration.</returns>
         public async Task<Migration> Start(string org, StartMigrationRequest migration)
         {
             Ensure.ArgumentNotNullOrEmptyString(org, "org");
@@ -38,6 +74,15 @@ namespace Octokit
             return await ApiConnection.Post<Migration>(endpoint, migration);
         }
 
+        /// <summary>
+        /// Fetches the URL to a migration archive.
+        /// </summary>
+        /// <remarks>
+        /// https://developer.github.com/v3/migration/migrations/#download-a-migration-archive
+        /// </remarks>
+        /// <param name="org">The organization of which the migration was.</param>
+        /// <param name="id">The ID of the migration.</param>
+        /// <returns>URL as a string of the download link of the archive.</returns>
         public async Task<string> GetArchive(string org, int id)
         {
             Ensure.ArgumentNotNullOrEmptyString(org, "org");
@@ -47,6 +92,15 @@ namespace Octokit
             return await ApiConnection.Get<string>(endpoint);
         }
 
+        /// <summary>
+        /// Deletes a previous migration archive.
+        /// </summary>
+        /// <remarks>
+        /// https://developer.github.com/v3/migration/migrations/#delete-a-migration-archive
+        /// </remarks>
+        /// <param name="org">The organization of which the migration was.</param>
+        /// <param name="id">The ID of the migration.</param>
+        /// <returns></returns>
         public Task DeleteArchive(string org, int id)
         {
             Ensure.ArgumentNotNullOrEmptyString(org, "org");
@@ -56,6 +110,16 @@ namespace Octokit
             return ApiConnection.Delete(endpoint);
         }
 
+        /// <summary>
+        /// Unlocks a repository that was locked for migration.
+        /// </summary>
+        /// <remarks>
+        /// https://developer.github.com/v3/migration/migrations/#unlock-a-repository
+        /// </remarks>
+        /// <param name="org">The organization of which the migration was.</param>
+        /// <param name="id">The ID of the migration.</param>
+        /// <param name="repo">The repo to unlock.</param>
+        /// <returns></returns>
         public Task UnlockRepository(string org, int id, string repo)
         {
             Ensure.ArgumentNotNullOrEmptyString(org, "org");
--- Octokit/Clients/Enterprise/IEnterpriseMigrationsClient.cs
+++ Octokit/Clients/Enterprise/IEnterpriseMigrationsClient.cs
@@ -3,27 +3,89 @@ using System.Threading.Tasks;
 
 namespace Octokit
 {
+    /// <summary>
+    /// An interface for GitHub's Migrations API client.
+    /// </summary>
+    /// <remarks>
+    /// See <a href="https://developer.github.com/v3/migration/migrations/">docs</a>
+    /// for more information.
+    /// </remarks>
     public interface IEnterpriseMigrationsClient
     {
+        /// <summary>
+        /// Get the status of a migration
+        /// </summary>
+        /// <remarks>
+        /// https://developer.github.com/v3/migration/migrations/#get-the-status-of-a-migration
+        /// </remarks>
+        /// <param name="org">The organization which is migrating.</param>
+        /// <param name="id">Migration ID of the organization.</param>
+        /// <returns>A <see cref="Migration"/> object representing the state of migration.</returns>
         Task<Migration> GetStatus(
             string org,
             int id);
 
+        /// <summary>
+        /// Gets the list of the most recent migrations of the the organization.
+        /// </summary>
+        /// <remarks>
+        /// https://developer.github.com/v3/migration/migrations/#get-a-list-of-migrations
+        /// </remarks>
+        /// <param name="org">The organization of which to list migrations.</param>
+        /// <returns>List of most recent <see cref="Migration"/>s.</returns>
         Task<List<Migration>> GetMigrations(
             string org);
 
+        /// <summary>
+        /// Starts a new migration specified for the given organization.
+        /// </summary>
+        /// <remarks>
+        /// https://developer.github.com/v3/migration/migrations/#start-a-migration
+        /// </remarks>
+        /// <param name="org">The organization for which to start a migration.</param>
+        /// <param name="migration">Sprcifies parameters for the migration in a 
+        /// <see cref="StartMigrationRequest"/> object.</param>
+        /// <returns>The started migration.</returns>
         Task<Migration> Start(
             string org,
             StartMigrationRequest migration);
 
+        /// <summary>
+        /// Fetches the URL to a migration archive.
+        /// </summary>
+        /// <remarks>
+        /// https://developer.github.com/v3/migration/migrations/#download-a-migration-archive
+        /// </remarks>
+        /// <param name="org">The organization of which the migration was.</param>
+        /// <param name="id">The ID of the migration.</param>
+        /// <returns>URL as a string of the download link of the archive.</returns>
         Task<string> GetArchive(
             string org,
             int id);
 
+        /// <summary>
+        /// Deletes a previous migration archive.
+        /// </summary>
+        /// <remarks>
+        /// https://developer.github.com/v3/migration/migrations/#delete-a-migration-archive
+        /// </remarks>
+        /// <param name="org">The organization of which the migration was.</param>
+        /// <param name="id">The ID of the migration.</param>
+        /// <returns></returns>
         Task DeleteArchive(
             string org,
             int id);
 
+        /// <summary>
+        /// Unlocks a repository that was locked for migration.
+        /// </summary>
+        /// <remarks>
+        /// https://developer.github.com/v3/migration/migrations/#unlock-a-repository
+        /// </remarks>
+        /// <param name="org">The organization of which the migration was.</param>
+        /// <param name="id">The ID of the migration.</param>
+        /// <param name="repo">The repo to unlock.</param>
+        /// <returns></returns>
         Task UnlockRepository(
             string org,
             int id,
