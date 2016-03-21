c34d904f1ab50972a7fe8d02dd762144033c5155
Roger Tinsley <mail@rogertinsley.co.uk>
2016-03-21 19:23:18 +0000

    Created ObservableRepositoryCommitsClientTests
    Added integration test for GetSha1 for observableclient

---
+++ Octokit.Tests.Integration/Reactive/ObservableRepositoryCommitsClientTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Octokit.Reactive;
+using Xunit;
+
+namespace Octokit.Tests.Integration.Reactive
+{
+    public class ObservableRepositoryCommitsClientTests
+    {
+        readonly IObservableRepositoryCommitsClient _fixture;
+
+        public ObservableRepositoryCommitsClientTests()
+        {
+            var github = Helper.GetAuthenticatedClient();
+
+            _fixture = new ObservableRepositoryCommitsClient(github);
+        }
+
+        [IntegrationTest]
+        public async Task CanGetSha1()
+        {
+            var observable = _fixture.GetSha1("octokit", "octokit.net", "master");
+
+            var sha1 = await observable;
+
+            Assert.NotNull(sha1);
+        }
+    }
+}
