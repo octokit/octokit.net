2c60e890d946ec432e6d6384ab62e132d3e33a96
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-04-03 19:24:14 +0530

    Add HTTP 451: Legal Takedown Exception.

---
+++ Octokit/Exceptions/LegalRestrictionException.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Runtime.Serialization;
+
+namespace Octokit
+{
+    /// <summary>
+    /// Represents a HTTP 451 - Unavailable For Legal Reasons response returned from the API.
+    /// This will returned if GitHub has been asked to takedown the requested resource due to
+    /// a DMCA takedown.
+    /// </summary>
+#if !NETFX_CORE
+    [Serializable]
+#endif
+    [SuppressMessage("Microsoft.Design", "CA1032:ImplementStandardExceptionConstructors",
+        Justification = "These exceptions are specific to the GitHub API and not general purpose exceptions")]
+    public class LegalRestrictionException : ApiException
+    {
+        /// <summary>
+        /// Constructs an instance of LegalRestrictionException
+        /// </summary>
+        /// <param name="response">The HTTP payload from the server</param>
+        public LegalRestrictionException(IResponse response) : this(response, null)
+        {
+        }
+
+        /// <summary>
+        /// Constructs an instance of LegalRestrictionException
+        /// </summary>
+        /// <param name="message">The exception message</param>
+        /// <param name="statusCode">The http status code returned by the response</param>
+        public LegalRestrictionException(string message, HttpStatusCode statusCode) : base(message, statusCode)
+        {
+        }
+
+        /// <summary>
+        /// Constructs an instance of LegalRestrictionException
+        /// </summary>
+        /// <param name="response">The HTTP payload from the server</param>
+        /// <param name="innerException">The inner exception</param>
+        public LegalRestrictionException(IResponse response, Exception innerException)
+            : base(response, innerException)
+        {
+            Debug.Assert(response != null && response.StatusCode == 451,
+                "LegalRestrictionException created with wrong status code");
+        }
+
+#if !NETFX_CORE
+        /// <summary>
+        /// Constructs an instance of LegalRestrictionException
+        /// </summary>
+        /// <param name="info">
+        /// The <see cref="SerializationInfo"/> that holds the
+        /// serialized object data about the exception being thrown.
+        /// </param>
+        /// <param name="context">
+        /// The <see cref="StreamingContext"/> that contains
+        /// contextual information about the source or destination.
+        /// </param>
+        protected LegalRestrictionException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+#endif
+    }
+}
