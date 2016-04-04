e655b402fa7938a6ab023722aedb567ed5615d73
Abdelkhalek Oussama Elhamer <oussama.elhamer@outlook.com>
2016-04-04 18:34:01 +0100

    Implement the GetAll* overloads in the ObservableEventsClient class

--- Octokit.Reactive/Clients/ObservableEventsClient.cs
+++ Octokit.Reactive/Clients/ObservableEventsClient.cs
@@ -23,7 +23,22 @@ namespace Octokit.Reactive
         /// <returns>All the public <see cref="Activity"/>s for the particular user.</returns>
         public IObservable<Activity> GetAll()
         {
-            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.Events());
+            return GetAll(ApiOptions.None);
+        }
+
+        /// <summary>
+        /// Gets all the public events
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/activity/events/#list-public-events
+        /// </remarks>
+        /// <param name="options">Options for changing the API response</param>
+        /// <returns>All the public <see cref="Activity"/>s for the particular user.</returns>        
+        public IObservable<Activity> GetAll(ApiOptions options)
+        {
+            Ensure.ArgumentNotNull(options, "options");
+
+            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.Events(), options);
         }
 
         /// <summary>
@@ -40,7 +55,26 @@ namespace Octokit.Reactive
             Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
             Ensure.ArgumentNotNullOrEmptyString(name, "name");
 
-            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.IssuesEvents(owner, name));
+            return GetAllForRepository(owner, name, ApiOptions.None);
+        }
+
+        /// <summary>
+        /// Gets all the events for a given repository
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/activity/events/#list-issue-events-for-a-repository
+        /// </remarks>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        /// <param name="options">Options for changing the API response</param>
+        /// <returns>All the <see cref="Activity"/>s for the particular repository.</returns>
+        public IObservable<Activity> GetAllForRepository(string owner, string name, ApiOptions options)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
+            Ensure.ArgumentNotNullOrEmptyString(name, "name");
+            Ensure.ArgumentNotNull(options, "options");
+
+            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.IssuesEvents(owner, name), options);
         }
 
         /// <summary>
@@ -57,7 +91,26 @@ namespace Octokit.Reactive
             Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
             Ensure.ArgumentNotNullOrEmptyString(name, "name");
 
-            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.NetworkEvents(owner, name));
+            return GetAllForRepositoryNetwork(owner, name, ApiOptions.None);
+        }
+
+        /// <summary>
+        /// Gets all the events for a given repository network
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/activity/events/#list-public-events-for-a-network-of-repositories
+        /// </remarks>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        /// <param name="options">Options for changing the API response</param>
+        /// <returns>All the <see cref="Activity"/>s for the particular repository network.</returns>
+        public IObservable<Activity> GetAllForRepositoryNetwork(string owner, string name, ApiOptions options)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
+            Ensure.ArgumentNotNullOrEmptyString(name, "name");
+            Ensure.ArgumentNotNull(options, "options");
+
+            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.NetworkEvents(owner, name), options);
         }
 
         /// <summary>
@@ -72,7 +125,24 @@ namespace Octokit.Reactive
         {
             Ensure.ArgumentNotNullOrEmptyString(organization, "organization");
 
-            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.OrganizationEvents(organization));
+            return GetAllForOrganization(organization, ApiOptions.None);
+        }
+
+        /// <summary>
+        /// Gets all the events for a given organization
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/activity/events/#list-public-events-for-an-organization
+        /// </remarks>
+        /// <param name="organization">The name of the organization</param>
+        /// <param name="options">Options for changing the API response</param>
+        /// <returns>All the <see cref="Activity"/>s for the particular organization.</returns>
+        public IObservable<Activity> GetAllForOrganization(string organization, ApiOptions options)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(organization, "organization");
+            Ensure.ArgumentNotNull(options, "options");
+
+            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.OrganizationEvents(organization), options);
         }
 
         /// <summary>
@@ -87,7 +157,24 @@ namespace Octokit.Reactive
         {
             Ensure.ArgumentNotNullOrEmptyString(user, "user");
 
-            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.ReceivedEvents(user));
+            return GetAllUserReceived(user, ApiOptions.None);
+        }
+
+        /// <summary>
+        /// Gets all the events that have been received by a given user.
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/activity/events/#list-events-that-a-user-has-received
+        /// </remarks>
+        /// <param name="user">The login of the user</param>
+        /// <param name="options">Options for changing the API response</param>
+        /// <returns>All the <see cref="Activity"/>s that a particular user has received.</returns>
+        public IObservable<Activity> GetAllUserReceived(string user, ApiOptions options)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(user, "user");
+            Ensure.ArgumentNotNull(options, "options");
+
+            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.ReceivedEvents(user), options);
         }
 
         /// <summary>
@@ -102,7 +189,24 @@ namespace Octokit.Reactive
         {
             Ensure.ArgumentNotNullOrEmptyString(user, "user");
 
-            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.ReceivedEvents(user, true));
+            return GetAllUserReceivedPublic(user, ApiOptions.None);
+        }
+
+        /// <summary>
+        /// Gets all the events that have been received by a given user.
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/activity/events/#list-public-events-that-a-user-has-received
+        /// </remarks>
+        /// <param name="user">The login of the user</param>
+        /// <param name="options">Options for changing the API response</param>
+        /// <returns>All the <see cref="Activity"/>s that a particular user has received.</returns>
+        public IObservable<Activity> GetAllUserReceivedPublic(string user, ApiOptions options)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(user, "user");
+            Ensure.ArgumentNotNull(options, "options");
+
+            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.ReceivedEvents(user, true), options);
         }
 
         /// <summary>
@@ -117,7 +221,24 @@ namespace Octokit.Reactive
         {
             Ensure.ArgumentNotNullOrEmptyString(user, "user");
 
-            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.PerformedEvents(user));
+            return GetAllUserPerformed(user, ApiOptions.None);
+        }
+
+        /// <summary>
+        /// Gets all the events that have been performed by a given user.
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/activity/events/#list-events-performed-by-a-user
+        /// </remarks>
+        /// <param name="user">The login of the user</param>
+        /// <param name="options">Options for changing the API response</param>
+        /// <returns>All the <see cref="Activity"/>s that a particular user has performed.</returns>
+        public IObservable<Activity> GetAllUserPerformed(string user, ApiOptions options)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(user, "user");
+            Ensure.ArgumentNotNull(options, "options");
+
+            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.PerformedEvents(user), options);
         }
 
         /// <summary>
@@ -132,7 +253,24 @@ namespace Octokit.Reactive
         {
             Ensure.ArgumentNotNullOrEmptyString(user, "user");
 
-            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.PerformedEvents(user, true));
+            return GetAllUserPerformedPublic(user, ApiOptions.None);
+        }
+
+        /// <summary>
+        /// Gets all the public events that have been performed by a given user.
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/activity/events/#list-public-events-performed-by-a-user
+        /// </remarks>
+        /// <param name="user">The login of the user</param>
+        /// <param name="options">Options for changing the API response</param>
+        /// <returns>All the public <see cref="Activity"/>s that a particular user has performed.</returns>
+        public IObservable<Activity> GetAllUserPerformedPublic(string user, ApiOptions options)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(user, "user");
+            Ensure.ArgumentNotNull(options, "options");
+
+            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.PerformedEvents(user, true), options);
         }
 
         /// <summary>
@@ -149,7 +287,26 @@ namespace Octokit.Reactive
             Ensure.ArgumentNotNullOrEmptyString(user, "user");
             Ensure.ArgumentNotNullOrEmptyString(organization, "organization");
 
-            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.OrganizationEvents(user, organization));
+            return GetAllForAnOrganization(user, organization, ApiOptions.None);
+        }
+
+        /// <summary>
+        /// Gets all the events that are associated with an organization.
+        /// </summary>
+        /// <remarks>
+        /// http://developer.github.com/v3/activity/events/#list-events-for-an-organization
+        /// </remarks>
+        /// <param name="user">The login of the user</param>
+        /// <param name="organization">The name of the organization</param>
+        /// <param name="options">Options for changing the API response</param>
+        /// <returns>All the public <see cref="Activity"/>s that are associated with an organization.</returns>
+        public IObservable<Activity> GetAllForAnOrganization(string user, string organization, ApiOptions options)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(user, "user");
+            Ensure.ArgumentNotNullOrEmptyString(organization, "organization");
+            Ensure.ArgumentNotNull(options, "options");
+
+            return _connection.GetAndFlattenAllPages<Activity>(ApiUrls.OrganizationEvents(user, organization),options);
         }
     }
 }
