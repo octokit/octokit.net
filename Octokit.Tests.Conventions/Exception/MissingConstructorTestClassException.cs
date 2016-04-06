3f8f7822738872c29fbc8af8a605433d7883f345
aedampir@gmail.com <aedampir@gmail.com>
2016-04-06 13:48:13 +0700

    Additional convention test were added.

---
+++ Octokit.Tests.Conventions/Exception/MissingConstructorTestClassException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Octokit.Tests.Conventions
+{
+    public class MissingConstructorTestClassException : Exception
+    {
+        public MissingConstructorTestClassException(Type modelType)
+            : base(CreateMessage(modelType))
+        { }
+
+        static string CreateMessage(Type ctorTest)
+        {
+            return string.Format("Constructor test method is missing {0}.", ctorTest.FullName);
+        }
+    }
+}
---
+++ Octokit.Tests.Conventions/TestConstructorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Octokit.Tests.Clients;
+using Xunit;
+
+namespace Octokit.Tests.Conventions
+{
+    public class TestConstructorTests
+    {
+        [Theory]
+        [MemberData("GetTestConstructorsClasses")]
+        public void CheckTestConstructorsNames(Type type)
+        {
+            const string constructorClassName = "TheCtor";
+            var classes = new HashSet<string>(type.GetNestedTypes().Select(t => t.Name));
+            
+            if (!classes.Contains(constructorClassName))
+            {
+                throw new MissingConstructorTestClassException(type);
+            }
+        }
+
+        public static IEnumerable<object[]> GetTestConstructorsClasses()
+        {
+            var tests = typeof(EventsClientTests)
+                .Assembly
+                .ExportedTypes
+                .Where(type => type.IsClass && type.IsPublic && type.Name.EndsWith("ClientTests"))
+                .Select(type => new[] { type }).ToList();
+            return tests;
+        }
+    }
+}
