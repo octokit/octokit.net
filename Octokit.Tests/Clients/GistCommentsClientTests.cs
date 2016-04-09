2b03f244ef7c70e791391ae57c79d1e374b82ad9
Abdelkhalek Oussama Elhamer <Abdelkhalekdev@gmail.com>
2016-04-09 04:58:19 +0100

    Clean up GistCommentsClientTests class

--- Octokit.Tests/Clients/GistCommentsClientTests.cs
+++ Octokit.Tests/Clients/GistCommentsClientTests.cs
@@ -1,140 +1,140 @@
 ﻿using System;
 using System.Threading.Tasks;
 using NSubstitute;
-using Octokit;
-using Octokit.Tests;
-using Octokit.Tests.Helpers;
 using Xunit;
 
-public class GistCommentsClientTests
+namespace Octokit.Tests.Clients
 {
-    public class TheCtor
+    public class GistCommentsClientTests
     {
-        [Fact]
-        public void EnsuresArgument()
+        public class TheCtor
         {
-            Assert.Throws<ArgumentNullException>(() => new GistCommentsClient(null));
-        }
-    }
-
-    public class TheGetMethod
-    {
-        [Fact]
-        public async Task RequestsCorrectUrl()
-        {
-            var connection = Substitute.For<IApiConnection>();
-            var client = new GistCommentsClient(connection);
-
-            await client.Get("24", 1337);
-
-            connection.Received().Get<GistComment>(Arg.Is<Uri>(u => u.ToString() == "gists/24/comments/1337"));
-        }
-    }
-
-    public class TheGetAllForGistMethod
-    {
-        [Fact]
-        public async Task RequestsCorrectUrl()
-        {
-            var connection = Substitute.For<IApiConnection>();
-            var client = new GistCommentsClient(connection);
-
-            await client.GetAllForGist("24");
-
-            connection.Received().GetAll<GistComment>(Arg.Is<Uri>(u => u.ToString() == "gists/24/comments"), Args.ApiOptions);
-        }
-
-        [Fact]
-        public async Task RequestsCorrectUrlWithApiOptions()
-        {
-            var connection = Substitute.For<IApiConnection>();
-            var client = new GistCommentsClient(connection);
-
-            var options = new ApiOptions
+            [Fact]
+            public void EnsuresArgument()
             {
-                PageSize = 1,
-                PageCount = 1,
-                StartPage = 1
-            };
-
-            await client.GetAllForGist("24", options);
-
-            connection.Received().GetAll<GistComment>(Arg.Is<Uri>(u => u.ToString() == "gists/24/comments"), options);
+                Assert.Throws<ArgumentNullException>(() => new GistCommentsClient(null));
+            }
         }
 
-        [Fact]
-        public async Task EnsuresNonNullArguments()
+        public class TheGetMethod
         {
-            var connection = Substitute.For<IApiConnection>();
-            var client = new GistCommentsClient(connection);
+            [Fact]
+            public async Task RequestsCorrectUrl()
+            {
+                var connection = Substitute.For<IApiConnection>();
+                var client = new GistCommentsClient(connection);
 
-            await Assert.ThrowsAsync<ArgumentNullException>(() => client.GetAllForGist(null));
-            await Assert.ThrowsAsync<ArgumentException>(() => client.GetAllForGist(""));
-            await Assert.ThrowsAsync<ArgumentNullException>(() => client.GetAllForGist("24", null));
-            await Assert.ThrowsAsync<ArgumentException>(() => client.GetAllForGist("", ApiOptions.None));
+                await client.Get("24", 1337);
+
+                connection.Received().Get<GistComment>(Arg.Is<Uri>(u => u.ToString() == "gists/24/comments/1337"));
+            }
+        }
+
+        public class TheGetAllForGistMethod
+        {
+            [Fact]
+            public async Task RequestsCorrectUrl()
+            {
+                var connection = Substitute.For<IApiConnection>();
+                var client = new GistCommentsClient(connection);
+
+                await client.GetAllForGist("24");
+
+                connection.Received().GetAll<GistComment>(Arg.Is<Uri>(u => u.ToString() == "gists/24/comments"), Args.ApiOptions);
+            }
+
+            [Fact]
+            public async Task RequestsCorrectUrlWithApiOptions()
+            {
+                var connection = Substitute.For<IApiConnection>();
+                var client = new GistCommentsClient(connection);
+
+                var options = new ApiOptions
+                {
+                    PageSize = 1,
+                    PageCount = 1,
+                    StartPage = 1
+                };
+
+                await client.GetAllForGist("24", options);
+
+                connection.Received().GetAll<GistComment>(Arg.Is<Uri>(u => u.ToString() == "gists/24/comments"), options);
+            }
+
+            [Fact]
+            public async Task EnsuresNonNullArguments()
+            {
+                var connection = Substitute.For<IApiConnection>();
+                var client = new GistCommentsClient(connection);
+
+                await Assert.ThrowsAsync<ArgumentNullException>(() => client.GetAllForGist(null));
+                await Assert.ThrowsAsync<ArgumentException>(() => client.GetAllForGist(""));
+                await Assert.ThrowsAsync<ArgumentNullException>(() => client.GetAllForGist("24", null));
+                await Assert.ThrowsAsync<ArgumentException>(() => client.GetAllForGist("", ApiOptions.None));
             
-        }
-    }
-
-    public class TheCreateMethod
-    {
-        [Fact]
-        public async Task EnsuresNonNullArguments()
-        {
-            var client = new GistCommentsClient(Substitute.For<IApiConnection>());
-
-            await Assert.ThrowsAsync<ArgumentNullException>(() => client.Create("24", null));
-            await Assert.ThrowsAsync<ArgumentException>(() => client.Create("24", ""));
+            }
         }
 
-        [Fact]
-        public async Task PostsToCorrectUrl()
+        public class TheCreateMethod
         {
-            var comment = "This is a comment.";
-            var connection = Substitute.For<IApiConnection>();
-            var client = new GistCommentsClient(connection);
+            [Fact]
+            public async Task EnsuresNonNullArguments()
+            {
+                var client = new GistCommentsClient(Substitute.For<IApiConnection>());
 
-            await client.Create("24", comment);
+                await Assert.ThrowsAsync<ArgumentNullException>(() => client.Create("24", null));
+                await Assert.ThrowsAsync<ArgumentException>(() => client.Create("24", ""));
+            }
 
-            connection.Received().Post<GistComment>(Arg.Is<Uri>(u => u.ToString() == "gists/24/comments"), Arg.Is<BodyWrapper>(x => x.Body == comment));
+            [Fact]
+            public async Task PostsToCorrectUrl()
+            {
+                var comment = "This is a comment.";
+                var connection = Substitute.For<IApiConnection>();
+                var client = new GistCommentsClient(connection);
+
+                await client.Create("24", comment);
+
+                connection.Received().Post<GistComment>(Arg.Is<Uri>(u => u.ToString() == "gists/24/comments"), Arg.Is<BodyWrapper>(x => x.Body == comment));
+            }
         }
-    }
 
-    public class TheUpdateMethod
-    {
-        [Fact]
-        public async Task EnsuresNonNullArguments()
+        public class TheUpdateMethod
         {
-            var client = new GistCommentsClient(Substitute.For<IApiConnection>());
+            [Fact]
+            public async Task EnsuresNonNullArguments()
+            {
+                var client = new GistCommentsClient(Substitute.For<IApiConnection>());
 
-            await Assert.ThrowsAsync<ArgumentNullException>(() => client.Update("24", 1337, null));
-            await Assert.ThrowsAsync<ArgumentException>(() => client.Update("24", 1337, ""));
+                await Assert.ThrowsAsync<ArgumentNullException>(() => client.Update("24", 1337, null));
+                await Assert.ThrowsAsync<ArgumentException>(() => client.Update("24", 1337, ""));
+            }
+
+            [Fact]
+            public async Task PostsToCorrectUrl()
+            {
+                var comment = "This is a comment.";
+                var connection = Substitute.For<IApiConnection>();
+                var client = new GistCommentsClient(connection);
+
+                await client.Update("24", 1337, comment);
+
+                connection.Received().Patch<GistComment>(Arg.Is<Uri>(u => u.ToString() == "gists/24/comments/1337"), Arg.Is<BodyWrapper>(x => x.Body == comment));
+            }
         }
 
-        [Fact]
-        public async Task PostsToCorrectUrl()
+        public class TheDeleteMethod
         {
-            var comment = "This is a comment.";
-            var connection = Substitute.For<IApiConnection>();
-            var client = new GistCommentsClient(connection);
+            [Fact]
+            public async Task PostsToCorrectUrl()
+            {
+                var connection = Substitute.For<IApiConnection>();
+                var client = new GistCommentsClient(connection);
 
-            await client.Update("24", 1337, comment);
+                await client.Delete("24", 1337);
 
-            connection.Received().Patch<GistComment>(Arg.Is<Uri>(u => u.ToString() == "gists/24/comments/1337"), Arg.Is<BodyWrapper>(x => x.Body == comment));
-        }
-    }
-
-    public class TheDeleteMethod
-    {
-        [Fact]
-        public async Task PostsToCorrectUrl()
-        {
-            var connection = Substitute.For<IApiConnection>();
-            var client = new GistCommentsClient(connection);
-
-            await client.Delete("24", 1337);
-
-            connection.Received().Delete(Arg.Is<Uri>(u => u.ToString() == "gists/24/comments/1337"));
+                connection.Received().Delete(Arg.Is<Uri>(u => u.ToString() == "gists/24/comments/1337"));
+            }
         }
     }
 }
