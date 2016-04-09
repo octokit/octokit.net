d2513e2ce25399d1b4d542f3069cf688328977c0
Abdelkhalek Oussama Elhamer <Abdelkhalekdev@gmail.com>
2016-04-09 05:30:44 +0100

    Clean up

--- Octokit.Tests.Integration/Reactive/ObservableGistCommentsClientTests.cs
+++ Octokit.Tests.Integration/Reactive/ObservableGistCommentsClientTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reactive.Linq;
-using System.Text;
+﻿using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Octokit.Reactive;
 using Xunit;
