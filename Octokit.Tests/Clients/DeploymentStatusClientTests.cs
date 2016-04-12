cf1f1885948794aa6774f8e11f21523fa6022cb5
Abdelkhalek Oussama Elhamer <Abdelkhalekdev@gmail.com>
2016-04-12 23:16:32 +0100

    Unit tests for DeploymentStatusClient

--- Octokit.Tests/Clients/DeploymentStatusClientTests.cs
+++ Octokit.Tests/Clients/DeploymentStatusClientTests.cs
@@ -1,113 +1,134 @@
 ﻿using System;
 using System.Threading.Tasks;
 using NSubstitute;
-using Octokit;
 using Xunit;
 
-public class DeploymentStatusClientTests
+namespace Octokit.Tests.Clients
 {
-    public class TheGetAllMethod
+    public class DeploymentStatusClientTests
     {
-        [Fact]
-        public async Task EnsuresNonNullArguments()
+        public class TheCtor
         {
-            var client = new DeploymentStatusClient(Substitute.For<IApiConnection>());
+            [Fact]
+            public void EnsuresArgument()
+            {
+                Assert.Throws<ArgumentNullException>(() => new DeploymentStatusClient(null));
+            }
+        }
+        public class TheGetAllMethod
+        {
+            [Fact]
+            public async Task EnsuresNonNullArguments()
+            {
+                var client = new DeploymentStatusClient(Substitute.For<IApiConnection>());
 
-            await Assert.ThrowsAsync<ArgumentNullException>(() => client.GetAll(null, "name", 1));
-            await Assert.ThrowsAsync<ArgumentNullException>(() => client.GetAll("owner", null, 1));
+                await Assert.ThrowsAsync<ArgumentNullException>(() => client.GetAll(null, "name", 1));
+                await Assert.ThrowsAsync<ArgumentNullException>(() => client.GetAll("owner", null, 1));
+                await Assert.ThrowsAsync<ArgumentNullException>(() => client.GetAll("owner", "name", 1, null));
+            }
+
+            [Fact]
+            public async Task EnsuresNonEmptyArguments()
+            {
+                var client = new DeploymentStatusClient(Substitute.For<IApiConnection>());
+
+                await Assert.ThrowsAsync<ArgumentException>(() => client.GetAll("", "name", 1));
+                await Assert.ThrowsAsync<ArgumentException>(() => client.GetAll("owner", "", 1));
+                await Assert.ThrowsAsync<ArgumentException>(() => client.GetAll("", "name", 1, ApiOptions.None));
+                await Assert.ThrowsAsync<ArgumentException>(() => client.GetAll("owner", "", 1, ApiOptions.None));
+            }
+
+            [Theory]
+            [InlineData(" ")]
+            [InlineData("\n")]
+            [InlineData("\t")]
+            [InlineData("  ")]
+            [InlineData("\n\r")]
+            public async Task EnsureNonWhitespaceArguments(string whitespace)
+            {
+                var client = new DeploymentStatusClient(Substitute.For<IApiConnection>());
+
+                await Assert.ThrowsAsync<ArgumentException>(() => client.GetAll(whitespace, "name", 1));
+                await Assert.ThrowsAsync<ArgumentException>(() => client.GetAll("owner", whitespace, 1));
+            }
+
+            [Fact]
+            public void RequestsCorrectUrl()
+            {
+                var connection = Substitute.For<IApiConnection>();
+                var client = new DeploymentStatusClient(connection);
+                var expectedUrl = "repos/owner/name/deployments/1/statuses";
+
+                client.GetAll("owner", "name", 1);
+                connection.Received().GetAll<DeploymentStatus>(Arg.Is<Uri>(u => u.ToString() == expectedUrl), Args.ApiOptions);
+            }
+
+            [Fact]
+            public void RequestsCorrectUrlWithApiOptions()
+            {
+                var connection = Substitute.For<IApiConnection>();
+                var client = new DeploymentStatusClient(connection);
+                var expectedUrl = "repos/owner/name/deployments/1/statuses";
+
+                var options=new ApiOptions()
+                {
+                    StartPage = 1,
+                    PageSize = 1,
+                    PageCount = 1
+                };
+                client.GetAll("owner", "name", 1, options);
+                connection.Received().GetAll<DeploymentStatus>(Arg.Is<Uri>(u => u.ToString() == expectedUrl), options);
+            }
         }
 
-        [Fact]
-        public async Task EnsuresNonEmptyArguments()
+        public class TheCreateMethod
         {
-            var client = new DeploymentStatusClient(Substitute.For<IApiConnection>());
+            readonly NewDeploymentStatus newDeploymentStatus = new NewDeploymentStatus(DeploymentState.Success);
 
-            await Assert.ThrowsAsync<ArgumentException>(() => client.GetAll("", "name", 1));
-            await Assert.ThrowsAsync<ArgumentException>(() => client.GetAll("owner", "", 1));
-        }
+            [Fact]
+            public async Task EnsuresNonNullArguments()
+            {
+                var client = new DeploymentStatusClient(Substitute.For<IApiConnection>());
 
-        [Theory]
-        [InlineData(" ")]
-        [InlineData("\n")]
-        [InlineData("\t")]
-        [InlineData("  ")]
-        [InlineData("\n\r")]
-        public async Task EnsureNonWhitespaceArguments(string whitespace)
-        {
-            var client = new DeploymentStatusClient(Substitute.For<IApiConnection>());
+                await Assert.ThrowsAsync<ArgumentNullException>(() => client.Create(null, "name", 1, newDeploymentStatus));
+                await Assert.ThrowsAsync<ArgumentNullException>(() => client.Create("owner", null, 1, newDeploymentStatus));
+            }
 
-            await Assert.ThrowsAsync<ArgumentException>(() => client.GetAll(whitespace, "name", 1));
-            await Assert.ThrowsAsync<ArgumentException>(() => client.GetAll("owner", whitespace, 1));
-        }
+            [Fact]
+            public async Task EnsuresNonEmptyArguments()
+            {
+                var client = new DeploymentStatusClient(Substitute.For<IApiConnection>());
 
-        [Fact]
-        public void RequestsCorrectUrl()
-        {
-            var connection = Substitute.For<IApiConnection>();
-            var client = new DeploymentStatusClient(connection);
-            var expectedUrl = "repos/owner/name/deployments/1/statuses";
+                await Assert.ThrowsAsync<ArgumentException>(() => client.GetAll("", "name", 1));
+                await Assert.ThrowsAsync<ArgumentException>(() => client.GetAll("owner", "", 1));
+            }
 
-            client.GetAll("owner", "name", 1);
-            connection.Received().GetAll<DeploymentStatus>(Arg.Is<Uri>(u => u.ToString() == expectedUrl));
-        }
-    }
+            [Theory]
+            [InlineData(" ")]
+            [InlineData("\n")]
+            [InlineData("\t")]
+            [InlineData("  ")]
+            [InlineData("\n\r")]
+            public async Task EnsureNonWhitespaceArguments(string whitespace)
+            {
+                var client = new DeploymentStatusClient(Substitute.For<IApiConnection>());
 
-    public class TheCreateMethod
-    {
-        readonly NewDeploymentStatus newDeploymentStatus = new NewDeploymentStatus(DeploymentState.Success);
+                await Assert.ThrowsAsync<ArgumentException>(() => client.Create(whitespace, "repo", 1, newDeploymentStatus));
+                await Assert.ThrowsAsync<ArgumentException>(() => client.Create("owner", whitespace, 1, newDeploymentStatus));
+            }
 
-        [Fact]
-        public async Task EnsuresNonNullArguments()
-        {
-            var client = new DeploymentStatusClient(Substitute.For<IApiConnection>());
+            [Fact]
+            public void PostsToCorrectUrl()
+            {
+                var connection = Substitute.For<IApiConnection>();
+                var client = new DeploymentStatusClient(connection);
+                var expectedUrl = "repos/owner/repo/deployments/1/statuses";
 
-            await Assert.ThrowsAsync<ArgumentNullException>(() => client.Create(null, "name", 1, newDeploymentStatus));
-            await Assert.ThrowsAsync<ArgumentNullException>(() => client.Create("owner", null, 1, newDeploymentStatus));
-        }
+                client.Create("owner", "repo", 1, newDeploymentStatus);
 
-        [Fact]
-        public async Task EnsuresNonEmptyArguments()
-        {
-            var client = new DeploymentStatusClient(Substitute.For<IApiConnection>());
-
-            await Assert.ThrowsAsync<ArgumentException>(() => client.GetAll("", "name", 1));
-            await Assert.ThrowsAsync<ArgumentException>(() => client.GetAll("owner", "", 1));
-        }
-
-        [Theory]
-        [InlineData(" ")]
-        [InlineData("\n")]
-        [InlineData("\t")]
-        [InlineData("  ")]
-        [InlineData("\n\r")]
-        public async Task EnsureNonWhitespaceArguments(string whitespace)
-        {
-            var client = new DeploymentStatusClient(Substitute.For<IApiConnection>());
-
-            await Assert.ThrowsAsync<ArgumentException>(() => client.Create(whitespace, "repo", 1, newDeploymentStatus));
-            await Assert.ThrowsAsync<ArgumentException>(() => client.Create("owner", whitespace, 1, newDeploymentStatus));
-        }
-
-        [Fact]
-        public void PostsToCorrectUrl()
-        {
-            var connection = Substitute.For<IApiConnection>();
-            var client = new DeploymentStatusClient(connection);
-            var expectedUrl = "repos/owner/repo/deployments/1/statuses";
-
-            client.Create("owner", "repo", 1, newDeploymentStatus);
-
-            connection.Received().Post<DeploymentStatus>(Arg.Is<Uri>(u => u.ToString() == expectedUrl),
-                                                         Arg.Any<NewDeploymentStatus>());
-        }
-    }
-
-    public class TheCtor
-    {
-        [Fact]
-        public void EnsuresArgument()
-        {
-            Assert.Throws<ArgumentNullException>(() => new DeploymentStatusClient(null));
-        }
+                connection.Received().Post<DeploymentStatus>(Arg.Is<Uri>(u => u.ToString() == expectedUrl),
+                    Arg.Any<NewDeploymentStatus>());
+            }
+        }      
     }
 }
