05bdbab842598d38962045d30679f5472f6ee913
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-04-22 10:55:29 +0530

    Minor touch-up.
    - `StartMigration()` not considered as a test.
    - Creating repo with timestamp for uniqueness.

--- Octokit.Tests.Integration/Clients/MigrationsClientTests.cs
+++ Octokit.Tests.Integration/Clients/MigrationsClientTests.cs
@@ -29,23 +29,23 @@ public class MigrationsClientTests : IDisposable
 
     private async Task CreateTheWorld()
     {
-        _repos.Add(await _gitHub.CreateRepositoryContext(_orgName, new NewRepository("migrationtest-repo1")
-        {
-            GitignoreTemplate = "VisualStudio", LicenseTemplate = "mit"
-        }));
-        _repos.Add(await _gitHub.CreateRepositoryContext(_orgName, new NewRepository("migrationtest-repo2")
+        _repos.Add(await _gitHub.CreateRepositoryContext(_orgName, new NewRepository(Helper.MakeNameWithTimestamp("migrationtest-repo1"))
         {
             GitignoreTemplate = "VisualStudio",
             LicenseTemplate = "mit"
         }));
-        _repos.Add(await _gitHub.CreateRepositoryContext(_orgName, new NewRepository("migrationtest-repo3")
+        _repos.Add(await _gitHub.CreateRepositoryContext(_orgName, new NewRepository(Helper.MakeNameWithTimestamp("migrationtest-repo2"))
+        {
+            GitignoreTemplate = "VisualStudio",
+            LicenseTemplate = "mit"
+        }));
+        _repos.Add(await _gitHub.CreateRepositoryContext(_orgName, new NewRepository(Helper.MakeNameWithTimestamp("migrationtest-repo3"))
         {
             GitignoreTemplate = "VisualStudio",
             LicenseTemplate = "mit"
         }));
     }
 
-    [IntegrationTest(Skip = "This will be automatically tested as all other tests call it. See: https://gist.github.com/ryangribble/fc14239ecad3f65d96f5ebd2fecf722e#file-octokit-migrationsclienttests-cs-L40")]
     public async Task StartNewMigration()
     {
         var repoNames = _repos.Select(repo => repo.Repository.FullName).ToList();
