dc096afcafbb48955a717b042a94755744c6fff9
aedampir@gmail.com <aedampir@gmail.com>
2016-05-02 18:41:35 +0700

    added XML documentation for PullRequestFiles method

--- Octokit/Helpers/ApiUrls.cs
+++ Octokit/Helpers/ApiUrls.cs
@@ -945,7 +945,13 @@ namespace Octokit
         {
             return "repos/{0}/{1}/pulls/{2}/commits".FormatUri(owner, name, number);
         }
-        
+
+        /// <summary>
+        /// Returns the <see cref="Uri"/> that returns the files on a pull request.
+        /// </summary>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        /// <param name="number">The pull request number</param>
         public static Uri PullRequestFiles(string owner, string name, int number)
         {
             return "repos/{0}/{1}/pulls/{2}/files".FormatUri(owner, name, number);
