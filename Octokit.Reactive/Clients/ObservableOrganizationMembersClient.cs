face36878845eabbecbe2fb49a49c0c2c804a5f8
aedampir@gmail.com <aedampir@gmail.com>
2016-05-05 14:11:52 +0700

    Revert "fixed in OrganizationMembersClient.Delete method."
    This reverts commit f47405f5337e60945fdb3ccf1368f16069599217.

--- Octokit.Reactive/Clients/ObservableOrganizationMembersClient.cs
+++ Octokit.Reactive/Clients/ObservableOrganizationMembersClient.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Reactive;
-using System.Reactive.Threading.Tasks;
-using Octokit.Reactive.Internal;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Octokit.Internal;
 
 namespace Octokit.Reactive
 {
@@ -162,7 +163,22 @@ namespace Octokit.Reactive
             Ensure.ArgumentNotNullOrEmptyString(org, "org");
             Ensure.ArgumentNotNullOrEmptyString(user, "user");
 
-            return _client.CheckMember(org, user).ToObservable();
+            try
+            {
+                var response = await Connection.Get<object>(ApiUrls.CheckMember(org, user), null, null).ConfigureAwait(false);
+                var statusCode = response.HttpResponse.StatusCode;
+                if (statusCode != HttpStatusCode.NotFound
+                    && statusCode != HttpStatusCode.NoContent
+                    && statusCode != HttpStatusCode.Found)
+                {
+                    throw new ApiException("Invalid Status Code returned. Expected a 204, a 302 or a 404", statusCode);
+                }
+                return statusCode == HttpStatusCode.NoContent;
+            }
+            catch (NotFoundException)
+            {
+                return false;
+            }
         }
 
         /// <summary>
