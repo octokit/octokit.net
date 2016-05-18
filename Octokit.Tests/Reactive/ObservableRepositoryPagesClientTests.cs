809bd3d0da8cdecfb0960b09c1becb81a57fb508
Brendan Forster <brendan@github.com>
2016-05-18 14:21:08 +0300

    added missing convention test

--- Octokit.Tests/Reactive/ObservableRepositoryPagesClientTests.cs
+++ Octokit.Tests/Reactive/ObservableRepositoryPagesClientTests.cs
@@ -7,6 +7,15 @@ namespace Octokit.Tests.Reactive
 {
     public class ObservableRepositoryPagesClientTests
     {
+        public class TheCtor
+        {
+            [Fact]
+            public void EnsuresNonNullArguments()
+            {
+                Assert.Throws<ArgumentNullException>(() => new ObservableRepositoryPagesClient(null));
+            }
+        }
+
         public class TheGetAllMethod
         {
             [Fact]
