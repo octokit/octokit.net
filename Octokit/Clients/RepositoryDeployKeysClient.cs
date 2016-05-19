1b3075eb8284e0d4b9c540aa60b3963d579a5caa
aedampir@gmail.com <aedampir@gmail.com>
2016-05-19 23:35:08 +0700

    added needed overloads on I(Observable)RepositoryDeployKeysClient

--- Octokit/Clients/RepositoryDeployKeysClient.cs
+++ Octokit/Clients/RepositoryDeployKeysClient.cs
@@ -53,7 +53,25 @@ namespace Octokit
             Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
             Ensure.ArgumentNotNullOrEmptyString(name, "name");
 
-            return ApiConnection.GetAll<DeployKey>(ApiUrls.RepositoryDeployKeys(owner, name));
+            return GetAll(owner, name, ApiOptions.None);
+        }
+
+        /// <summary>
+        /// Get all deploy keys for a repository.
+        /// </summary>
+        /// <remarks>
+        /// See the <a href="https://developer.github.com/v3/repos/keys/#list"> API documentation</a> for more information.
+        /// </remarks>
+        /// <param name="owner">The owner of the repository.</param>
+        /// <param name="name">The name of the repository.</param>
+        /// <param name="options">Options for changing the API response</param>
+        public Task<IReadOnlyList<DeployKey>> GetAll(string owner, string name, ApiOptions options)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
+            Ensure.ArgumentNotNullOrEmptyString(name, "name");
+            Ensure.ArgumentNotNull(options, "options");
+
+            return ApiConnection.GetAll<DeployKey>(ApiUrls.RepositoryDeployKeys(owner, name), options);
         }
 
         /// <summary>
