a68663db667134ffc48159a2a0a03e25b1ca8e88
lrz-hal <martin.scholz83@outlook.com>
2016-05-23 15:26:39 +0200

    Add observable unit tests for reactions

---
+++ Octokit.Tests/Reactive/ObservableRepositoryCommentsClientTests.cs
@@ -0,0 +1,33 @@
+﻿using NSubstitute;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Octokit.Reactive;
+
+namespace Octokit.Tests.Reactive
+{
+    class ObservableRepositoryCommentsClientTests
+    {
+        public class TheReactionMethod
+        {
+            [Fact]
+            public void RequestsCorrectUrl()
+            {
+                NewCommitCommentReaction newCommitCommentReaction = new NewCommitCommentReaction(Reaction.Heart);
+
+                var connection = Substitute.For<IApiConnection>();
+
+                client.CreateReaction("fake", "repo", 1, newCommitCommentReaction);
+
+                connection.Received().Post<CommitCommentReaction>(Arg.Is<Uri>(u => u.ToString() == "repos/fake/repo/comments/1/reactions"), Arg.Any<object>(), AcceptHeaders.Reactions);
+            }
+
+            [Fact]
+            public async Task EnsuresArgumentsNotNull()
+            {
+                var connection = Substitute.For<IApiConnection>();
+
+            }
+        }
+    }
+}
