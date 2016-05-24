3020e489b487775d8b24d239803026cba3f2bbf7
lrz-hal <martin.scholz83@outlook.com>
2016-05-24 08:25:22 +0200

    Merge ObersevableRepositoryCommentsClientTests

--- Octokit.Tests/Reactive/ObservableRepositoryCommentsClientTests.cs
+++ Octokit.Tests/Reactive/ObservableRepositoryCommentsClientTests.cs
@@ -1,50 +1,4 @@
-<<<<<<< HEAD
-﻿using NSubstitute;
 using System;
-using System.Threading.Tasks;
-using Xunit;
-using Octokit.Reactive;
-
-namespace Octokit.Tests.Reactive
-{
-    class ObservableRepositoryCommentsClientTests
-    {
-        public class TheReactionMethod
-        {
-            private readonly IGitHubClient _githubClient;
-            private readonly ObservableRepositoryCommentsClient _client;
-
-            public TheReactionMethod()
-            {
-                _githubClient = Substitute.For<IGitHubClient>();
-                _client = new ObservableRepositoryCommentsClient(_githubClient);
-            }
-
-            [Fact]
-            public void RequestsCorrectUrl()
-            {
-                NewCommitCommentReaction newCommitCommentReaction = new NewCommitCommentReaction(Reaction.Heart);
-
-                var connection = Substitute.For<IApiConnection>();               
-
-                _client.CreateReaction("fake", "repo", 1, newCommitCommentReaction);
-
-                _githubClient.Connection.Received().Post<CommitCommentReaction>(Arg.Is<Uri>(u => u.ToString() == "repos/fake/repo/comments/1/reactions"), Arg.Any<object>(), AcceptHeaders.Reactions, Arg.Any<string>());
-            }
-
-            [Fact]
-            public async Task EnsuresArgumentsNotNull()
-            {
-                Assert.Throws<ArgumentNullException>(() => _client.CreateReaction(null, "name", 1, new NewCommitCommentReaction(Reaction.Heart)));
-                Assert.Throws<ArgumentException>(() => _client.CreateReaction("", "name", 1, new NewCommitCommentReaction(Reaction.Heart)));
-                Assert.Throws<ArgumentNullException>(() => _client.CreateReaction("owner", null, 1, new NewCommitCommentReaction(Reaction.Heart)));
-                Assert.Throws<ArgumentException>(() => _client.CreateReaction("owner", "", 1, new NewCommitCommentReaction(Reaction.Heart)));
-            }
-        }
-    }
-}
-=======
-﻿using System;
 using System.Collections.Generic;
 using NSubstitute;
 using Octokit.Reactive;
@@ -52,7 +6,7 @@ using Xunit;
 
 namespace Octokit.Tests.Reactive
 {
-    public class ObservableRepositoryCommentsClientTests
+    class ObservableRepositoryCommentsClientTests
     {
         public class TheCtor
         {
@@ -181,4 +135,3 @@ namespace Octokit.Tests.Reactive
         }
     }
 }
->>>>>>> refs/heads/master
