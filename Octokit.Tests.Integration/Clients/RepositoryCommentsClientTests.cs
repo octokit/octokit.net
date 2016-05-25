b30fadec3e32c393b8004b07e72099a880e307ff
maddin2016 <martin.scholz83@outlook.com>
2016-05-25 13:54:58 +0200

    add integration test for repository comment reaction

--- Octokit.Tests.Integration/Clients/RepositoryCommentsClientTests.cs
+++ Octokit.Tests.Integration/Clients/RepositoryCommentsClientTests.cs
@@ -367,4 +367,62 @@ public class RepositoryCommentsClientTests
             _context.Dispose();
         }
     }
+    public class TheReactionMethod : IDisposable
+    {
+        private readonly IGitHubClient _github;
+        private readonly RepositoryContext _context;
+
+        public TheReactionMethod()
+        {
+            _github = Helper.GetAuthenticatedClient();
+
+            _context = _github.CreateRepositoryContext("public-repo").Result;
+        }
+        private async Task<Commit> SetupCommitForRepository(IGitHubClient client)
+        {
+            var blob = new NewBlob
+            {
+                Content = "Hello World!",
+                Encoding = EncodingType.Utf8
+            };
+            var blobResult = await client.Git.Blob.Create(_context.RepositoryOwner, _context.RepositoryName, blob);
+
+            var newTree = new NewTree();
+            newTree.Tree.Add(new NewTreeItem
+            {
+                Type = TreeType.Blob,
+                Mode = FileMode.File,
+                Path = "README.md",
+                Sha = blobResult.Sha
+            });
+
+            var treeResult = await client.Git.Tree.Create(_context.RepositoryOwner, _context.RepositoryName, newTree);
+
+            var newCommit = new NewCommit("test-commit", treeResult.Sha);
+
+            return await client.Git.Commit.Create(_context.RepositoryOwner, _context.RepositoryName, newCommit);
+        }
+
+        [IntegrationTest]
+        public async Task CanCreateReaction()
+        {
+            var commit = await SetupCommitForRepository(_github);
+
+            var comment = new NewCommitComment("test");
+
+            var result = await _github.Repository.Comment.Create(_context.RepositoryOwner, _context.RepositoryName,
+                commit.Sha, comment);
+
+            Assert.NotNull(result);
+
+            var newCommitCommentReaction = new NewCommitCommentReaction(Reaction.Confused);
+            var reaction = await _github.Repository.Comment.CreateReaction(_context.RepositoryOwner, _context.RepositoryName, result.Id, newCommitCommentReaction);
+
+            Assert.IsType<CommitCommentReaction>(reaction);
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
 }
