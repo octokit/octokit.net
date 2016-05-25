9f98916bf401c92a962e26ddda3fdfa2d91c450c
maddin2016 <martin.scholz83@outlook.com>
2016-05-25 12:21:44 +0200

    add reaction for observable repository comment client

--- Octokit.Reactive/Clients/IObservableRepositoryCommentsClient.cs
+++ Octokit.Reactive/Clients/IObservableRepositoryCommentsClient.cs
@@ -89,5 +89,16 @@ namespace Octokit.Reactive
         /// <param name="number">The comment id</param>
         /// <returns></returns>
         IObservable<Unit> Delete(string owner, string name, int number);
+
+        /// <summary>
+        /// Creates a reaction for specified Commit Comment
+        /// </summary>
+        /// <remarks>http://developer.github.com/v3/repos/comments/#create-reaction-for-a-commit-comment</remarks>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        /// <param name="number">The comment id</param>
+        /// <param name="reaction">The reaction for </param>
+        /// <returns></returns>
+        IObservable<CommitCommentReaction> CreateReaction(string owner, string name, int number, NewCommitCommentReaction reaction);
     }
 }
