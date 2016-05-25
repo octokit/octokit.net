1a2396b0fbaa802ebf53118a37be187795137105
maddin2016 <martin.scholz83@outlook.com>
2016-05-25 14:00:25 +0200

    add apiurl for commit comment reaction

--- Octokit/Helpers/ApiUrls.cs
+++ Octokit/Helpers/ApiUrls.cs
@@ -387,6 +387,18 @@ namespace Octokit
         }
 
         /// <summary>
+        /// Returns the <see cref="Uri"/> for the reaction of a specified commit comment.
+        /// </summary>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        /// <param name="number">The comment number</param>
+        /// <returns></returns>
+        public static Uri CommitCommentReaction(string owner, string name, int number)
+        {
+            return "repos/{0}/{1}/comments/{2}/reactions".FormatUri(owner, name, number);
+        }
+
+        /// <summary>
         /// Returns the <see cref="Uri"/> that returns all of the assignees to which issues may be assigned.
         /// </summary>
         /// <param name="owner">The owner of the repository</param>
