963b979c8036e27f12efe4566950ee116eae594f
maddin2016 <martin.scholz83@outlook.com>
2016-05-25 14:36:20 +0200

    change content of newCommitCommentReaction

--- Octokit/Models/Request/NewCommitCommentReaction.cs
+++ Octokit/Models/Request/NewCommitCommentReaction.cs
@@ -12,26 +12,7 @@ namespace Octokit
         /// <param name="content">The reaction type.</param>
         public NewCommitCommentReaction(Reaction content)
         {
-            switch (content){
-                case Reaction.Plus1:
-                    Content = Reaction.Plus1;
-                    break;
-                case Reaction.Minus1:
-                    Content = Reaction.Minus1;
-                    break;
-                case Reaction.Laugh:
-                    Content = Reaction.Laugh;
-                    break;
-                case Reaction.Hooray:
-                    Content = Reaction.Hooray;
-                    break;
-                case Reaction.Heart:
-                    Content = Reaction.Heart;
-                    break;
-                case Reaction.Confused:
-                    Content = Reaction.Confused;
-                    break;
-            }            
+            Content = content;
         }
 
         /// <summary>
