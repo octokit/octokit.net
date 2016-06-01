28f454a7bb60b2689a92c1eb32990c1d3499b8c8
maddin2016 <martin.scholz83@outlook.com>
2016-06-01 11:34:37 +0200

    add descriptions for observable reactions client

--- Octokit.Reactive/Clients/IObservableReactionsClient.cs
+++ Octokit.Reactive/Clients/IObservableReactionsClient.cs
@@ -2,12 +2,36 @@
 {
     public interface IObservableReactionsClient
     {
+        /// <summary>
+        /// Access GitHub's Reactions API for Commit Comments.
+        /// </summary>
+        /// <remarks>
+        /// Refer to the API documentation for more information: https://developer.github.com/v3/reactions/
+        /// </remarks>
         IObservableCommitCommentReactionsClient CommitComment { get; }
 
+        /// <summary>
+        /// Access GitHub's Reactions API for Issues.
+        /// </summary>
+        /// <remarks>
+        /// Refer to the API documentation for more information: https://developer.github.com/v3/reactions/
+        /// </remarks>
         IObservableIssueReactionsClient Issue { get; }
 
+        /// <summary>
+        /// Access GitHub's Reactions API for Issue Comments.
+        /// </summary>
+        /// <remarks>
+        /// Refer to the API documentation for more information: https://developer.github.com/v3/reactions/
+        /// </remarks>
         IObservableIssueCommentReactionsClient IssueComment { get; }
 
+        /// <summary>
+        /// Access GitHub's Reactions API for Pull Request Review Comments.
+        /// </summary>
+        /// <remarks>
+        /// Refer to the API documentation for more information: https://developer.github.com/v3/reactions/
+        /// </remarks>
         IObservablePullRequestReviewCommentReactionsClient PullRequestReviewComment { get; }
     }
 }
--- Octokit.Reactive/Clients/ObservableReactionsClient.cs
+++ Octokit.Reactive/Clients/ObservableReactionsClient.cs
@@ -12,12 +12,36 @@
             PullRequestReviewComment = new ObservablePullRequestReviewCommentReactionsClient(client);
         }
 
+        /// <summary>
+        /// Access GitHub's Reactions API for Commit Comments.
+        /// </summary>
+        /// <remarks>
+        /// Refer to the API documentation for more information: https://developer.github.com/v3/reactions/
+        /// </remarks>
         public IObservableCommitCommentReactionsClient CommitComment { get; private set; }
 
+        /// <summary>
+        /// Access GitHub's Reactions API for Issues.
+        /// </summary>
+        /// <remarks>
+        /// Refer to the API documentation for more information: https://developer.github.com/v3/reactions/
+        /// </remarks>
         public IObservableIssueReactionsClient Issue { get; private set; }
 
+        /// <summary>
+        /// Access GitHub's Reactions API for Issue Comments.
+        /// </summary>
+        /// <remarks>
+        /// Refer to the API documentation for more information: https://developer.github.com/v3/reactions/
+        /// </remarks>
         public IObservableIssueCommentReactionsClient IssueComment { get; private set; }
 
+        /// <summary>
+        /// Access GitHub's Reactions API for Pull Request Review Comments.
+        /// </summary>
+        /// <remarks>
+        /// Refer to the API documentation for more information: https://developer.github.com/v3/reactions/
+        /// </remarks>
         public IObservablePullRequestReviewCommentReactionsClient PullRequestReviewComment { get; private set; }
     }
 }
