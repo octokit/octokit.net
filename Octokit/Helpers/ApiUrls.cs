17e613786aa9a8762da1aae9453ccc547201b93f
maddin2016 <martin.scholz83@outlook.com>
2016-06-01 09:27:12 +0200

    add api url for IssueCommentReaction

--- Octokit/Helpers/ApiUrls.cs
+++ Octokit/Helpers/ApiUrls.cs
@@ -364,6 +364,18 @@ namespace Octokit
         }
 
         /// <summary>
+        /// Returns the <see cref="Uri"/> for the reaction of an specified issue.
+        /// </summary>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        /// <param name="number">The comment number</param>
+        /// <returns></returns>
+        public static Uri IssueCommentReactions(string owner, string name, int number)
+        {
+            return "repos/{0}/{1}/issues/comment/{2}/reactions".FormatUri(owner, name, number);
+        }
+
+        /// <summary>
         /// Returns the <see cref="Uri"/> for the specified comment.
         /// </summary>
         /// <param name="owner">The owner of the repository</param>
