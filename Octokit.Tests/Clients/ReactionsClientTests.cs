2ebe0eed8959232d24f2fc9b7468dce511f66f55
maddin2016 <martin.scholz83@outlook.com>
2016-06-01 11:58:02 +0200

    add unit test for reaction client

---
+++ Octokit.Tests/Clients/ReactionsClientTests.cs
@@ -0,0 +1,33 @@
+﻿using NSubstitute;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Octokit.Tests.Clients
+{
+    public class ReactionsClientTests
+    {
+        public class TheCtor
+        {
+            [Fact]
+            public void EnsuresNonNullArguments()
+            {
+                Assert.Throws<ArgumentNullException>(() => new ReactionsClient(null));
+            }
+        }
+
+        public class TheDeleteMethod
+        {
+            [Fact]
+            public async Task DeletesCorrectUrl()
+            {
+                var connection = Substitute.For<IApiConnection>();
+                var client = new ReactionsClient(connection);
+
+                await client.Delete(42);
+
+                connection.Received().Delete(Arg.Is<Uri>(u => u.ToString() == "reactions/42"));
+            }
+        }
+    }
+}
