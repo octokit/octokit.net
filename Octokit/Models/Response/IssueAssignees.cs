596c450eaf94b5bc7548917ebbde452996902c87
maddin2016 <martin.scholz83@outlook.com>
2016-06-03 08:44:52 +0200

    add constructor for IssueAssignees

--- Octokit/Models/Response/IssueAssignees.cs
+++ Octokit/Models/Response/IssueAssignees.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Octokit
 {
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class IssueAssignees : Issue
     {
+        public IssueAssignees() { }
+
         public IssueAssignees(Uri url, Uri htmlUrl, Uri commentsUrl, Uri eventsUrl, int number, ItemState state, string title, string body, User user, IReadOnlyList<Label> labels, User assignee, Milestone milestone, int comments, PullRequest pullRequest, DateTimeOffset? closedAt, DateTimeOffset createdAt, DateTimeOffset? updatedAt, int id, bool locked, Repository repository, IReadOnlyList<User> assignees)
             : base(url, htmlUrl, commentsUrl, eventsUrl, number, state, title, body, user, labels, assignee, milestone, comments, pullRequest, closedAt, createdAt, updatedAt, id, locked, repository)
         {
