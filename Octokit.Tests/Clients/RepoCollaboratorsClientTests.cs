4850b839067b39dc10a500812fded54b75d6bc98
Ryan Gribble <ryan.gribble@gmail.com>
2016-06-04 23:35:23 +1000

    Add Permissions attribute to User Update integration tests to ensure Permissions field is populated Add Accept header for this functionality, so it works on GHE 2.5 Fix impacted URL unit tests

--- Octokit.Tests/Clients/RepoCollaboratorsClientTests.cs
+++ Octokit.Tests/Clients/RepoCollaboratorsClientTests.cs
@@ -32,7 +32,7 @@ namespace Octokit.Tests.Clients
                 var client = new RepoCollaboratorsClient(connection);
 
                 client.GetAll("owner", "test");
-                connection.Received().GetAll<User>(Arg.Is<Uri>(u => u.ToString() == "repos/owner/test/collaborators"), Args.ApiOptions);
+                connection.Received().GetAll<User>(Arg.Is<Uri>(u => u.ToString() == "repos/owner/test/collaborators"), null, "application/vnd.github.ironman-preview+json", Args.ApiOptions);
             }
 
             [Fact]
@@ -51,7 +51,7 @@ namespace Octokit.Tests.Clients
                 client.GetAll("owner", "test", options);
 
                 connection.Received()
-                    .GetAll<User>(Arg.Is<Uri>(u => u.ToString() == "repos/owner/test/collaborators"), options);
+                    .GetAll<User>(Arg.Is<Uri>(u => u.ToString() == "repos/owner/test/collaborators"), null, "application/vnd.github.ironman-preview+json", options);
             }
 
             [Fact]
