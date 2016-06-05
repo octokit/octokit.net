df5d5b144f2a27caab4c0356e530d23d0d9cdd9c
Henrik Andersson <henrik.a.andersson@outlook.com>
2016-06-05 10:48:51 +1000

    Add UserGpgKeysClient

---
+++ Octokit/Models/Response/GpgKey.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit.Internal;
+
+namespace Octokit
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Gpg")]
+    public class GpgKey
+    {
+        public int Id { get; protected set; }
+        public int? PrimaryKeyId { get; protected set; }
+        public string KeyId { get; protected set; }
+        public string PublicKey { get; protected set; }
+        public IReadOnlyList<EmailAddress> Emails { get; protected set; }
+        public IReadOnlyList<GpgKey> Subkeys { get; protected set; }
+        public bool CanSign { get; protected set; }
+        [Parameter(Key = "can_encrypt_comms")]
+        public bool CanEncryptCommunications { get; protected set; }
+        public bool CanEncryptStorage { get; protected set; }
+        public bool CanCertify { get; protected set; }
+        public DateTimeOffset CreatedAt { get; protected set; }
+        public DateTimeOffset? ExpiresAt { get; protected set; }
+    }
+}
