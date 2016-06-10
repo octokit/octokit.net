0e96778929eb28ffe56c52a632d2b25991161143
aedampir@gmail.com <aedampir@gmail.com>
2016-06-10 23:40:30 +0700

    added new unit tests

--- Octokit.Tests/Clients/CommitsClientTests.cs
+++ Octokit.Tests/Clients/CommitsClientTests.cs
@@ -11,6 +11,28 @@ public class CommitsClientTests
     public class TheGetMethod
     {
         [Fact]
+        public async Task RequestsCorrectUrl()
+        {
+            var connection = Substitute.For<IApiConnection>();
+            var client = new CommitsClient(connection);
+
+            await client.Get("owner", "repo", "reference");
+
+            connection.Received().Get<Commit>(Arg.Is<Uri>(u => u.ToString() == "repos/owner/repo/git/commits/reference"));
+        }
+
+        [Fact]
+        public async Task RequestsCorrectUrlWithRepositoryId()
+        {
+            var connection = Substitute.For<IApiConnection>();
+            var client = new CommitsClient(connection);
+
+            await client.Get(1, "reference");
+
+            connection.Received().Get<Commit>(Arg.Is<Uri>(u => u.ToString() == "repositories/1/git/commits/reference"));
+        }
+
+        [Fact]
         public async Task EnsuresNonNullArguments()
         {
             var client = new CommitsClient(Substitute.For<IApiConnection>());
@@ -18,20 +40,14 @@ public class CommitsClientTests
             await Assert.ThrowsAsync<ArgumentNullException>(() => client.Get(null, "name", "reference"));
             await Assert.ThrowsAsync<ArgumentNullException>(() => client.Get("owner", null, "reference"));
             await Assert.ThrowsAsync<ArgumentNullException>(() => client.Get("owner", "name", null));
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => client.Get(1, null));
+
             await Assert.ThrowsAsync<ArgumentException>(() => client.Get("", "name", "reference"));
             await Assert.ThrowsAsync<ArgumentException>(() => client.Get("owner", "", "reference"));
             await Assert.ThrowsAsync<ArgumentException>(() => client.Get("owner", "name", ""));
-        }
 
-        [Fact]
-        public void RequestsCorrectUrl()
-        {
-            var connection = Substitute.For<IApiConnection>();
-            var client = new CommitsClient(connection);
-
-            client.Get("owner", "repo", "reference");
-
-            connection.Received().Get<Commit>(Arg.Is<Uri>(u => u.ToString() == "repos/owner/repo/git/commits/reference"), Arg.Any<Dictionary<string, string>>(), "application/vnd.github.cryptographer-preview+sha");
+            await Assert.ThrowsAsync<ArgumentException>(() => client.Get(1, ""));
         }
     }
 
@@ -48,9 +64,26 @@ public class CommitsClientTests
             client.Create("owner", "repo", newCommit);
 
             connection.Received().Post<Commit>(Arg.Is<Uri>(u => u.ToString() == "repos/owner/repo/git/commits"),
-                                               Arg.Is<NewCommit>(nc => nc.Message == "message"
-                                                                    && nc.Tree == "tree"
-                                                                    && nc.Parents.Count() == 2));
+                Arg.Is<NewCommit>(nc => nc.Message == "message"
+                    && nc.Tree == "tree"
+                    && nc.Parents.Count() == 2));
+        }
+
+        [Fact]
+        public void PostsToTheCorrectUrlWithRepositoryId()
+        {
+            var connection = Substitute.For<IApiConnection>();
+            var client = new CommitsClient(connection);
+
+            var parents = new List<string> { "sha-reference1", "sha-reference2" };
+            var newCommit = new NewCommit("message", "tree", parents);
+
+            client.Create(1, newCommit);
+
+            connection.Received().Post<Commit>(Arg.Is<Uri>(u => u.ToString() == "repositories/1/git/commits"),
+                Arg.Is<NewCommit>(nc => nc.Message == "message"
+                    && nc.Tree == "tree"
+                    && nc.Parents.Count() == 2));
         }
 
         [Fact]
@@ -59,9 +92,13 @@ public class CommitsClientTests
             var client = new CommitsClient(Substitute.For<IApiConnection>());
 
             var newCommit = new NewCommit("message", "tree", new[] { "parent1", "parent2" });
+
             await Assert.ThrowsAsync<ArgumentNullException>(() => client.Create(null, "name", newCommit));
             await Assert.ThrowsAsync<ArgumentNullException>(() => client.Create("owner", null, newCommit));
             await Assert.ThrowsAsync<ArgumentNullException>(() => client.Create("owner", "name", null));
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => client.Create(1, null));
+
             await Assert.ThrowsAsync<ArgumentException>(() => client.Create("", "name", newCommit));
             await Assert.ThrowsAsync<ArgumentException>(() => client.Create("owner", "", newCommit));
         }
