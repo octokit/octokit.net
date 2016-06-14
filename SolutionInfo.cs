9e9ac8eab8d9aea7b5a677f3beb0a1bad4774092
Brendan Forster <brendan@github.com>
2016-06-14 10:30:13 +1000

    version bump

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,11 +3,11 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProductAttribute("Octokit")]
-[assembly: AssemblyVersionAttribute("0.19.0")]
-[assembly: AssemblyFileVersionAttribute("0.19.0")]
+[assembly: AssemblyVersionAttribute("0.20.0")]
+[assembly: AssemblyFileVersionAttribute("0.20.0")]
 [assembly: ComVisibleAttribute(false)]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.19.0";
+        internal const string Version = "0.20.0";
     }
 }
