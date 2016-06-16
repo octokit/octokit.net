de00adc2d658460b99ed10b2a74c977176a4d3fe
aedampir@gmail.com <aedampir@gmail.com>
2016-06-16 15:13:17 +0700

    modified XML docs a little bit

--- Octokit.Reactive/Clients/ObservableReferencesClient.cs
+++ Octokit.Reactive/Clients/ObservableReferencesClient.cs
@@ -5,6 +5,12 @@ using Octokit.Reactive.Internal;
 
 namespace Octokit.Reactive
 {
+    /// <summary>
+    /// A client for GitHub's References API.
+    /// </summary>
+    /// <remarks>
+    /// See the <a href="http://developer.github.com/v3/git/refs/">References API documentation</a> for more information.
+    /// </remarks>
     public class ObservableReferencesClient : IObservableReferencesClient
     {
         readonly IReferencesClient _reference;
