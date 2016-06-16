fcab9e073caab3c0264acdc2547468ff2f593498
Alexander Efremov <aedampir@gmail.com>
2016-06-17 06:10:53 +0700

    cleared <returns> tags

--- Octokit.Reactive/Clients/IObservablePullRequestReviewCommentReactionsClient.cs
+++ Octokit.Reactive/Clients/IObservablePullRequestReviewCommentReactionsClient.cs
@@ -17,7 +17,7 @@ namespace Octokit.Reactive
         /// <param name="owner">The owner of the repository</param>
         /// <param name="name">The name of the repository</param>
         /// <param name="number">The comment id</param>        
-        /// <returns>An <see cref="IObservable{Reaction}"/> representing <see cref="Reaction"/>s for a specified pull request review comment.</returns>
+        /// <returns></returns>
         IObservable<Reaction> GetAll(string owner, string name, int number);
 
         /// <summary>
@@ -26,7 +26,7 @@ namespace Octokit.Reactive
         /// <remarks>https://developer.github.com/v3/reactions/#list-reactions-for-a-pull-request-review-comment</remarks>
         /// <param name="repositoryId">The ID of the repository</param>
         /// <param name="number">The comment id</param>        
-        /// <returns>An <see cref="IObservable{Reaction}"/> representing <see cref="Reaction"/>s for a specified pull request review comment.</returns>
+        /// <returns></returns>
         IObservable<Reaction> GetAll(int repositoryId, int number);
 
         /// <summary>
@@ -37,7 +37,7 @@ namespace Octokit.Reactive
         /// <param name="name">The name of the repository</param>
         /// <param name="number">The comment id</param>
         /// <param name="reaction">The reaction to create</param>
-        /// <returns>An <see cref="IObservable{Reaction}"/> representing created <see cref="Reaction"/> for a specified pull request review comment.</returns>
+        /// <returns></returns>
         IObservable<Reaction> Create(string owner, string name, int number, NewReaction reaction);
 
         /// <summary>
@@ -47,7 +47,7 @@ namespace Octokit.Reactive
         /// <param name="repositoryId">The owner of the repository</param>
         /// <param name="number">The comment id</param>
         /// <param name="reaction">The reaction to create</param>
-        /// <returns>An <see cref="IObservable{Reaction}"/> representing created <see cref="Reaction"/> for a specified pull request review comment.</returns>
+        /// <returns></returns>
         IObservable<Reaction> Create(int repositoryId, int number, NewReaction reaction);
     }
 }
--- Octokit.Reactive/Clients/ObservablePullRequestReviewCommentReactionsClient.cs
+++ Octokit.Reactive/Clients/ObservablePullRequestReviewCommentReactionsClient.cs
@@ -30,7 +30,7 @@ namespace Octokit.Reactive
         /// <param name="owner">The owner of the repository</param>
         /// <param name="name">The name of the repository</param>
         /// <param name="number">The comment id</param>        
-        /// <returns>An <see cref="IObservable{Reaction}"/> representing <see cref="Reaction"/>s for a specified pull request review comment.</returns>
+        /// <returns></returns>
         public IObservable<Reaction> GetAll(string owner, string name, int number)
         {
             Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
@@ -45,7 +45,7 @@ namespace Octokit.Reactive
         /// <remarks>https://developer.github.com/v3/reactions/#list-reactions-for-a-pull-request-review-comment</remarks>
         /// <param name="repositoryId">The ID of the repository</param>
         /// <param name="number">The comment id</param>        
-        /// <returns>An <see cref="IObservable{Reaction}"/> representing <see cref="Reaction"/>s for a specified pull request review comment.</returns>
+        /// <returns></returns>
         public IObservable<Reaction> GetAll(int repositoryId, int number)
         {
             return _connection.GetAndFlattenAllPages<Reaction>(ApiUrls.PullRequestReviewCommentReaction(repositoryId, number), null, AcceptHeaders.ReactionsPreview);
@@ -59,7 +59,7 @@ namespace Octokit.Reactive
         /// <param name="name">The name of the repository</param>
         /// <param name="number">The comment id</param>
         /// <param name="reaction">The reaction to create</param>
-        /// <returns>An <see cref="IObservable{Reaction}"/> representing created <see cref="Reaction"/> for a specified pull request review comment.</returns>
+        /// <returns></returns>
         public IObservable<Reaction> Create(string owner, string name, int number, NewReaction reaction)
         {
             Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
@@ -76,7 +76,7 @@ namespace Octokit.Reactive
         /// <param name="repositoryId">The owner of the repository</param>
         /// <param name="number">The comment id</param>
         /// <param name="reaction">The reaction to create</param>
-        /// <returns>An <see cref="IObservable{Reaction}"/> representing created <see cref="Reaction"/> for a specified pull request review comment.</returns>
+        /// <returns></returns>
         public IObservable<Reaction> Create(int repositoryId, int number, NewReaction reaction)
         {
             Ensure.ArgumentNotNull(reaction, "reaction");
