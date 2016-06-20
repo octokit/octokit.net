0d380183ddd71329af9849b6ba59ceaf759c3ef0
Devesh Khandelwal <devesh.khandelwal@outlook.com>
2016-06-20 14:27:50 +0530

    Signature Verification: models + tests.

---
+++ Octokit/Models/Response/VerificationReason.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Octokit
+{
+	public enum VerificationReason
+	{
+		ExpiredKey,
+		NotSigningKey,
+		GpgverifyUnavailable,
+		Unsigned,
+		UnknownSignatureType,
+		NoUser,
+		VerifiedEmail,
+		BadEmail,
+		UnknownKey,
+		MalformedSignature,
+		Invalid,
+		Valid
+	}
+}
+
