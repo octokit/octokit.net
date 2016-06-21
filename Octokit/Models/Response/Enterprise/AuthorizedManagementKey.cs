deb61aa5efa88af23fc2dc85e7fbb97f9ae96b0b
Ryan Gribble <ryan.gribble@gmail.com>
2016-06-22 07:54:05 +1000

    Implement AuthorizedManagementKey GetAll() Add() Delete()

---
+++ Octokit/Models/Response/Enterprise/AuthorizedManagementKey.cs
@@ -0,0 +1,24 @@
+﻿using Octokit.Internal;
+
+namespace Octokit
+{
+    public class AuthorizedManagementKey
+    {
+        public AuthorizedManagementKey()
+        { }
+
+        public AuthorizedManagementKey(string key, string prettyPrint, string comment)
+        {
+            Key = key;
+            PrettyPrint = prettyPrint;
+            Comment = comment;
+        }
+
+        public string Key { get; private set; }
+
+        //[Parameter(Key="pretty-print")]
+        public string PrettyPrint { get; private set; }
+
+        public string Comment { get; private set; }
+    }
+}
