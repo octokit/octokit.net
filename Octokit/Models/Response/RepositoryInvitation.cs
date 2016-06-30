9f9c5e0f48d7d8a85e3ce60e0a4fad3a32a01892
Martin Scholz <martin.scholz83@outlook.com>
2016-06-30 14:26:54 +0200

    create class RepositoryInvitation

---
+++ Octokit/Models/Response/RepositoryInvitation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Octokit
+{
+    public enum RepositoryInvitationPermissions
+    {
+        Read,
+        Write,
+        Admin
+    }
+    public class RepositoryInvitation
+    {
+        public RepositoryInvitation()
+        {
+        }
+
+        public RepositoryInvitation(int id, Repository repository, User invitee, User inviter, RepositoryInvitationPermissions permissions, DateTimeOffset createdAt, string url, string htmlUrl)
+        {
+            Id = id;
+            Repository = repository;
+            Invitee = invitee;
+            Inviter = inviter;
+            Permissions = permissions;
+            CreatedAt = createdAt;
+            Url = url;
+            HtmlUrl = htmlUrl;
+        }
+
+        public int Id { get; protected set; }
+
+        public Repository Repository { get; protected set; }
+
+        public User Invitee { get; protected set; }
+
+        public User Inviter { get; protected set; }
+
+        public RepositoryInvitationPermissions Permissions { get; protected set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public string Url { get; protected set; }
+
+        public string HtmlUrl { get; protected set; }
+    }
+}
