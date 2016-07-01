cd95700ed8aa0debd6d2bae69f2d2c931713a3d2
Martin Scholz <martin.scholz83@outlook.com>
2016-07-01 10:00:22 +0200

    add missing observable client

--- Octokit.Reactive/Clients/ObservableRepositoriesClient.cs
+++ Octokit.Reactive/Clients/ObservableRepositoriesClient.cs
@@ -42,6 +42,7 @@ namespace Octokit.Reactive
             Content = new ObservableRepositoryContentsClient(client);
             Merging = new ObservableMergingClient(client);
             Page = new ObservableRepositoryPagesClient(client);
+            Invitation = new ObservableRepositoryInvitationsClient(client);
         }
 
         /// <summary>
@@ -900,5 +901,13 @@ namespace Octokit.Reactive
         /// See the <a href="https://developer.github.com/v3/repos/pages/">Repository Pages API documentation</a> for more information.
         /// </remarks>
         public IObservableRepositoryPagesClient Page { get; private set; }
+
+        /// <summary>
+        /// A client for GitHub's Repository Invitations API.
+        /// </summary>
+        /// <remarks>
+        /// See the <a href="https://developer.github.com/v3/repos/invitations/">Repository Invitations API documentation</a> for more information.
+        /// </remarks>
+        public IObservableRepositoryInvitationsClient Invitation { get; private set; }        
     }
 }
--- Octokit/Models/Request/InvitationUpdate.cs
+++ Octokit/Models/Request/InvitationUpdate.cs
@@ -5,12 +5,9 @@ using System.Globalization;
 namespace Octokit
 {
     /// <summary>
-    /// Used to update a gist and its contents.
+    /// Used to update a invitation.
     /// </summary>
-    /// <remarks>
-    /// Note: All files from the previous version of the gist are carried over by default if not included in the
-    ///  object. Deletes can be performed by including the filename with a null object.
-    /// API docs: https://developer.github.com/v3/gists/
+    /// <remarks>    
     /// </remarks>
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class InvitationUpdate
