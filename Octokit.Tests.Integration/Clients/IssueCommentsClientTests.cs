6552b2e8220708f728dbbb504d94b7e3f9299714
Martin Scholz <martin.scholz83@outlook.com>
2016-07-04 14:28:58 +0200

    create integration test for issue comment client

--- Octokit.Tests.Integration/Clients/IssueCommentsClientTests.cs
+++ Octokit.Tests.Integration/Clients/IssueCommentsClientTests.cs
@@ -6,48 +6,59 @@ using System.Threading.Tasks;
 using Octokit;
 using Octokit.Tests.Integration;
 using Xunit;
+using Octokit.Tests.Integration.Helpers;
 
 public class IssueCommentsClientTests
 {
-    private readonly IIssueCommentsClient _issueCommentsClient;
-
-    public IssueCommentsClientTests()
+    public class TheCreateMethod : IDisposable
     {
-        var github = Helper.GetAuthenticatedClient();
-        _issueCommentsClient = github.Issue.Comment;
-    }
+        private readonly IGitHubClient _github;
+        private readonly RepositoryContext _context;
 
-    [IntegrationTest]
-    public async Task CanDeserializeIssueCommentWhenGettingAllForRepository()
-    {
-        var comments = await _issueCommentsClient.GetAllForRepository("alfhenrik-test", "repo-with-issue-comment-reactions");
+        public TheCreateMethod()
+        {
+            _github = Helper.GetAuthenticatedClient();
+            _context = _github.CreateRepositoryContext("public-repo").Result;
+        }
 
-        Assert.NotEmpty(comments);
-        var comment = comments[0];
-        Assert.NotNull(comment.Reactions);
-        Assert.Equal(3, comment.Reactions.TotalCount);
-        Assert.Equal(1, comment.Reactions.Plus1);
-        Assert.Equal(1, comment.Reactions.Hooray);
-        Assert.Equal(1, comment.Reactions.Heart);
-        Assert.Equal(0, comment.Reactions.Laugh);
-        Assert.Equal(0, comment.Reactions.Confused);
-        Assert.Equal(0, comment.Reactions.Minus1);
-    }
+        [IntegrationTest]
+        public async Task CanGetReactionPayload()
+        {
+            var newIssue = new NewIssue("a test issue") { Body = "A new unassigned issue" };
+            var issue = await _github.Issue.Create(_context.RepositoryOwner, _context.RepositoryName, newIssue);
 
-    [IntegrationTest]
-    public async Task CanDeserializeIssueComment()
-    {
-        var comments = await _issueCommentsClient.GetAllForIssue("alfhenrik-test", "repo-with-issue-comment-reactions", 1);
+            Assert.NotNull(issue);
 
-        Assert.NotEmpty(comments);
-        var comment = comments[0];
-        Assert.NotNull(comment.Reactions);
-        Assert.Equal(3, comment.Reactions.TotalCount);
-        Assert.Equal(1, comment.Reactions.Plus1);
-        Assert.Equal(1, comment.Reactions.Hooray);
-        Assert.Equal(1, comment.Reactions.Heart);
-        Assert.Equal(0, comment.Reactions.Laugh);
-        Assert.Equal(0, comment.Reactions.Confused);
-        Assert.Equal(0, comment.Reactions.Minus1);
+            var issueComment = await _github.Issue.Comment.Create(_context.RepositoryOwner, _context.RepositoryName, issue.Number, "A test comment");
+
+            Assert.NotNull(issueComment);
+
+            foreach (ReactionType reactionType in Enum.GetValues(typeof(ReactionType)))
+            {
+                var newReaction = new NewReaction(reactionType);
+
+                var reaction = await _github.Reaction.IssueComment.Create(_context.RepositoryOwner, _context.RepositoryName, issueComment.Id, newReaction);
+
+                Assert.IsType<Reaction>(reaction);
+                Assert.Equal(reactionType, reaction.Content);
+                Assert.Equal(issueComment.User.Id, reaction.User.Id);
+            }
+
+            var retrieved = await _github.Issue.Comment.Get(_context.RepositoryOwner, _context.RepositoryName, issueComment.Id);
+
+            Assert.True(retrieved.Id > 0);
+            Assert.Equal(6, retrieved.Reactions.TotalCount);
+            Assert.Equal(1, retrieved.Reactions.Plus1);
+            Assert.Equal(1, retrieved.Reactions.Hooray);
+            Assert.Equal(1, retrieved.Reactions.Heart);
+            Assert.Equal(1, retrieved.Reactions.Laugh);
+            Assert.Equal(1, retrieved.Reactions.Confused);
+            Assert.Equal(1, retrieved.Reactions.Minus1);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
     }
 }
