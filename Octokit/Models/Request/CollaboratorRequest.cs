24920c9e9f988434a7d5f2ccb7b312af044240c7
Martin Scholz <martin.scholz83@outlook.com>
2016-07-04 08:35:52 +0200

    add collaborator request model

---
+++ Octokit/Models/Request/CollaboratorRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace Octokit
+{
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class CollaboratorRequest
+    {
+        public CollaboratorRequest(Permission permissions)
+        {
+            Permissions = permissions;
+        }
+
+        public Permission Permissions { get; private set; }
+
+        internal string DebuggerDisplay
+        {
+            get
+            {
+                return string.Format(CultureInfo.InvariantCulture, "Permission: {0}", Permissions);
+            }
+        }
+    }
+}
