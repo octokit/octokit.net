85313478ea9d8fe3c620c723603aeee00535a275
Martin Scholz <martin.scholz83@outlook.com>
2016-07-05 15:57:36 +0200

    change constructor

--- Octokit/Models/Response/RepositoryInvitation.cs
+++ Octokit/Models/Response/RepositoryInvitation.cs
@@ -14,9 +14,7 @@ namespace Octokit
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class RepositoryInvitation
     {
-        public RepositoryInvitation()
-        {
-        }
+        public RepositoryInvitation() { }
 
         public RepositoryInvitation(int id, Repository repository, User invitee, User inviter, InvitationPermissionType permissions, DateTimeOffset createdAt, string url, string htmlUrl)
         {
