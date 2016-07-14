41c663f6af4e728e4c280e081e7847e175718118
aedampir@gmail.com <aedampir@gmail.com>
2016-07-14 20:49:42 +0700

    added new overloads

--- Octokit.Reactive/Clients/ObservableRepositoryPagesClient.cs
+++ Octokit.Reactive/Clients/ObservableRepositoryPagesClient.cs
@@ -141,5 +141,33 @@ namespace Octokit.Reactive
         {
             return _client.GetLatest(repositoryId).ToObservable();
         }
+
+        /// <summary>
+        /// Requests your site be built from the latest revision on the default branch for a given repository
+        /// </summary>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        ///  <remarks>
+        /// See the <a href="https://developer.github.com/v3/repos/pages/#request-a-page-build">API documentation</a> for more information.
+        /// </remarks>
+        public IObservable<PagesBuild> RequestPageBuild(string owner, string name)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, "owner");
+            Ensure.ArgumentNotNullOrEmptyString(name, "name");
+
+            return _client.RequestPageBuild(owner, name).ToObservable();
+        }
+
+        /// <summary>
+        /// Requests your site be built from the latest revision on the default branch for a given repository
+        /// </summary>
+        /// <param name="repositoryId">The ID of the repository</param>
+        ///  <remarks>
+        /// See the <a href="https://developer.github.com/v3/repos/pages/#request-a-page-build">API documentation</a> for more information.
+        /// </remarks>
+        public IObservable<PagesBuild> RequestPageBuild(int repositoryId)
+        {
+            return _client.RequestPageBuild(repositoryId).ToObservable();
+        }
     }
 }
--- Octokit/Clients/IRepositoryPagesClient.cs
+++ Octokit/Clients/IRepositoryPagesClient.cs
@@ -91,5 +91,24 @@ namespace Octokit
         /// See the <a href="https://developer.github.com/v3/repos/pages/#list-latest-pages-build">API documentation</a> for more information.
         /// </remarks>
         Task<PagesBuild> GetLatest(int repositoryId);
+
+        /// <summary>
+        /// Requests your site be built from the latest revision on the default branch for a given repository
+        /// </summary>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        ///  <remarks>
+        /// See the <a href="https://developer.github.com/v3/repos/pages/#request-a-page-build">API documentation</a> for more information.
+        /// </remarks>
+        Task<PagesBuild> RequestPageBuild(string owner, string name);
+
+        /// <summary>
+        /// Requests your site be built from the latest revision on the default branch for a given repository
+        /// </summary>
+        /// <param name="repositoryId">The ID of the repository</param>
+        ///  <remarks>
+        /// See the <a href="https://developer.github.com/v3/repos/pages/#request-a-page-build">API documentation</a> for more information.
+        /// </remarks>
+        Task<PagesBuild> RequestPageBuild(int repositoryId);
     }
 }
