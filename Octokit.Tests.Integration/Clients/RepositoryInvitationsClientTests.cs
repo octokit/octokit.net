40e36fc991062beb20885fa76d11eeff5f68aa24
maddin2016 <martin.scholz83@outlook.com>
2016-07-15 22:03:29 +0200

    change integration tests

--- Octokit.Tests.Integration/Clients/RepositoryInvitationsClientTests.cs
+++ Octokit.Tests.Integration/Clients/RepositoryInvitationsClientTests.cs
@@ -69,41 +69,7 @@ public class RepositoryInvitationsClientTests
                 Assert.NotNull(invitations.FirstOrDefault(i => i.Inviter.Login == response.Inviter.Login));
                 Assert.NotNull(invitations.FirstOrDefault(i => i.Invitee.Login == response.Invitee.Login));
                 Assert.NotNull(invitations.FirstOrDefault(i => i.Permissions == response.Permissions));
-                Assert.NotNull(invitations.FirstOrDefault(i => i.Repository.Id == response.Repository.Id));                
-            }
-        }
-    }
-
-    public class TheDeclineMethod
-    {
-        [IntegrationTest]
-        public async Task CanDeclineInvitation()
-        {
-            var github = Helper.GetAuthenticatedClient();
-            var repoName = Helper.MakeNameWithTimestamp("public-repo");
-
-            using (var context = await github.CreateRepositoryContext(new NewRepository(repoName)))
-            {
-                var fixture = github.Repository.Collaborator;
-                var permission = new CollaboratorRequest(Permission.Push);
-
-                // invite a collaborator
-                var response = await fixture.Invite(context.RepositoryOwner, context.RepositoryName, context.RepositoryOwner);
-
-                Assert.Equal(context.RepositoryOwner, response.Invitee.Login);
-                Assert.Equal(InvitationPermissionType.Write, response.Permissions);
-
-                var declineResponse = await github.Repository.Invitation.Decline(response.Id);
-
-                Assert.True(declineResponse);
-
-                //Assert.True(invitations.Count >= 1);
-                //Assert.NotNull(invitations.FirstOrDefault(i => i.CreatedAt == response.CreatedAt));
-                //Assert.NotNull(invitations.FirstOrDefault(i => i.Id == response.Id));
-                //Assert.NotNull(invitations.FirstOrDefault(i => i.Inviter.Login == response.Inviter.Login));
-                //Assert.NotNull(invitations.FirstOrDefault(i => i.Invitee.Login == response.Invitee.Login));
-                //Assert.NotNull(invitations.FirstOrDefault(i => i.Permissions == response.Permissions));
-                //Assert.NotNull(invitations.FirstOrDefault(i => i.Repository.Id == response.Repository.Id));
+                Assert.NotNull(invitations.FirstOrDefault(i => i.Repository.Id == response.Repository.Id));
             }
         }
     }
