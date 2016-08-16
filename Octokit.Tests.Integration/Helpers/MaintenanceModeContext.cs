4a945543f8a802dbd7eb24a675e82db4b28c2d14
Ryan Gribble <ryan.gribble@gmail.com>
2016-08-16 21:34:36 +1000

    Tidy up maintenance mode tests by using a context/destructor to manage the initial/end state of maintenance mode

---
+++ Octokit.Tests.Integration/Helpers/MaintenanceModeContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit.Reactive;
+
+namespace Octokit.Tests.Integration.Helpers
+{
+    internal sealed class MaintenanceModeContext : IDisposable
+    {
+        internal MaintenanceModeContext(IConnection connection, bool enabled)
+        {
+            _connection = connection;
+            _initialState = enabled;
+
+            // Ensure maintenance mode is in the desired state
+            EnterpriseHelper.SetMaintenanceMode(_connection, _initialState);
+        }
+
+        private IConnection _connection;
+
+        private bool _initialState;
+        
+        public void Dispose()
+        {
+            // Ensure maintenance mode is restored to desired state
+            EnterpriseHelper.SetMaintenanceMode(_connection, _initialState);
+        }
+    }
+}
