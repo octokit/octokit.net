fb85a7a4bfd9014359b6228ddf4bfbd4f55dfcd1
Ryan Gribble <ryan.gribble@gmail.com>
2016-08-19 20:27:33 +1000

    Add integration tests for observable client

--- Octokit.Tests.Integration/Reactive/Enterprise/ObservableEnterpriseManagementConsoleTests.cs
+++ Octokit.Tests.Integration/Reactive/Enterprise/ObservableEnterpriseManagementConsoleTests.cs
@@ -88,5 +88,59 @@ namespace Octokit.Tests.Integration
                 Assert.Equal(MaintenanceModeStatus.Scheduled, maintenance.Status);
             }
         }
+
+        private async Task DeleteManagementAuthorizedKeyIfExists(string keyData)
+        {
+            // Check if key already exists
+            var keys = await _github.Enterprise.ManagementConsole.GetAllAuthorizedKeys(
+                EnterpriseHelper.ManagementConsolePassword).ToList();
+
+            // Delete it if so
+            if (keys.Any(x => x.Key == keyData))
+            {
+                await _github.Enterprise.ManagementConsole.DeleteAuthorizedKey(
+                    new AuthorizedKeyRequest(keyData),
+                    EnterpriseHelper.ManagementConsolePassword).ToList();
+            }
+        }
+
+        [GitHubEnterpriseManagementConsoleTest]
+        public async Task CanGetAllAuthorizedKeys()
+        {
+            var keys = await _github.Enterprise.ManagementConsole.GetAllAuthorizedKeys(
+                EnterpriseHelper.ManagementConsolePassword).ToList();
+
+            Assert.NotNull(keys);
+            Assert.True(keys.Count > 0);
+            Assert.True(keys.All(x => !string.IsNullOrEmpty(x.Key)));
+            Assert.True(keys.All(x => !string.IsNullOrEmpty(x.PrettyPrint)));
+            Assert.True(keys.All(x => !string.IsNullOrEmpty(x.Comment)));
+        }
+
+        [GitHubEnterpriseManagementConsoleTest]
+        public async Task CanAddAndDeleteAuthorizedKey()
+        {
+            var keyData = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAjo4DqFKg8dOxiz/yjypmN1A4itU5QOStyYrfOFuTinesU/2zm9hqxJ5BctIhgtSHJ5foxkhsiBji0qrUg73Q25BThgNg8YFE8njr4EwjmqSqW13akx/zLV0GFFU0SdJ2F6rBldhi93lMnl0ex9swBqa3eLTY8C+HQGBI6MQUMw+BKp0oFkz87Kv+Pfp6lt/Uo32ejSxML1PT5hTH5n+fyl0ied+sRmPGZWmWoHB5Bc9mox7lB6I6A/ZgjtBqbEEn4HQ2/6vp4ojKfSgA4Mm7XMu0bZzX0itKjH1QWD9Lr5apV1cmZsj49Xf8SHucTtH+bq98hb8OOXEGFzplwsX2MQ== my-test-key";
+
+            // Ensure key doesn't already exist
+            await DeleteManagementAuthorizedKeyIfExists(keyData).ConfigureAwait(false);
+
+            // Add key
+            var keys = await _github.Enterprise.ManagementConsole.AddAuthorizedKey(
+                new AuthorizedKeyRequest(keyData),
+                EnterpriseHelper.ManagementConsolePassword).ToList();
+
+            // Ensure key was added
+            Assert.NotNull(keys);
+            Assert.Contains(keys, x => x.Key == keyData);
+
+            // Now delete it
+            var remainingKeys = await _github.Enterprise.ManagementConsole.DeleteAuthorizedKey(
+                new AuthorizedKeyRequest(keyData),
+                EnterpriseHelper.ManagementConsolePassword).ToList();
+
+            // Ensure key no longer exists
+            Assert.DoesNotContain(remainingKeys, x => x.Key == keyData);
+        }
     }
 }
