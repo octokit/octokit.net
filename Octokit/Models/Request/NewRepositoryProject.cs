732deb32c10ee81ae6e9df1e12252bd6f8ab99f8
Martin Scholz <martin.scholz83@outlook.com>
2016-09-29 12:33:14 +0200

    Add response for new project

---
+++ Octokit/Models/Request/NewRepositoryProject.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace Octokit
+{
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class NewRepositoryProject
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NewRepositoryProject"/> class.
+        /// </summary>
+        /// <param name="name">The name of the project.</param>
+        public NewRepositoryProject(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// Required. Gets or sets the name of the project
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Optional. Gets or sets the body of the project
+        /// </summary>
+        public string Body { get; set; }
+
+        internal string DebuggerDisplay
+        {
+            get
+            {
+                return string.Format(CultureInfo.InvariantCulture, "Name: {0}, Body: {1}", Name, Body);
+            }
+        }
+    }
+}
