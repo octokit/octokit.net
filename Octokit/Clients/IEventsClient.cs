754e9373d1ae31607b05055e22e29ef81afc68c1
Stanley Goldman <Stanley.Goldman@gmail.com>
2016-10-16 14:03:15 -0400

    Correcting convention unit tests

--- Octokit/Clients/IEventsClient.cs
+++ Octokit/Clients/IEventsClient.cs
@@ -78,7 +78,7 @@ namespace Octokit
         /// </remarks>
         /// <param name="owner">The owner of the repository</param>
         /// <param name="name">The name of the repository</param>
-        Task<IReadOnlyList<Activity>> GetAllIssuesForRepository(string owner, string name);
+        Task<IReadOnlyList<IssueEvent>> GetAllIssuesForRepository(string owner, string name);
 
         /// <summary>
         /// Gets all the issue events for a given repository
@@ -87,7 +87,7 @@ namespace Octokit
         /// http://developer.github.com/v3/activity/events/#list-issue-events-for-a-repository
         /// </remarks>
         /// <param name="repositoryId">The Id of the repository</param>
-        Task<IReadOnlyList<Activity>> GetAllIssuesForRepository(long repositoryId);
+        Task<IReadOnlyList<IssueEvent>> GetAllIssuesForRepository(long repositoryId);
 
         /// <summary>
         /// Gets all the issue events for a given repository
@@ -98,7 +98,7 @@ namespace Octokit
         /// <param name="owner">The owner of the repository</param>
         /// <param name="name">The name of the repository</param>
         /// <param name="options">Options for changing the API response</param>
-        Task<IReadOnlyList<Activity>> GetAllIssuesForRepository(string owner, string name, ApiOptions options);
+        Task<IReadOnlyList<IssueEvent>> GetAllIssuesForRepository(string owner, string name, ApiOptions options);
 
         /// <summary>
         /// Gets all the issue events for a given repository
@@ -108,7 +108,7 @@ namespace Octokit
         /// </remarks>
         /// <param name="repositoryId">The Id of the repository</param>
         /// <param name="options">Options for changing the API response</param>
-        Task<IReadOnlyList<Activity>> GetAllIssuesForRepository(long repositoryId, ApiOptions options);
+        Task<IReadOnlyList<IssueEvent>> GetAllIssuesForRepository(long repositoryId, ApiOptions options);
 
         /// <summary>
         /// Gets all the events for a given repository network
