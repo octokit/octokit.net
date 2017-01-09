2acb69e69d05c6f5b240a981ca0280ed732a655c
Sean Killeen <SeanKilleen@gmail.com>
2017-01-09 09:51:13 -0500

    Add GetObjectData override to AbuseException
    To include data for RetryAfterSeconds variable, and satisfy the build
    check.

--- Octokit/Exceptions/AbuseException.cs
+++ Octokit/Exceptions/AbuseException.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Runtime.Serialization;
+using System.Security;
 
 namespace Octokit
 {
@@ -95,6 +96,14 @@ namespace Octokit
             : base(info, context)
         {
         }
+
+        [SecurityCritical]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("RetryAfterSeconds", RetryAfterSeconds);
+        }
+
 #endif
     }
 }
