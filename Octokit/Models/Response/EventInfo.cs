6ddba155d4461342a83965141b4e03a595831c85
Linda Naughton <linda@careevolution.com>
2017-01-11 14:38:19 -0500

    Adding missing review types to event info.

--- Octokit/Models/Response/EventInfo.cs
+++ Octokit/Models/Response/EventInfo.cs
@@ -152,10 +152,10 @@ namespace Octokit
         /// </summary>
         Unlocked,
 
-        /// <summary>
-        /// The pull request’s branch was deleted.
-        /// </summary>
-        HeadRefDeleted,
+		/// <summary>
+		/// The pull request’s branch was deleted.
+		/// </summary>
+		HeadRefDeleted,
 
         /// <summary>
         /// The pull request’s branch was restored.
@@ -177,6 +177,26 @@ namespace Octokit
         /// Only provided for pull requests.
         /// </summary>
         Committed,
+
+		/// <summary>
+		/// The actor requested review from the subject on this pull request.
+		/// </summary>
+		ReviewRequested,
+
+		/// <summary>
+		/// The actor dismissed a review from the pull request.
+		/// </summary>
+		ReviewDismissed,
+
+		/// <summary>
+		/// The actor removed the review request for the subject on this pull request.
+		/// </summary>
+		ReviewRequestRemoved,
+
+		/// <summary>
+		/// Not documented in GitHub API.
+		/// </summary>
+		BaseRefChanged,
 
         /// <summary>
         /// The issue was referenced from another issue.
