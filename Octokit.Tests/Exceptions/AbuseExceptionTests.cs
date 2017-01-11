4cec3a6532191747ee3c993805d90a7a56d03314
Sean Killeen <SeanKilleen@gmail.com>
2017-01-11 16:30:43 -0500

    Introduce AbuseException (#1528)
    * Add some tests to be completed
    
    * Actually fail the tests
    
    * Create AbuseException class
    
    Copied from ForbiddenException and then gone over to inherit from it.
    
    * Actually add AbuseException to csproj
    
    * Update test file
    
    * Ran .\build FixProjects
    
    * Test updates
    
    * Default message update for AbuseException
    
    * Separate the exception creation logic
    
    * Remove message assertion -- doesn't matter here
    
    * Additional test for abuse message
    
    * Remove unnecessary variable assignment
    
    * Failing test for unsafe request
    
    * Attempt to fix test
    
    Still broken -- I don't think empty strings count to trigger the default
    message
    
    * Remove test that will always fail due to another issue
    
    Opened #1529 to explore this.
    
    * New tests (some failing)
    
    * Passing tests are, like, better than failing tests.
    
    * Last passing test
    
    * Cleanup
    
    * Add test for zero value and fix code
    
    Lol boundary cases.
    
    * cleanup
    
    * Mark ParseRetryAfterSeconds as static
    
    * Add GetObjectData override to AbuseException
    
    To include data for RetryAfterSeconds variable, and satisfy the build
    check.
    
    * Add back failing test & skip it
    
    * Change to nullable int with null default
    
    * Fix tests around nullable default
    
    * whitespace fixes
    
    * Compact the logic; tests still pass
    
    * Invert the if statements for compactness / clarity
    
    * Test subclasses & reformatting
    
    * Test name changes
    
    * Whitespace fix
    
    * Remove redundant line

---
+++ Octokit.Tests/Exceptions/AbuseExceptionTests.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Octokit.Internal;
+using Xunit;
+
+namespace Octokit.Tests.Exceptions
+{
+    public class AbuseExceptionTests
+    {
+        public class TheConstructor
+        {
+            public class Message
+            {
+                [Fact]
+                public void ContainsAbuseMessageFromApi()
+                {
+                    const string responseBody = "{\"message\":\"You have triggered an abuse detection mechanism. Please wait a few minutes before you try again.\"," +
+                                                "\"documentation_url\":\"https://developer.github.com/v3/#abuse-rate-limits\"}";
+
+                    var response = new Response(
+                        HttpStatusCode.Forbidden,
+                        responseBody,
+                        new Dictionary<string, string>(),
+                        "application/json");
+
+                    var abuseException = new AbuseException(response);
+
+                    Assert.Equal("You have triggered an abuse detection mechanism. Please wait a few minutes before you try again.", abuseException.ApiError.Message);
+                }
+
+                [Fact]
+                public void HasDefaultMessage()
+                {
+                    var response = new Response(HttpStatusCode.Forbidden, null, new Dictionary<string, string>(), "application/json");
+                    var abuseException = new AbuseException(response);
+
+                    Assert.Equal("Request Forbidden - Abuse Detection", abuseException.Message);
+                }
+            }
+
+            public class RetryAfterSeconds
+            {
+                public class HappyPath
+                {
+                    [Fact]
+                    public void WithRetryAfterHeader_PopulatesRetryAfterSeconds()
+                    {
+                        var headerDictionary = new Dictionary<string, string> { { "Retry-After", "30" } };
+
+                        var response = new Response(HttpStatusCode.Forbidden, null, headerDictionary, "application/json");
+                        var abuseException = new AbuseException(response);
+
+                        Assert.Equal(30, abuseException.RetryAfterSeconds);
+                    }
+
+                    [Fact]
+                    public void WithZeroHeaderValue_RetryAfterSecondsIsZero()
+                    {
+                        var headerDictionary = new Dictionary<string, string> { { "Retry-After", "0" } };
+
+                        var response = new Response(HttpStatusCode.Forbidden, null, headerDictionary, "application/json");
+                        var abuseException = new AbuseException(response);
+
+                        Assert.Equal(0, abuseException.RetryAfterSeconds);
+                    }
+                }
+
+                public class UnhappyPaths
+                {
+                    [Fact]
+                    public void NoRetryAfterHeader_RetryAfterSecondsIsSetToTheDefaultOfNull()
+                    {
+                        var headerDictionary = new Dictionary<string, string>();
+
+                        var response = new Response(HttpStatusCode.Forbidden, null, headerDictionary, "application/json");
+                        var abuseException = new AbuseException(response);
+
+                        Assert.False(abuseException.RetryAfterSeconds.HasValue);
+                    }
+
+                    [Theory]
+                    [InlineData(null)]
+                    [InlineData("")]
+                    [InlineData("    ")]
+                    public void EmptyHeaderValue_RetryAfterSecondsDefaultsToNull(string emptyValueToTry)
+                    {
+                        var headerDictionary = new Dictionary<string, string> { { "Retry-After", emptyValueToTry } };
+
+                        var response = new Response(HttpStatusCode.Forbidden, null, headerDictionary, "application/json");
+                        var abuseException = new AbuseException(response);
+
+                        Assert.False(abuseException.RetryAfterSeconds.HasValue);
+                    }
+
+                    [Fact]
+                    public void NonParseableIntHeaderValue_RetryAfterSecondsDefaultsToNull()
+                    {
+                        var headerDictionary = new Dictionary<string, string> { { "Retry-After", "ABC" } };
+
+                        var response = new Response(HttpStatusCode.Forbidden, null, headerDictionary, "application/json");
+                        var abuseException = new AbuseException(response);
+
+                        Assert.False(abuseException.RetryAfterSeconds.HasValue);
+                    }
+
+                    [Fact]
+                    public void NegativeHeaderValue_RetryAfterSecondsDefaultsToNull()
+                    {
+                        var headerDictionary = new Dictionary<string, string> { { "Retry-After", "-123" } };
+
+                        var response = new Response(HttpStatusCode.Forbidden, null, headerDictionary, "application/json");
+                        var abuseException = new AbuseException(response);
+
+                        Assert.False(abuseException.RetryAfterSeconds.HasValue);
+                    }
+                }
+            }
+        }
+    }
+}
