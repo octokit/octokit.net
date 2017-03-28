d28633aa4a6be49ef316b0489dacfc91c22762a5
Mickaël Derriey <mderriey@gmail.com>
2017-03-29 09:33:19 +1100

    revert overriding the target framework when building the solution

--- build/Tasks/Build.cs
+++ build/Tasks/Build.cs
@@ -1,4 +1,3 @@
-using Cake.Common;
 using Cake.Common.Tools.DotNetCore;
 using Cake.Common.Tools.DotNetCore.Build;
 using Cake.Core;
@@ -9,17 +8,10 @@ public class Build : FrostingTask<Context>
 {
     public override void Run(Context context)
     {
-        var settings = new DotNetCoreBuildSettings
+        context.DotNetCoreBuild("./Octokit.sln", new DotNetCoreBuildSettings
         {
             Configuration = context.Configuration,
             ArgumentCustomization = args => args.Append("/p:Version={0}", context.Version.GetSemanticVersion())
-        };
-
-        if (!context.IsRunningOnWindows())
-        {
-            settings.Framework = "netstandard1.1";
-        }
-
-        context.DotNetCoreBuild("./Octokit.sln", settings);
+        });
     }
 }
