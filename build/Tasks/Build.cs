b9c89c56650074d643f7141918da64444df4b177
Ryan Gribble <ryan.gribble@gmail.com>
2017-03-28 23:30:00 +1000

    only build netstandard framework on non windows

--- build/Tasks/Build.cs
+++ build/Tasks/Build.cs
@@ -1,3 +1,4 @@
+using Cake.Common;
 using Cake.Common.Tools.DotNetCore;
 using Cake.Common.Tools.DotNetCore.Build;
 using Cake.Core;
@@ -8,10 +9,17 @@ public class Build : FrostingTask<Context>
 {
     public override void Run(Context context)
     {
-        context.DotNetCoreBuild("./Octokit.sln", new DotNetCoreBuildSettings
+        var settings = new DotNetCoreBuildSettings
         {
             Configuration = context.Configuration,
             ArgumentCustomization = args => args.Append("/p:Version={0}", context.Version.GetSemanticVersion())
-        });
+        };
+
+        if (!context.IsRunningOnWindows())
+        {
+            settings.Framework = "netstandard1.1";
+        }
+
+        context.DotNetCoreBuild("./Octokit.sln", settings);
     }
 }
