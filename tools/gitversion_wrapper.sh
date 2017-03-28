1e6ead8e235ec6b74e2add5c43ae2892f566be21
Ryan Gribble <ryan.gribble@gmail.com>
2017-03-28 20:47:46 +1000

    let's see if we are running the script at all!

--- tools/gitversion_wrapper.sh
+++ tools/gitversion_wrapper.sh
@@ -1,4 +1,4 @@
 echo == Octokit.net GitVersion wrapper ==
 
 echo mono ./tools/GitVersion.CommandLine/tools/GitVersion.exe "$@"
-mono ./tools/GitVersion.CommandLine/tools/GitVersion.exe "$@"
+# mono ./tools/GitVersion.CommandLine/tools/GitVersion.exe "$@"
