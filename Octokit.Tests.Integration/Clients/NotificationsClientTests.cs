8c0c09ac4f9a91341a1c40326688a20e7e13beee
Ryan Gribble <ryan.gribble@gmail.com>
2017-04-11 21:48:19 +1000

    add integration tests for MarkRead methods

---
+++ Octokit.Tests.Integration/Clients/NotificationsClientTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Octokit.Tests.Integration;
+using Xunit;
+
+public class NotificationsClientTests
+{
+    public class TheMarkAsReadMethod
+    {
+        [IntegrationTest]
+        public async Task MarksNotificationsRead()
+        {
+            var github = Helper.GetAuthenticatedClient();
+
+            await github.Activity.Notifications.MarkAsRead();
+        }
+    }
+
+    public class TheMarkAsReadForRepositoryMethod
+    {
+        [IntegrationTest]
+        public async Task MarksNotificationsRead()
+        {
+            var owner = "octokit";
+            var repo = "octokit.net";
+
+            var github = Helper.GetAuthenticatedClient();
+
+            await github.Activity.Notifications.MarkAsReadForRepository(owner, repo);
+        }
+
+        [IntegrationTest]
+        public async Task MarksNotificationsReadForRepositoryId()
+        {
+            var repositoryId = 7528679;
+
+            var github = Helper.GetAuthenticatedClient();
+
+            await github.Activity.Notifications.MarkAsReadForRepository(repositoryId);
+        }
+    }
+}
