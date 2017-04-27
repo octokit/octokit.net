25cd1602d0ed804c7c95d237c3c089bc817f06c1
Kristian Hellang <kristian.hellang@iris.no>
2017-04-27 13:49:38 +0200

    Added tests

---
+++ Octokit.Tests/Models/StringEnumTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Xunit;
+
+namespace Octokit.Tests.Models
+{
+    public class StringEnumTests
+    {
+        public class TheConstructor
+        {
+            [Fact]
+            public void ShouldSetValue()
+            {
+                var stringEnum = new StringEnum<AccountType>("User");
+
+                Assert.Equal("User", stringEnum.Value);
+            }
+
+            [Fact]
+            public void ShouldSetValueAndParsedValue()
+            {
+                var stringEnum = new StringEnum<AccountType>(AccountType.Bot);
+
+                Assert.Equal("Bot", stringEnum.Value);
+                Assert.Equal(AccountType.Bot, stringEnum.ParsedValue);
+            }
+
+            [Fact]
+            public void ShouldThrowForInvalidEnumValue()
+            {
+                Assert.Throws<ArgumentException>(() => new StringEnum<AccountType>((AccountType) 1337));
+            }
+        }
+
+        public class TheParsedValueProperty
+        {
+            [Theory]
+            [InlineData("")]
+            [InlineData(null)]
+            [InlineData("Cow")]
+            public void ShouldThrowForInvalidValue(string value)
+            {
+                var stringEnum = new StringEnum<AccountType>(value);
+
+                Assert.Throws<ArgumentException>(() => stringEnum.ParsedValue);
+            }
+        }
+
+        public class TheTryParseMethod
+        {
+            [Fact]
+            public void ShouldReturnTrueForValidValue()
+            {
+                var stringEnum = new StringEnum<AccountType>("Bot");
+
+                AccountType type;
+                var result = stringEnum.TryParse(out type);
+
+                Assert.True(result);
+            }
+
+            [Theory]
+            [InlineData("")]
+            [InlineData(null)]
+            [InlineData("Cow")]
+            public void ShouldReturnFalseForInvalidValue(string value)
+            {
+                var stringEnum = new StringEnum<AccountType>(value);
+
+                AccountType type;
+                var result = stringEnum.TryParse(out type);
+
+                Assert.False(result);
+            }
+        }
+
+        public class TheImplicitConversionOperator
+        {
+            [Fact]
+            public void ShouldSetValue()
+            {
+                StringEnum<AccountType> stringEnum = "User";
+
+                Assert.Equal("User", stringEnum.Value);
+            }
+
+            [Fact]
+            public void ShouldSetValueAndParsedValue()
+            {
+                StringEnum<AccountType> stringEnum = AccountType.Bot;
+
+                Assert.Equal("Bot", stringEnum.Value);
+                Assert.Equal(AccountType.Bot, stringEnum.ParsedValue);
+            }
+
+            [Fact]
+            public void ShouldThrowForInvalidEnumValue()
+            {
+                StringEnum<AccountType> stringEnum;
+                Assert.Throws<ArgumentException>(() => stringEnum = (AccountType) 1337);
+            }
+        }
+
+        public class TheEqualityOperator
+        {
+            [Fact]
+            public void IsCaseInsensitive()
+            {
+                var first = new StringEnum<AccountType>("bot");
+                var second = new StringEnum<AccountType>("BoT");
+
+                Assert.True(first == second);
+            }
+        }
+    }
+}
