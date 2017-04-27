4cf1ca8b590467134afaf3c0a396f3b15be63c59
Kristian Hellang <kristian.hellang@iris.no>
2017-04-27 13:49:00 +0200

    Added StringEnum<TEnum>

---
+++ Octokit/Models/Response/StringEnum.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace Octokit
+{
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    [SuppressMessage("Microsoft.Naming", "CA1711:IdentifiersShouldNotHaveIncorrectSuffix")]
+    public struct StringEnum<TEnum> : IEquatable<StringEnum<TEnum>>
+        where TEnum : struct
+    {
+        private readonly string _value;
+
+        private TEnum? _parsedValue;
+
+        public StringEnum(TEnum parsedValue)
+        {
+            if (!Enum.IsDefined(typeof(TEnum), parsedValue))
+            {
+                throw GetArgumentException(parsedValue.ToString());
+            }
+
+            _value = parsedValue.ToString();
+            _parsedValue = parsedValue;
+        }
+
+        public StringEnum(string value)
+        {
+            _value = value;
+            _parsedValue = null;
+        }
+
+        public string Value
+        {
+            get { return _value; }
+        }
+
+        public TEnum ParsedValue
+        {
+            get { return _parsedValue ?? (_parsedValue = ParseValue()).Value; }
+        }
+
+        internal string DebuggerDisplay
+        {
+            get { return Value; }
+        }
+
+        public bool TryParse(out TEnum value)
+        {
+            if (_parsedValue.HasValue)
+            {
+                // the value has been parsed already.
+                value = _parsedValue.Value;
+                return true;
+            }
+
+            if (string.IsNullOrEmpty(Value))
+            {
+                value = default(TEnum);
+                return false;
+            }
+
+            if (Enum.TryParse(Value, ignoreCase: true, result: out value))
+            {
+                // cache the parsed value for subsequent calls.
+                _parsedValue = value;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Equals(StringEnum<TEnum> other)
+        {
+            return string.Equals(Value, other.Value, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            return obj is StringEnum<TEnum> && Equals((StringEnum<TEnum>) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value != null ? StringComparer.OrdinalIgnoreCase.GetHashCode(Value) : 0;
+        }
+
+        public static bool operator ==(StringEnum<TEnum> left, StringEnum<TEnum> right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(StringEnum<TEnum> left, StringEnum<TEnum> right)
+        {
+            return !left.Equals(right);
+        }
+
+        public static implicit operator StringEnum<TEnum>(string value)
+        {
+            return new StringEnum<TEnum>(value);
+        }
+
+        public static implicit operator StringEnum<TEnum>(TEnum parsedValue)
+        {
+            return new StringEnum<TEnum>(parsedValue);
+        }
+
+        public override string ToString()
+        {
+            return Value ?? "null";
+        }
+
+        private TEnum ParseValue()
+        {
+            TEnum value;
+            if (TryParse(out value))
+            {
+                return value;
+            }
+
+            throw GetArgumentException(ToString());
+        }
+
+        private static ArgumentException GetArgumentException(string value)
+        {
+            return new ArgumentException(string.Format(
+                CultureInfo.InvariantCulture,
+                "Value '{0}' is not a valid '{1}' enum value.",
+                value,
+                typeof(TEnum).Name));
+        }
+    }
+}
