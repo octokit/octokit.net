72d59191f921d320e7da2bdcb0a38b54088a584b
Henrik Andersson <henrik.a.andersson@outlook.com>
2017-07-24 17:21:52 +1000

    Add methods to preview a collaborators permission

---
+++ Octokit/Models/Response/CollaboratorPermission.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace Octokit
+{
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class CollaboratorPermission
+    {
+        public CollaboratorPermissions Permission { get; protected set; }
+        public User User { get; protected set; }
+
+        internal string DebuggerDisplay => $"User: {User.Id} Permission: {Permission}";
+    }
+}
