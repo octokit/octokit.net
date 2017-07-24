486273d8544e0bd06c8b4fa35d6dae550cb3d2bd
Henrik Andersson <henrik.a.andersson@outlook.com>
2017-07-25 07:11:37 +1000

    Fix convention test failure

--- Octokit/Models/Response/CollaboratorPermission.cs
+++ Octokit/Models/Response/CollaboratorPermission.cs
@@ -5,7 +5,7 @@ namespace Octokit
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class CollaboratorPermission
     {
-        public CollaboratorPermissions Permission { get; protected set; }
+        public StringEnum<CollaboratorPermissions> Permission { get; protected set; }
         public User User { get; protected set; }
 
         internal string DebuggerDisplay => $"User: {User.Id} Permission: {Permission}";
