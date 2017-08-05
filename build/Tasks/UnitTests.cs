ec8b202da5fe2aa58e9fe56cac7d6aeacff48954
Mickaël Derriey <mderriey@gmail.com>
2017-08-05 20:14:41 +1000

    Run tests against netcoreapp1.0 only when not on Windows

--- build/Tasks/UnitTests.cs
+++ build/Tasks/UnitTests.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using Cake.Common;
 using Cake.Common.Diagnostics;
 using Cake.Common.Tools.DotNetCore;
 using Cake.Common.Tools.DotNetCore.Test;
@@ -9,15 +10,25 @@ public sealed class UnitTests : FrostingTask<Context>
 {
     public override void Run(Context context)
     {
+        var testSettings = new DotNetCoreTestSettings
+        {
+            Configuration = context.Configuration,
+            NoBuild = true,
+            Verbose = false
+        };
+
+        if (!context.IsRunningOnWindows())
+        {
+            var testFramework = "netcoreapp1.0";
+
+            context.Information($"Running tests against {testFramework} only as we're not on Windows.");
+            testSettings.Framework = testFramework;
+        }
+
         foreach (var project in context.Projects.Where(x => x.UnitTests))
         {
             context.Information("Executing Unit Tests Project {0}...", project.Name);
-            context.DotNetCoreTest(project.Path.FullPath, new DotNetCoreTestSettings
-            {
-                Configuration = context.Configuration,
-                NoBuild = true,
-                Verbose = false
-            });
+            context.DotNetCoreTest(project.Path.FullPath, testSettings);
         }
     }
 }
