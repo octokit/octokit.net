c7c6653303c4aff14dfd12723687e27654e4314e
Henrik Andersson <henrik.a.andersson@outlook.com>
2017-08-08 06:40:42 +1000

    Move new enum to it's own correct file and location

--- Octokit/Clients/OrganizationMembersClient.cs
+++ Octokit/Clients/OrganizationMembersClient.cs
@@ -37,20 +37,6 @@ namespace Octokit
         Member
     }
 
-    public enum OrganizationMembershipRole
-    {
-        [Parameter(Value = "direct_member")]
-        DirectMember,
-        [Parameter(Value = "admin")]
-        Admin,
-        [Parameter(Value = "billing_manager")]
-        BillingManager,
-        [Parameter(Value = "hiring_manager")]
-        HiringManager,
-        [Parameter(Value = "reinstate")]
-        Reinstate
-    }
-
     /// <summary>
     /// A client for GitHub's Organization Members API.
     /// </summary>
