c8ff57b24ca092d83f64027ee48bb6aae92290b4
Philippe Miossec <pmiossec@gmail.com>
2017-10-29 02:51:37 +0200

    Add possibility to configure GitHubClient timeout (#963) (#1693)
    * Add possibility to configure GitHubClient timeout (#963)
    
    A first attempt to fix the problem describe in #963 by adding a possibility
    to extend the default timeout value (100s)
    that is too short to be able to post assets in github release.
    
    * Rename to SetRequestTimeout
    Make comments consistent

--- Octokit/Http/IHttpClient.cs
+++ Octokit/Http/IHttpClient.cs
@@ -19,5 +19,12 @@ namespace Octokit.Internal
         /// <param name="cancellationToken">Used to cancel the request</param>
         /// <returns>A <see cref="Task" /> of <see cref="IResponse"/></returns>
         Task<IResponse> Send(IRequest request, CancellationToken cancellationToken);
+
+
+        /// <summary>
+        /// Set the GitHub Api request timeout.
+        /// </summary>
+        /// <param name="timeout">The Timeout value</param>
+        void SetRequestTimeout(TimeSpan timeout);
     }
 }
