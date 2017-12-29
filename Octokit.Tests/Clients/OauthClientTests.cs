de3b60e5748bee10f1574330518466be868850bd
Ryan Gribble <ryan.gribble@gmail.com>
2017-12-29 22:50:31 +1000

    add test for GHE URL on oauth CreateAccessToken

--- Octokit.Tests/Clients/OauthClientTests.cs
+++ Octokit.Tests/Clients/OauthClientTests.cs
@@ -95,6 +95,40 @@ public class OauthClientTests
         }
 
         [Fact]
+        public async Task PostsWithCorrectBodyAndContentTypeForGHE()
+        {
+            var responseToken = new OauthToken(null, null, null);
+            var response = Substitute.For<IApiResponse<OauthToken>>();
+            response.Body.Returns(responseToken);
+            var connection = Substitute.For<IConnection>();
+            connection.BaseAddress.Returns(new Uri("https://example.com/api/v3"));
+            Uri calledUri = null;
+            FormUrlEncodedContent calledBody = null;
+            Uri calledHostAddress = null;
+            connection.Post<OauthToken>(
+                Arg.Do<Uri>(uri => calledUri = uri),
+                Arg.Do<object>(body => calledBody = body as FormUrlEncodedContent),
+                "application/json",
+                null,
+                Arg.Do<Uri>(uri => calledHostAddress = uri))
+                .Returns(_ => Task.FromResult(response));
+            var client = new OauthClient(connection);
+
+            var token = await client.CreateAccessToken(new OauthTokenRequest("secretid", "secretsecret", "code")
+            {
+                RedirectUri = new Uri("https://example.com/foo")
+            });
+
+            Assert.Same(responseToken, token);
+            Assert.Equal("login/oauth/access_token", calledUri.ToString());
+            Assert.NotNull(calledBody);
+            Assert.Equal("https://example.com/", calledHostAddress.ToString());
+            Assert.Equal(
+                "client_id=secretid&client_secret=secretsecret&code=code&redirect_uri=https%3A%2F%2Fexample.com%2Ffoo",
+                await calledBody.ReadAsStringAsync());
+        }
+
+        [Fact]
         public async Task DeserializesOAuthScopeFormat()
         {
             var responseText =
