43132da9d0e17dca3ffeb5f88477ef0d6478be70
Itai Bar-Haim <itai.barhaim@applitools.com>
2018-01-29 18:52:43 +0200

    added integration test for InstallationsClient.

---
+++ Octokit.Tests.Integration/Clients/InstallationsClientTests.cs
@@ -0,0 +1,42 @@
+﻿using Octokit.Tests.Integration.Helpers;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Octokit.Tests.Integration.Clients
+{
+    public class InstallationsClientTests
+    {
+        public class TheGetAllMethod : IDisposable
+        {
+            IGitHubClient _github;
+            RepositoryContext _context;
+
+            public TheGetAllMethod()
+            {
+                _github = Helper.GetAuthenticatedClient();
+                var repoName = Helper.MakeNameWithTimestamp("public-repo");
+
+                _context = _github.CreateRepositoryContext(new NewRepository(repoName)).Result;
+            }
+
+            [IntegrationTest]
+            public async Task GetsAllInstallations()
+            {
+                var result = await _github.Installation.GetAll();
+
+                // TODO - find a way to run this test, and see the results.
+
+                //Assert.Equal(2, result.Count);
+                //Assert.True(result.FirstOrDefault(x => x.Id == card1.Id).Id == card1.Id);
+                //Assert.True(result.FirstOrDefault(x => x.Id == card2.Id).Id == card2.Id);
+            }
+
+            public void Dispose()
+            {
+                if (_context != null)
+                    _context.Dispose();
+            }
+        }
+    }
+}
