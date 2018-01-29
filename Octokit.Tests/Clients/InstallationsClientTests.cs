7189a504aa2a7a5df6ab94785a24017381038d05
Itai Bar-Haim <itai.barhaim@applitools.com>
2018-01-29 18:09:26 +0200

    added InstallationsClient tests.

--- Octokit.Tests/Clients/InstallationsClientTests.cs
+++ Octokit.Tests/Clients/InstallationsClientTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NSubstitute;
+using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Octokit.Tests.Clients
@@ -12,6 +14,50 @@ namespace Octokit.Tests.Clients
             {
                 Assert.Throws<ArgumentNullException>(() => new InstallationsClient(null));
             }
+
+            public class TheGetAllMethod
+            {
+                [Fact]
+                public async Task EnsuresNonNullArguments()
+                {
+                    var connection = Substitute.For<IApiConnection>();
+                    var client = new InstallationsClient(connection);
+
+                    Assert.ThrowsAsync<ArgumentNullException>(() => client.GetAll(null));
+                }
+
+                [Fact]
+                public async Task RequestsCorrectUrl()
+                {
+                    var connection = Substitute.For<IApiConnection>();
+                    var client = new InstallationsClient(connection);
+
+                    client.GetAll();
+
+                    connection.Received().GetAll<Installation>(Arg.Is<Uri>(u => u.ToString() == "app/installations"), null, AcceptHeaders.MachineManPreview);
+                }
+
+
+                [Fact]
+                public void RequestsTheCorrectUrlWithApiOptions()
+                {
+                    var connection = Substitute.For<IApiConnection>();
+                    var client = new InstallationsClient(connection);
+
+                    var options = new ApiOptions
+                    {
+                        PageSize = 1,
+                        PageCount = 1,
+                        StartPage = 1
+                    };
+
+                    client.GetAll(options);
+
+                    connection.Received().GetAll<Installation>(Arg.Is<Uri>(u => u.ToString() == "app/installations"), null, AcceptHeaders.MachineManPreview, options);
+                }
+
+            }
+
         }
     }
 }
