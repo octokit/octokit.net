595cc1a04a488e58f707a3288aa8a62fdb13393b
Cyberboss <Cyberboss@users.noreply.github.com>
2018-02-22 12:14:50 -0500

    Adds PullRequestReviewEventPayload

---
+++ Octokit/Models/Response/ActivityPayloads/PullRequestReviewEventPayload.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace Octokit
+{
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class PullRequestReviewEventPayload : ActivityPayload
+    {
+        public string Action { get; protected set; }
+        public PullRequest PullRequest { get; protected set; }
+        public PullRequestReview Review { get; protected set; }
+    }
+}
