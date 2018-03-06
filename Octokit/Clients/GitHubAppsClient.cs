6bd5ac4c0f98be2e56814089ca466ca516bb191f
Ryan Gribble <ryan.gribble@gmail.com>
2018-03-06 21:10:29 +1000

    Tidy up XmlComments and make consistent across client and observable client and interfaces

--- Octokit/Clients/GitHubAppsClient.cs
+++ Octokit/Clients/GitHubAppsClient.cs
@@ -20,53 +20,57 @@ namespace Octokit
         }
 
         /// <summary>
-        /// Get a single GitHub App
+        /// Get a single GitHub App.
         /// </summary>
+        /// <remarks>https://developer.github.com/v3/apps/#get-a-single-github-app</remarks>
         /// <param name="slug">The URL-friendly name of your GitHub App. You can find this on the settings page for your GitHub App.</param>
-        /// <returns>The GitHub App</returns>
         public Task<GitHubApp> Get(string slug)
         {
             return ApiConnection.Get<GitHubApp>(ApiUrls.App(slug), null, AcceptHeaders.MachineManPreview);
         }
 
         /// <summary>
-        /// Returns the GitHub App associated with the authentication credentials used.
+        /// Returns the GitHub App associated with the authentication credentials used (requires GitHubApp JWT token auth).
         /// </summary>
-        /// <returns>The GitHub App associated with the authentication credentials used.</returns>
+        /// <remarks>https://developer.github.com/v3/apps/#get-the-authenticated-github-app</remarks>
         public Task<GitHubApp> GetCurrent()
         {
             return ApiConnection.Get<GitHubApp>(ApiUrls.App(), null, AcceptHeaders.MachineManPreview);
         }
 
         /// <summary>
-        /// Create a new installation token
+        /// List installations of the authenticated GitHub App (requires GitHubApp JWT token auth).
         /// </summary>
-        /// <param name="installationId">The Id of the GitHub App installation.</param>
-        /// <returns>The GitHub App installation access token.</returns>
-        public Task<AccessToken> CreateInstallationToken(long installationId)
-        {
-            return ApiConnection.Post<AccessToken>(ApiUrls.AccessTokens(installationId), string.Empty, AcceptHeaders.MachineManPreview);
-        }
-
-        /// <summary>
-        /// List installations that are accessible to the authenticated user.
-        /// </summary>
-        /// <returns>A list (read-only) that includes all the GitHub App installations accessible to the current authenticated user.</returns>
+        /// <remarks>https://developer.github.com/v3/apps/#find-installations</remarks>
         public Task<IReadOnlyList<Installation>> GetAllInstallationsForCurrent()
         {
             return ApiConnection.GetAll<Installation>(ApiUrls.Installations(), null, AcceptHeaders.MachineManPreview);
         }
 
         /// <summary>
-        /// List installations that are accessible to the authenticated user.
+        /// List installations of the authenticated GitHub App (requires GitHubApp JWT token auth).
         /// </summary>
-        /// <param name="options"></param>
-        /// <returns>A list (read-only) that includes all the GitHub App installations accessible to the current authenticated user.</returns>
+        /// <param name="options">Options for changing the API response</param>
+        /// <remarks>https://developer.github.com/v3/apps/#find-installations</remarks>
         public Task<IReadOnlyList<Installation>> GetAllInstallationsForCurrent(ApiOptions options)
         {
             Ensure.ArgumentNotNull(options, nameof(options));
 
             return ApiConnection.GetAll<Installation>(ApiUrls.Installations(), null, AcceptHeaders.MachineManPreview, options);
+        }
+
+        /// <summary>
+        /// Create a time bound access token for a GitHubApp Installation that can be used to access other API endpoints (requires GitHubApp JWT token auth).
+        /// </summary>
+        /// <remarks>
+        /// https://developer.github.com/v3/apps/#create-a-new-installation-token
+        /// https://developer.github.com/apps/building-github-apps/authentication-options-for-github-apps/#authenticating-as-an-installation
+        /// https://developer.github.com/v3/apps/available-endpoints/
+        /// </remarks>
+        /// <param name="installationId">The Id of the GitHub App Installation</param>
+        public Task<AccessToken> CreateInstallationToken(long installationId)
+        {
+            return ApiConnection.Post<AccessToken>(ApiUrls.AccessTokens(installationId), string.Empty, AcceptHeaders.MachineManPreview);
         }
     }
 }
