70fe467bac026953ec0b4e133e6b02cfa1fecd81
Ryan Gribble <ryan.gribble@gmail.com>
2018-03-14 20:22:18 +1000

    tweak build configs to use 2.0.3 SDK

--- build.ps1
+++ build.ps1
@@ -32,9 +32,9 @@ Param(
     [string[]]$ScriptArgs
 )
 
-$DotNetChannel = "preview";
-$DotNetVersion = "1.0.1";
-$DotNetInstallerUri = "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.1/scripts/obtain/dotnet-install.ps1";
+$DotNetChannel = "2.0";
+$DotNetVersion = "2.0.3";
+$DotNetInstallerUri = "https://dot.net/v1/dotnet-install.ps1";
 $NugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 
 # Make sure tools folder exists
@@ -65,7 +65,7 @@ if (Get-Command dotnet -ErrorAction SilentlyContinue) {
     $FoundDotNetCliVersion = dotnet --version;
 }
 
-if($FoundDotNetCliVersion -ne $DotNetVersion) {
+if($FoundDotNetCliVersion -lt $DotNetVersion) {
     $InstallPath = Join-Path $PSScriptRoot ".dotnet"
     if (!(Test-Path $InstallPath)) {
         mkdir -Force $InstallPath | Out-Null;
@@ -113,7 +113,7 @@ if($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE;
 }
 Write-Host "Running build..."
-Invoke-Expression "dotnet run -- $Arguments"
+Invoke-Expression "dotnet run $Arguments"
 if($LASTEXITCODE -ne 0) {
     Pop-Location;
     exit $LASTEXITCODE;
