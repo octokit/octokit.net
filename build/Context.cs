89a152a36c0c796a07431836d9bc0b47523a9d54
Ryan Gribble <ryan.gribble@gmail.com>
2018-04-04 21:39:32 +1000

    go back to SDK 1 since SDK 2 is having sporadic travisCI faliures in TestSourceLink build step

--- build/Context.cs
+++ build/Context.cs
@@ -36,7 +36,7 @@ public class Context : FrostingContext
 
         if (!this.IsRunningOnWindows())
         {
-            var testFramework = "netcoreapp2.0";
+            var testFramework = "netcoreapp1.0";
 
             this.Information($"Running tests against {testFramework} only as we're not on Windows.");
             settings.Framework = testFramework;
