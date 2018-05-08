6688ecee4c4b92550ca27690ead36a9956072d26
Cyberboss <Cyberboss@users.noreply.github.com>
2018-05-08 10:43:40 -0400

    Skeleton check suite client implementation

--- Octokit/Clients/CheckSuitesClient.cs
+++ Octokit/Clients/CheckSuitesClient.cs
@@ -1,9 +1,92 @@
-﻿namespace Octokit
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Octokit
 {
     public class CheckSuitesClient : ApiClient, ICheckSuitesClient
     {
         public CheckSuitesClient(ApiConnection apiConnection) : base(apiConnection)
         {
+        }
+
+        public Task Create(long repositoryId, NewCheckSuite newCheckSuite)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Create(string owner, string name, NewCheckSuite newCheckSuite)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<CheckSuite> Get(long repositoryId, long checkSuiteId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<CheckSuite> Get(string owner, string name, long checkSuiteId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<CheckSuite>> GetAllForReference(long repositoryId, string reference)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<CheckSuite>> GetAllForReference(string owner, string name, string reference)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<CheckSuite>> GetAllForReference(long repositoryId, string reference, CheckSuiteRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<CheckSuite>> GetAllForReference(string owner, string name, string reference, CheckSuiteRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<CheckSuite>> GetAllForReference(long repositoryId, string reference, ApiOptions options)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<CheckSuite>> GetAllForReference(string owner, string name, string reference, ApiOptions options)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<CheckSuite>> GetAllForReference(long repositoryId, string reference, CheckSuiteRequest request, ApiOptions options)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<CheckSuite>> GetAllForReference(string owner, string name, string reference, CheckSuiteRequest request, ApiOptions options)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Request(long repositoryId, CheckSuiteTriggerRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Request(string owner, string name, CheckSuiteTriggerRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdatePreferences(long repositoryId, AutoTriggerChecksObject preferences)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdatePreferences(string owner, string name, AutoTriggerChecksObject preferences)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
--- Octokit/Clients/ICheckSuitesClient.cs
+++ Octokit/Clients/ICheckSuitesClient.cs
@@ -1,6 +1,25 @@
-﻿namespace Octokit
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Octokit
 {
     public interface ICheckSuitesClient
     {
+        Task<CheckSuite> Get(long repositoryId, long checkSuiteId);
+        Task<CheckSuite> Get(string owner, string name, long checkSuiteId);
+        Task<IReadOnlyList<CheckSuite>> GetAllForReference(long repositoryId, string reference);
+        Task<IReadOnlyList<CheckSuite>> GetAllForReference(string owner, string name, string reference);
+        Task<IReadOnlyList<CheckSuite>> GetAllForReference(long repositoryId, string reference, CheckSuiteRequest request);
+        Task<IReadOnlyList<CheckSuite>> GetAllForReference(string owner, string name, string reference, CheckSuiteRequest request);
+        Task<IReadOnlyList<CheckSuite>> GetAllForReference(long repositoryId, string reference, ApiOptions options);
+        Task<IReadOnlyList<CheckSuite>> GetAllForReference(string owner, string name, string reference, ApiOptions options);
+        Task<IReadOnlyList<CheckSuite>> GetAllForReference(long repositoryId, string reference, CheckSuiteRequest request, ApiOptions options);
+        Task<IReadOnlyList<CheckSuite>> GetAllForReference(string owner, string name, string reference, CheckSuiteRequest request, ApiOptions options);
+        Task UpdatePreferences(long repositoryId, AutoTriggerChecksObject preferences);
+        Task UpdatePreferences(string owner, string name, AutoTriggerChecksObject preferences);
+        Task Create(long repositoryId, NewCheckSuite newCheckSuite);
+        Task Create(string owner, string name, NewCheckSuite newCheckSuite);
+        Task Request(long repositoryId, CheckSuiteTriggerRequest request);
+        Task Request(string owner, string name, CheckSuiteTriggerRequest request);
     }
 }
