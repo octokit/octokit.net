70d84bfc6f51dd8b46b49dd1b1f4542526eb5af0
Cyberboss <Cyberboss@users.noreply.github.com>
2018-05-08 10:18:23 -0400

    Flesh out CheckSuite model

--- Octokit/Models/Response/CheckSuite.cs
+++ Octokit/Models/Response/CheckSuite.cs
@@ -1,7 +1,38 @@
-﻿namespace Octokit
+﻿using System.Collections.Generic;
+
+namespace Octokit
 {
     public class CheckSuite
     {
+        public CheckSuite()
+        {
+        }
+
+        public CheckSuite(long id, string headBranch, string headSha, CheckStatus status, CheckConclusion conclusion, string url, string before, string after, IReadOnlyList<PullRequest> pullRequests, GitHubApp app, Repository repository)
+        {
+            Id = id;
+            HeadBranch = headBranch;
+            HeadSha = headSha;
+            Status = status;
+            Conclusion = conclusion;
+            Url = url;
+            Before = before;
+            After = after;
+            PullRequests = pullRequests;
+            App = app;
+            Repository = repository;
+        }
+
         public long Id { get; protected set; }
+        public string HeadBranch { get; protected set; }
+        public string HeadSha { get; protected set; }
+        public StringEnum<CheckStatus> Status { get; protected set; }
+        public StringEnum<CheckConclusion> Conclusion { get; protected set; }
+        public string Url { get; protected set; }
+        public string Before { get; protected set; }
+        public string After { get; protected set; }
+        public IReadOnlyList<PullRequest> PullRequests { get; protected set; }
+        public GitHubApp App { get; protected set; }
+        public Repository Repository { get; protected set; }
     }
 }
