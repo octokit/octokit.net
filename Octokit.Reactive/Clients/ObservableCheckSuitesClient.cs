86c19a507c5f191f5c159027e78d748002cfc531
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-05-08 20:55:16 -0400

    Remove rogue tabs

--- Octokit.Reactive/Clients/ObservableCheckSuitesClient.cs
+++ Octokit.Reactive/Clients/ObservableCheckSuitesClient.cs
@@ -4,145 +4,145 @@ using Octokit.Reactive.Internal;
 
 namespace Octokit.Reactive
 {
-	public class ObservableCheckSuitesClient : IObservableCheckSuitesClient
-	{
-		readonly ICheckSuitesClient _client;
-		readonly IConnection _connection;
+    public class ObservableCheckSuitesClient : IObservableCheckSuitesClient
+    {
+        readonly ICheckSuitesClient _client;
+        readonly IConnection _connection;
 
-		public ObservableCheckSuitesClient(IGitHubClient gitHubClient)
-		{
-			_client = gitHubClient.Checks.Suites;
-			_connection = gitHubClient.Connection;
-		}
+        public ObservableCheckSuitesClient(IGitHubClient gitHubClient)
+        {
+            _client = gitHubClient.Checks.Suites;
+            _connection = gitHubClient.Connection;
+        }
 
-		public IObservable<CheckSuite> Create(long repositoryId, NewCheckSuite newCheckSuite)
-		{
-			Ensure.ArgumentNotNull(newCheckSuite, nameof(newCheckSuite));
+        public IObservable<CheckSuite> Create(long repositoryId, NewCheckSuite newCheckSuite)
+        {
+            Ensure.ArgumentNotNull(newCheckSuite, nameof(newCheckSuite));
 
-			return _client.Create(repositoryId, newCheckSuite).ToObservable();
-		}
+            return _client.Create(repositoryId, newCheckSuite).ToObservable();
+        }
 
-		public IObservable<CheckSuite> Create(string owner, string name, NewCheckSuite newCheckSuite)
-		{
-			Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
-			Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
-			Ensure.ArgumentNotNull(newCheckSuite, nameof(newCheckSuite));
-			
-			return _client.Create(owner, name, newCheckSuite).ToObservable();
-		}
+        public IObservable<CheckSuite> Create(string owner, string name, NewCheckSuite newCheckSuite)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
+            Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
+            Ensure.ArgumentNotNull(newCheckSuite, nameof(newCheckSuite));
+            
+            return _client.Create(owner, name, newCheckSuite).ToObservable();
+        }
 
-		public IObservable<CheckSuite> Get(long repositoryId, long checkSuiteId)
-		{
-			return _client.Get(repositoryId, checkSuiteId).ToObservable();
-		}
+        public IObservable<CheckSuite> Get(long repositoryId, long checkSuiteId)
+        {
+            return _client.Get(repositoryId, checkSuiteId).ToObservable();
+        }
 
-		public IObservable<CheckSuite> Get(string owner, string name, long checkSuiteId)
-		{
-			Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
-			Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
+        public IObservable<CheckSuite> Get(string owner, string name, long checkSuiteId)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
+            Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
 
-			return _client.Get(owner, name, checkSuiteId).ToObservable();
-		}
+            return _client.Get(owner, name, checkSuiteId).ToObservable();
+        }
 
-		public IObservable<CheckSuite> GetAllForReference(long repositoryId, string reference)
-		{
-			Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
+        public IObservable<CheckSuite> GetAllForReference(long repositoryId, string reference)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
 
-			return GetAllForReference(repositoryId, reference, ApiOptions.None);
-		}
+            return GetAllForReference(repositoryId, reference, ApiOptions.None);
+        }
 
-		public IObservable<CheckSuite> GetAllForReference(string owner, string name, string reference)
-		{
-			Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
-			Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
-			Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
+        public IObservable<CheckSuite> GetAllForReference(string owner, string name, string reference)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
+            Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
+            Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
 
-			return GetAllForReference(owner, name, reference, ApiOptions.None);
-		}
+            return GetAllForReference(owner, name, reference, ApiOptions.None);
+        }
 
-		public IObservable<CheckSuite> GetAllForReference(long repositoryId, string reference, CheckSuiteRequest request)
-		{
-			Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
-			Ensure.ArgumentNotNull(request, nameof(request));
+        public IObservable<CheckSuite> GetAllForReference(long repositoryId, string reference, CheckSuiteRequest request)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
+            Ensure.ArgumentNotNull(request, nameof(request));
 
-			return GetAllForReference(repositoryId, reference, request, ApiOptions.None);
-		}
+            return GetAllForReference(repositoryId, reference, request, ApiOptions.None);
+        }
 
-		public IObservable<CheckSuite> GetAllForReference(string owner, string name, string reference, CheckSuiteRequest request)
-		{
-			Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
-			Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
-			Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
-			Ensure.ArgumentNotNull(request, nameof(request));
+        public IObservable<CheckSuite> GetAllForReference(string owner, string name, string reference, CheckSuiteRequest request)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
+            Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
+            Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
+            Ensure.ArgumentNotNull(request, nameof(request));
 
-			return GetAllForReference(owner, name, reference, request, ApiOptions.None);
-		}
+            return GetAllForReference(owner, name, reference, request, ApiOptions.None);
+        }
 
-		public IObservable<CheckSuite> GetAllForReference(long repositoryId, string reference, ApiOptions options)
-		{
-			Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
-			Ensure.ArgumentNotNull(options, nameof(options));
+        public IObservable<CheckSuite> GetAllForReference(long repositoryId, string reference, ApiOptions options)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
+            Ensure.ArgumentNotNull(options, nameof(options));
 
-			throw new NotImplementedException();
-		}
+            throw new NotImplementedException();
+        }
 
-		public IObservable<CheckSuite> GetAllForReference(string owner, string name, string reference, ApiOptions options)
-		{
-			Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
-			Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
-			Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
-			Ensure.ArgumentNotNull(options, nameof(options));
+        public IObservable<CheckSuite> GetAllForReference(string owner, string name, string reference, ApiOptions options)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
+            Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
+            Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
+            Ensure.ArgumentNotNull(options, nameof(options));
 
-			throw new NotImplementedException();
-		}
+            throw new NotImplementedException();
+        }
 
-		public IObservable<CheckSuite> GetAllForReference(long repositoryId, string reference, CheckSuiteRequest request, ApiOptions options)
-		{
-			Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
-			Ensure.ArgumentNotNull(request, nameof(request));
-			Ensure.ArgumentNotNull(options, nameof(options));
+        public IObservable<CheckSuite> GetAllForReference(long repositoryId, string reference, CheckSuiteRequest request, ApiOptions options)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
+            Ensure.ArgumentNotNull(request, nameof(request));
+            Ensure.ArgumentNotNull(options, nameof(options));
 
-			throw new NotImplementedException();
-		}
+            throw new NotImplementedException();
+        }
 
-		public IObservable<CheckSuite> GetAllForReference(string owner, string name, string reference, CheckSuiteRequest request, ApiOptions options)
-		{
-			Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
-			Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
-			Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
-			Ensure.ArgumentNotNull(request, nameof(request));
-			Ensure.ArgumentNotNull(options, nameof(options));
+        public IObservable<CheckSuite> GetAllForReference(string owner, string name, string reference, CheckSuiteRequest request, ApiOptions options)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
+            Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
+            Ensure.ArgumentNotNullOrEmptyString(reference, nameof(reference));
+            Ensure.ArgumentNotNull(request, nameof(request));
+            Ensure.ArgumentNotNull(options, nameof(options));
 
-			throw new NotImplementedException();
-		}
+            throw new NotImplementedException();
+        }
 
-		public IObservable<CheckSuite> Request(long repositoryId, CheckSuiteTriggerRequest request)
-		{
-			return _client.Request(repositoryId, request).ToObservable();
-		}
+        public IObservable<CheckSuite> Request(long repositoryId, CheckSuiteTriggerRequest request)
+        {
+            return _client.Request(repositoryId, request).ToObservable();
+        }
 
-		public IObservable<CheckSuite> Request(string owner, string name, CheckSuiteTriggerRequest request)
-		{
-			Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
-			Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
+        public IObservable<CheckSuite> Request(string owner, string name, CheckSuiteTriggerRequest request)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
+            Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
 
-			return _client.Request(owner, name, request).ToObservable();
-		}
+            return _client.Request(owner, name, request).ToObservable();
+        }
 
-		public IObservable<CheckSuitePreferences> UpdatePreferences(long repositoryId, AutoTriggerChecksObject preferences)
-		{
-			Ensure.ArgumentNotNull(preferences, nameof(preferences));
+        public IObservable<CheckSuitePreferences> UpdatePreferences(long repositoryId, AutoTriggerChecksObject preferences)
+        {
+            Ensure.ArgumentNotNull(preferences, nameof(preferences));
 
-			return _client.UpdatePreferences(repositoryId, preferences).ToObservable();
-		}
+            return _client.UpdatePreferences(repositoryId, preferences).ToObservable();
+        }
 
-		public IObservable<CheckSuitePreferences> UpdatePreferences(string owner, string name, AutoTriggerChecksObject preferences)
-		{
-			Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
-			Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
-			Ensure.ArgumentNotNull(preferences, nameof(preferences));
+        public IObservable<CheckSuitePreferences> UpdatePreferences(string owner, string name, AutoTriggerChecksObject preferences)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
+            Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
+            Ensure.ArgumentNotNull(preferences, nameof(preferences));
 
-			return _client.UpdatePreferences(owner, name, preferences).ToObservable();
-		}
-	}
+            return _client.UpdatePreferences(owner, name, preferences).ToObservable();
+        }
+    }
 }
--- Octokit.Reactive/Clients/ObservableChecksClient.cs
+++ Octokit.Reactive/Clients/ObservableChecksClient.cs
@@ -3,13 +3,13 @@
     public class ObservableChecksClient : IObservableChecksClient
     {
         public ObservableChecksClient(IGitHubClient gitHubClient)
-		{
-			Runs = new ObservableCheckRunsClient(gitHubClient);
-			Suites = new ObservableCheckSuitesClient(gitHubClient);
-		}
+        {
+            Runs = new ObservableCheckRunsClient(gitHubClient);
+            Suites = new ObservableCheckSuitesClient(gitHubClient);
+        }
 
         public IObservableCheckRunsClient Runs { get; private set; }
 
         public IObservableCheckSuitesClient Suites { get; private set; }
-	}
+    }
 }
