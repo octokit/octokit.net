913ce8d6e8c956eb3f232c2b7daf120aaa1fa803
Cyberboss <Cyberboss@users.noreply.github.com>
2018-05-09 11:42:36 -0400

    Add CheckRunEventPayload

---
+++ Octokit/Models/Response/ActivityPayloads/CheckRunEventPayload.cs
@@ -0,0 +1,8 @@
+﻿namespace Octokit
+{
+    public class CheckRunEventPayload : ActivityPayload
+    {
+        public string Action { get; protected set; }
+        public CheckRun CheckRun { get; protected set; }
+    }
+}
