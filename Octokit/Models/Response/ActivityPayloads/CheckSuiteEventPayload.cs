1002e4a97aa6ed2d073f38293e8a2359745a5796
Cyberboss <Cyberboss@users.noreply.github.com>
2018-05-09 11:41:45 -0400

    Add CheckSuiteEventPayload

---
+++ Octokit/Models/Response/ActivityPayloads/CheckSuiteEventPayload.cs
@@ -0,0 +1,8 @@
+﻿namespace Octokit
+{
+    public class CheckSuiteEventPayload : ActivityPayload
+    {
+        public string Action { get; protected set; }
+        public CheckSuite CheckSuite { get; protected set; }
+    }
+}
