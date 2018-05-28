c861eecd7b3d9f157967ca02c55489a2e38b1128
Ryan Gribble <ryan.gribble@gmail.com>
2018-05-29 00:27:12 +1000

    simplify Get CheckSuite test to not require as much data setup

--- Octokit.Tests.Integration/Clients/CheckSuitesClientTests.cs
+++ Octokit.Tests.Integration/Clients/CheckSuitesClientTests.cs
@@ -26,27 +26,16 @@ namespace Octokit.Tests.Integration.Clients
             {
                 using (var repoContext = await _github.CreateRepositoryContext(new NewRepository(Helper.MakeNameWithTimestamp("public-repo")) { AutoInit = true }))
                 {
-                    // Turn off auto creation of check suite for this repo
-                    var preference = new AutoTriggerChecksObject(new[] { new CheckSuitePreference(Helper.GitHubAppId, false) });
-                    var response = await _githubAppInstallation.Check.Suite.UpdatePreferences(repoContext.RepositoryOwner, repoContext.RepositoryName, preference);
-
-                    // Create a new feature branch
+                    // Need to get a CheckSuiteId so we can test the Get method
                     var headCommit = await _github.Repository.Commit.Get(repoContext.RepositoryId, "master");
-                    var featureBranch = await Helper.CreateFeatureBranch(repoContext.RepositoryOwner, repoContext.RepositoryName, headCommit.Sha, "my-feature");
-
-                    // Create a check suite for the feature branch
-                    var newCheckSuite = new NewCheckSuite(featureBranch.Object.Sha)
-                    {
-                        HeadBranch = "my-feature"
-                    };
-                    var checkSuite = await _githubAppInstallation.Check.Suite.Create(repoContext.RepositoryOwner, repoContext.RepositoryName, newCheckSuite);
-
-                    // Get Check Suite
+                    var checkSuite = (await _githubAppInstallation.Check.Suite.GetAllForReference(repoContext.RepositoryOwner, repoContext.RepositoryName, headCommit.Sha)).First();
+                    
+                    // Get Check Suite by Id
                     var result = await _github.Check.Suite.Get(repoContext.RepositoryOwner, repoContext.RepositoryName, checkSuite.Id);
 
                     // Check result
                     Assert.Equal(checkSuite.Id, result.Id);
-                    Assert.Equal(newCheckSuite.HeadSha, result.HeadSha);
+                    Assert.Equal(headCommit.Sha, result.HeadSha);
                 }
             }
         }
