de73792e90563ec5023a7d378a27345deb6dcf39
wqferr <wqferr@gmail.com>
2018-06-06 17:25:14 -0300

    Add DebuggerDIsplayAttribute do RepositoryTransfer

--- Octokit/Models/Request/RepositoryTransfer.cs
+++ Octokit/Models/Request/RepositoryTransfer.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Text;
+using System.Diagnostics;
+using System.Globalization;
 
 namespace Octokit
 {
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class RepositoryTransfer
     {
         public RepositoryTransfer(string newOwner)
@@ -23,5 +27,27 @@ namespace Octokit
         public string NewOwner { get; set; }
 
         public int[] TeamId { get; set; }
+
+        internal string DebuggerDisplay
+        {
+            get
+            {
+                StringBuilder strBuilder = new StringBuilder();
+                strBuilder.AppendFormat(CultureInfo.InvariantCulture, "NewOwner: {0}", NewOwner);
+                strBuilder.Append(", TeamId: ");
+                if (TeamId == null) {
+                    strBuilder.Append("null");
+                    return strBuilder.ToString();
+                }
+
+                strBuilder.Append("[ ");
+                foreach (int id in TeamId)
+                {
+                    strBuilder.AppendFormat(CultureInfo.InvariantCulture, "{0} ", id.ToString());
+                }
+                strBuilder.Append("]");
+                return strBuilder.ToString();
+            }
+        }
     }
 }
