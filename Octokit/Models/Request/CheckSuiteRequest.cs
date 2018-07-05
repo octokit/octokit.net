47482b6a64650d8ac22dca9bf6d5e7425da482a8
Ryan Gribble <ryan.gribble@gmail.com>
2018-07-05 17:00:47 +1000

    fix request parameter names

--- Octokit/Models/Request/CheckSuiteRequest.cs
+++ Octokit/Models/Request/CheckSuiteRequest.cs
@@ -1,12 +1,16 @@
 ﻿using System.Diagnostics;
 using System.Globalization;
+using Octokit.Internal;
 
 namespace Octokit
 {
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class CheckSuiteRequest : RequestParameters
     {
-        public long AppId { get; set; }
+        [Parameter(Key = "app_id")]
+        public long? AppId { get; set; }
+
+        [Parameter(Key = "check_name")]
         public string CheckName { get; set; }
 
         internal string DebuggerDisplay => string.Format(CultureInfo.InvariantCulture, "AppId: {0}, CheckName: {1}", AppId, CheckName);
