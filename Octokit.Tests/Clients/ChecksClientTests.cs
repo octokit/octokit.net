aadf84cb047c2db0583dffe81020af9e44668614
Ryan Gribble <ryan.gribble@gmail.com>
2018-07-05 16:59:33 +1000

    oh so many unit tests for CheckSuites methods

---
+++ Octokit.Tests/Clients/ChecksClientTests.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using NSubstitute;
+using Xunit;
+
+namespace Octokit.Tests.Clients
+{
+    /// <summary>
+    /// Client tests mostly just need to make sure they call the IApiConnection with the correct 
+    /// relative Uri. No need to fake up the response. All *those* tests are in ApiConnectionTests.cs.
+    /// </summary>
+    public class ChecksClientTests
+    {
+        public class TheCtor
+        {
+            [Fact]
+            public void EnsuresNonNullArguments()
+            {
+                Assert.Throws<ArgumentNullException>(() => new ChecksClient(null));
+            }
+        }
+    }
+}
