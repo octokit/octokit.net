363075a5f15ecfb7558fb22eb8ae2ebf0127a092
Henrik Andersson <henrik@henrikandersson.org>
2019-02-22 07:12:41 +1000

    Add pagination to *CommitCommentReactionsClient

--- Octokit/Clients/ICommitCommentReactionsClient.cs
+++ Octokit/Clients/ICommitCommentReactionsClient.cs
@@ -40,8 +40,18 @@ namespace Octokit
         /// <param name="name">The name of the repository</param>
         /// <param name="number">The comment id</param>        
         /// <returns></returns>
-        [ExcludeFromPaginationApiOptionsConventionTest("TODO: Implement pagination for this method")]
         Task<IReadOnlyList<Reaction>> GetAll(string owner, string name, int number);
+
+        /// <summary>
+        /// Get all reactions for a specified Commit Comment
+        /// </summary>
+        /// <remarks>https://developer.github.com/v3/reactions/#list-reactions-for-a-commit-comment</remarks>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        /// <param name="number">The comment id</param>
+        /// <param name="options">Options for changing the API response</param>
+        /// <returns></returns>
+        Task<IReadOnlyList<Reaction>> GetAll(string owner, string name, int number, ApiOptions options);
 
         /// <summary>
         /// Get all reactions for a specified Commit Comment
@@ -50,7 +60,16 @@ namespace Octokit
         /// <param name="repositoryId">The owner of the repository</param>
         /// <param name="number">The comment id</param>        
         /// <returns></returns>
-        [ExcludeFromPaginationApiOptionsConventionTest("TODO: Implement pagination for this method")]
         Task<IReadOnlyList<Reaction>> GetAll(long repositoryId, int number);
+
+        /// <summary>
+        /// Get all reactions for a specified Commit Comment
+        /// </summary>
+        /// <remarks>https://developer.github.com/v3/reactions/#list-reactions-for-a-commit-comment</remarks>
+        /// <param name="repositoryId">The owner of the repository</param>
+        /// <param name="number">The comment id</param>
+        /// <param name="options">Options for changing the API response</param> 
+        /// <returns></returns>
+        Task<IReadOnlyList<Reaction>> GetAll(long repositoryId, int number, ApiOptions options);
     }
 }
