261758b6472bfda40aeef6aa7bf00fb3bedcb74c
Henrik Andersson <henrik@henrikandersson.org>
2019-03-06 08:09:08 +1000

    Add new settings to the Organization model

--- Octokit/Models/Response/Organization.cs
+++ Octokit/Models/Response/Organization.cs
@@ -10,10 +10,12 @@ namespace Octokit
     {
         public Organization() { }
 
-        public Organization(string avatarUrl, string bio, string blog, int collaborators, string company, DateTimeOffset createdAt, int diskUsage, string email, int followers, int following, bool? hireable, string htmlUrl, int totalPrivateRepos, int id, string nodeId, string location, string login, string name, int ownedPrivateRepos, Plan plan, int privateGists, int publicGists, int publicRepos, string url, string billingAddress)
+        public Organization(string avatarUrl, string bio, string blog, int collaborators, string company, DateTimeOffset createdAt, int diskUsage, string email, int followers, int following, bool? hireable, string htmlUrl, int totalPrivateRepos, int id, string nodeId, string location, string login, string name, int ownedPrivateRepos, Plan plan, int privateGists, int publicGists, int publicRepos, string url, string billingAddress, string defaultRepositoryPermission, bool membersCanCreateRepositories)
             : base(avatarUrl, bio, blog, collaborators, company, createdAt, diskUsage, email, followers, following, hireable, htmlUrl, totalPrivateRepos, id, location, login, name, nodeId, ownedPrivateRepos, plan, privateGists, publicGists, publicRepos, AccountType.Organization, url)
         {
             BillingAddress = billingAddress;
+            DefaultRepositoryPermission = defaultRepositoryPermission;
+            MembersCanCreateRepositories = membersCanCreateRepositories;
         }
 
         /// <summary>
@@ -21,6 +23,16 @@ namespace Octokit
         /// an organization.
         /// </summary>
         public string BillingAddress { get; protected set; }
+        
+        /// <summary>
+        /// Default permission level members have for organization repositories.
+        /// </summary>
+        public string DefaultRepositoryPermission { get; protected set; }
+
+        /// <summary>
+        /// Toggles the ability of non-admin organization members to create repositories.
+        /// </summary>
+        public bool MembersCanCreateRepositories { get; protected set; }
 
         internal string DebuggerDisplay
         {
