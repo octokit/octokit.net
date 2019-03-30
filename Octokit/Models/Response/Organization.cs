383a9cec13c910f6fc9e71a7ee6ca153590931a3
Henrik Andersson <henrik@henrikandersson.org>
2019-03-30 14:48:35 +1000

    Use enum types in ctor

--- Octokit/Models/Response/Organization.cs
+++ Octokit/Models/Response/Organization.cs
@@ -10,7 +10,7 @@ namespace Octokit
     {
         public Organization() { }
 
-        public Organization(string avatarUrl, string bio, string blog, int collaborators, string company, DateTimeOffset createdAt, int diskUsage, string email, int followers, int following, bool? hireable, string htmlUrl, int totalPrivateRepos, int id, string nodeId, string location, string login, string name, int ownedPrivateRepos, Plan plan, int privateGists, int publicGists, int publicRepos, string url, string billingAddress, string defaultRepositoryPermission, bool membersCanCreateRepositories, string membersAllowedRepositoryCreationType)
+        public Organization(string avatarUrl, string bio, string blog, int collaborators, string company, DateTimeOffset createdAt, int diskUsage, string email, int followers, int following, bool? hireable, string htmlUrl, int totalPrivateRepos, int id, string nodeId, string location, string login, string name, int ownedPrivateRepos, Plan plan, int privateGists, int publicGists, int publicRepos, string url, string billingAddress, StringEnum<DefaultRepositoryPermission> defaultRepositoryPermission, bool membersCanCreateRepositories, StringEnum<MembersAllowedRepositoryCreationType> membersAllowedRepositoryCreationType)
             : base(avatarUrl, bio, blog, collaborators, company, createdAt, diskUsage, email, followers, following, hireable, htmlUrl, totalPrivateRepos, id, location, login, name, nodeId, ownedPrivateRepos, plan, privateGists, publicGists, publicRepos, AccountType.Organization, url)
         {
             BillingAddress = billingAddress;
