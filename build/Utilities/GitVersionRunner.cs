2f1f62b1cd97729caf61e1e64e2f8e91bc81da48
Mattias Karlsson <matkar@wcom.se>
2019-07-31 15:37:57 +0200

    Update GritVersion from 3.6.2 to 5.0.0

--- build/Utilities/GitVersionRunner.cs
+++ build/Utilities/GitVersionRunner.cs
@@ -1,29 +1,13 @@
-﻿using Cake.Common;
-using Cake.Common.Diagnostics;
-using Cake.Common.Tools.GitVersion;
-using Cake.Core;
+﻿using Cake.Common.Tools.GitVersion;
 
 public static class GitVersionRunner
 {
-    public static GitVersion Run(ICakeContext context, GitVersionOutput outputType)
+    public static GitVersion Run(Context context, GitVersionOutput outputType)
     {
-        if (context.IsRunningOnWindows())
+        return context.GitVersion(new GitVersionSettings
         {
-            return context.GitVersion(new GitVersionSettings
-            {
-                OutputType = outputType
-            });
-        }
-        else
-        {
-            // On non windows platform, point the GitVersion task at our wrapper script that uses mono to run GitVersion.exe
-            context.Information("Overriding GitVersion ToolPath to /bin/sh ./tools/gitversion_wrapper.sh");
-            return context.GitVersion(new GitVersionSettings
-            {
-                OutputType = outputType,
-                ToolPath = "/bin/sh",
-                ArgumentCustomization = args => args.Prepend("./tools/gitversion_wrapper.sh")
-            });
-        }
+            OutputType = outputType,
+            ToolPath = context.GitVersionToolPath
+        });
     }
 }
