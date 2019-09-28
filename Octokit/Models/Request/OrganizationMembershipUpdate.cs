ed85dc3d29c6885eef34f95a526a67511a2579fd
Henrik Andersson <henrik@henrikandersson.org>
2019-09-28 18:36:38 +1000

    Default organization membership role to Member

--- Octokit/Models/Request/OrganizationMembershipUpdate.cs
+++ Octokit/Models/Request/OrganizationMembershipUpdate.cs
@@ -5,6 +5,11 @@ namespace Octokit
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class OrganizationMembershipUpdate
     {
+        public OrganizationMembershipUpdate()
+        {
+            Role = OrganizationMembershipsRole.Member;
+        }
+        
         /// <summary>
         /// The role to give the user in the organization. The default is <see cref="OrganizationMembershipsRole.Member"/>.
         /// </summary>
