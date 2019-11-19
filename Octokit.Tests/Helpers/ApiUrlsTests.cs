2d00980cd841e7855f932a1e588b0b7f45672de6
Sean Killeen <SeanKilleen@gmail.com>
2019-11-18 21:42:25 -0500

    Add tests

---
+++ Octokit.Tests/Helpers/ApiUrlsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Octokit.Tests.Helpers
+{
+    public class ApiUrlsTests
+    {
+        public class TeamByName
+        {
+            [Fact]
+            public void UsesCorrectUrl()
+            {
+                var result = ApiUrls.TeamByName("theOrg", "theRepoTeam");
+
+                Assert.Equal("orgs/theOrg/teams/therepoteam", result.OriginalString);
+            }
+
+            [Fact]
+            public void ConvertsSpacesToDashes()
+            {
+                var result = ApiUrls.TeamByName("theOrg", "the repo team");
+
+                Assert.Equal("orgs/theOrg/teams/the-repo-team", result.OriginalString);
+            }
+
+            [Fact]
+            public void ConvertsToLowercase()
+            {
+                var result = ApiUrls.TeamByName("theOrg", "THETEAM");
+
+                Assert.Equal("orgs/theOrg/teams/theteam", result.OriginalString);
+            }
+        }
+    }
+}
