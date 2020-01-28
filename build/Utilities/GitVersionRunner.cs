c775dbdc02d68726259793587bb3b8e0c4d71f38
Brendan Forster <github@brendanforster.com>
2020-01-28 12:37:11 -0400

    switch over to global tool but emulate what cake does

--- build/Utilities/GitVersionRunner.cs
+++ build/Utilities/GitVersionRunner.cs
@@ -1,12 +1,62 @@
 ﻿using Cake.Common.Tools.GitVersion;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using Cake.Core;
+using Cake.Core.Diagnostics;
+using Cake.Core.IO;
+using Cake.Core.Tooling;
+using Cake.Common;
+using Cake.Common.IO;
+using Cake.Frosting;
 
 public static class GitVersionRunner
 {
     public static GitVersion Run(Context context, GitVersionOutput outputType)
     {
-        return context.GitVersion(new GitVersionSettings
+        if (outputType == GitVersionOutput.BuildServer)
         {
-            OutputType = outputType
-        });
+            var exitCode = context.StartProcess("dotnet", new ProcessSettings
+            {
+                Arguments = $"gitversion -output buildserver"
+            });
+
+            if (exitCode != 0)
+            {
+                throw new Exception("gitversion failed!");
+            }
+
+            return new GitVersion();
+        }
+        else if (outputType == GitVersionOutput.Json)
+        {
+            IEnumerable<string> stdout;
+            var jsonString = string.Empty;
+
+            var exitCode = context.StartProcess("dotnet", new ProcessSettings
+            {
+                Arguments = $"gitversion -output json",
+                RedirectStandardOutput = true,
+            },
+            out stdout);
+
+            if (exitCode != 0)
+            {
+                throw new Exception("gitversion failed!");
+            }
+
+            jsonString = string.Join("\n", stdout);
+
+            var jsonSerializer = new DataContractJsonSerializer(typeof(GitVersion));
+
+            using (var jsonStream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString)))
+            {
+                return (jsonSerializer.ReadObject(jsonStream) as GitVersion);
+            }
+        }
+
+        return new GitVersion();
     }
 }
