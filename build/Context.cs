5218d043726d1781c5361d08faa02a733f9d54b7
Brendan Forster <github@brendanforster.com>
2020-02-03 16:28:57 -0400

    bump required SDK version to 3.1

--- build/Context.cs
+++ build/Context.cs
@@ -40,7 +40,7 @@ public class Context : FrostingContext
 
         if (!this.IsRunningOnWindows())
         {
-            var testFramework = "netcoreapp2.1";
+            var testFramework = "netcoreapp3.1";
 
             this.Information($"Running tests against {testFramework} only as we're not on Windows.");
             settings.Framework = testFramework;
