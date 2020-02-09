8dac9688d9af55842b5b7549917055abb2e82eb6
Brendan Forster <github@brendanforster.com>
2020-02-09 12:44:24 -0400

    add new field to model for indicating whether repository is a template (#2090)

--- Octokit/Models/Response/Repository.cs
+++ Octokit/Models/Response/Repository.cs
@@ -14,7 +14,7 @@ namespace Octokit
             Id = id;
         }
 
-        public Repository(string url, string htmlUrl, string cloneUrl, string gitUrl, string sshUrl, string svnUrl, string mirrorUrl, long id, string nodeId, User owner, string name, string fullName, string description, string homepage, string language, bool @private, bool fork, int forksCount, int stargazersCount, string defaultBranch, int openIssuesCount, DateTimeOffset? pushedAt, DateTimeOffset createdAt, DateTimeOffset updatedAt, RepositoryPermissions permissions, Repository parent, Repository source, LicenseMetadata license, bool hasIssues, bool hasWiki, bool hasDownloads, bool hasPages, int subscribersCount, long size, bool? allowRebaseMerge, bool? allowSquashMerge, bool? allowMergeCommit, bool archived)
+        public Repository(string url, string htmlUrl, string cloneUrl, string gitUrl, string sshUrl, string svnUrl, string mirrorUrl, long id, string nodeId, User owner, string name, string fullName, bool isTemplate, string description, string homepage, string language, bool @private, bool fork, int forksCount, int stargazersCount, string defaultBranch, int openIssuesCount, DateTimeOffset? pushedAt, DateTimeOffset createdAt, DateTimeOffset updatedAt, RepositoryPermissions permissions, Repository parent, Repository source, LicenseMetadata license, bool hasIssues, bool hasWiki, bool hasDownloads, bool hasPages, int subscribersCount, long size, bool? allowRebaseMerge, bool? allowSquashMerge, bool? allowMergeCommit, bool archived)
         {
             Url = url;
             HtmlUrl = htmlUrl;
@@ -28,6 +28,7 @@ namespace Octokit
             Owner = owner;
             Name = name;
             FullName = fullName;
+            IsTemplate = isTemplate;
             Description = description;
             Homepage = homepage;
             Language = language;
@@ -82,6 +83,8 @@ namespace Octokit
         public string Name { get; protected set; }
 
         public string FullName { get; protected set; }
+
+        public bool IsTemplate { get; protected set; }
 
         public string Description { get; protected set; }
 
