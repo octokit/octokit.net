6fdd800f729c40c9b9cfa7cd71e49b75dbce1ad9
Itai Bar-Haim <itaibh@gmail.com>
2020-02-24 19:37:03 +0200

    add new types for webhook commit payload (#1844)
    Co-authored-by: Itai Bar-Haim <itai.barhaim@applitools.com>

---
+++ Octokit/Models/Response/ActivityPayloads/PushWebhookPayload.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Octokit
+{
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class PushWebhookPayload : ActivityPayload
+    {
+        public string Head { get; protected set; }
+        public string Before { get; protected set; }
+        public string After { get; protected set; }
+        public string Ref { get; protected set; }
+        public string BaseRef { get; protected set; }
+        public bool Created { get; protected set; }
+        public bool Deleted { get; protected set; }
+        public bool Forced { get; protected set; }
+        public string Compare { get; protected set; }
+        public int Size { get; protected set; }
+        public IReadOnlyList<PushWebhookCommit> Commits { get; protected set; }
+        public PushWebhookCommit HeadCommit { get; protected set; }
+    }
+}
