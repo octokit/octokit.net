178381cf51d27948484ada0bf4b9fc8de424c2a9
Brendan Forster <github@brendanforster.com>
2020-03-09 16:27:18 -0300

    another test moved

--- test/Octokit.CodeGen.Tests/Builders/AddMethodForEachVerbTests.cs
+++ test/Octokit.CodeGen.Tests/Builders/AddMethodForEachVerbTests.cs
@@ -77,5 +77,45 @@ namespace Octokit.CodeGen.Tests
             Assert.Equal("username", getOrCreateParameter.Name);
             Assert.Equal("string", getOrCreateParameter.Type);
         }
+
+
+        [Fact]
+        public async Task Build_ForPathReturningObjectResponse_GeneratesRequiredModel()
+        {
+            var stream = TestFixtureLoader.LoadPathWithGet();
+
+            var paths = await PathProcessor.Process(stream);
+            var path = paths[0];
+
+            var data = new ApiClientFileMetadata();
+
+            data = Builders.AddResponseModels(path, data);
+            var result = Builders.AddMethodForEachVerb(path, data);
+
+            Assert.Equal(5, result.Models.Count);
+
+            var account = Assert.Single(result.Models.Where(m => m.Name == "MarketplaceListingAccount"));
+            Assert.Single(account.Properties.Where(p => p.Name == "MarketplacePendingChange" && p.Type == "MarketplacePendingChange"));
+            Assert.Single(account.Properties.Where(p => p.Name == "MarketplacePurchase" && p.Type == "MarketplacePurchase"));
+
+            var pendingChange = Assert.Single(result.Models.Where(m => m.Name == "MarketplacePendingChange"));
+            Assert.Single(pendingChange.Properties.Where(p => p.Name == "Plan" && p.Type == "MarketplacePendingChangePlan"));
+
+            var purchase = Assert.Single(result.Models.Where(m => m.Name == "MarketplacePurchase"));
+            Assert.Single(purchase.Properties.Where(p => p.Name == "Plan" && p.Type == "MarketplacePurchasePlan"));
+
+            // TODO: as these are structurally the same and scoped to the same client,
+            //       can we merge them into a single `Plan` type?
+
+            var purchasePlan = Assert.Single(result.Models.Where(m => m.Name == "MarketplacePurchasePlan"));
+            Assert.Single(purchasePlan.Properties.Where(p => p.Name == "Bullets" && p.Type == "IReadOnlyList<string>"));
+
+            var plan = Assert.Single(result.Models.Where(m => m.Name == "MarketplacePendingChangePlan"));
+            Assert.Single(purchasePlan.Properties.Where(p => p.Name == "Bullets" && p.Type == "IReadOnlyList<string>"));
+
+            var get = Assert.Single(result.Client.Methods.Where(m => m.Name == "Get"));
+            var returnType = Assert.IsType<TaskOfType>(get.ReturnType.AsT0);
+            Assert.Equal("MarketplaceListingAccount", returnType.Type);
+        }
     }
 }
