3e562ba51a00da932e96df7ddb81a535317b3a1c
kentahikaru <quazarstar@gmail.com>
2020-03-11 22:07:28 +0100

    Added ObservableUsersClientTests.cs with Users.GetAll Test

---
+++ Octokit.Tests/Reactive/ObservableUsersClientTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using NSubstitute;
+using Octokit.Reactive;
+using Xunit;
+
+namespace Octokit.Tests.Reactive
+{
+    class ObservableUsersClientTests
+    {
+        public class TheGetAllMethod
+        {
+            [Fact]
+            public void GetsCorrectUrl()
+            {
+                Uri expectedUri = new Uri("users?since=1", UriKind.Relative);
+                var github = Substitute.For<IGitHubClient>();
+                var client = new ObservableUsersClient(github);
+
+                client.GetAll("1");
+
+                github.Connection.Received(1).Get<List<User>>(expectedUri, Arg.Is<Dictionary<string, string>>(dictionary => dictionary.Count == 0), null);
+            }
+        }
+        }
+}
