29a6d010bce133a78f29443d0d031a3c7653edee
Cédric Luthi <cedric.luthi@gmail.com>
2020-03-13 08:40:31 +0100

    Mark ReleasesClientTests.TheCreateReleasesMethod tests as integration
    They were using [Fact] instead of [IntegrationTest] so they would run and fail if no credentials are supplied.

--- Octokit.Tests.Integration/Clients/ReleasesClientTests.cs
+++ Octokit.Tests.Integration/Clients/ReleasesClientTests.cs
@@ -35,7 +35,7 @@ public class ReleasesClientTests
             _context = github.CreateRepositoryContext("public-repo").Result;
         }
 
-        [Fact]
+        [IntegrationTest]
         public async Task SendsCreateToCorrectUrl()
         {
             var releaseWithNoUpdate = new NewRelease("0.1") { Draft = true };
@@ -45,7 +45,7 @@ public class ReleasesClientTests
             Assert.NotNull(release);
         }
 
-        [Fact]
+        [IntegrationTest]
         public async Task SendsCreateToCorrectUrlWithRepositoryId()
         {
             var releaseWithNoUpdate = new NewRelease("0.1") { Draft = true };
