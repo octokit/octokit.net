749b395633f567b30045d7e4472304441aa9230f
Brendan Forster <github@brendanforster.com>
2020-03-15 16:13:56 -0300

    mark all APIs that are no longer documented

--- Octokit/Helpers/AcceptHeaders.cs
+++ Octokit/Helpers/AcceptHeaders.cs
@@ -9,59 +9,74 @@ namespace Octokit
 
         public const string StableVersionHtml = "application/vnd.github.v3.html";
 
+        [Obsolete("API has graduated")]
         public const string RedirectsPreviewThenStableVersionJson = "application/vnd.github.quicksilver-preview+json; charset=utf-8, application/vnd.github.v3+json; charset=utf-8";
 
         public const string CommitReferenceSha1MediaType = "application/vnd.github.v3.sha";
 
+        [Obsolete("API has graduated")]
         public const string OrganizationPermissionsPreview = "application/vnd.github.ironman-preview+json";
 
         /// <summary>
         /// Support for retrieving information about open source license usage on GitHub.com.
         /// Custom media type: drax-preview Announced: 2015-03-09 Update 1: 2015-06-24 Update 2: 2015-08-04
         /// </summary>
+        [Obsolete("API has graduated")]
         public const string LicensesApiPreview = "application/vnd.github.drax-preview+json";
 
+        [Obsolete("API has graduated")]
         public const string ProtectedBranchesApiPreview = "application/vnd.github.loki-preview+json";
 
         public const string StarCreationTimestamps = "application/vnd.github.v3.star+json";
 
+        [Obsolete("API has graduated")]
         public const string IssueLockingUnlockingApiPreview = "application/vnd.github.the-key-preview+json";
 
+        [Obsolete("API has graduated")]
         public const string SquashCommitPreview = "application/vnd.github.polaris-preview+json";
 
         public const string MigrationsApiPreview = "application/vnd.github.wyandotte-preview+json";
 
         public const string ReactionsPreview = "application/vnd.github.squirrel-girl-preview+json";
 
+        [Obsolete("API has graduated")]
         public const string SignatureVerificationPreview = "application/vnd.github.cryptographer-preview+sha";
 
         [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Gpg")]
+        [Obsolete("API has graduated")]
         public const string GpgKeysPreview = "application/vnd.github.cryptographer-preview";
 
         public const string DeploymentApiPreview = "application/vnd.github.ant-man-preview+json";
 
+        [Obsolete("API has graduated")]
         public const string InvitationsApiPreview = "application/vnd.github.swamp-thing-preview+json";
 
+        [Obsolete("API has graduated")]
         public const string PagesApiPreview = "application/vnd.github.mister-fantastic-preview+json";
 
         public const string IssueTimelineApiPreview = "application/vnd.github.mockingbird-preview+json";
 
+        [Obsolete("API has graduated")]
         public const string RepositoryTrafficApiPreview = "application/vnd.github.spiderman-preview";
 
         public const string DraftPullRequestApiPreview = "application/vnd.github.shadow-cat-preview+json";
 
         public const string ProjectsApiPreview = "application/vnd.github.inertia-preview+json";
 
+        [Obsolete("API is considered legacy")]
         public const string OrganizationMembershipPreview = "application/vnd.github.korra-preview+json";
 
+        [Obsolete("API has graduated")]
         public const string NestedTeamsPreview = "application/vnd.github.hellcat-preview+json";
 
         public const string GitHubAppsPreview = "application/vnd.github.machine-man-preview+json";
 
         public const string PreReceiveEnvironmentsPreview = "application/vnd.github.eye-scream-preview+json";
 
+        [Obsolete("API has graduated")]
         public const string LabelsApiPreview = "application/vnd.github.symmetra-preview+json";
 
+        [Obsolete("API has graduated")]
         public const string RepositoryTransferPreview = "application/vnd.github.nightshade-preview+json";
 
         public const string ChecksApiPreview = "application/vnd.github.antiope-preview+json";
