ebbe3187a01a72bfd61ea144a2ac1e48e9d0cfc0
Sean Killeen <SeanKilleen@gmail.com>
2020-09-09 11:15:20 -0400

    Add RepositoryTopics from #1721

---
+++ Octokit/Models/Response/RepositoryTopics.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+
+namespace Octokit
+{
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class RepositoryTopics
+    {
+        public RepositoryTopics() { }
+
+        public RepositoryTopics(IEnumerable<string> names)
+        {
+            Ensure.ArgumentNotNull(names, "names");
+            Names = new ReadOnlyCollection<string>(names.ToList());
+        }
+
+        public IReadOnlyList<string> Names { get; protected set; }
+
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal string DebuggerDisplay
+        {
+            get
+            {
+                return string.Format(CultureInfo.InvariantCulture,
+                    "RepositoryTopics: Names: {0}", string.Join(", ", Names));
+            }
+        }
+    }
+}
