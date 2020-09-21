2f4e46bd1e4b130919297c690089b07e05fd348b
Mindaugas Laganeckas <Mindaugas.Laganeckas@3shape.com>
2020-09-21 13:42:34 +0200

    Initial implementation of workflow dispatch event creation

--- Octokit/Clients/WorkflowsClient.cs
+++ Octokit/Clients/WorkflowsClient.cs
@@ -1,4 +1,8 @@
-﻿namespace Octokit
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Octokit.Models.Response;
+
+namespace Octokit
 {
     /// <summary>
     /// A client for GitHub's Workflows API.
@@ -17,6 +21,38 @@
         {
         }
 
-       
+        /// <summary>
+        /// list repository workflows
+        /// https://docs.github.com/en/rest/reference/actions#list-repository-workflows
+        /// </summary>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        [ManualRoute("GET", "/repos/{owner}/{repo}/actions/workflows")]
+        public Task<IReadOnlyList<Workflow>> GetAllForRepository(string owner, string name)
+        {
+            Ensure.ArgumentNotNull(owner, nameof(owner));
+            Ensure.ArgumentNotNull(name, nameof(name));
+            return ApiConnection.GetAll<Workflow>(ApiUrls.Workflows(owner, name));
+        }
+
+        /// <summary>
+        /// create a workflow dispatch event
+        /// https://docs.github.com/en/rest/reference/actions#create-a-workflow-dispatch-event
+        /// </summary>
+        /// <param name="owner">The owner of the repository</param>
+        /// <param name="name">The name of the repository</param>
+        /// <param name="workflowId">The ID of the workflow</param>
+        /// <param name="workflowDispatchEvent">The input to the workflow dispatch event creation</param>
+        [ManualRoute("POST", "/repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches")]
+        public void CreateWorkflowDispatchEvent(string owner, string name, string workflowId, WorkflowDispatchEvent workflowDispatchEvent)
+        {
+            Ensure.ArgumentNotNullOrEmptyString(owner, nameof(owner));
+            Ensure.ArgumentNotNullOrEmptyString(name, nameof(name));
+            Ensure.ArgumentNotNullOrEmptyString(workflowId, nameof(workflowId));
+            Ensure.ArgumentNotNull(workflowDispatchEvent, nameof(workflowDispatchEvent));
+            
+            ApiConnection.Post(ApiUrls.CreateWorkflowDispatchEvent(owner, name, workflowId), workflowDispatchEvent);
+        }
+        
     }
 }
