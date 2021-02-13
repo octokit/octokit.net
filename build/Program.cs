70a380a4d0879eeadc4d2703ce38ab281fd47a91
Mattias Karlsson <matkar@wcom.se>
2021-02-13 20:40:41 +0100

    (build) Updated Cake Frosting to version 1.0.0 (#2289)

--- build/Program.cs
+++ build/Program.cs
@@ -1,24 +1,13 @@
-using Cake.Core;
 using Cake.Frosting;
 
-public class Program : IFrostingStartup
+public class Program
 {
     public static int Main(string[] args)
     {
-        // Create the host.
-        var host = new CakeHostBuilder()
-            .WithArguments(args)
-            .UseStartup<Program>()
-            .Build();
-
-        // Run the host.
-        return host.Run();
-    }
-
-    public void Configure(ICakeServices services)
-    {
-        services.UseContext<Context>();
-        services.UseLifetime<Lifetime>();
-        services.UseWorkingDirectory("..");
+        return new CakeHost()
+            .UseContext<Context>()
+            .UseLifetime<Lifetime>()
+            .Run(args);
+;
     }
 }
