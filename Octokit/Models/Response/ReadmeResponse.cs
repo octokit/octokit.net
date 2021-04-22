1baf88d8a632d826a2d9be7a12a16fd0cc989faf
littlemisslilycane <vandy14@gmail.com>
2021-04-21 23:17:06 -0400

    Added Size fields to the Readme and ReadmeResponse classes.

--- Octokit/Models/Response/ReadmeResponse.cs
+++ Octokit/Models/Response/ReadmeResponse.cs
@@ -4,10 +4,11 @@ namespace Octokit
     {
         public ReadmeResponse() { }
 
-        public ReadmeResponse(string content, string name, string htmlUrl, string url, string encoding)
+        public ReadmeResponse(string content, string name, long size, string htmlUrl, string url, string encoding)
         {
             Content = content;
             Name = name;
+            Size = size;
             HtmlUrl = htmlUrl;
             Url = url;
             Encoding = encoding;
@@ -15,8 +16,9 @@ namespace Octokit
 
         public string Content { get; protected set; }
         public string Name { get; protected set; }
+        public long Size { get; protected set; }
         public string HtmlUrl { get; protected set; }
         public string Url { get; protected set; }
-        public string Encoding { get; protected set; }
+        public string Encoding { get; protected set; }        
     }
 }
