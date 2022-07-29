256120d758c99eb18fadc6336114c5751d8c8268
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-07-29 03:19:51 +0200

    Add AsyncPaginationExtensions

---
+++ Octokit.AsyncPaginationExtension/IPaginatedList.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Octokit.AsyncPaginationExtension
+{
+    /// <summary>
+    /// <cref cref="IAsyncEnumerable{T}"/> additionally allowing for random access.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Intended to abstract from a series of a API calls requiring pagination.
+    /// </para>
+    /// <para>
+    /// Intended to be implemented supporting caching, making repeated enumerations not require any API calls.
+    /// </para>
+    /// </remarks>
+    /// <typeparam name="T">The type of values to access.</typeparam>
+    public interface IPaginatedList<T> : IAsyncEnumerable<T>
+    {
+        /// <summary>
+        /// Gets a value at the specified index.
+        /// </summary>
+        /// <param name="index">The index at which to fetch the value.</param>
+        /// <returns>The value at the specified index or null if it is outside of the range.</returns>
+        public Task<T?> this[int index] { get; }
+    }
+}
---
+++ Octokit.Tests/AsyncEnumerableExtensionTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NSubstitute;
+using Octokit.AsyncPaginationExtension;
+using Xunit;
+
+namespace Octokit.Tests
+{
+    public class AsyncEnumerableExtensionTests
+    {
+        public class ThePaginatedList
+        {
+            [Fact]
+            public void RejectsInvalidValues()
+            {
+                var client = Substitute.For<IRepositoriesClient>();
+
+                Assert.ThrowsAsync<ArgumentOutOfRangeException>(() => client.GetAllForOrgAsync("octokit")[-1]);
+            }
+
+            [Fact]
+            public async Task ReturnsNullOnExceedingTotalSize()
+            {
+                var client = Substitute.For<IRepositoriesClient>();
+
+                Assert.Null(await client.GetAllForOrgAsync("octokit")[int.MaxValue]);
+            }
+
+            [Fact]
+            public async Task EnumeratesCorrectValues()
+            {
+                var client = Substitute.For<IRepositoriesClient>();
+
+                var list = new List<Repository>();
+                var enumerator = client.GetAllForOrgAsync("octokit").GetAsyncEnumerator();
+                while (await enumerator.MoveNextAsync())
+                {
+                    list.Add(enumerator.Current);
+                }
+
+                Assert.Equal(await client.GetAllForOrg("octokit"), list);
+            }
+
+            [Fact]
+            public async Task HandlesZeroCorrectly()
+            {
+                var client = Substitute.For<IRepositoriesClient>();
+                client.GetAllForOrg(Arg.Any<string>(), Arg.Any<ApiOptions>())
+                    .Returns(Enumerable.Repeat(new Repository(), 24).ToList());
+
+                Assert.NotNull(await client.GetAllForOrgAsync("octokit")[0]);
+            }
+
+            [Fact]
+            public async Task HandlesPageEdgesCorrectly()
+            {
+                var client = Substitute.For<IRepositoriesClient>();
+                client.GetAllForOrg(Arg.Any<string>(), Arg.Any<ApiOptions>())
+                    .Returns(Enumerable.Repeat(new Repository(), 5).ToList());
+
+                var repos = client.GetAllForOrgAsync("octokit", 5);
+                Assert.NotNull(await repos[4]);
+                Assert.NotNull(await repos[5]);
+            }
+        }
+
+        public class ThePaginationOverloads
+        {
+            [Fact]
+            public void RejectInvalidValues()
+            {
+                var client = Substitute.For<IRepositoriesClient>();
+                
+                Assert.Throws<ArgumentOutOfRangeException>(() => client.GetAllForUserAsync("fake", -1));
+                Assert.Throws<ArgumentOutOfRangeException>(() => client.GetAllForUserAsync("fake", 0));
+            }
+        }
+    }
+}
