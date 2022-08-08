eaef1eee2610c52e5a4ebd6e9fae25cb56c0218f
Stefan <29021710+Saalvage@users.noreply.github.com>
2022-08-08 17:27:46 +0200

    feat: Adds AsyncPaginationExtensions (#2516)

---
+++ Octokit.AsyncPaginationExtension/LazyList.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Collections;
+
+namespace Octokit.AsyncPaginationExtension
+{
+    internal class LazyList<T> : IReadOnlyList<T>
+    {
+        private readonly Func<int, T> _generator;
+        private readonly List<T?> _list = new();
+
+        public LazyList(Func<int, T> generator)
+        {
+            _generator = generator;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var i = 0;
+            while (true) yield return this[i++];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Count => int.MaxValue;
+
+        public T this[int index]
+        {
+            get
+            {
+                if (_list.Count <= index) _list.AddRange(Enumerable.Repeat<T?>(default, index - _list.Count + 1));
+                return _list[index] ??= _generator(index);
+            }
+        }
+    }
+}
---
+++ Octokit.AsyncPaginationExtension/PaginatedList.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+
+namespace Octokit.AsyncPaginationExtension
+{
+    internal class PaginatedList<T> : IPaginatedList<T>
+    {
+        private readonly LazyList<Task<IReadOnlyList<T>>> _pages;
+        private readonly int _pageSize;
+
+        internal PaginatedList(Func<ApiOptions, Task<IReadOnlyList<T>>> getPage, int pageSize)
+        {
+            _pages = new(i => getPage(new()
+            {
+                StartPage = i,
+                PageSize = pageSize,
+            }));
+            _pageSize = pageSize;
+        }
+
+        private async Task<T?> Get(int index)
+        {
+            var page = await _pages[index / _pageSize].ConfigureAwait(false);
+            index %= _pageSize;
+            return page.Count > index ? page[index] : default;
+        }
+
+        public Task<T?> this[int index] => index >= 0 ? Get(index) : throw new ArgumentOutOfRangeException(nameof(index), index, "The index must be positive.");
+
+        public async IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = new())
+        {
+            var pageNum = 0;
+            while (await _pages[pageNum++].ConfigureAwait(false) is { Count: > 0 } page)
+            {
+                foreach (var t in page)
+                {
+                    yield return t;
+                }
+            }
+        }
+    }
+}
