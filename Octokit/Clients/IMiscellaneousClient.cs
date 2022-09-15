0606ed34b93dea0d755374790e00a186b7230b53
Sean Killeen <SeanKilleen@gmail.com>
2022-09-15 11:42:10 -0400

    Add some of the method level messages

--- Octokit/Clients/IMiscellaneousClient.cs
+++ Octokit/Clients/IMiscellaneousClient.cs
@@ -4,91 +4,97 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Octokit
 {
+  /// <summary>
+  /// A client for GitHub's miscellaneous APIs.
+  /// </summary>
+  /// <remarks>
+  /// See the <a href="http://developer.github.com/v3/misc/">Miscellaneous API documentation</a> for more details.
+  /// </remarks>
+  public interface IMiscellaneousClient
+  {
     /// <summary>
-    /// A client for GitHub's miscellaneous APIs.
+    /// Gets all the emojis available to use on GitHub.
     /// </summary>
+    /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+    /// <returns>An <see cref="IReadOnlyDictionary{TKey,TValue}"/> of emoji and their URI.</returns>
     /// <remarks>
-    /// See the <a href="http://developer.github.com/v3/misc/">Miscellaneous API documentation</a> for more details.
+    /// Refer to the API documentation for more information: https://docs.github.com/en/rest/emojis
     /// </remarks>
-    public interface IMiscellaneousClient
-    {
-        /// <summary>
-        /// Gets all the emojis available to use on GitHub.
-        /// </summary>
-        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
-        /// <returns>An <see cref="IReadOnlyDictionary{TKey,TValue}"/> of emoji and their URI.</returns>
-        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
-        [ExcludeFromPaginationApiOptionsConventionTest("Pagination not supported by GitHub API (tested 29/08/2017)")]
-        Task<IReadOnlyList<Emoji>> GetAllEmojis();
+    [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
+    [ExcludeFromPaginationApiOptionsConventionTest("Pagination not supported by GitHub API (tested 29/08/2017)")]
+    Task<IReadOnlyList<Emoji>> GetAllEmojis();
 
-        /// <summary>
-        /// Gets the rendered Markdown for the specified plain-text Markdown document.
-        /// </summary>
-        /// <param name="markdown">A plain-text Markdown document</param>
-        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
-        /// <returns>The rendered Markdown.</returns>
-        Task<string> RenderRawMarkdown(string markdown);
+    /// <summary>
+    /// Gets the rendered Markdown for the specified plain-text Markdown document.
+    /// </summary>
+    /// <param name="markdown">A plain-text Markdown document</param>
+    /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+    /// <returns>The rendered Markdown.</returns>
+    /// <remarks>
+    /// Refer to the API documentation for more information: https://docs.github.com/en/rest/markdown
+    /// </remarks>
+    Task<string> RenderRawMarkdown(string markdown);
 
-        /// <summary>
-        /// Gets the rendered Markdown for an arbitrary markdown document.
-        /// </summary>
-        /// <param name="markdown">An arbitrary Markdown document</param>
-        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
-        /// <returns>The rendered Markdown.</returns>
-        Task<string> RenderArbitraryMarkdown(NewArbitraryMarkdown markdown);
+    /// <summary>
+    /// Gets the rendered Markdown for an arbitrary markdown document.
+    /// </summary>
+    /// <param name="markdown">An arbitrary Markdown document</param>
+    /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+    /// <returns>The rendered Markdown.</returns>
+    Task<string> RenderArbitraryMarkdown(NewArbitraryMarkdown markdown);
 
-        /// <summary>
-        /// List all templates available to pass as an option when creating a repository.
-        /// </summary>
-        /// <returns>A list of template names</returns>
-        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
-        [ExcludeFromPaginationApiOptionsConventionTest("Pagination not supported by GitHub API (tested 29/08/2017)")]
-        Task<IReadOnlyList<string>> GetAllGitIgnoreTemplates();
+    /// <summary>
+    /// List all templates available to pass as an option when creating a repository.
+    /// </summary>
+    /// <returns>A list of template names</returns>
+    [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
+    [ExcludeFromPaginationApiOptionsConventionTest("Pagination not supported by GitHub API (tested 29/08/2017)")]
+    Task<IReadOnlyList<string>> GetAllGitIgnoreTemplates();
 
-        /// <summary>
-        /// Retrieves the source for a single GitIgnore template
-        /// </summary>
-        /// <param name="templateName"></param>
-        /// <returns>A template and its source</returns>
-        Task<GitIgnoreTemplate> GetGitIgnoreTemplate(string templateName);
+    /// <summary>
+    /// Retrieves the source for a single GitIgnore template
+    /// </summary>
+    /// <param name="templateName"></param>
+    /// <returns>A template and its source</returns>
+    Task<GitIgnoreTemplate> GetGitIgnoreTemplate(string templateName);
 
-        /// <summary>
-        /// Returns a list of the licenses shown in the license picker on GitHub.com. This is not a comprehensive
-        /// list of all possible OSS licenses.
-        /// </summary>
-        /// <returns>A list of licenses available on the site</returns>
-        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
-        Task<IReadOnlyList<LicenseMetadata>> GetAllLicenses();
+    /// <summary>
+    /// Returns a list of the licenses shown in the license picker on GitHub.com. This is not a comprehensive
+    /// list of all possible OSS licenses.
+    /// </summary>
+    /// <returns>A list of licenses available on the site</returns>
+    [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
+    Task<IReadOnlyList<LicenseMetadata>> GetAllLicenses();
 
-        /// <summary>
-        /// Returns a list of the licenses shown in the license picker on GitHub.com. This is not a comprehensive
-        /// list of all possible OSS licenses.
-        /// </summary>
-        /// <param name="options">Options for changing the API response</param>
-        /// <returns>A list of licenses available on the site</returns>
-        Task<IReadOnlyList<LicenseMetadata>> GetAllLicenses(ApiOptions options);
+    /// <summary>
+    /// Returns a list of the licenses shown in the license picker on GitHub.com. This is not a comprehensive
+    /// list of all possible OSS licenses.
+    /// </summary>
+    /// <param name="options">Options for changing the API response</param>
+    /// <returns>A list of licenses available on the site</returns>
+    Task<IReadOnlyList<LicenseMetadata>> GetAllLicenses(ApiOptions options);
 
-        /// <summary>
-        /// Retrieves a license based on the license key such as "MIT"
-        /// </summary>
-        /// <param name="key">The license identifier to look for</param>
-        /// <returns>A <see cref="License" /> that includes the license key, text, and attributes of the license.</returns>
-        Task<License> GetLicense(string key);
+    /// <summary>
+    /// Retrieves a license based on the license key such as "MIT"
+    /// </summary>
+    /// <param name="key">The license identifier to look for</param>
+    /// <returns>A <see cref="License" /> that includes the license key, text, and attributes of the license.</returns>
+    Task<License> GetLicense(string key);
 
-        /// <summary>
-        /// Gets API Rate Limits (API service rather than header info).
-        /// </summary>
-        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
-        /// <returns>An <see cref="MiscellaneousRateLimit"/> of Rate Limits.</returns>
-        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
-        Task<MiscellaneousRateLimit> GetRateLimits();
+    /// <summary>
+    /// Gets API Rate Limits (API service rather than header info).
+    /// </summary>
+    /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+    /// <returns>An <see cref="MiscellaneousRateLimit"/> of Rate Limits.</returns>
+    [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
+    Task<MiscellaneousRateLimit> GetRateLimits();
 
-        /// <summary>
-        /// Retrieves information about GitHub.com, the service or a GitHub Enterprise installation.
-        /// </summary>
-        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
-        /// <returns>An <see cref="Meta"/> containing metadata about the GitHub instance.</returns>
-        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
-        Task<Meta> GetMetadata();
-    }
+    /// <summary>
+    /// Retrieves information about GitHub.com, the service or a GitHub Enterprise installation.
+    /// </summary>
+    /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+    /// <returns>An <see cref="Meta"/> containing metadata about the GitHub instance.</returns>
+    [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
+    Task<Meta> GetMetadata();
+  }
 }
--- Octokit/Clients/MiscellaneousClient.cs
+++ Octokit/Clients/MiscellaneousClient.cs
@@ -5,139 +5,145 @@ using System.Threading.Tasks;
 
 namespace Octokit
 {
+  /// <summary>
+  /// A client for GitHub's miscellaneous APIs.
+  /// </summary>
+  /// <remarks>
+  /// See the <a href="http://developer.github.com/v3/misc/">Miscellaneous API documentation</a> for more details.
+  /// </remarks>
+  public class MiscellaneousClient : ApiClient, IMiscellaneousClient
+  {
     /// <summary>
-    /// A client for GitHub's miscellaneous APIs.
+    ///     Initializes a new GitHub miscellaneous API client.
     /// </summary>
-    /// <remarks>
-    /// See the <a href="http://developer.github.com/v3/misc/">Miscellaneous API documentation</a> for more details.
-    /// </remarks>
-    public class MiscellaneousClient : ApiClient, IMiscellaneousClient
+    /// <param name="apiConnection">An API connection.</param>
+    public MiscellaneousClient(IApiConnection apiConnection)
+        : base(apiConnection)
     {
-        /// <summary>
-        ///     Initializes a new GitHub miscellaneous API client.
-        /// </summary>
-        /// <param name="apiConnection">An API connection.</param>
-        public MiscellaneousClient(IApiConnection apiConnection)
-            : base(apiConnection)
-        {
-        }
-
-        /// <summary>
-        /// Gets all the emojis available to use on GitHub.
-        /// </summary>
-        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
-        /// <returns>An <see cref="IReadOnlyDictionary{TKey,TValue}"/> of emoji and their URI.</returns>
-        [ManualRoute("GET", "/emojis")]
-        public Task<IReadOnlyList<Emoji>> GetAllEmojis()
-        {
-            return ApiConnection.GetAll<Emoji>(ApiUrls.Emojis());
-        }
-
-        /// <summary>
-        /// Gets the rendered Markdown for the specified plain-text Markdown document.
-        /// </summary>
-        /// <param name="markdown">A plain-text Markdown document</param>
-        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
-        /// <returns>The rendered Markdown.</returns>
-        [ManualRoute("POST", "/markdown/raw")]
-        public Task<string> RenderRawMarkdown(string markdown)
-        {
-            return ApiConnection.Post<string>(ApiUrls.RawMarkdown(), markdown, "text/html", "text/plain");
-        }
-
-        /// <summary>
-        /// Gets the rendered Markdown for an arbitrary markdown document.
-        /// </summary>
-        /// <param name="markdown">An arbitrary Markdown document</param>
-        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
-        /// <returns>The rendered Markdown.</returns>
-        [ManualRoute("POST", "/markdown")]
-        public Task<string> RenderArbitraryMarkdown(NewArbitraryMarkdown markdown)
-        {
-            return ApiConnection.Post<string>(ApiUrls.Markdown(), markdown, "text/html", "text/plain");
-        }
-
-        /// <summary>
-        /// List all templates available to pass as an option when creating a repository.
-        /// </summary>
-        /// <returns>A list of template names</returns>
-        [ManualRoute("GET", "/gitignore/templates")]
-        public Task<IReadOnlyList<string>> GetAllGitIgnoreTemplates()
-        {
-            return ApiConnection.GetAll<string>(ApiUrls.GitIgnoreTemplates());
-        }
-
-        /// <summary>
-        /// Retrieves the source for a single GitIgnore template
-        /// </summary>
-        /// <param name="templateName"></param>
-        /// <returns>A template and its source</returns>
-        [ManualRoute("GET", "/gitignore/templates/{name}")]
-        public Task<GitIgnoreTemplate> GetGitIgnoreTemplate(string templateName)
-        {
-            Ensure.ArgumentNotNullOrEmptyString(templateName, nameof(templateName));
-
-            return ApiConnection.Get<GitIgnoreTemplate>(ApiUrls.GitIgnoreTemplates(templateName));
-        }
-
-        /// <summary>
-        /// Returns a list of the licenses shown in the license picker on GitHub.com. This is not a comprehensive
-        /// list of all possible OSS licenses.
-        /// </summary>
-        /// <returns>A list of licenses available on the site</returns>
-        [ManualRoute("GET", "/licenses")]
-        public Task<IReadOnlyList<LicenseMetadata>> GetAllLicenses()
-        {
-            return GetAllLicenses(ApiOptions.None);
-        }
-
-        /// <summary>
-        /// Returns a list of the licenses shown in the license picker on GitHub.com. This is not a comprehensive
-        /// list of all possible OSS licenses.
-        /// </summary>
-        /// <param name="options">Options for changing the API response</param>
-        /// <returns>A list of licenses available on the site</returns>
-        [ManualRoute("GET", "/licenses")]
-        public Task<IReadOnlyList<LicenseMetadata>> GetAllLicenses(ApiOptions options)
-        {
-            Ensure.ArgumentNotNull(options, "options");
-
-            return ApiConnection.GetAll<LicenseMetadata>(ApiUrls.Licenses(), options);
-        }
-
-        /// <summary>
-        /// Retrieves a license based on the license key such as "mit"
-        /// </summary>
-        /// <param name="key"></param>
-        /// <returns>A <see cref="License" /> that includes the license key, text, and attributes of the license.</returns>
-        [ManualRoute("GET", "/licenses/{key}")]
-        public Task<License> GetLicense(string key)
-        {
-            return ApiConnection.Get<License>(ApiUrls.Licenses(key));
-        }
-
-        /// <summary>
-        /// Gets API Rate Limits (API service rather than header info).
-        /// </summary>
-        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
-        /// <returns>An <see cref="MiscellaneousRateLimit"/> of Rate Limits.</returns>
-        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
-        [ManualRoute("GET", "/rate_limit")]
-        public Task<MiscellaneousRateLimit> GetRateLimits()
-        {
-            return ApiConnection.Get<MiscellaneousRateLimit>(ApiUrls.RateLimit());
-        }
-
-        /// <summary>
-        /// Retrieves information about GitHub.com, the service or a GitHub Enterprise installation.
-        /// </summary>
-        /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
-        /// <returns>An <see cref="Meta"/> containing metadata about the GitHub instance.</returns>
-        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
-        [ManualRoute("GET", "/meta")]
-        public Task<Meta> GetMetadata()
-        {
-            return ApiConnection.Get<Meta>(ApiUrls.Meta());
-        }
     }
+
+    /// <summary>
+    /// Gets all the emojis available to use on GitHub.
+    /// </summary>
+    /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+    /// <returns>An <see cref="IReadOnlyDictionary{TKey,TValue}"/> of emoji and their URI.</returns>
+    /// <remarks>
+    /// Refer to the API documentation for more information: https://docs.github.com/en/rest/emojis
+    /// </remarks>
+    [ManualRoute("GET", "/emojis")]
+    public Task<IReadOnlyList<Emoji>> GetAllEmojis()
+    {
+      return ApiConnection.GetAll<Emoji>(ApiUrls.Emojis());
+    }
+
+    /// <summary>
+    /// Gets the rendered Markdown for the specified plain-text Markdown document.
+    /// </summary>
+    /// <param name="markdown">A plain-text Markdown document</param>
+    /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+    /// <returns>The rendered Markdown.</returns>
+    /// <remarks>
+    /// Refer to the API documentation for more information: https://docs.github.com/en/rest/markdown
+    /// </remarks>
+    [ManualRoute("POST", "/markdown/raw")]
+    public Task<string> RenderRawMarkdown(string markdown)
+    {
+      return ApiConnection.Post<string>(ApiUrls.RawMarkdown(), markdown, "text/html", "text/plain");
+    }
+
+    /// <summary>
+    /// Gets the rendered Markdown for an arbitrary markdown document.
+    /// </summary>
+    /// <param name="markdown">An arbitrary Markdown document</param>
+    /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+    /// <returns>The rendered Markdown.</returns>
+    [ManualRoute("POST", "/markdown")]
+    public Task<string> RenderArbitraryMarkdown(NewArbitraryMarkdown markdown)
+    {
+      return ApiConnection.Post<string>(ApiUrls.Markdown(), markdown, "text/html", "text/plain");
+    }
+
+    /// <summary>
+    /// List all templates available to pass as an option when creating a repository.
+    /// </summary>
+    /// <returns>A list of template names</returns>
+    [ManualRoute("GET", "/gitignore/templates")]
+    public Task<IReadOnlyList<string>> GetAllGitIgnoreTemplates()
+    {
+      return ApiConnection.GetAll<string>(ApiUrls.GitIgnoreTemplates());
+    }
+
+    /// <summary>
+    /// Retrieves the source for a single GitIgnore template
+    /// </summary>
+    /// <param name="templateName"></param>
+    /// <returns>A template and its source</returns>
+    [ManualRoute("GET", "/gitignore/templates/{name}")]
+    public Task<GitIgnoreTemplate> GetGitIgnoreTemplate(string templateName)
+    {
+      Ensure.ArgumentNotNullOrEmptyString(templateName, nameof(templateName));
+
+      return ApiConnection.Get<GitIgnoreTemplate>(ApiUrls.GitIgnoreTemplates(templateName));
+    }
+
+    /// <summary>
+    /// Returns a list of the licenses shown in the license picker on GitHub.com. This is not a comprehensive
+    /// list of all possible OSS licenses.
+    /// </summary>
+    /// <returns>A list of licenses available on the site</returns>
+    [ManualRoute("GET", "/licenses")]
+    public Task<IReadOnlyList<LicenseMetadata>> GetAllLicenses()
+    {
+      return GetAllLicenses(ApiOptions.None);
+    }
+
+    /// <summary>
+    /// Returns a list of the licenses shown in the license picker on GitHub.com. This is not a comprehensive
+    /// list of all possible OSS licenses.
+    /// </summary>
+    /// <param name="options">Options for changing the API response</param>
+    /// <returns>A list of licenses available on the site</returns>
+    [ManualRoute("GET", "/licenses")]
+    public Task<IReadOnlyList<LicenseMetadata>> GetAllLicenses(ApiOptions options)
+    {
+      Ensure.ArgumentNotNull(options, "options");
+
+      return ApiConnection.GetAll<LicenseMetadata>(ApiUrls.Licenses(), options);
+    }
+
+    /// <summary>
+    /// Retrieves a license based on the license key such as "mit"
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns>A <see cref="License" /> that includes the license key, text, and attributes of the license.</returns>
+    [ManualRoute("GET", "/licenses/{key}")]
+    public Task<License> GetLicense(string key)
+    {
+      return ApiConnection.Get<License>(ApiUrls.Licenses(key));
+    }
+
+    /// <summary>
+    /// Gets API Rate Limits (API service rather than header info).
+    /// </summary>
+    /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+    /// <returns>An <see cref="MiscellaneousRateLimit"/> of Rate Limits.</returns>
+    [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
+    [ManualRoute("GET", "/rate_limit")]
+    public Task<MiscellaneousRateLimit> GetRateLimits()
+    {
+      return ApiConnection.Get<MiscellaneousRateLimit>(ApiUrls.RateLimit());
+    }
+
+    /// <summary>
+    /// Retrieves information about GitHub.com, the service or a GitHub Enterprise installation.
+    /// </summary>
+    /// <exception cref="ApiException">Thrown when a general API error occurs.</exception>
+    /// <returns>An <see cref="Meta"/> containing metadata about the GitHub instance.</returns>
+    [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate")]
+    [ManualRoute("GET", "/meta")]
+    public Task<Meta> GetMetadata()
+    {
+      return ApiConnection.Get<Meta>(ApiUrls.Meta());
+    }
+  }
 }
