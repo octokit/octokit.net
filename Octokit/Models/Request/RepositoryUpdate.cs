0129f1ad9c4972a17d7e58db0428e305fad4846a
Dirty Gooback <19241000+dirtygooback@users.noreply.github.com>
2022-10-01 09:43:18 -0400

    Add missing repository items

--- Octokit/Models/Request/RepositoryUpdate.cs
+++ Octokit/Models/Request/RepositoryUpdate.cs
@@ -1,119 +1,132 @@
 ﻿using Octokit.Internal;
+
 using System;
 using System.Diagnostics;
 
 namespace Octokit
 {
-    /// <summary>
-    /// Represents updatable fields on a repository. Values that are null will not be sent in the request.
-    /// Use string.empty if you want to clear a value.
-    /// </summary>
-    [DebuggerDisplay("{DebuggerDisplay,nq}")]
-    public class RepositoryUpdate
-    {
-        /// <summary>
-        /// Creates an object that describes an update to a repository on GitHub.
-        /// </summary>
-        public RepositoryUpdate() { }
+	/// <summary>
+	/// Represents updatable fields on a repository. Values that are null will not be sent in the request.
+	/// Use string.empty if you want to clear a value.
+	/// </summary>
+	[DebuggerDisplay("{DebuggerDisplay,nq}")]
+	public class RepositoryUpdate
+	{
+		/// <summary>
+		/// Creates an object that describes an update to a repository on GitHub.
+		/// </summary>
+		public RepositoryUpdate() { }
 
-        /// <summary>
-        /// Required. Gets or sets the repository name.
-        /// </summary>
-        public string Name { get; set; }
+		/// <summary>
+		/// Required. Gets or sets the repository name.
+		/// </summary>
+		public string Name { get; set; }
 
-        /// <summary>
-        /// Optional. Gets or sets the repository description. The default is null (do not update)
-        /// </summary>
-        public string Description { get; set; }
+		/// <summary>
+		/// Optional. Gets or sets the repository description. The default is null (do not update)
+		/// </summary>
+		public string Description { get; set; }
 
-        /// <summary>
-        /// Optional. Gets or sets the repository homepage url. The default is null (do not update).
-        /// </summary>
-        public string Homepage { get; set; }
+		/// <summary>
+		/// Optional. Gets or sets the repository homepage url. The default is null (do not update).
+		/// </summary>
+		public string Homepage { get; set; }
 
-        /// <summary>
-        /// Gets or sets whether to make the repository private. The default is null (do not update).
-        /// </summary>
-        public bool? Private { get; set; }
+		/// <summary>
+		/// Gets or sets whether to make the repository private. The default is null (do not update).
+		/// </summary>
+		public bool? Private { get; set; }
 
-        /// <summary>
-        /// Optional. Gets or sets whether the new repository is public, private, or internal. A value provided here overrides any value set in the existing private field.
-        /// </summary>
-        public RepositoryVisibility? Visibility { get; set; }
+		/// <summary>
+		/// Optional. Gets or sets whether the new repository is public, private, or internal. A value provided here overrides any value set in the existing private field.
+		/// </summary>
+		public RepositoryVisibility? Visibility { get; set; }
 
-        // Yet to be implemented
-        //public object SecurityAndAnalysis { get; set; }
+		// Yet to be implemented
+		//public object SecurityAndAnalysis { get; set; }
 
-        /// <summary>
-        /// Gets or sets whether to enable issues for the repository. The default is null (do not update). The default when created is true.
-        /// </summary>
-        public bool? HasIssues { get; set; }
+		/// <summary>
+		/// Gets or sets whether to enable issues for the repository. The default is null (do not update). The default when created is true.
+		/// </summary>
+		public bool? HasIssues { get; set; }
 
-        /// <summary>
-        /// Gets or sets whether to enable projects for the repository. The default is null (do not update). The default when created is true.
-        /// </summary>
-        public bool? HasProjects { get; set; }
+		/// <summary>
+		/// Gets or sets whether to enable projects for the repository. The default is null (do not update). The default when created is true.
+		/// </summary>
+		public bool? HasProjects { get; set; }
 
-        /// <summary>
-        /// Optional. Gets or sets whether to enable the wiki for the repository. The default is null (do not update). The default when created is true.
-        /// </summary>
-        public bool? HasWiki { get; set; }
+		/// <summary>
+		/// Optional. Gets or sets whether to enable the wiki for the repository. The default is null (do not update). The default when created is true.
+		/// </summary>
+		public bool? HasWiki { get; set; }
 
-        /// <summary>
-        /// Optional. Gets or sets whether to enable downloads for the repository. The default is null (do not update). No longer appears on the documentation but still works.
-        /// </summary>
-        public bool? HasDownloads { get; set; }
+		/// <summary>
+		/// Optional. Gets or sets whether to enable downloads for the repository. The default is null (do not update). No longer appears on the documentation but still works.
+		/// </summary>
+		public bool? HasDownloads { get; set; }
 
-        /// <summary>
-        /// Optional. Gets or sets whether the repository is a template. The default is null (do not update). The default when created is false.
-        /// </summary>
-        public bool? IsTemplate { get; set; }
+		/// <summary>
+		/// Optional. Gets or sets whether the repository is a template. The default is null (do not update). The default when created is false.
+		/// </summary>
+		public bool? IsTemplate { get; set; }
 
-        /// <summary>
-        /// Optional. Gets or sets the default branch. The default is null (do not update).
-        /// </summary>
-        public string DefaultBranch { get; set; }
+		/// <summary>
+		/// Optional. Gets or sets the default branch. The default is null (do not update).
+		/// </summary>
+		public string DefaultBranch { get; set; }
 
-        /// <summary>
-        /// Optional. Allows the "Squash Merge" merge method to be used. The default is null (do not update). The default when created is true.
-        /// </summary>
-        public bool? AllowSquashMerge { get; set; }
+		/// <summary>
+		/// Optional. Allows the "Squash Merge" merge method to be used. The default is null (do not update). The default when created is true.
+		/// </summary>
+		public bool? AllowSquashMerge { get; set; }
 
-        /// <summary>
-        /// Optional. Allows the "Create a merge commit" merge method to be used. The default is null (do not update). The default when created is true.
-        /// </summary>
-        public bool? AllowMergeCommit { get; set; }
+		/// <summary>
+		/// Optional. Allows the "Create a merge commit" merge method to be used. The default is null (do not update). The default when created is true.
+		/// </summary>
+		public bool? AllowMergeCommit { get; set; }
 
-        /// <summary>
-        /// Optional. Allows the "Rebase and Merge" method to be used. The default is null (do not update). The default when created is true.
-        /// </summary>
-        public bool? AllowRebaseMerge { get; set; }
+		/// <summary>
+		/// Optional. Allows the "Rebase and Merge" method to be used. The default is null (do not update). The default when created is true.
+		/// </summary>
+		public bool? AllowRebaseMerge { get; set; }
 
-        /// <summary>
-        /// Optional. Allows the auto merge feature to be used. The default is null (do not update). The default when created is false.
-        /// </summary>
-        public bool? AllowAutoMerge { get; set; }
+		/// <summary>
+		/// Optional. Allows the auto merge feature to be used. The default is null (do not update). The default when created is false.
+		/// </summary>
+		public bool? AllowAutoMerge { get; set; }
 
-        /// <summary>
-        /// Optional. Automatically delete branches on PR merge. The default is null (do not update). The default when created is false.
-        /// </summary>
-        public bool? DeleteBranchOnMerge { get; set; }
+		/// <summary>
+		/// Optional. Automatically delete branches on PR merge. The default is null (do not update). The default when created is false.
+		/// </summary>
+		public bool? DeleteBranchOnMerge { get; set; }
 
-        /// <summary>
-        /// Optional. Automatically set the title of squashed commits to be the PR title. The default is null (do not update). The default when created is false.
-        /// </summary>
-        public bool? UseSquashPrTitleAsDefault { get; set; }
+		/// <summary>
+		/// Optional. Automatically set the title of squashed commits to be the PR title. The default is null (do not update). The default when created is false.
+		/// </summary>
+		[Obsolete("Use SquashMergeCommitTitle instead")]
 
-        /// <summary>
-        /// Optional. True to archive this repository.  Note: you cannot unarchive repositories through the API. The default is null (do not update). The default when created is false.
-        /// </summary>
-        public bool? Archived { get; set; }
+		public bool? UseSquashPrTitleAsDefault { get; set; }
 
-        /// <summary>
-        /// Optional. Get or set whether to allow this repository to be forked or not. The default is null (do not update). The default when created is false.
-        /// </summary>
-        public bool? AllowForking { get; set; }
+		/// <summary>
+		/// Optional. True to archive this repository.  Note: you cannot unarchive repositories through the API. The default is null (do not update). The default when created is false.
+		/// </summary>
+		public bool? Archived { get; set; }
 
-        internal string DebuggerDisplay => new SimpleJsonSerializer().Serialize(this);
-    }
+		/// <summary>
+		/// Optional. Get or set whether to allow this repository to be forked or not. The default is null (do not update). The default when created is false.
+		/// </summary>
+		public bool? AllowForking { get; set; }
+
+		/// <summary>
+		/// Optional. Get or set whether to require contributors to sign off on web-based commits. The default is null (no not require). The default when created is false.
+		/// </summary>
+		public bool? WebCommitSignoffRequired { get; set; }
+
+		/// <summary>
+		/// Optional. Get or set to always allow a pull request head branch that is behind its base branch to be updated even if it is not required to be up to date before merging. The default is null (do not allow). The default when created is false.
+		/// </summary>
+		public bool? AllowUpdateBranch { get; set; }
+
+		internal string DebuggerDisplay => new SimpleJsonSerializer().Serialize(this);
+	}
 }
--- Octokit/Models/Response/Repository.cs
+++ Octokit/Models/Response/Repository.cs
@@ -7,165 +7,168 @@ using System.Linq;
 
 namespace Octokit
 {
-    [DebuggerDisplay("{DebuggerDisplay,nq}")]
-    public class Repository
-    {
-        public Repository() { }
+	[DebuggerDisplay("{DebuggerDisplay,nq}")]
+	public class Repository
+	{
+		public Repository() { }
 
-        public Repository(long id)
-        {
-            Id = id;
-        }
+		public Repository(long id)
+		{
+			Id = id;
+		}
 
-        public Repository(string url, string htmlUrl, string cloneUrl, string gitUrl, string sshUrl, string svnUrl, string mirrorUrl, long id, string nodeId, User owner, string name, string fullName, bool isTemplate, string description, string homepage, string language, bool @private, bool fork, int forksCount, int stargazersCount, string defaultBranch, int openIssuesCount, DateTimeOffset? pushedAt, DateTimeOffset createdAt, DateTimeOffset updatedAt, RepositoryPermissions permissions, Repository parent, Repository source, LicenseMetadata license, bool hasIssues, bool hasWiki, bool hasDownloads, bool hasPages, int subscribersCount, long size, bool? allowRebaseMerge, bool? allowSquashMerge, bool? allowMergeCommit, bool archived, int watchersCount, bool? deleteBranchOnMerge, RepositoryVisibility visibility, IEnumerable<string> topics, bool? allowAutoMerge)
-        {
-            Url = url;
-            HtmlUrl = htmlUrl;
-            CloneUrl = cloneUrl;
-            GitUrl = gitUrl;
-            SshUrl = sshUrl;
-            SvnUrl = svnUrl;
-            MirrorUrl = mirrorUrl;
-            Id = id;
-            NodeId = nodeId;
-            Owner = owner;
-            Name = name;
-            FullName = fullName;
-            IsTemplate = isTemplate;
-            Description = description;
-            Homepage = homepage;
-            Language = language;
-            Private = @private;
-            Fork = fork;
-            ForksCount = forksCount;
-            StargazersCount = stargazersCount;
-            DefaultBranch = defaultBranch;
-            OpenIssuesCount = openIssuesCount;
-            PushedAt = pushedAt;
-            CreatedAt = createdAt;
-            UpdatedAt = updatedAt;
-            Permissions = permissions;
-            Parent = parent;
-            Source = source;
-            License = license;
-            HasIssues = hasIssues;
-            HasWiki = hasWiki;
-            HasDownloads = hasDownloads;
-            HasPages = hasPages;
-            SubscribersCount = subscribersCount;
-            Size = size;
-            AllowRebaseMerge = allowRebaseMerge;
-            AllowSquashMerge = allowSquashMerge;
-            AllowMergeCommit = allowMergeCommit;
-            Archived = archived;
+		public Repository(string url, string htmlUrl, string cloneUrl, string gitUrl, string sshUrl, string svnUrl, string mirrorUrl, long id, string nodeId, User owner, string name, string fullName, bool isTemplate, string description, string homepage, string language, bool @private, bool fork, int forksCount, int stargazersCount, string defaultBranch, int openIssuesCount, DateTimeOffset? pushedAt, DateTimeOffset createdAt, DateTimeOffset updatedAt, RepositoryPermissions permissions, Repository parent, Repository source, LicenseMetadata license, bool hasIssues, bool hasWiki, bool hasDownloads, bool hasPages, bool hasProjects, int subscribersCount, long size, bool? allowRebaseMerge, bool? allowSquashMerge, bool? allowMergeCommit, bool archived, int watchersCount, bool? deleteBranchOnMerge, RepositoryVisibility visibility, IEnumerable<string> topics, bool? allowAutoMerge)
+		{
+			Url = url;
+			HtmlUrl = htmlUrl;
+			CloneUrl = cloneUrl;
+			GitUrl = gitUrl;
+			SshUrl = sshUrl;
+			SvnUrl = svnUrl;
+			MirrorUrl = mirrorUrl;
+			Id = id;
+			NodeId = nodeId;
+			Owner = owner;
+			Name = name;
+			FullName = fullName;
+			IsTemplate = isTemplate;
+			Description = description;
+			Homepage = homepage;
+			Language = language;
+			Private = @private;
+			Fork = fork;
+			ForksCount = forksCount;
+			StargazersCount = stargazersCount;
+			DefaultBranch = defaultBranch;
+			OpenIssuesCount = openIssuesCount;
+			PushedAt = pushedAt;
+			CreatedAt = createdAt;
+			UpdatedAt = updatedAt;
+			Permissions = permissions;
+			Parent = parent;
+			Source = source;
+			License = license;
+			HasIssues = hasIssues;
+			HasWiki = hasWiki;
+			HasDownloads = hasDownloads;
+			HasPages = hasPages;
+			HasProjects = hasProjects;
+			SubscribersCount = subscribersCount;
+			Size = size;
+			AllowRebaseMerge = allowRebaseMerge;
+			AllowSquashMerge = allowSquashMerge;
+			AllowMergeCommit = allowMergeCommit;
+			Archived = archived;
 #pragma warning disable CS0618 // Type or member is obsolete
-            WatchersCount = watchersCount;
+			WatchersCount = watchersCount;
 #pragma warning restore CS0618 // Type or member is obsolete
-            Topics = topics.ToList();
-            DeleteBranchOnMerge = deleteBranchOnMerge;
-            Visibility = visibility;
-            AllowAutoMerge = allowAutoMerge;
-        }
+			Topics = topics.ToList();
+			DeleteBranchOnMerge = deleteBranchOnMerge;
+			Visibility = visibility;
+			AllowAutoMerge = allowAutoMerge;
+		}
 
-        public string Url { get; private set; }
+		public string Url { get; private set; }
 
-        public string HtmlUrl { get; private set; }
+		public string HtmlUrl { get; private set; }
 
-        public string CloneUrl { get; private set; }
+		public string CloneUrl { get; private set; }
 
-        public string GitUrl { get; private set; }
+		public string GitUrl { get; private set; }
 
-        public string SshUrl { get; private set; }
+		public string SshUrl { get; private set; }
 
-        public string SvnUrl { get; private set; }
+		public string SvnUrl { get; private set; }
 
-        public string MirrorUrl { get; private set; }
+		public string MirrorUrl { get; private set; }
 
-        public long Id { get; private set; }
+		public long Id { get; private set; }
 
-        /// <summary>
-        /// GraphQL Node Id
-        /// </summary>
-        public string NodeId { get; private set; }
+		/// <summary>
+		/// GraphQL Node Id
+		/// </summary>
+		public string NodeId { get; private set; }
 
-        public User Owner { get; private set; }
+		public User Owner { get; private set; }
 
-        public string Name { get; private set; }
+		public string Name { get; private set; }
 
-        public string FullName { get; private set; }
+		public string FullName { get; private set; }
 
-        public bool IsTemplate { get; private set; }
+		public bool IsTemplate { get; private set; }
 
-        public string Description { get; private set; }
+		public string Description { get; private set; }
 
-        public string Homepage { get; private set; }
+		public string Homepage { get; private set; }
 
-        public string Language { get; private set; }
+		public string Language { get; private set; }
 
-        public bool Private { get; private set; }
+		public bool Private { get; private set; }
 
-        public bool Fork { get; private set; }
+		public bool Fork { get; private set; }
 
-        public int ForksCount { get; private set; }
+		public int ForksCount { get; private set; }
 
-        public int StargazersCount { get; private set; }
+		public int StargazersCount { get; private set; }
 
-        [Obsolete("WatchersCount returns the same data as StargazersCount. You are likely looking to use SubscribersCount. Update your code to use SubscribersCount, as this field will stop containing data in the future")]
-        public int WatchersCount { get; private set; }
+		[Obsolete("WatchersCount returns the same data as StargazersCount. You are likely looking to use SubscribersCount. Update your code to use SubscribersCount, as this field will stop containing data in the future")]
+		public int WatchersCount { get; private set; }
 
-        public string DefaultBranch { get; private set; }
+		public string DefaultBranch { get; private set; }
 
-        public int OpenIssuesCount { get; private set; }
+		public int OpenIssuesCount { get; private set; }
 
-        public DateTimeOffset? PushedAt { get; private set; }
+		public DateTimeOffset? PushedAt { get; private set; }
 
-        public DateTimeOffset CreatedAt { get; private set; }
+		public DateTimeOffset CreatedAt { get; private set; }
 
-        public DateTimeOffset UpdatedAt { get; private set; }
+		public DateTimeOffset UpdatedAt { get; private set; }
 
-        public RepositoryPermissions Permissions { get; private set; }
+		public RepositoryPermissions Permissions { get; private set; }
 
-        public Repository Parent { get; private set; }
+		public Repository Parent { get; private set; }
 
-        public Repository Source { get; private set; }
+		public Repository Source { get; private set; }
 
-        public LicenseMetadata License { get; private set; }
+		public LicenseMetadata License { get; private set; }
 
-        public bool HasIssues { get; private set; }
+		public bool HasIssues { get; private set; }
 
-        public bool HasWiki { get; private set; }
+		public bool HasWiki { get; private set; }
 
-        public bool HasDownloads { get; private set; }
+		public bool HasDownloads { get; private set; }
 
-        public bool? AllowRebaseMerge { get; private set; }
+		public bool? AllowRebaseMerge { get; private set; }
 
-        public bool? AllowSquashMerge { get; private set; }
+		public bool? AllowSquashMerge { get; private set; }
 
-        public bool? AllowMergeCommit { get; private set; }
+		public bool? AllowMergeCommit { get; private set; }
 
-        public bool HasPages { get; private set; }
+		public bool HasPages { get; private set; }
 
-        public int SubscribersCount { get; private set; }
+		public bool HasProjects { get; private set; }
 
-        public long Size { get; private set; }
+		public int SubscribersCount { get; private set; }
 
-        public bool Archived { get; private set; }
+		public long Size { get; private set; }
 
-        public IReadOnlyList<string> Topics { get; private set; }
+		public bool Archived { get; private set; }
 
-        public bool? DeleteBranchOnMerge { get; private set; }
+		public IReadOnlyList<string> Topics { get; private set; }
 
-        public RepositoryVisibility? Visibility { get; private set; }
+		public bool? DeleteBranchOnMerge { get; private set; }
 
-        public bool? AllowAutoMerge { get; private set; }
+		public RepositoryVisibility? Visibility { get; private set; }
 
-        internal string DebuggerDisplay
-        {
-            get
-            {
-                return string.Format(CultureInfo.InvariantCulture,
-                    "Repository: Id: {0} Owner: {1}, Name: {2}", Id, Owner, Name);
-            }
-        }
-    }
+		public bool? AllowAutoMerge { get; private set; }
+
+		internal string DebuggerDisplay
+		{
+			get
+			{
+				return string.Format(CultureInfo.InvariantCulture,
+					"Repository: Id: {0} Owner: {1}, Name: {2}", Id, Owner, Name);
+			}
+		}
+	}
 }
