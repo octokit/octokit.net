87ad0669c148b0dcbd8eb1623368f41914c10496
martincostello <martin@martincostello.com>
2022-10-20 15:05:30 +0100

    Fix URLs
    Fix documentation link for rerun API.

--- Octokit.Reactive/Clients/IObservableActionsWorkflowJobsClient.cs
+++ Octokit.Reactive/Clients/IObservableActionsWorkflowJobsClient.cs
@@ -15,7 +15,7 @@ namespace Octokit.Reactive
         /// Re-runs a specific workflow job in a repository.
         /// </summary>
         /// <remarks>
-        /// https://developer.github.com/v3/actions/workflow-jobs/#re-run-a-job-from-a-workflow-run
+        /// https://developer.github.com/v3/actions/workflow-runs/#re-run-a-job-from-a-workflow-run
         /// </remarks>
         /// <param name="owner">The owner of the repository.</param>
         /// <param name="name">The name of the repository.</param>
