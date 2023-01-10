d9c82dce02aa47b43b3da31ffb0e7bde85bb4925
Yankai Ren <renyankai532@hotmail.com>
2023-01-10 13:01:01 +0000

    added tests for CachingHttpClient constructor

---
+++ Octokit.Tests/Caching/CachingHttpClientTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NSubstitute;
+using Octokit.Caching;
+using Octokit.Internal;
+using Xunit;
+
+namespace Octokit.Tests.Caching
+{
+    public class CachingHttpClientTests
+    {
+        public class TheCtor
+        {
+            [Theory]
+            [InlineData(true, true)]
+            [InlineData(true, false)]
+            [InlineData(false, true)]
+            public void EnsuresNonNullArguments(bool httpClientIsNull, bool responseCacheIsNull)
+            {
+                var httpClient = httpClientIsNull ? null : Substitute.For<IHttpClient>();
+                var responseCache = responseCacheIsNull ? null : Substitute.For<IResponseCache>();
+
+                Assert.Throws<ArgumentNullException>(() => new CachingHttpClient(httpClient, responseCache));
+            }
+        }
+    }
+}
