cb8a7b2fca10670ae7e8df647b1f88da22bb64db
Yankai Ren <renyankai532@hotmail.com>
2023-01-10 18:08:29 +0000

    added unit tests for CachingHttpClient::Send

---
+++ Octokit.Tests/Helpers/CollectionComparer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Octokit.Tests.Helpers
+{
+    public class CollectionComparer<T> : IEqualityComparer<IReadOnlyCollection<T>>
+    {
+        public bool Equals(IReadOnlyCollection<T> x, IReadOnlyCollection<T> y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+
+            return x.Count == y.Count && x.Intersect(y).Count() == x.Count();
+        }
+
+        public int GetHashCode(IReadOnlyCollection<T> obj)
+        {
+            return obj.Count;
+        }
+    }
+}
---
+++ Octokit.Tests/Helpers/RateLimitComparer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Octokit.Tests.Helpers
+{
+    public class RateLimitComparer : IEqualityComparer<RateLimit>
+    {
+        public bool Equals(RateLimit x, RateLimit y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+
+            return x.Limit == y.Limit &&
+                   x.Remaining == y.Remaining &&
+                   x.ResetAsUtcEpochSeconds == y.ResetAsUtcEpochSeconds;
+        }
+
+        public int GetHashCode(RateLimit obj)
+        {
+            unchecked
+            {
+                var hashCode = obj.Limit;
+                hashCode = (hashCode * 397) ^ obj.Remaining;
+                hashCode = (hashCode * 397) ^ obj.ResetAsUtcEpochSeconds.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+}
