b7af2b1965dcd06fe122f5979cffd55834e98b09
notauserx <notauserx@users.noreply.github.com>
2023-01-24 20:38:21 +0600

    rename permission to TeamPermissionLegacy

--- Octokit/Models/Common/Permission.cs
+++ Octokit/Models/Common/Permission.cs
@@ -6,40 +6,17 @@ using Octokit.Internal;
 namespace Octokit
 {
     /// <summary>
-    /// Used to describe a permission level.
+    /// The permission to grant the team on this repository(Legacy).
     /// </summary>
     [SuppressMessage("Microsoft.Naming", "CA1711:IdentifiersShouldNotHaveIncorrectSuffix")]
-    public enum Permission
+    public enum TeamPermissionLegacy
     {
-        /// <summary>
-        /// team members can pull, push and administer these repositories.
-        /// </summary>
-        [Parameter(Value = "admin")]
-        Admin,
-                
-        /// <summary>
-        /// team members can manage the repository without access to sensitive or destructive actions. Recommended for project managers. Only applies to repositories owned by organizations.
-        /// </summary>
-        [Parameter(Value = "maintain")]
-        Maintain,
-        
-        /// <summary>
-        /// team members can proactively manage issues and pull requests without write access. Recommended for contributors who triage a repository. Only applies to repositories owned by organizations.
-        /// </summary>
-        [Parameter(Value = "triage")]
-        Triage,
-        
-        /// <summary>
-        /// team members can pull and push, but not administer these repositories
-        /// </summary>
+        [Parameter(Value = "pull")]
+        Pull,
         [Parameter(Value = "push")]
         Push,
-
-        /// <summary>
-        /// team members can pull, but not push to or administer these repositories
-        /// </summary>
-        [Parameter(Value = "pull")]
-        Pull
+        [Parameter(Value = "admin")]
+        Admin
     }
 
     /// <summary>
