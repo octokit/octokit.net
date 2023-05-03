897a2e913bd0b5ae8948e94f48c6c0bd0b9cde86
Matisse Hack <matissehack@github.com>
2023-05-03 10:09:13 -0700

    Fix runner application deserialization

--- Octokit/Models/Response/RunnerApplication.cs
+++ Octokit/Models/Response/RunnerApplication.cs
@@ -8,25 +8,27 @@ namespace Octokit
     {
         public RunnerApplication() { }
 
-        public RunnerApplication(string os, string architecture, string downloadUrl, string fileName)
+        public RunnerApplication(string os, string architecture, string downloadUrl, string filename, string tempDownloadToken)
         {
             Os = os;
             Architecture = architecture;
             DownloadUrl = downloadUrl;
-            FileName = fileName;
+            Filename = filename;
+            TempDownloadToken = tempDownloadToken;
         }
 
         public string Os { get; private set; }
         public string Architecture { get; private set; }
         public string DownloadUrl { get; private set; }
-        public string FileName { get; private set; }
+        public string Filename { get; private set; }
+        public string TempDownloadToken { get; private set; }
 
         internal string DebuggerDisplay
         {
             get
             {
-                return string.Format("Os: {0}; Architecture: {1}; DownloadUrl: {2}; FileName: {3};",
-                  Os, Architecture, DownloadUrl, FileName);
+                return string.Format("Os: {0}; Architecture: {1}; DownloadUrl: {2}; Filename: {3}; TempDownloadToken: {4}",
+                  Os, Architecture, DownloadUrl, Filename, TempDownloadToken);
             }
         }
     }
---
+++ Octokit/Models/Response/RunnerLabel.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+
+namespace Octokit
+{
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class RunnerLabel
+    {
+        public RunnerLabel() { }
+
+        public RunnerLabel(long id, string name, string type)
+        {
+            Id = id;
+            Name = name;
+            Type = type;
+        }
+
+        public long Id { get; private set; }
+        public string Name { get; private set; }
+        public string Type { get; private set; }
+
+        internal string DebuggerDisplay
+        {
+            get { return string.Format(CultureInfo.InvariantCulture, "Id: {0}; Name: {1}; Type: {2};", Id, Name, Type); }
+        }
+    }
+}
