729f261ecbe7e43ccf59ae22a166bab58e047e45
Jasper Arildslund <47112524+Simplicitdk@users.noreply.github.com>
2023-08-03 23:26:58 +0200

    [feat]: Support make_latest for create and update release endpoints

--- Octokit/Models/Request/ReleaseUpdate.cs
+++ Octokit/Models/Request/ReleaseUpdate.cs
@@ -63,6 +63,15 @@ namespace Octokit
         /// </value>
         public bool? Prerelease { get; set; }
 
+        /// <summary>
+        /// Specifies whether this release should be set as the latest release for the repository.
+        /// Drafts and prereleases cannot be set as latest. Defaults to true for newly published releases.
+        /// </summary>
+        /// <value>
+        ///   <c>True</c> set release as latest;
+        ///   <c>Legacy</c> specifies that the latest release should be determined based on the release creation date and higher semantic version.
+        /// </value>
+        public MakeLatestQualifier? MakeLatest { get; set; }
         internal string DebuggerDisplay
         {
             get
