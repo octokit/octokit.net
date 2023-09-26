af96bb6c34e312a1d0947f9c771773576c4c0c8c
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2023-09-26 07:00:50 +0100

    Fix tests

--- Octokit.Tests/Clients/StarredClientTests.cs
+++ Octokit.Tests/Clients/StarredClientTests.cs
@@ -412,7 +412,7 @@ namespace Octokit.Tests.Clients
 
                 await client.GetAllStargazersWithTimestamps("fake", "repo");
 
-                connection.Received().GetAll<UserStar>(endpoint, null, Args.ApiOptions);
+                connection.Received().GetAll<UserStar>(endpoint, null, AcceptHeaders.StarJson, Args.ApiOptions);
             }
 
             [Fact]
@@ -424,7 +424,7 @@ namespace Octokit.Tests.Clients
 
                 await client.GetAllStargazersWithTimestamps(1);
 
-                connection.Received().GetAll<UserStar>(endpoint, null, Args.ApiOptions);
+                connection.Received().GetAll<UserStar>(endpoint, null, AcceptHeaders.StarJson, Args.ApiOptions);
             }
 
             [Fact]
