5dd3cfe5e44c09f8e01301410860a18def122da6
Perry <62119804+perrylets@users.noreply.github.com>
2023-10-09 15:20:03 -0300

    [feat]: Add archive url to repository info
    * Add archive url to repository info
    
    * Add test
    
    ---------
    
    Co-authored-by: Perry <perry@perry.dev.br>

--- Octokit/Models/Response/Repository.cs
+++ Octokit/Models/Response/Repository.cs
@@ -17,7 +17,7 @@ namespace Octokit
             Id = id;
         }
 
-        public Repository(string url, string htmlUrl, string cloneUrl, string gitUrl, string sshUrl, string svnUrl, string mirrorUrl, long id, string nodeId, User owner, string name, string fullName, bool isTemplate, string description, string homepage, string language, bool @private, bool fork, int forksCount, int stargazersCount, string defaultBranch, int openIssuesCount, DateTimeOffset? pushedAt, DateTimeOffset createdAt, DateTimeOffset updatedAt, RepositoryPermissions permissions, Repository parent, Repository source, LicenseMetadata license, bool hasDiscussions, bool hasIssues, bool hasWiki, bool hasDownloads, bool hasPages, int subscribersCount, long size, bool? allowRebaseMerge, bool? allowSquashMerge, bool? allowMergeCommit, bool archived, int watchersCount, bool? deleteBranchOnMerge, RepositoryVisibility visibility, IEnumerable<string> topics, bool? allowAutoMerge, bool? allowUpdateBranch, bool? webCommitSignoffRequired)
+        public Repository(string url, string htmlUrl, string cloneUrl, string gitUrl, string sshUrl, string svnUrl, string mirrorUrl, string archiveUrl, long id, string nodeId, User owner, string name, string fullName, bool isTemplate, string description, string homepage, string language, bool @private, bool fork, int forksCount, int stargazersCount, string defaultBranch, int openIssuesCount, DateTimeOffset? pushedAt, DateTimeOffset createdAt, DateTimeOffset updatedAt, RepositoryPermissions permissions, Repository parent, Repository source, LicenseMetadata license, bool hasDiscussions, bool hasIssues, bool hasWiki, bool hasDownloads, bool hasPages, int subscribersCount, long size, bool? allowRebaseMerge, bool? allowSquashMerge, bool? allowMergeCommit, bool archived, int watchersCount, bool? deleteBranchOnMerge, RepositoryVisibility visibility, IEnumerable<string> topics, bool? allowAutoMerge, bool? allowUpdateBranch, bool? webCommitSignoffRequired)
         {
             Url = url;
             HtmlUrl = htmlUrl;
@@ -26,6 +26,7 @@ namespace Octokit
             SshUrl = sshUrl;
             SvnUrl = svnUrl;
             MirrorUrl = mirrorUrl;
+            ArchiveUrl = archiveUrl;
             Id = id;
             NodeId = nodeId;
             Owner = owner;
@@ -83,6 +84,7 @@ namespace Octokit
         public string SvnUrl { get; private set; }
 
         public string MirrorUrl { get; private set; }
+        public string ArchiveUrl { get; private set; }
 
         public long Id { get; private set; }
 
