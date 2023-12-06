41a513c043d8ffd161a7842e2e9610ffeab31282
Dylan Morley <dylanm@asos.com>
2023-12-06 13:01:23 +0000

    updated billing settings documentation

---
+++ Octokit/Models/Response/Copilot/BillingSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace Octokit
+{
+    /// <summary>
+    /// The billing settings for a Copilot-enabled organization.
+    /// </summary>
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public partial class BillingSettings
+    {
+        /// <summary>
+        /// A summary of the current billing settings for the organization.
+        /// </summary>
+        public SeatBreakdown SeatBreakdown { get; set; }
+    
+        /// <summary>
+        /// A string that indicates how seats are billed for the organization. 
+        /// </summary>
+        public string SeatManagementSetting { get; set; }
+    
+        /// <summary>
+        /// A string that indicates if public code suggestions are enabled or blocked for the organization.
+        /// </summary>
+        public string PublicCodeSuggestions { get; set; }
+    }
+}
+
