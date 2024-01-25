5dec6d689e959a0713468961348eb510b37f3df5
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2024-01-25 13:10:13 +0000

    Fix namespaces for Artifacts models

--- Octokit/Models/Response/ArtifactWorkflowRun.cs
+++ Octokit/Models/Response/ArtifactWorkflowRun.cs
@@ -1,46 +1,49 @@
 using System.Diagnostics;
 using System.Globalization;
 
-[DebuggerDisplay("{DebuggerDisplay,nq}")]
-public class ArtifactWorkflowRun
+namespace Octokit
 {
-    public ArtifactWorkflowRun()
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class ArtifactWorkflowRun
     {
-    }
-    
-    public ArtifactWorkflowRun(long id, long repositoryId, long headRepositoryId, string headBranch, string headSha)
-    {
-        Id = id;
-        RepositoryId = repositoryId;
-        HeadRepositoryId = headRepositoryId;
-        HeadBranch = headBranch;
-        HeadSha = headSha;
-    }
+        public ArtifactWorkflowRun()
+        {
+        }
 
-    /// <summary>
-    /// The workflow run Id
-    /// </summary>
-    public long Id { get; private set; }
-    
-    /// <summary>
-    /// The repository Id
-    /// </summary>
-    public long RepositoryId { get; private set; }
-    
-    /// <summary>
-    /// The head repository Id
-    /// </summary>
-    public long HeadRepositoryId { get; private set; }
-    
-    /// <summary>
-    /// The head branch
-    /// </summary>
-    public string HeadBranch { get; private set; }
-    
-    /// <summary>
-    /// The head Sha
-    /// </summary>
-    public string HeadSha { get; private set; }
-    
-    internal string DebuggerDisplay => string.Format(CultureInfo.InvariantCulture, "Id: {0}", Id);
+        public ArtifactWorkflowRun(long id, long repositoryId, long headRepositoryId, string headBranch, string headSha)
+        {
+            Id = id;
+            RepositoryId = repositoryId;
+            HeadRepositoryId = headRepositoryId;
+            HeadBranch = headBranch;
+            HeadSha = headSha;
+        }
+
+        /// <summary>
+        /// The workflow run Id
+        /// </summary>
+        public long Id { get; private set; }
+
+        /// <summary>
+        /// The repository Id
+        /// </summary>
+        public long RepositoryId { get; private set; }
+
+        /// <summary>
+        /// The head repository Id
+        /// </summary>
+        public long HeadRepositoryId { get; private set; }
+
+        /// <summary>
+        /// The head branch
+        /// </summary>
+        public string HeadBranch { get; private set; }
+
+        /// <summary>
+        /// The head Sha
+        /// </summary>
+        public string HeadSha { get; private set; }
+
+        internal string DebuggerDisplay => string.Format(CultureInfo.InvariantCulture, "Id: {0}", Id);
+    }
 }
--- Octokit/Models/Response/ListArtifactsResponse.cs
+++ Octokit/Models/Response/ListArtifactsResponse.cs
@@ -2,28 +2,31 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 
-[DebuggerDisplay("{DebuggerDisplay,nq}")]
-public class ListArtifactsResponse
+namespace Octokit
 {
-    public ListArtifactsResponse()
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class ListArtifactsResponse
     {
-    }
-    
-    public ListArtifactsResponse(int totalCount, IReadOnlyList<Artifact> artifacts)
-    {
-        TotalCount = totalCount;
-        Artifacts = artifacts;
-    }
+        public ListArtifactsResponse()
+        {
+        }
 
-    /// <summary>
-    /// The number of artifacts found
-    /// </summary>
-    public int TotalCount { get; private set; }
-    
-    /// <summary>
-    /// The list of found artifacts
-    /// </summary>
-    public IReadOnlyList<Artifact> Artifacts { get; private set; } = new List<Artifact>();
-    
-    internal string DebuggerDisplay => string.Format(CultureInfo.InvariantCulture, "Artifacts: {0}", TotalCount);
+        public ListArtifactsResponse(int totalCount, IReadOnlyList<Artifact> artifacts)
+        {
+            TotalCount = totalCount;
+            Artifacts = artifacts;
+        }
+
+        /// <summary>
+        /// The number of artifacts found
+        /// </summary>
+        public int TotalCount { get; private set; }
+
+        /// <summary>
+        /// The list of found artifacts
+        /// </summary>
+        public IReadOnlyList<Artifact> Artifacts { get; private set; } = new List<Artifact>();
+
+        internal string DebuggerDisplay => string.Format(CultureInfo.InvariantCulture, "Artifacts: {0}", TotalCount);
+    }
 }
