372848ec921153392314c2dbb22cad6769b27888
Dirty Gooback <19241000+dirtygooback@users.noreply.github.com>
2024-01-27 14:01:39 -0500

    cleanup

--- Octokit/Models/Response/Repository.cs
+++ Octokit/Models/Response/Repository.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
-using Octokit.Models.Response;
 
 namespace Octokit
 {
