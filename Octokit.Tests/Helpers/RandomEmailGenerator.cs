0edfab51e6d61bc236f2f6315dc7473a8eab9c90
Skye McLeman <118782658+skyemcleman@users.noreply.github.com>
2024-03-08 14:38:36 +1100

    Add tests for create org invitation

---
+++ Octokit.Tests/Helpers/RandomEmailGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Octokit.Tests.Helpers
+{
+    public static class RandomEmailGenerator
+    {
+        public static string GenerateRandomEmail()
+        {
+            var randomUsername = Guid.NewGuid().ToString();
+            var randomDomain = Guid.NewGuid().ToString();
+            
+            return $"{randomUsername}@{randomDomain}.com";
+        }
+    }
+}
