f9fb116eabe0028b7e61d25bbe81403673338824
Colby Williams <ColbyLWilliams@gmail.com>
2024-12-27 14:05:00 -0600

    [FEAT]: Add support for Public Keys API (#2945)
    * Add support for /mets/public_keys/<keyType>
    
    * "files.insertFinalNewline": false
    
    * revert and make setttings.json change csharp only
    
    * formatting
    
    * remove final new line
    
    ---------
    
    Co-authored-by: Nick Floyd <139819+nickfloyd@users.noreply.github.com>

---
+++ Octokit.Tests.Integration/Clients/PublicKeysClientTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Octokit.Tests.Integration.Clients
+{
+    public class PublicKeysClientTests
+    {
+        public class TheGetMethod
+        {
+            [IntegrationTest]
+            public async Task CanRetrievePublicKeys()
+            {
+                var github = Helper.GetAnonymousClient();
+
+                var result = await github.Meta.PublicKeys.Get(PublicKeyType.SecretScanning);
+
+                Assert.NotNull(result);
+                Assert.Equal(2, result.PublicKeys.Count);
+
+                Assert.NotNull(result.PublicKeys[0].KeyIdentifier);
+                Assert.NotNull(result.PublicKeys[0].Key);
+
+                Assert.NotNull(result.PublicKeys[1].KeyIdentifier);
+                Assert.NotNull(result.PublicKeys[1].Key);
+            }
+        }
+    }
+}
---
+++ Octokit.Tests/Reactive/ObservablePublicKeysClientTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NSubstitute;
+using Octokit.Reactive;
+using Xunit;
+
+namespace Octokit.Tests.Reactive
+{
+    public class ObservablePublicKeysClientTests
+    {
+        public class TheGetMethod
+        {
+            [Fact]
+            public void CallsIntoClient()
+            {
+                var gitHubClient = Substitute.For<IGitHubClient>();
+                var client = new ObservablePublicKeysClient(gitHubClient);
+
+                client.Get(PublicKeyType.SecretScanning);
+
+                gitHubClient.Meta.PublicKeys.Received(1).Get(PublicKeyType.SecretScanning);
+            }
+        }
+
+        public class TheCtor
+        {
+            [Fact]
+            public void EnsuresNonNullArguments()
+            {
+                Assert.Throws<ArgumentNullException>(() => new ObservablePublicKeysClient((IGitHubClient)null));
+            }
+        }
+    }
+}
