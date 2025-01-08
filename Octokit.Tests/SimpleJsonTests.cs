7c6c08f86114c2e452524f8ca1c9b670443b3857
Todd Grunke <toddgrun@microsoft.com>
2025-01-08 08:56:18 -0800

    fix: Reduce string allocations during SimpleJson.ParseString (#2977)
    Noticed this allocation while looking at a profile of solution load in visual studio. These StringBuilder allocations were showing up as 0.5% of total allocations. I took a peek at the code, and reaslized the StringBuilder need not be created unless there is a '\' in the string value being parsed. In that case, just copy already seen characters into a StringBuilder and continue as previously.
    
    Co-authored-by: Nick Floyd <139819+nickfloyd@users.noreply.github.com>

---
+++ Octokit.Tests/SimpleJsonTests.cs
@@ -0,0 +1,36 @@
+﻿using Octokit;
+using System.Threading.Tasks;
+using Xunit;
+
+public class SimpleJsonTests
+{
+    [Theory]
+    [InlineData("\"abc\"", "abc")]
+    [InlineData(" \"abc\" ", "abc")]
+    [InlineData("\" abc \" ", " abc ")]
+    [InlineData("\"abc\\\"def\"", "abc\"def")]
+    [InlineData("\"abc\\r\\ndef\"", "abc\r\ndef")]
+    public async Task ParseStringSuccess(string input, string expected)
+    {
+        int index = 0;
+        bool success = true;
+
+        string actual = SimpleJson.ParseString(input.ToCharArray(), ref index, ref success);
+
+        Assert.True(success);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("\"abc")]
+    public async Task ParseStringIncomplete(string input)
+    {
+        int index = 0;
+        bool success = true;
+
+        string actual = SimpleJson.ParseString(input.ToCharArray(), ref index, ref success);
+
+        Assert.False(success);
+        Assert.Null(actual);
+    }
+}
