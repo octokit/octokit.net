3f7a2e14031cce14cc4d1ecfb6823b2014c2c2ea
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-01-25 14:26:24 +0000

    IssueUpdateTests

---
+++ Octokit.Tests/Models/IssueUpdateTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace Octokit.Tests.Models
+{
+    public class IssueUpdateTests
+    {
+        [Fact]
+        public void Can_Initialise_With_Label()
+        {
+            _ = new IssueUpdate
+            {
+                Labels = { "Foo" }
+            };
+        }
+    
+        [Fact]
+        public void Can_Initialise_With_Assignee()
+        {
+            _ = new IssueUpdate
+            {
+                Assignees = { "Foo" }
+            };
+        }
+    }
+}
